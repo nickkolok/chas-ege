ded2bf5cdc1222da23e4fc5f9f775f1c74cf2a26
Nikolay Avdeyev <nickolaus@nicknout.(none)>
2013-08-04 01:42:25 +0400

    Движок B14

--- zdn/mat/B14/6_.js
+++ zdn/mat/B14/6_.js
@@ -2,43 +2,28 @@
 
 var a=sluchch(2,90);
 var b=sluchch(a+1,99);
-//var c=sluchch(-99,99);
+var c=sluchch(-99,99);
 
 var m=sluchch(3,6);
 var n=sluchch(1,(m/2).ceil()-1);
 var g=n.pina(m);
 
-/*var v1=sluchch(1);
+var v1=sluchch(1);
 var v2=sluchch(1);
 var v3=sluchch(1);
 var t1=['наибольшее значение','наименьшее значение'];
-*/
+
 var tg='\\mathrm{tg} x';
-//var t3=['-',''];
+var t3=['-',''];
 
-//var h=[t3[v2]+a+tg,t3[1-v2]+a+'x'/*,c*/]//.slag();
+var h=[t3[v2]+a+tg,t3[1-v2]+a+'x',c].slag();
 
-//var t4=['0',g];
-//var t5=['-'+g,'0'];
-//var v4=(v1!=v2);
-//window.vopr.txt=fn_txt(t1[v1],h,t4[v4],t5[v4]);
+var t4=['0',g];
+var t5=['-'+g,'0'];
+var v4=0+(v1!=v2);
+window.vopr.txt=fn_txt(t1[v1],h,t4[v4],t5[v4]);
 
-//window.vopr.ver=[c.ts()];
-
-var fn=fn_zadan({
-	slag:[a+tg,'-'+a+'x'],
-	miny:0,
-	nech:1,
-	prnz:'0',
-	prkz:g,
-});
-
-window.vopr.txt=fn.txt;
-window.vopr.ver=[fn.ver];
-
-
-
-
+window.vopr.ver=[c.ts()];
 
 window.vopr.kat['log']=0;
 window.vopr.kat['prz']=1;
---
+++ zdn/mat/B14/8.js
@@ -0,0 +1,28 @@
+(function() {
+
+var a=sluchch(2,9);
+
+
+
+var fn=fn_zadan({
+	slag:[a.pow(2).frac('x'),'x'],
+	minx:a,
+	maxx:-a,
+	nech:1,
+});
+
+window.vopr.txt=fn.txt;
+window.vopr.ver=[fn.ver];
+
+
+
+
+
+window.vopr.kat['log']=0;
+window.vopr.kat['prz']=1;
+window.vopr.kat['drs']=0;
+window.vopr.kat['tri']=1;
+})();
+
+//Обзад 26694 26695 26696 26697 
+//Николай Авдеев
