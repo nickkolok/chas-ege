efb9c4a74fa318aa568e7018e158bc7023aa86dc
Nikolay Avdeyev <nickolaus@nicknout.(none)>
2013-08-04 14:56:11 +0400

    B14/10

---
+++ zdn/mat/B14/10.js
@@ -0,0 +1,23 @@
+(function() {
+
+var c=[0.1,0.2,0.25,0.5].iz().pm();
+var x=sl(-10,10,0.1);
+var b=c-x;
+var a=(1/c).ts();
+
+var fn=fn_zadan({
+	slag:[a+'x','-\\ln(x+'+b+')'],
+	minx:x,
+});
+
+window.vopr.txt=fn.txt;
+window.vopr.ver=[fn.ver];
+
+window.vopr.kat['log']=1;
+window.vopr.kat['prz']=1;
+window.vopr.kat['drs']=0;
+window.vopr.kat['tri']=1;
+})();
+
+//Обзад 26694 26695 26696 26697 
+//Николай Авдеев
