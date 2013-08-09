731c71953ef87149ccb33815f37aa2a91440eaaf
Nikolay Avdeyev <nickolaus@nicknout.(none)>
2013-08-10 00:12:06 +0400

    Чистка старого кода в make-chas-uijs.sh

--- lib/make-chas-uijs.sh
+++ lib/make-chas-uijs.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 ./make-chas-lib.sh
 cat chas-lib.js dvig_fn.js dvig_rz.js osnmas.js menu.js browser.js cache.js ../ext/anyslider/js/jquery.anythingslider.js sovety.js > chas-uijs.js;
-#echo -n ';chas.version="'  >> head.js
-#DAT=`date date +%Y%m%d%H%M%S`;
-#date | echo -n >>head.js
-#echo -n $DAT'";' >> head.js
 date +window.chas.version=\"%Y%m%d%H%M%S\" >>head.js;
