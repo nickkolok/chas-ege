eac93252a7d5d3c9d6af909a14b03812f08b0b03
Nikolay Avdeyev <nickolaus@nicknout.(none)>
2013-08-10 00:11:01 +0400

    Вынос движка расчётных задач в отдельный файл dvig_rz.js, чтобы не захламлять func.js

--- lib/make-chas-uijs.sh
+++ lib/make-chas-uijs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ./make-chas-lib.sh
-cat chas-lib.js dvig_fn.js osnmas.js menu.js browser.js cache.js ../ext/anyslider/js/jquery.anythingslider.js sovety.js > chas-uijs.js;
+cat chas-lib.js dvig_fn.js dvig_rz.js osnmas.js menu.js browser.js cache.js ../ext/anyslider/js/jquery.anythingslider.js sovety.js > chas-uijs.js;
 #echo -n ';chas.version="'  >> head.js
 #DAT=`date date +%Y%m%d%H%M%S`;
 #date | echo -n >>head.js
