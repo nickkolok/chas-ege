ca9ad65d5ae764050663524f478de29cd94717cc
Nikolay Avdeyev <nickolaus@nicknout.(none)>
2013-08-10 09:43:33 +0400

    Выделен файл object.js

--- lib/make-chas-lib.sh
+++ lib/make-chas-lib.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat func.js array.js canvas.js number.js string.js function.js regexp.js lx.js> chas-lib.js;
+cat func.js array.js canvas.js number.js string.js function.js object.js regexp.js lx.js> chas-lib.js;
