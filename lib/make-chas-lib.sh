180a4a6eb66c37d0040cc8dc35a822f9b59c7f51
NickKolok (Николай Авдеев, Nikolay Avdeyev) <nickkolok@mail.ru>
2013-09-01 19:39:06 +0400

    Выделен array_mn.js

--- lib/make-chas-lib.sh
+++ lib/make-chas-lib.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat func.js array.js array_mt.js array_mp.js canvas.js number.js string.js function.js object.js regexp.js lx.js lxskl.js sklon.js> chas-lib.js;
+cat func.js array.js array_mt.js array_mp.js array_mn.js canvas.js number.js string.js function.js object.js regexp.js lx.js lxskl.js sklon.js> chas-lib.js;
