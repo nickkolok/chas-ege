7f7501595f8564fcc8175a1a12cd2ddd01e15b7f
NickKolok (Николай Авдеев, Nikolay Avdeyev) <nickkolok@mail.ru>
2013-09-18 01:55:32 +0400

    Линал В1/1 - причёсывание

--- zdn/linal/B1/1.js
+++ zdn/linal/B1/1.js
@@ -1,11 +1,7 @@
 (function() {
 
-window.vopr.txt=sl(10000);
 var a=sl(1,9);
-
 window.vopr.txt='Сколько всего существует перестановок '+a+'-й степени?';
-
 window.vopr.ver=[a.fct().ts()];
-
 
 })();
