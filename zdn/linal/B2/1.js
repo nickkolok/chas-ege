7d2cf41f940b06ea2072fb1602d95e7ebc472bcf
NickKolok (Николай Авдеев, Nikolay Avdeyev) <nickkolok@mail.ru>
2013-09-18 02:37:04 +0400

    Линал В2/1 - готов

--- zdn/linal/B2/1.js
+++ zdn/linal/B2/1.js
@@ -1,8 +1,8 @@
 (function() {
 
 var a=sl(1,9);
-var perest=[].N(a);
-window.vopr.txt='Сколько всего существует перестановок '+a+'-й степени?';
-window.vopr.ver=[a.fct().ts()];
+var perest=[].N(a).shuffle();
+window.vopr.txt='Сколько инверсий содержится в перестановке '+perest.pe_txt()+'?';
+window.vopr.ver=[perest.pe_inv()];
 
 })();
