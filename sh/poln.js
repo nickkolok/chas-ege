a52ec4f864d6e33935baefdfa5b7f8f6c5555499
NickKolok (Николай Авдеев, Nikolay Avdeyev) <nickkolok@mail.ru>
2013-10-27 22:16:56 +0400

    Полный тест

--- sh/poln.js
+++ sh/poln.js
@@ -1,7 +1,7 @@
 $('#zadaniya').html('');
 for(var i=1;i<=nabor.nZad;i++){
 	document.getElementById('zadaniya').innerHTML+='<tr><td><label for="cB'+i+'" >B'+i+'</label></td>'+
-	'<td><input type="text" class="kolvo" value="1" id="cB'+i+'" data-jstorage-id="pech-cB'+i+'"></td></tr>';
+	'<td><input type="text" class="kolvo" value="1" id="cB'+i+'" data-jstorage-id="poln-cB'+i+'"></td></tr>';
 }
 
 $('#gotov').hide();
@@ -10,8 +10,6 @@ var vr2=svinta?100:1500;
 
 var startxt='';
 window.vopr.txt='';
-var stardate=new Date().getTime();
-var novdate;
 var intervPole;
 var nV=1;
 var nZ=1;
@@ -20,140 +18,105 @@ var iZ=[];
 var aV;
 var strVopr='';
 var strOtv='';
-var ogran=[];
+var voprosy=[];
+
 function vse1(){
 	for(var i=1;i<=nabor.nZad;i++)
 		$('#cB'+i).val(1);
 	$('#cV').val(1);
 }
-function zapusk(){
-	$.jStorage.sohrData()
-	nV=1*$('#cV').val();
-	aV=nV;
+
+function vse0(){
 	for(var i=1;i<=nabor.nZad;i++)
-		aZ[i]=1*($('#cB'+i).val());
-	
-	ogran['prz']=$('#cPRZ').is(':checked');
-	ogran['log']=$('#cLOG').is(':checked');
-	ogran['tri']=$('#cTRI').is(':checked');
-	ogran['drs']=$('#cDRS').is(':checked');
-	kZ=aZ.sum()*aV;
-	iZ=aZ.slice();
-	nZ=0;
-	$('#panel').html('Тесты составляются, подождите...');
-	$('#gotov').show();
-	zadan();
+		$('#cB'+i).val(0);
+	$('#cV').val(1);
 }
 
-function razrstr(){
-	var d2=document.createElement('div');
-	d2.innerHTML='<hr class="pbb"/><div class="d"></div>';
-	d2.className='d d2';
-	document.getElementById('rez').appendChild(d2);
-
+function zapusk(){
+	$.jStorage.sohrData()
+	for(var i=1;i<=nabor.nZad;i++)
+		aZ[i]=1*($('#cB'+i).val());
+	cacheKat();
+	kZ=aZ.sum();
+	if(!kZ){
+		alert('Ни одно задание не выбрано.');
+		return;
+	}
+	iZ=aZ.slice();
+	nZ=1;
+	$('#panel').html('Тест составляется, подождите...');
+	$('#gotov').show();
+	zadan();
 }
 
 function testGotov(){
 	$('#gotov').hide();
 	allCanvasToBackgroundImage();
-	alert('Тесты составлены.\nТеперь Вы можете распечатать их с помощью Вашего браузера.');
-}
-
-function udalPanel(){
 	$('#panel').remove();
-	$('#menucenter').remove();
-	$('#inf').remove();
+	alert('Тест составлен.\nМожно приступать к решению!');
 }
 
 function konecSozd(){
-	strOtv='<hr class="pbb"/><h2>Ответы</h2>'+strOtv;
-	$('#otv').html(strOtv);
-	MathJax.Hub.Typeset();
-	udalPanel();
-	$("hr:first").remove();
-	$("hr:first").remove();
-	document.body.style.backgroundColor="#FFF";
-	if(!bGecko)
-		razrstr();
-	$('body').append('<script>udalPanel()</script>');			
-
-	setTimeout("testGotov()",3000);
-}
-
-function vNachaloVarianta(){
-	nV--;
-	nZ=0;
-	strOtv+='</table>';
-	zadan();
+	$('#prov_knopki').show();
+	MathJax.Hub.Typeset('rez',testGotov);
 }
 
 function zadan(){
-	if(nZ==nabor.nZad+1){
-		vNachaloVarianta()
+	if (nZ>nabor.nZad){
+		konecSozd();
 		return;
-	}else if (nZ==0){
-		if(nV==0){
-			konecSozd();
-			return;
-		}else{
-			MathJax.Hub.Typeset();
-			iZ=aZ.slice();
-			stardate=novdate;
-			novdate=new Date().getTime();
-			strVopr='<div class="d"><h2>Вариант №'+novdate+'</h2></div>';
-
-			if(!bGecko && aV!=nV)
-				razrstr();
-
-			var din=document.createElement('div');
-			din.innerHTML=strVopr;
-			din.class='d d4';
-			document.getElementById('rez').appendChild(din);
-			strOtv+='<table class="normtabl tablpech"><tr><th colspan="3">';
-			strOtv+='Ответы к варианту<br/>№'+novdate+'</th></tr>';
-			nZ=1;
-			zadan();
-			return;
-		}
+	}else if(iZ[nZ]==0){
+		nZ++;
+		zadan();
 	}else{
-		if(iZ[nZ]==0){
-			nZ++;
-			zadan();
-		}else{
-			iZ[nZ]--;
-			zagr(nabor.adres+nabor.prefix+nZ+'/main.js');
-			vopr.podg();
-			intervPole=setTimeout("zagr(nabor.adres+nabor.prefix+nZ+'/'+nomer+'.js');",vr1);
-			intervPole=setTimeout('obnov();',vr1+vr2);
-		}
-		return;
+		iZ[nZ]--;
+		zagr(nabor.adres+nabor.prefix+nZ+'/main.js');
+		vopr.podg();
+		intervPole=setTimeout("zagr(nabor.adres+nabor.prefix+nZ+'/'+nomer+'.js');",vr1);
+		intervPole=setTimeout('obnov();',vr1+vr2);
 	}
+	return;
 }
+
 function obnov(){
-	if((window.vopr.txt!=0)*(startxt!=window.vopr.txt)){
+	if((window.vopr.txt!=0)&&(startxt!=window.vopr.txt)){
 		clearInterval(intervPole);
-		if(
-			(ogran['prz'])*(window.vopr.kat["prz"])+
-			(ogran['log'])*(window.vopr.kat["log"])+
-			(ogran['tri'])*(window.vopr.kat["tri"])+
-			(ogran['drs'])*(window.vopr.kat["drs"])
-		){
+		if(!sootvKat()){
 			iZ[nZ]++;
 			zadan();
 			return;
 		}
+		var sdel=aZ.sum()-iZ.sum();
 		starttxt=window.vopr.txt;
-		strVopr='<br/><div class="d"><div class="b">B'+nZ+(aZ[nZ]==1?'':'-'+(aZ[nZ]-iZ[nZ]))+'</div>'+window.vopr.txt+'</div>';
+		strVopr=
+			'<br/>'+
+			'<div class="d">'+
+				'<div class="b">B'+nZ+(aZ[nZ]==1?'':'-'+(aZ[nZ]-iZ[nZ]))+
+				'</div>'+
+				window.vopr.txt+
+				'<div class="r">'+
+					'Ответ:'+
+					'<textarea class="text_otv" rows="1"></textarea>'+
+					'<div class="otv_ver" id="otv_ver'+(sdel-1)+'">'+
+						'Правильно!'+
+					'</div>'+
+					'<div class="otv_nev" id="otv_nev'+(sdel-1)+'">'+
+						'Неправильно!<br/>'+
+						'Правильный ответ: '+window.vopr.ver.join('или')+
+					'</div>'+
+				'</div>';
 		var din=document.createElement('div');
 		din.innerHTML=strVopr;
 		din.class='d d3';
 		din.width="100%";
 		document.getElementById('rez').appendChild(din);
-		strOtv+='<tr><td>'+novdate+'</td><td>B'+nZ+(aZ[nZ]==1?'':'-'+(aZ[nZ]-iZ[nZ]))+'</td><td>'+window.vopr.ver.join('; ')+'</td></tr>';
 		try{
 			window.vopr.dey();
 		}catch(e){}
-		var sdel=aZ.sum()*(aV-nV+1)-iZ.sum();
+		
+		//Копируем вопрос в массив
+		voprosy.push(vopr.clone());
+		
 		var w=sdel/kZ;
 		$('.tx').text((100*w).toFixedLess(1).dopdo(' ',4)+'%');
 		$('#pr1').width($('#pr0').width()*w);
@@ -165,3 +128,25 @@ function obnov(){
 		intervPole=setTimeout('obnov();',vr1+vr2);		
 	}
 }
+
+function prov(){
+	var nVer=0;
+	var nNev=0;
+	var textareas=$('textarea');
+	console.log(textareas[0]);
+	for(var i=0;i<aZ.sum();i++)
+	{
+		if(voprosy[i].vrn.call(voprosy[i],textareas[i].value)){
+			nVer++;
+			$('#otv_ver'+i).show();
+		}else{
+			nNev++;
+			$('#otv_nev'+i).show();		
+		}
+	}
+	$('#but_prov').hide();
+	$('#prov_knopki').append('<br/>Правильно решено '+chislitlx(nVer,'задание')+' из '+aZ.sum());
+}
+
+galkiKat('#galki_kat','pech');
+$('#prov_knopki').hide();
