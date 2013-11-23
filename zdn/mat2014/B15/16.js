5fd5cf701b82b25b6d352db32ca55ee97aa9090a
NickKolok (Николай Авдеев, Nikolay Avdeyev) <nickkolok@mail.ru>
2013-11-24 03:46:14 +0400

    mat2014/B15/16

---
+++ zdn/mat2014/B15/16.js
@@ -0,0 +1,28 @@
+(function(){'use strict';
+
+var a=sl(2,19,0.5);
+
+var fn=fn_zadan({
+	slag:[
+		''+(4*a).ts()+'x',
+		'-'+(2*a).ts()+'\\mathrm{tg~}x',
+		'-'+a.ts()+'\\pi',
+	],
+	maxy: -2*a,
+	prnz: sl(-2,1).pina(6),
+	prnb: 0,
+	prkz: 1 .pina(sl(3,4)),
+	prkb: 0,
+});
+
+window.vopr.txt=fn.txt;
+window.vopr.ver=[fn.ver];
+
+window.vopr.kat['log']=0;
+window.vopr.kat['prz']=1;
+window.vopr.kat['drs']=0;
+window.vopr.kat['tri']=1;
+})();
+
+//Обзад 26708 26709
+//Николай Авдеев
