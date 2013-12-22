ca6fc14e33e80b8c908b9e0b906bb16c63b1b974
NickKolok (Николай Авдеев, Nikolay Avdeyev) <nickkolok@mail.ru>
2013-12-23 00:55:21 +0400

    jstorage.zapomni.js - последние штрихи

--- lib/make-chas-uijs.sh
+++ lib/make-chas-uijs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./make-chas-lib.sh
-cat chas-lib.js dvig_fn.js dvig_rz.js osnmas.js menu.js yametrika.js style.js browser.js cache.js ../ext/anyslider/js/jquery.anythingslider.js sovety.js jquery.jstorage.zapomni.js> chas-uijs.js;
+cat chas-lib.js dvig_fn.js dvig_rz.js osnmas.js menu.js yametrika.js style.js browser.js cache.js ../ext/anyslider/js/jquery.anythingslider.js sovety.js jstorage.zapomni.js> chas-uijs.js;
 date +window.chas.version=\"%Y%m%d%H%M%S\" >>head.js;
