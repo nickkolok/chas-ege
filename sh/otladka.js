ae9eb5d2fa7a9cf4c75b489497ff61449fbd1f51
NickKolok (Николай Авдеев, Nikolay Avdeyev) <nickkolok@mail.ru>
2013-12-29 01:48:27 +0400

    Оболочка отладки перенесена на новый движок

--- sh/otladka.js
+++ sh/otladka.js
@@ -2,39 +2,33 @@ var startxt;
 var intervPole;
 
 function obnov(){
-	if((window.vopr.txt)&&(startxt!=window.vopr.txt)){
-		clearInterval(intervPole);
-		intervPole=0;
-		$('#pole').html(window.vopr.txt);
-		window.vopr.dey();
-		MathJax.Hub.Typeset();
-		$('#otvet').html(window.vopr.ver.join(';;'));
-		$('#never').html(window.vopr.nev.join(';;'));
-	}
+	$('#pole').html(window.vopr.txt);
+	window.vopr.dey();
+	MathJax.Hub.Typeset();
+	$('#otvet').html(window.vopr.ver.join(';;'));
+	$('#never').html(window.vopr.nev.join(';;'));
 }
+
 function sozdat(){
 	if(!checkJQuery('sozdat()','pole'))
 		return;
+	if(!checkMathJax('sozdat()','pole'))
+		return;
 	$('#pole').html('Задание составляется, подождите...');
-	startxt=window.vopr.txt;
 	v=$('#adres').val();
 	if(!v.length){
 		$('#pole').html('Нужно указать путь к загружаемому файлу!');
 		return;
 	}
 	$('#shabl').attr('src',v);
-	window.vopr.podg();
 	zagr(v+'?'+Math.random());
-	if(!intervPole){
-		intervPole=setInterval("obnov();",500);
-	}
-	var otvet=$('#otv').val('');
-	if(!checkMathJax('sozdat()','pole'))
-		return;
-
+	dvig.flObn=0;
+	dvig.obnov(obnov);
+	$('#otv').val('');
 	setVKI();
 	VKI_attach(document.getElementById('otv'));
 }
+
 function prover(){
 	if(window.vopr.vrn($('#otv').val()))
 		alert('Правильно!');
