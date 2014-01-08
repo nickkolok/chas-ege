ffe0e3f297f961a784bfdf0cc9e0b67c38ccece6
NickKolok (Николай Авдеев, Nikolay Avdeyev) <nickkolok@mail.ru>
2014-01-08 17:37:49 +0400

    Немного причёсан zdn/mat2014/B5/6

--- zdn/mat2014/B5/6.js
+++ zdn/mat2014/B5/6.js
@@ -6,13 +6,9 @@ var C={};
 var D={};
 var p=0;
 var d=[];
-var g='';
-for(;!p
-		||(g.ie=='четырёхугольник')&&sluchch(0,2000)
-		||(g.ie=='трапеция')&&sluchch(0,100)
-		||(g.ie=='дельтоид')&&sluchch(0,25)
-		||(g.ie=='параллелограмм')&&sluchch(0,50)
-	;){
+var g;
+
+do{
 	A.x=sluchch(1,16);
 	A.y=sluchch(1,16);
 	B.x=sluchch(1,16);
@@ -24,11 +20,15 @@ for(;!p
 	d=[A,B,C,D];
 	p=d.mt_isMnug(4);
 	g=d.mt_imen4ug();
-}
-p=d.mt_s4ug();
+}while(!p
+	||(g.ie=='четырёхугольник')&&sluchch(0,2000)
+	||(g.ie=='трапеция')&&sluchch(0,100)
+	||(g.ie=='дельтоид')&&sluchch(0,25)
+	||(g.ie=='параллелограмм')&&sluchch(0,50)
+)
 
 window.vopr.txt='Найдите площадь '+g.re+' с вершинами, имеющими координаты '+d.mt_join()+'.';
-window.vopr.ver=[p.ts()];
+window.vopr.ver=[d.mt_s4ug().ts()];
 
 window.vopr.kat['log']=0;
 window.vopr.kat['prz']=0;
