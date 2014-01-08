e6822a0d495bf471f9fdde73aaed009abcb01355
NickKolok (Николай Авдеев, Nikolay Avdeyev) <nickkolok@mail.ru>
2014-01-08 16:52:48 +0400

    Немного причёсано mat2014/B3/4

--- zdn/mat2014/B3/4.js
+++ zdn/mat2014/B3/4.js
@@ -1,8 +1,7 @@
 (function(){'use strict';
 	
 var a=sluchch(8,12);
-var b=[].N(a);
-b=b.map(function(p1){return om.porchisl[p1].i[2];});
+var b=[].N(a).map(function(p1){return om.porchisl[p1].i[2];});
 var c=[].zapslch(0,a-1,10,25);
 var d2=c.maxE()+1;
 var d1=c.minE()-2;
