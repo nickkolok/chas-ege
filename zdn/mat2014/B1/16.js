1a810c9e2c851677f0080e0407c151023c03e8f5
NickKolok (Николай Авдеев, Nikolay Avdeyev) <nickkolok@mail.ru>
2014-01-24 00:03:31 +0400

    mat2014/B1/16 - 26635

---
+++ zdn/mat2014/B1/16.js
@@ -0,0 +1,19 @@
+(function(){
+
+var kolvodet=sl(100,300);
+var kolvovzr=sl(10,40);
+var nazvdet=sklonlxkand(['школьник','ребёнок','пионер','учащийся',].iz());
+var nazvvzr=sklonlxkand(['взрослый','педагог','учитель','преподаватель','вожатый','родитель',].iz());
+var chel=sl(20,90);
+
+
+window.vopr.txt='В '+
+	['летнем','спортивном','пионерском','детском','детском оздоровительном','оздоровительном',].iz()+
+	' лагере '+chislitlx(kolvovzr,nazvvzr.ie)+' и '+chislitlx(kolvodet,nazvdet.ie)+
+	'. В автобус помещается не более '+chislitlx(chel,'пассажир')+
+	'. Сколько автобусов требуется, чтобы перевезти всех из лагеря в город?';
+	
+window.vopr.ver=[((kolvodet+kolvovzr)/chel).ceil().ts()];
+
+})();
+//Обзад 26635
