ceb141a4f1f5fc7ad2e63b5866beeb0dd740a37c
NickKolok (Николай Авдеев, Nikolay Avdeyev) <nickkolok@mail.ru>
2014-02-14 11:06:28 +0400

    zdn/mat2014/B15/9 - расширен

--- zdn/mat2014/B15/9.js
+++ zdn/mat2014/B15/9.js
@@ -1,26 +1,22 @@
 (function(){'use strict';
 
-var a=sluchch(2,90);
+var a=sluchch(1,90);
+var v1=sl1();//Если 1, то отрезок, иначе прямая.
 
 var fn=fn_zadan({
-	slag:[[a.pow(2).frac('x'),'x'],[[a.pow(2),'x^2'].slag().frac('x')]].iz(),
-	miny:2*a,
-	prnz:sl(0.1,a-0.1,0.1),
-	prkz:sl(a+0.1,2*a,0.1),
-	prnb:sl1(),
-	prkb:sl1(),
-	nech:1,
+	slag: [[a.pow(2).frac('x'),'x'],[[a.pow(2),'x^2'].slag().frac('x')]].iz(),
+	miny: v1 ? 2*a : undefined,
+	minx: v1 ? undefined : a,
+	prnz: v1 ? sl(0.1,a-0.1,0.1) : undefined,
+	prkz: v1 ? sl(a+0.1,2*a,0.1) : undefined,
+	prnb: sl1(),
+	prkb: sl1(),
+	nech: 1,
 });
 
 window.vopr.txt=fn.txt;
 window.vopr.ver=[fn.ver];
 
-window.vopr.kat['log']=0;
 window.vopr.kat['prz']=1;
-window.vopr.kat['drs']=0;
-window.vopr.kat['tri']=0;
 })();
-
-//Обзад 77473
-//Расширяемо на 77467 77468 77469 77470 77471 77472 77474
-//Николай Авдеев
+//Обзад 77467 77468 77469 77470 77471 77472 77473 77474
