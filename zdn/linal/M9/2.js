d02320e8005be2542d4439f48e8af003b04c67bf
NickKolok (Николай Авдеев, Nikolay Avdeyev) <nickkolok@mail.ru>
2014-03-03 09:28:07 +0400

    linal/M9/2 + сопутствующие правки в библиотеки

---
+++ zdn/linal/M9/2.js
@@ -0,0 +1,17 @@
+(function(){'use strict';
+
+var r=sl(2,4);
+do{
+var A=objUmn(generateMatrix(4,r,1,4),generateMatrix(r,5,-2,2));
+A=A.T();
+var B=A.splice(0,1);
+A=A.T();
+B=B.T();
+}while(A.hasNullVect() || B.T()[0].isNullVect())
+
+window.vopr.txt='Решить систему методом Гаусса и записать общее решение в виде суммы частного '+
+	'решения неоднородной и общего решения однородной системы:'+
+	'$$\\left('+A.matrixToTex().ts()+'\\left|'+B.matrixToTex().ts()+'\\right.\\right)$$'+
+	'';
+
+})();
