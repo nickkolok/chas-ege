d42bd8bf5c703bb900ead5a4cec221052ea90741
NickKolok (Николай Авдеев, Nikolay Avdeyev) <nickkolok@mail.ru>
2014-04-20 21:28:52 +0400

    mat2014/B8/12

---
+++ zdn/mat2014/B8/12.js
@@ -0,0 +1,21 @@
+(function() {
+
+var t1=om.latbukv.sluchiz(3);
+var p1=sl(1,100);
+var p2=sl(1,100);
+var p3=sl(1,100);
+
+window.vopr.ver=[p1+p2+p3];
+window.vopr.txt='К окружности, вписанной в треугольник '+t1.soed()+
+	', проведены три касательные. Периметры отсеченных треугольников равны '+p1+
+	', '+p2+', '+p3+'. Найдите периметр данного треугольника.';
+
+window.vopr.kat['log']=0;
+window.vopr.kat['prz']=0;
+window.vopr.kat['drs']=0;
+window.vopr.kat['tri']=0;
+})();
+
+//Обзад 54649
+//Николай Авдеев
+
--- zdn/mat2014/B8/main.js
+++ zdn/mat2014/B8/main.js
@@ -1,2 +1,2 @@
-window.nomer=sluchch(1,10);
+window.nomer=sluchch(1,12);
 window.comment='Планиметрия. Угол. Тригонометрия.';
