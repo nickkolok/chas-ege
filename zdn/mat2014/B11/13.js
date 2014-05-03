d9e283d866fffdff4afe39319c7f5cf6d4340dcf
Boris Evseev <evseev.boris@gmail.com>
2014-05-03 14:37:07 +0400

    Исправленное задание B11/13.js от Евсеева Бориса

--- zdn/mat2014/B11/13.js
+++ zdn/mat2014/B11/13.js
@@ -1,36 +1,21 @@
-var a = getRandomInt(-5,5);
-var b = getRandomInt(-5,5);
-
-if (a==0){a=1};
-if (b==0){b=1};
-
-var c = getRandomInt(-5,5);
-var d = getRandomInt(-5,5);
-var e = getRandomInt(-5,5);
-
-if (c==0){c+=1};
-if (d==0){d+=1};
-if (e==0){e+=1};
-
-var q = getRandomInt(-5,5);
-var r = getRandomInt(-5,5);
-var t = getRandomInt(-5,5);
-
-if (q==0){q+=1};
-if (r==0){r+=1};
-if (t==0){t+=1};
- 
-
-function p(_x)
-{
-return (a*_x+b);
-};
-
 
 (function(){'use strict';
 
+var a = slKrome(0,-5,5);
+var b = slKrome(0,-5,5);
+var c = slKrome(0,-5,5);
+var d = slKrome(0,-5,5);
+var e = slKrome(0,-5,5);
+var q = slKrome(0,-5,5);
+var r = slKrome(0,-5,5);
+var t = slKrome(0,-5,5);
+function p(_x)
+{
+	return (a*_x+b);
+};
 
-window.vopr.ver=[ (c*a*d+q*a*r)+'x+'+(c*a*e+c*b + q*a*t + q*b)];
+
+window.vopr.ver=[ ((c*a*d+q*a*r)+'x+'+(c*a*e+c*b + q*a*t + q*b)+'').plusminus()+''];
 
 var s =('Найдите значение выражения ('+c+'p('+d+'x+'+e+')+'+q+'p('+r+'x+'+t+'), если p=('+a+'x+'+b+').');
 s = s.plusminus();
