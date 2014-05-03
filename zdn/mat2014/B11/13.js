9e856e73a371967805209de6308da0a52c8d0995
Boris Evseev <evseev.boris@gmail.com>
2014-05-03 16:08:06 +0400

    Задание B11/13.js + TeX от Евсеева Бориса

--- zdn/mat2014/B11/13.js
+++ zdn/mat2014/B11/13.js
@@ -5,13 +5,13 @@ var b = slKrome(0,-5,5);
 var c = slKrome(0,-5,5);
 var d = slKrome(0,-5,5);
 var e = slKrome(0,-5,5);
-var q = -1*d;
-var r = c;
+//var q = -1*d;
+var r = slKrome(0,-5,5);
 var t = slKrome(0,-5,5);
 
-window.vopr.ver=[(c*a*e+c*b+q*a*t+q*b).plusminus()];
+window.vopr.ver=[r*(c*a*e+c*b-d*a*t-d*b).plusminus()];
 
-window.vopr.txt = ('Найдите значение выражения '+c+'p('+d+'x+'+e+')+'+q+'p('+r+'x+'+t+'), если p(x)='+a+'x+'+b+'.').plusminus();
+window.vopr.txt = ('Найдите значение выражения $'+ r+'(' +c+'p('+d+'x+'+e+')+'+(-1*d)+'p('+c+'x+'+t+'))$, если $ p(x)='+a+'x+'+b+'$.').plusminus();
 })();
 //Обзад 26821
 //by _zevs
