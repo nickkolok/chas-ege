a799bcf623a225c0ce702c0c63eae6dec34c58bd
nickkolok <nickkolok@mail.ru>
2014-05-25 22:00:27 +0400

    C5 - упрощённый вариант

---
+++ zdn/mat2014/C5/1.js
@@ -0,0 +1,10 @@
+(function(){
+
+var b=sluchch(1,50);
+var d=sluchch(2,50);
+
+window.vopr.txt='Решите уравнение при всех $a$:'+
+	('$$(a^2 - '+b*b+')x='+d+'(a-'+b+')$$').plusminus();
+
+window.vopr.ver=['При $a='+b+'$ - любое $x$, при $a=-'+b+'$ - решений нет, при $a\\neq\\pm'+b+'$ имеем $x=\\frac{'+d+'}{a+'+b+'}$'];
+})();
