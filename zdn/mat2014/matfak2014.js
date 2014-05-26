1ce84db0121d1d7f477d0052efdb94beeafa460b
nickkolok <nickkolok@mail.ru>
2014-05-27 00:45:51 +0400

    Заголовок набора для вступительных на матфак

---
+++ zdn/mat2014/matfak2014.js
@@ -0,0 +1,17 @@
+if(!window.nabor)
+	window.nabor={};
+window.nabor.importFrom({
+	nZad:21,
+	adres:'../zdn/mat2014/',
+	name:'ege2014',
+	prefix:'B',
+	kat:{
+		prz:'Без производной'		,
+	}
+});
+nabor.altz[16]='C1';
+nabor.altz[17]='C2';
+nabor.altz[18]='C3';
+nabor.altz[19]='C4';
+nabor.altz[20]='C5';
+nabor.altz[21]='C6';
