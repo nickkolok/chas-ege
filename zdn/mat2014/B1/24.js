b2192fe760795f31c7fa0c2379f994c5e69565f6
nickkolok <nickkolok@mail.ru>
2014-06-10 03:06:22 +0400

    mat2014/B1/24

---
+++ zdn/mat2014/B1/24.js
@@ -0,0 +1,16 @@
+(function(){'use strict';
+
+var napitka=['чая','кофе'].iz();//Вернёт случайным образом 'чая' или 'кофе'
+var meropr=sklonlxkand(['конференция','мероприятие','слёт'].iz());
+var up=sklonlxkand(['пачка','коробка','упаковка'].iz())
+var dni=sluchch(2,10);
+var pak=sluchch(50,200,10);
+var rash=sluchch(20,100);
+
+window.vopr.txt='В среднем за день во время '+meropr.re+' расходуется '+chislitlx(rash,'пакетик')+' '+napitka+'. '+
+	meropr.ie.toZagl()+' длится '+chislitlx(dni,'день')+'. В '+up.pe+' '+napitka+' '+chislitlx(pak,'пакетик')+'. '+
+	'Какого наименьшего количества '+up.rm+' '+napitka+' хватит на все дни '+meropr.re+'?';
+
+window.vopr.ver=[(rash*dni/pak).ceil()];
+})();
+//Обзад 77339
