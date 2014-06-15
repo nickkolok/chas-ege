f82b09368c60a19a9d4356700cc87a8f6b7b96f3
nickkolok <nickkolok@mail.ru>
2014-06-15 17:48:35 +0400

    Инструкция по использованию TeX и лёгкий рефакторинг шаблона-примера

--- zdn/mat2014/B8/2.js
+++ zdn/mat2014/B8/2.js
@@ -2,9 +2,9 @@
 
 var a=sluchch(1,5);
 var r=a*(3 .pow(sl1()))*(2 .pow(sluchch(0,3)));
-var b=(360*a*a/r/r).toFixedLess(4);
-window.vopr.txt='Найдите центральный угол сектора круга радиуса $\\frac{'+r+'}{\\sqrt{\\pi}}$, площадь которого равна '+(a*a)+'. Ответ дайте в градусах.';
-window.vopr.ver=[b];
+window.vopr.txt='Найдите центральный угол сектора круга радиуса $\\frac{'+r+
+	'}{\\sqrt{\\pi}}$, площадь которого равна '+(a*a)+'. Ответ дайте в градусах.';
+window.vopr.ver=[(360*a*a/r/r).ts()];
 
 window.vopr.kat['log']=0;
 window.vopr.kat['prz']=0;
