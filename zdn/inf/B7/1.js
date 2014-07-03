91a6272011d1b2f1ca902d0beea43624154d2db1
kinolog <yanapolyuk@gmail.com>
2014-07-03 15:40:43 +0400

    Update 1.js

--- zdn/inf/B7/1.js
+++ zdn/inf/B7/1.js
@@ -4,12 +4,12 @@
   var chislo=sluchch(20,200);
   var rez=intoAnotherSystem(chislo,10,osn);
  
-   window.vopr.txt='� ������� ��������� � ��������� ���������� ����� '+chislo+
-                    ' ������������ ��� '+rez+'. ������� ��� ���������.';
+   window.vopr.txt='В системе счисления с некоторым основанием число '+chislo+
+                    ' записывается как '+rez+'. Укажите это основание.';
  
   window.vopr.ver=[osn];
 })();
  
 //opengia 056134
-//��� �����
+//Яна Полюк
 //kinolog
