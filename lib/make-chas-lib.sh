5ea1ce5cdb630088d99ca057d38bd3307ae14219
nickkolok <nickkolok@mail.ru>
2014-07-07 02:02:37 +0400

    Файл под синонимы имён функций

--- lib/make-chas-lib.sh
+++ lib/make-chas-lib.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat func.js object.js array.js array_mt.js array_mp.js array_mn.js array_pe.js canvas.js number.js number_math.js string.js function.js regexp.js lx.js lxnar.js lxsoch.js lxskl.js sklon.js lxchisl.js complex.js> chas-lib.js;
+cat func.js object.js array.js array_mt.js array_mp.js array_mn.js array_pe.js canvas.js number.js number_math.js string.js function.js regexp.js lx.js lxnar.js lxsoch.js lxskl.js sklon.js lxchisl.js complex.js namealias.js> chas-lib.js;
