8d931c21568584faefceb148abac9ff3f9f1533a
Nikolay Avdeev <nickkolok@mail.ru>
2014-07-13 00:45:25 +0400

    Написание скрипта отсечения всего ненужного для запаковки в приложение

---
+++ dev/trunc.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#rsync -r --exclude={ext/fonts,ext/mathjax/fonts/HTML-CSS/TeX/png} . ../chas-ege-trunc/
+rm -rf ../chas-ege-trunc 
+mkdir ../chas-ege-trunc
+rsync -rW --exclude-from=dev/exclude.rsync . ../chas-ege-trunc/
+echo " var svinta=0; chas.mode.trunc=1; chas.mode.offline=1; 	addstyle('../css/chas-ui.css');" >> ../chas-ege-trunc/lib/head.js
