8f02268ac3552a7024bda3be7f25184dc5e2f9e8
Lopaste <example@mail.com>
2014-07-23 14:04:38 +0400

    77345

---
+++ zdn/mat2014/B2/34.js
@@ -0,0 +1,14 @@
+	
+
+    (function(){'use strict';
+    var percent=sluchch(85,95);
+    var people=sluchch(25000,31000);
+    var area=sklonlxkand(om.naspunkt.iz());
+    var example=sluchch(1,10);
+    var child=sklonlxkand(['ученик','выпускник'].iz());
+    window.vopr.txt='Только '+percent+'% из '+chislitlx(people,'выпускник','r')+' '+area.re+' правильно решили задачу B'+example+'. Сколько '+child.rm+' правильно решили задачу В'+example+'?';
+    window.vopr.ver=[(percent*people/100).round()];
+    })();
+    // Обзад 77345
+    // Павел Lopaste
+
