7734c0c4d896d4a019301a4e7a00bdd2c728c50b
nickkolok <nickkolok@mail.ru>
2014-08-26 00:53:19 +0400

    Проверка скорости кода в оболочке отладки

--- sh/otladka.js
+++ sh/otladka.js
@@ -48,3 +48,13 @@ function izTextarea(){
 	eval($('#textarea-script').val());
 	obnov();
 }
+
+function tt(){
+	var t1=new Date().getTime();
+	var code=$('#textarea-script').val();
+	var iter=1*$('#iter').val();
+	for(var i=iter;i;i--)
+		eval(code);
+	var t2=new Date().getTime();
+	alert('Примерно '+(t2-t1)/iter+' сек.');
+}
