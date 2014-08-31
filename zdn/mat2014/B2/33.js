dadc7e08b41ce53be71a880df69f4ce8b7358d4c
nickkolok <nickkolok@mail.ru>
2014-08-31 16:54:37 +0400

    Косметические правки в B2/33.js

--- zdn/mat2014/B2/33.js
+++ zdn/mat2014/B2/33.js
@@ -2,9 +2,9 @@
 var product=['пачка '+['масла','соли'].iz(),'пакет молока','батон хлеба','банка тушенки','упаковка пельменей'].iz(); //продукт
 var cost = sluchch(50,70);                                      //стоимость
 var people=sklonlxkand(['пенсионер','студент','ветеран'].iz()); //льготники
-var build=['магазин','ларек','киоск'].iz();                     //заведение
+var build=['магазин','ларёк','киоск'].iz();                     //заведение
 var discount=sluchch(2,10);                                      //скидка
-window.vopr.txt=product.toZagl()+' стоит '+chislitlx(cost,'рубль')+'.'+
+window.vopr.txt=product.toZagl()+' стоит '+chislitlx(cost,'рубль')+'. '+
                 people.dm.toZagl()+' '+build+' делает скидку '+
                 discount+'%. Сколько рублей стоит   '+product+' для '+people.re+'?'
 window.vopr.ver=[(cost*(1-discount/100)).ts()]
