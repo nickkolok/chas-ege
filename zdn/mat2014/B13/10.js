a25792801be03928b922f53d5bb2ae6715f9e39e
nickkolok <nickkolok@mail.ru>
2014-11-10 00:04:39 +0300

    Расширение и рефакторинг mat2014/B13/10

--- zdn/mat2014/B13/10.js
+++ zdn/mat2014/B13/10.js
@@ -7,11 +7,13 @@ var v=sluchch(1,10)*a*b*b/2 .pow(sluchch(3))/5 .pow(sluchch(3));
 var va=sl1();
 var vb=sl1();
 var t1=['меньше','больше'];
+var telo=sklonlxkand(['цилиндр','конус'].iz());
 var ca=va?a:1/a;
 var cb=vb?b*b:1/b/b;
 window.vopr.ver=[(v*ca*cb).ts()];
-window.vopr.txt='Объем первого цилиндра равен $'+v.ts()+'~м^3$. У второго цилиндра высота в '+chislitM(a,'раз','раза','раз')+
-				' '+t1[va]+', а радиус основания — в '+chislitM(b,'раз','раза','раз')+' '+t1[vb]+', чем у первого. Найдите объем второго цилиндра. Ответ дайте в кубических метрах.';
+window.vopr.txt='Объём первого '+telo.re+' равен $'+v.ts()+'~м^3$. У второго '+telo.re+' высота в '+chislitlx(a,'раз')+
+	' '+t1[va]+', а радиус основания — в '+chislitlx(b,'раз')+' '+t1[vb]+', чем у первого. '+
+	om.otvnaydite.iz().toZagl()+' объём второго '+telo.re+'. Ответ '+om.otvdayte.iz()+' в кубических метрах.';
 
 window.vopr.kat['log']=0;
 window.vopr.kat['prz']=0;
