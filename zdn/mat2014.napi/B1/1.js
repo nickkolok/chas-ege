767f85f8670f1614c79c3386075d22b4745e785e
Sergey Naumov <drmgc@yandex.ru>
2014-11-21 18:57:41 +0300

    Основа NApi

---
+++ zdn/mat2014.napi/B1/1.js
@@ -0,0 +1,28 @@
+(function(){
+
+var a=sluchch(0,window.dlina.m.length-1);
+var b=sluchch(10,100,5);
+var g=window.dlina.m[a];
+var d=window.transportm.ie.iz();
+window.vopr.txt=window.profesj.ie.iz().toZagl()+' '+om.imenaj.ie.iz()+' '+window.otchestvaj.ie.iz()+
+	' '+window.deistviaj.iz()+
+	' '+d+', на спидометре которого скорость отображается в '+window.dlina.pm[a]+' в час,'+
+	' и отправилась в путь. По дороге ей встретился сотрудник ГАИ с прибором для выявления нарушителей скоростного режима,'+
+	' на котором скорость отображается в километрах в час с округлением до целого числа.'+
+	' '+d.toZagl()+' двигался со скоростью '+b+' '+window.dlina.rm[a]+' в час. '+
+	window.dlina.ie[a].toZagl()+' - это '+g+' метра.'+
+	' Какое число отобразится на экране прибора для выявления нарушителей скоростного режима?';
+
+window.vopr.ver=[(b*g/1000).round()];
+
+window.vopr.kat['log']=0;
+window.vopr.kat['prz']=0;
+window.vopr.kat['drs']=0;
+window.vopr.kat['tri']=0;
+})();
+//Обзад 26640
+
+(function(NApi) {
+	
+})(get_NApi());
+
