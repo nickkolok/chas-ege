58a0bcb774b8c101e510628f6bda98a46dd298eb
Sergey Naumov <drmgc@yandex.ru>
2014-12-03 17:23:48 +0300

    Проверка шаблона в NApi.task.setTask

--- zdn/mat2014/B1/1.js
+++ zdn/mat2014/B1/1.js
@@ -1,3 +1,4 @@
+/*
 (function(){
 
 var a=sluchch(0,window.dlina.m.length-1);
@@ -19,5 +20,27 @@ window.vopr.kat['log']=0;
 window.vopr.kat['prz']=0;
 window.vopr.kat['drs']=0;
 window.vopr.kat['tri']=0;
+})();
+//Обзад 26640
+*/
+
+(function(){
+
+var a=sluchch(0,window.dlina.m.length-1);
+var b=sluchch(10,100,5);
+var g=window.dlina.m[a];
+var d=window.transportm.ie.iz();
+
+var question = window.profesj.ie.iz().toZagl()+' '+om.imenaj.ie.iz()+' '+window.otchestvaj.ie.iz()+
+	' '+window.deistviaj.iz()+
+	' '+d+', на спидометре которого скорость отображается в '+window.dlina.pm[a]+' в час,'+
+	' и отправилась в путь. По дороге ей встретился сотрудник ГАИ с прибором для выявления нарушителей скоростного режима,'+
+	' на котором скорость отображается в километрах в час с округлением до целого числа.'+
+	' '+d.toZagl()+' двигался со скоростью '+b+' '+window.dlina.rm[a]+' в час. '+
+	window.dlina.ie[a].toZagl()+' - это '+g+' метра.'+
+	' Какое число отобразится на экране прибора для выявления нарушителей скоростного режима?';
+
+
+NAtask.setTask({ text : question, answers : [(b*g/1000).round()], tags : ["log", "prz", "drs", "tri"] });
 })();
 //Обзад 26640
