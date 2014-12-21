527b25656316e6bbc55c93dfafcc304b2fbb0b83
nickkolok <nickkolok@mail.ru>
2014-12-21 18:43:48 +0300

    Выпуск

---
+++ zdn/mlegko/upak.js
@@ -0,0 +1,1 @@
+nabor.upak={M1:{1:function(){var a=[1,2,3,4,5,6,9,12,15,18,20].iz(),b=sl(1==a?0:1,a).pm();window.vopr.txt="Переведите $"+b.pina(a)+"$ радиан в градусы.";window.vopr.ver=[(180*b/a).ts()]}}};
