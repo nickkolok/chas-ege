16a6b2a76ca5f9b9b0d538391161a0eacef09cd8
nickkolok <nickkolok@mail.ru>
2015-01-15 14:49:37 +0300

    Расширение zdn/fiztest1/T2/1.js

--- zdn/fiztest1/T2/1.js
+++ zdn/fiztest1/T2/1.js
@@ -1,27 +1,30 @@
 (function(){
-NAinfo.requireApiVersion(0, 0);
+	NAinfo.requireApiVersion(0, 0);
 
-var raz=sl(2,5);
-var v1=sl1();
-var v2=sl1();
-var izm=["уменьши","увеличи"];
-var vel=["период","частота"];
+	var raz=sl(2,5);
+	var v1=sl1();
+	var v2=sl1();
+	var v3=sl1(); //Масса (0) или жёсткость (1)
+	var izm=["уменьши","увеличи"];
+	var vel=["период","частота"];
+	var v=(v1+v2+v3)%2;
+	
+	NAtask.setTask({
+		text:"Как изменится "+vel[v2]+" колебаний груза на пружине, если "+
+			["маccу груза","жёсткость пружины"][v3]+" "+izm[v1]+"ть в "+chislitlx(raz*raz,"раз")+"?",
+		answers:izm[v]+"тся в "+chislitlx(raz,"раз"),
+		wrongAnswers:[
+			izm[v]+"тся в "+chislitlx(raz*raz,"раз"),
+			izm[1-v]+"тся в "+chislitlx(raz*raz,"раз"),
+			izm[1-v]+"тся в "+chislitlx(raz*raz*raz,"раз"),
+			izm[v]+"тся в "+chislitlx(raz*raz*raz,"раз"),
+			izm[1-v]+"тся в "+chislitlx(raz*raz*raz*raz,"раз"),
+			izm[v]+"тся в "+chislitlx(raz*raz*raz*raz,"раз"),
+			izm[1-v]+"тся в "+chislitlx(raz,"раз"),
+		],
+		analys:"Период "+["прямо","обратно"][v3]+" пропорционален квадратному корню из "+["массы","жёсткости"][v3]+". "+
+			"Частота есть величина, обратная к периоду.",
+	});
 
-NAtask.setTask({
-    text:"Как изменится "+vel[v2]+" колебаний груза на пружине, если маccу груза "+izm[v1]+"ть в "+chislitlx(raz*raz,"раз")+"?",
-    answers:izm[1*(v1!=v2)]+"тся в "+chislitlx(raz,"раз"),
-    wrongAnswers:[
-       izm[1*(v1==v2)]+"тся в "+chislitlx(raz*raz,"раз"),
-       izm[1*(v1!=v2)]+"тся в "+chislitlx(raz*raz,"раз"),
-       izm[1*(v1==v2)]+"тся в "+chislitlx(raz*raz*raz,"раз"),
-       izm[1*(v1!=v2)]+"тся в "+chislitlx(raz*raz*raz,"раз"),
-       izm[1*(v1==v2)]+"тся в "+chislitlx(raz*raz*raz*raz,"раз"),
-       izm[1*(v1!=v2)]+"тся в "+chislitlx(raz*raz*raz*raz,"раз"),
-       izm[1*(v1==v2)]+"тся в "+chislitlx(raz,"раз"),
-    ],
-    analys:"Период прямо пропорционален квадратному корню из массы. "+
-        "Частота есть величина, обратная к периоду.",
-});
-
-AtoB();
+	AtoB();
 })();
