08749509cdf3b128fbc00336146071afbcc77dfa
nickkolok <nickkolok@mail.ru>
2015-01-29 01:19:44 +0300

    Косметические правки в шаблоны

--- zdn/mat2014/B2/37.js
+++ zdn/mat2014/B2/37.js
@@ -1,10 +1,10 @@
 (function(){'use strict';
 
 var ForOne=sluchch(50,900,10);
-var InCan=sluchch(1,20,0.5).ts();
+var InCan=sluchch(1,20,0.5);
 var Area=sluchch(20,200,10);
 var Surface=['потолка','стены'].iz();
-window.vopr.txt='Для покраски 1 кв. м '+Surface+' требуется '+ForOne+' г краски. Краска продаётся в банках по '+InCan+
+window.vopr.txt='Для покраски 1 кв. м '+Surface+' требуется '+ForOne+' г краски. Краска продаётся в банках по '+InCan.ts()+
 	' кг. Какое'+' наименьшее количество банок краски нужно купить для покраски '+Surface+' площадью '+Area+' кв. м?';
 
 window.vopr.ver=[(Area*ForOne/1000/InCan).ceil().ts()];
