287b53a5fc8592ba2aaa16992e205dab53eb8bd4
nickkolok <nickkolok@mail.ru>
2015-02-26 00:46:18 +0300

    Очередная магия со сборкой и пересборкой

--- css/make-css-ui.sh
+++ css/make-css-ui.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cat browser.css main.css menu.css ../ext/anyslider/css/anythingslider.css ../ext/anyslider/css/theme-minimalist-square.css ../ext/fonts/stylesheet.css ../ext/keyboard/keyboard.css ../ext/jqplot/jquery.jqplot.css > chas-ui.css;
+#cat browser.css main.css menu.css ../ext/anyslider/css/anythingslider.css ../ext/anyslider/css/theme-minimalist-square.css ../ext/keyboard/keyboard.css ../ext/jqplot/jquery.jqplot.css > chas-ui.css;
 java -jar ../ext/yuicompressor-2.4.8.jar chas-ui.css -o chas-ui.min.css --charset utf-8
