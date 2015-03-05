da6f46966f7b39c5035ea9fab1f08386bf1a0b5f
Sergey Naumov <drmgc@yandex.ru>
2015-03-05 20:58:08 +0300

    buildsys: плагин для jinja

---
+++ tools/buildsys/plugins/jinja.py
@@ -0,0 +1,58 @@
+import logging
+from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader
+
+from api import IPluginApi, IApi
+from mode import Task
+
+
+def jinja_render(api: IApi, build_dir: Path, task: Task) -> bool:
+	"""Создание директорий"""
+	templates_dir_var = "templates-dir"
+	dest_dir_var = "dest"
+	templates_var = "templates"
+
+	if not templates_dir_var in task and not isinstance(task[templates_dir_var], str):
+		logging.error("Ожидалась строка %s" % templates_dir_var)
+		return False
+
+	if not dest_dir_var in task and not isinstance(task[dest_dir_var], str):
+		logging.error("Ожидалась строка %s" % dest_dir_var)
+		return False
+
+	if not templates_var in task and not isinstance(task[templates_var], list):
+		logging.error("Ожидалася массив строк %s" % templates_dir_var)
+		return False
+
+	templdir = Path(task[templates_dir_var])
+	dest = Path(build_dir / task[dest_dir_var])
+	templates = task[templates_var]
+
+	if not templdir.exists():
+		logging.error("Директория '%s' не существует" % templdir)
+		return False
+	if not templdir.is_dir():
+		logging.error("'%s' не является директорией" % templdir)
+		return False
+
+	if not dest.exists():
+		p.mkdir(parents=True)
+	elif not dest.is_dir():
+		logging.error("'%s' существует, но не является директорией" % templdir)
+		return False
+
+	env = Environment(loader=FileSystemLoader(str(templdir)))
+
+	for t in templates:
+		out = dest / Path(t)
+		logging.info("Сборка шаблона {t} в {d}".format(t=t, d=out))
+		templ = env.get_template(t)
+		with out.open("w") as f:
+			f.write(templ.render())
+
+	return True
+
+
+def bs_plugin(api: IPluginApi):
+	api.add_tool("jinja-render", jinja_render)
