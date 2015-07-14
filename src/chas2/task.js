acdb02e8ce01cd8fe0c64d4466edaf37adcf9e82
nickkolok <nickkolok@mail.ru>
2015-07-14 18:10:20 +0300

    #26 - спецфункции генерации заданий восстановлены

--- src/chas2/task.js
+++ src/chas2/task.js
@@ -81,6 +81,123 @@ chas2.task = {
 		if (voprcheck) {
 			chas2._.Lwarn("Результат проверки вопроса:\n\t" + voprcheck);
 		}
-	}
+	},
+
+
+	/** @function NApi.task.setCountableTask
+	 * Установить задание на движке расчётных задач
+	 * @param {Object[]} mainList основной массив величин
+	 * @param {String} mainList[].vel: название величины
+	 * @param {Number} mainList[].rod: род существительного
+		0: мужской
+		1: женский
+		2: средний
+		3: только во множественном числе
+	 * @param {String} mainList[].bkv: буква, которой обозначается величина. Если её нет, не упоминается.
+	 * @param {String|Number} mainList[].zna: значение величины
+	 * @param {String} mainList[].nmn: размерность величины. Опять же, если не указано - лесом.
+	 * @param {Number|Boolean} mainList[].nah: можно ли требовать найти величину
+	 * @param {String} mainList[].pre: префикс, то есть то, что пишется перед названием величины
+	 * @param {String} mainList[].utv: альтернативное величине утвердительное высказывание
+	 * @param {String} mainList[].vpr: альтернативный вопрос
+	 * @param {String|Number} mainList[].vin: величина в винительном падеже. Если равна 1, то именительный и винительный падежи совпадают. Если не определена, то конструкции, где нужен в. п., избегаются.
+	 * @param {Object} options: дополнительные опции
+	 * @param {String} options.pre то, что пишется в вопросе перед задачей
+	 */
+	setCountableTask : function(mainList,options) {
+		var tmpObject=svVel(mainList);
+		if(options){
+			if(options.preambula){
+				tmpObject.text=options.preambula+tmpObject.text;
+			}
+		}
+		NAtask.setTask(tmpObject);
+	},
+
+
+	/** @function NApi.task.variativeABC
+	 * Перемешать буквы латинского алфавита в заданиях, например, на геометрию 
+	 */
+	 //Она же - бывшая dvig.variativeABC, ибо там она не использовалась (не знаю, почему).
+	variativeABC : function(){
+		var alph='ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+		var alph2=alph.slice().shuffle();
+		vopr=mapRecursive(vopr,function(str){
+			return (''+str).cepZamena(alph,alph2);
+		});
+	},
+
+
+	/** @function NApi.task.setTwoStatementTask
+	 * Составить задание о двух утверждениях 
+	 * @param {String|Object[]} stA первое утверждение (или массив утверждений)
+	 * @param {Boolean} trueA истинность первого утверждения
+	 * @param {String} stB второе утверждение
+	 * @param {Boolean} trueB истинность второго утверждения
+	 * @param {String|String[]} pre префикс задания
+	 */
+	 setTwoStatementTask : function(stA, trueA, stB, trueB, pre){
+		if(stA.isArray){
+			var two=stA.iz(2);
+			NAtask.setTwoStatementTask(two[0][0],two[0][1],two[1][0],two[1][1],trueA);
+			return;
+		}
+		if(!pre){
+			pre=[[
+					"Какое утверждение является истинным?",
+					"Какое утверждение верно?",
+				],[
+					"Какое утверждение не является истинным?",
+					"Какое утверждение является ложным?",
+					"Какое утверждение неверно?",
+				]];
+		}
+		if(pre.isArray && pre[1] && sl1()){
+			if(pre[1].isArray){
+				pre=pre[1].iz();
+			}else{
+				pre=pre[1];
+			}	
+			trueA =! trueA;
+			trueB =! trueB;
+		} else if(pre.isArray){
+			if(pre[0].isArray)
+					pre=pre[0].iz();
+			else
+				pre=pre[0];
+		}
+		var wrongAnswers=[
+			"Ни А, ни Б",
+			"Только Б",
+			"Только А",
+			"А и Б",
+		];
+		var answers = wrongAnswers.splice(2*trueA+trueB,1);
+		var br="<br/>";
+		var text=pre+br+br+"А) "+stA+br+"Б) "+stB+br+br;
+		NAtask.setTask({
+			text:text,
+			answers:answers,
+			wrongAnswers:wrongAnswers,
+		});
+		AtoB();
+	},
+
+
+	/** @function NApi.task.replaceCodeInText
+	 * Заменить специальный код на пояснения
+	 * @param {String} code код
+	 * @param {String|String[]} explanations пояснения
+	 */
+	replaceCodeInText : function(code, explanations){
+		if(explanations.isString){
+			explanations=[explanations];
+		}
+		var len=explanations.length;
+		for(var i=0;i<len;i++){
+			vopr.txt=vopr.txt.replace(code,explanations[i]);
+			//Внимание: заменяется одно вхождение!
+		}
+	},
 }
 
