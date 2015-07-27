88301f7c9f0ef277b7a94d136933a012818028b9
Sergey Naumov <drmgc@yandex.ru>
2015-07-27 14:55:39 +0300

    Удаление неиспользуемых файлов

--- c2/timetest.js
+++
@@ -1,9 +0,0 @@
-function tt(){
-	var t1=new Date().getTime();
-	var code=$('#textarea-script').val();
-	var iter=1*$('#iter').val();
-	for(var i=iter;i;i--)
-		eval(code);
-	var t2=new Date().getTime();
-	alert('Примерно '+(t2-t1)/iter+' сек.');
-}
