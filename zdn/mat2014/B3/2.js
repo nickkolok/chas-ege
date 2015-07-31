e37783822cab01c4cdf7607a82d77aa0f9c9df3b
nickkolok <nickkolok@mail.ru>
2015-07-31 20:49:32 +0300

    Внедрение модификатора addJqplot везде, куда можно

--- zdn/mat2014/B3/2.js
+++ zdn/mat2014/B3/2.js
@@ -16,37 +16,9 @@ var t3=om.rusbukv.sluchiz(2);
 
 var v=sluchch(4);
 
-var slid=sl(1000000000);
-
 var ny=[].N(7);
 ny.unshift(0);
 ny.dopFixedLess(3);
-
-window.vopr.dey=function(){
-		$.jqplot('ris'+slid, [a,a2],{
-				axes:{
-					xaxis:{
-						min:d1,
-						max:d2,
-						ticks:n0,
-					},
-					yaxis:{
-						min:0,
-						max:7,
-						ticks:ny,
-					}
-				},
-				legend:{
-					show:true,
-					location:'se',
-				},
-				series:[
-					{label:t3[0],color:"#ccc",shadowAlpha:0},
-					{label:t3[1],color:"#111",shadowAlpha:0},
-				],
-			}
-		);
-};
 
 var i=0;
 var ur=b1.udFunc(function(p1){return p1==b2[i++]});
@@ -57,16 +29,36 @@ var t1=['больше'	,'не меньше'	,'меньше'		,'не больше
 var p1=[ub			,ub+ur			,b1.length-ub-ur,b1.length-ub	];
 var v1=sluchch(3);
 
-
-window.vopr.txt='<div id="ris'+slid+'" style="text-align:center;height:320px; width:600;" opozn="'+Math.random()+'"></div>';
-window.vopr.txt+='На рисунке жирными точками показано суточное количество осадков, выпадавших в городах ' +t3[0]+' и '+t3[1]+
-				' с '+d1+' по '+d2+' '+om.mesiacy.re.iz()+' '+sluchch(1950,2013)+' года. По горизонтали указываются числа месяца, по вертикали — количество '+
-				'осадков, выпавших в соответствующий день, в миллиметрах. Для наглядности жирные точки на рисунке'+
-				' соединены линией. Определите по рисунку, сколько дней в '+t3[0]+' выпадало осадков '+t1[v1]+', чем в '+t3[1]+'.';
-window.vopr.ver=[''+p1[v1]];
-
-window.vopr.kat['log']=0;
-window.vopr.kat['prz']=0;
-window.vopr.kat['drs']=0;
-window.vopr.kat['tri']=0;
+NAtask.setTask({
+	text: 'На рисунке жирными точками показано суточное количество осадков, выпадавших в городах ' +t3[0]+' и '+t3[1]+' с '+d1+
+		' по '+d2+' '+om.mesiacy.re.iz()+' '+sluchch(1950,2013)+' года. По горизонтали указываются числа месяца, по вертикали '+
+		'— количество осадков, выпавших в соответствующий день, в миллиметрах. Для наглядности жирные точки на рисунке'+
+		' соединены линией. Определите по рисунку, сколько дней в '+t3[0]+' выпадало осадков '+t1[v1]+', чем в '+t3[1]+'.',
+	answers: p1[v1],
+});
+NAtask.modifiers.addJqplot({
+	dataArray: [a,a2],
+	options: {
+		axes:{
+			xaxis:{
+				min:d1,
+				max:d2,
+				ticks:n0,
+			},
+			yaxis:{
+				min:0,
+				max:7,
+				ticks:ny,
+			}
+		},
+		legend:{
+			show:true,
+			location:'se',
+		},
+		series:[
+			{label:t3[0],color:"#ccc",shadowAlpha:0},
+			{label:t3[1],color:"#111",shadowAlpha:0},
+		],
+	},
+});
 })();
--- zdn/mat2014/B3/4.js
+++ zdn/mat2014/B3/4.js
@@ -15,49 +15,41 @@ f=f.T();
 f=[[].N(a),f[1]];
 f=f.T();
 
-var slid=sl(1000000000);
-
 var ny=[].N(d2);
 ny.splice(0,d1);
 ny=[ny,ny.toFixedLess(3)].T();
 
-window.vopr.dey=function(){
-		$.jqplot('ris'+slid, [f],{
-				axes:{
-					xaxis:{
-						min:1,
-						max:a,
-						ticks:h,
-						renderer:$.jqplot.CategoryAxisRenderer,
-					},
-					yaxis:{
-						min:d1,
-						max:d2,
-						ticks:ny,
-					}
-				},
-				seriesDefaults: {
-					renderer: $.jqplot.BarRenderer,
-					rendererOptions: { barMargin: 15 },
-				}
-			}
-		);
-};
-
-
 var t1=['превышает '+a1		,'не превышает '+a1	,'ниже '+a2			,'не ниже '+a2			,'находится между '+a1+' и '+a2+' (включительно)'];
 var p1=[c.kolvoMzhd(a1,100)	,c.kolvoMzhd(0,a1,1),c.kolvoMzhd(0,a2)	,c.kolvoMzhd(a2,100,1)	,c.kolvoMzhd(a1,a2,1)		];
 var v1=sluchch(4);
-window.vopr.txt='<div id="ris'+slid+'" style="text-align:center;height:320px; width:970;" opozn="'+Math.random()+'"></div>';
-window.vopr.txt+='На диаграмме показан уровень подоходного налога в '+a+' странах мира (в процентах), предлагаемый '+
-				'некоей программой по преодолению Всемирного Экономического Кризиса. По горизонтали указана страна, по '+
-				'вертикали — уровень подоходного налога в процентах. '+
-				'Определите, в скольких из перечисленных государств уровень подоходного налога '+t1[v1]+'%.';
 
-window.vopr.ver=[''+p1[v1]];
-
-window.vopr.kat['log']=0;
-window.vopr.kat['prz']=0;
-window.vopr.kat['drs']=0;
-window.vopr.kat['tri']=0;
+NAtask.setTask({
+	text: 'На диаграмме показан уровень подоходного налога в '+a+' странах мира (в процентах), предлагаемый ' +
+		'некоей программой по преодолению Всемирного Экономического Кризиса. По горизонтали указана страна, по ' +
+		'вертикали — уровень подоходного налога в процентах. ' +
+		'Определите, в скольких из перечисленных государств уровень подоходного налога ' + t1[v1] + '%.',
+	answers: p1[v1],
+});
+NAtask.modifiers.addJqplot({
+	dataArray: [f],
+	options: {
+		axes:{
+			xaxis:{
+				min:1,
+				max:a,
+				ticks:h,
+				renderer:$.jqplot.CategoryAxisRenderer,
+			},
+			yaxis:{
+				min:d1,
+				max:d2,
+				ticks:ny,
+			}
+		},
+		seriesDefaults: {
+			renderer: $.jqplot.BarRenderer,
+			rendererOptions: { barMargin: 15 },
+		}
+	}
+});
 })();
