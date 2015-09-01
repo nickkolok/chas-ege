4541af406978891786940a10d41c5ce8d60aba54
nickkolok <nickkolok@mail.ru>
2015-09-02 02:18:33 +0300

    String.parseHumanReadableToJSON - Функция парсинга JSON из человекочитаемого формата

---
+++ src/nlib/String.js
@@ -0,0 +1,18 @@
+'use strict';
+
+String.prototype.parseHumanReadableToJSON = function(){
+	var result = {};
+	var rows = this.split(/\s*[\n\r]+\s*/g);
+	var currentProperty = 0;
+	for(var i=0; i<rows.length; i++){
+		if(/:$/.test(rows[i])){
+			currentProperty=rows[i].replace(/:$/,'');
+			result[currentProperty]='';
+		}else{
+			result[currentProperty]+=' '.esli(result[currentProperty])+rows[i];
+		}
+	}
+	return result;
+}
+
+String.prototype.addToGlobal('docsString',1);
