5cd4c61f8e2907f5c084e1ad58408c36dc99daaa
nickkolok <nickkolok@mail.ru>
2015-12-15 22:50:28 +0300

    Сбойный шаблон mat2014/B12/11 исключён и снабжён указывающим на ошибку комментарием

--- zdn/mat2014/B12/11.js
+++ zdn/mat2014/B12/11.js
@@ -1,8 +1,6 @@
-// kbsx32
-
 (function() {
 
-    var body =  sklonlxkand((['мяч', 'стакан']).iz());
+    var body =  sklonlxkand((['мяч', 'стакан', 'тело', 'молот']).iz());
     var velbeg = sluchch(10.0, 30.0);
     var time = sluchch(1.0, 4.0);
     var phys_g = 10;
@@ -10,18 +8,21 @@
     var angname = ['alpha', 'beta', 'gamma'].iz();
     var angname_tex = '$\\' + angname + '$';
     
-    window.vopr.txt=(
-            body.ie.toZagl() + ' бросили под углом к плоской горизонтальной поверхности земли. ' + 
+    chas2.task.setTask({
+		text: body.ie.toZagl() + ' бросили под углом к плоской горизонтальной поверхности земли. ' + 
             'Время полёта ' + body.re +' задаётся законом $t=\\frac{2v_0\\sin\\' + angname + '}{g} с.$ ' +
             'При каком наименьшем значении угла ' + angname_tex + ' время полёта ' + 
             'будет не меньше, чем ' + chislitlx(time, 'секунда') + ', ' +
             ' если ' + body.ie + ' бросают с начальной скоростью $v_0=' + velbeg +'\\frac{м}{с}$? ' +
             'Считайте, что ускорение свободного падения $g=$' + phys_g + '$\\frac{м}{с^2}$. ' +
-            'Дайте в качестве ответа синус угла $\\'+ angname +'$. Ответ округлите до десятых.');
-
-    window.vopr.ver=[(time * phys_g / 2.0 / velbeg * 10).round() / 10];
+            'Дайте в качестве ответа синус угла $\\'+ angname +'$. Ответ округлите до десятых.',
+        answer: (time * phys_g / 2.0 / velbeg * 10).round() / 10,
+	});
+	//Синус больше единицы. Зло!
     window.vopr.kat['log'] = 0;
     window.vopr.kat['prz'] = 0;
     window.vopr.kat['drs'] = 0;
     window.vopr.kat['tri'] = 0;
 })();
+// kbsx32
+
