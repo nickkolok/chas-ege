d9992912e9a87125c19e32ebe5b0eef76041f4fd
nickkolok <nickkolok@mail.ru>
2015-12-15 22:25:39 +0300

    Косметические правки в тесты во имя спокойствия eslint

--- test/test.js
+++ test/test.js
@@ -102,9 +102,9 @@ test('chaslib.toArray', function() {
 });
 
 test('String.prototype.parseHumanReadableToJSON', function() {
-	expect('Задача: \n\r У Васи было три яблока \n\rОтвет:\n\r3'.parseHumanReadableToJSON()).to.eql({"Задача":"У Васи было три яблока","Ответ":"3"});
-	expect('Задача: \n\r У Васи было \nтри яблока \n\rОтвет:\n\r3'.parseHumanReadableToJSON()).to.eql({"Задача":"У Васи было три яблока","Ответ":"3"});
-	expect('Задача: \n\r У Васи было\r \nтри яблока \n\rОтвет:\n\r3'.parseHumanReadableToJSON()).to.eql({"Задача":"У Васи было три яблока","Ответ":"3"});
+	expect('Задача: \n\r У Васи было три яблока \n\rОтвет:\n\r3'    .parseHumanReadableToJSON()).to.eql({ 'Задача':'У Васи было три яблока', 'Ответ':'3' });
+	expect('Задача: \n\r У Васи было \nтри яблока \n\rОтвет:\n\r3'  .parseHumanReadableToJSON()).to.eql({ 'Задача':'У Васи было три яблока', 'Ответ':'3' });
+	expect('Задача: \n\r У Васи было\r \nтри яблока \n\rОтвет:\n\r3'.parseHumanReadableToJSON()).to.eql({ 'Задача':'У Васи было три яблока', 'Ответ':'3' });
 });
 
 test('isCppCode', function() {
