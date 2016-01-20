5eaff65f3b3b2a3430c4eb61507cc3b7fc14ffff
Никитка Пупкин <hkpw@yandex.ru>
2016-01-20 13:44:14 +0300

    Сделана задача №42,обзад №77352, mat2014/B2/42.js

---
+++ zdn/mat2014/B2/42.js
@@ -0,0 +1,22 @@
+(function() {
+	'use strict';
+	NAinfo.requireApiVersion(0, 2);
+	var komis = sluchch(2, 10);
+	var rubl = sluchch(200, 500, 10);
+	var kratn = [5, 10, 50].iz();
+	var imena = sklonlxkand(['Надя', 'Валера', 'Дима', 'Игорь', 'Галя', 'Иван', 'Гена', 'Григорий', 'Пётр', 'Роман'].iz());
+	var sche = ['qiwi', 'брата', 'мобильного телефона', 'webmoney'].iz();
+
+	NAtask.setTask({
+
+	        text: 'При оплате услуг через платежный терминал взимается комиссия ' + chislitlx(komis, 'процент') + ' . ' +
+		    'Терминал принимает суммы кратные ' + chislitlx(kratn, 'рубль') + '. ' +
+		    ' ' + imena.ie + ' хочет положить на счет ' + sche + ' не меньше ' + chislitlx(rubl, 'рубль') + ' . ' +
+		    'Какую минимальную сумму надо положить в приемное устройство данного терминала? ',
+
+		answers: ((rubl / (1 - komis / 100)) / kratn).ceil() * kratn,
+	});
+})();
+// Обзад 77352
+// WaziX
+
