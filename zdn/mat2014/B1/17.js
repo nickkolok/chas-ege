6a62796151a2cd4fcd2f8d7c0b20443c3a3a63d6
Valery Hripunov <hripunov@rocem.ru>
2016-01-23 18:58:27 +0300

    Обновлены задания по новым стандартам; zdn/mat2014/b1

--- zdn/mat2014/B1/17.js
+++ zdn/mat2014/B1/17.js
@@ -8,11 +8,11 @@ var stoim=cena*(kg+gr/1000);
 var d=[50,100,500].iz();
 var summa=okrugldo(stoim+d-0.01,d);
 
-window.vopr.txt='Летом килограмм '+yagoda.re+' стоит '+cena.rub()+'. '+
+NAtask.setTask({
+	text : 'Летом килограмм '+yagoda.re+' стоит '+cena.rub()+'. '+
 	om.imenaj.ie.iz()+' купила '+kg+' кг '+gr+' г '+yagoda.re+
-	'. Сколько рублей сдачи она должна получить с '+summa.rub()+'?';
-	
-window.vopr.ver=[(summa-stoim).ts()];
-
+	'. Сколько рублей сдачи она должна получить с '+summa.rub()+'?',
+	answers : (summa-stoim).ts(),
+});
 })();
 //Обзад 26636
--- zdn/mat2014/B1/2.js
+++ zdn/mat2014/B1/2.js
@@ -5,13 +5,10 @@ var c=sl(1,10);
 var a=sl(3,9);
 var b=sl(1,9);
 
-window.vopr.txt=om.eda.ie[t].toZagl()+' стоит '+chislitlx(a,'рубль')+' '+b+'0 копеек. '+
-	'Какое наибольшее число '+om.eda.rm[t]+' можно купить на '+c+'0 рублей?';
-window.vopr.ver=[(c*100/(10*a+b)).floor().ts()];
-
-window.vopr.kat['log']=0;
-window.vopr.kat['prz']=0;
-window.vopr.kat['drs']=0;
-window.vopr.kat['tri']=0;
+NAtask.setTask({
+	text : om.eda.ie[t].toZagl()+' стоит '+chislitlx(a,'рубль')+' '+b+'0 копеек. '+
+	'Какое наибольшее число '+om.eda.rm[t]+' можно купить на '+c+'0 рублей?',
+	answers : (c*100/(10*a+b)).floor().ts(),
+});
 })();
 //Обзад 26616
