82ed375ac8158dd0acf38e9444c5d82997be89e8
Коновалов <crazy_vrn@mail.ru>
2016-01-29 16:10:41 +0300

    Сделана задача №79, zdn/egeok/M7/7.js

---
+++ zdn/egeok/M7/7.js
@@ -0,0 +1,13 @@
+(function() {
+	'use strict';
+	NAinfo.requireApiVersion(0, 2);
+	var kan = sluchch(2, 9);
+	var cpecial = sluchch(9, 15);
+	var otvet = (cpecial.fct()) / (((cpecial - kan).fct()) * kan.fct());
+	NAtask.setTask({
+		text: 'Сколько существует различных вариантов выбора ' + chislitlx(kan, 'кандидатура', 'r') + ' из ' + chislitlx(cpecial, 'специалист') +' для поездки в ' + chislitlx(kan, 'различных страна') + '?',
+     
+		answers: otvet,
+	});
+	})();
+
