6b69b2f5329d0bbc12fd5c3e1882ac1b7e85ebbd
Коновалов игорь <crazy_vrn@mail.ru>
2016-02-09 12:56:05 +0300

    Рефакторинг mat2014-B2-2 перевод с window.vopr на setTask, zdn/mat2014/B1/2.js

--- zdn/mat2014/B1/2.js
+++ zdn/mat2014/B1/2.js
@@ -1,4 +1,5 @@
-(function(){
+(function(){'use strict';
+NAinfo.requireApiVersion(0, 0);
 
 var t=sl(om.eda.ie.length-1);
 var c=sl(1,10);
@@ -6,9 +7,12 @@ var a=sl(3,9);
 var b=sl(1,9);
 
 NAtask.setTask({
-	text : om.eda.ie[t].toZagl()+' стоит '+chislitlx(a,'рубль')+' '+b+'0 копеек. '+
+
+	text:om.eda.ie[t].toZagl()+' стоит '+chislitlx(a,'рубль')+' '+b+'0 копеек. '+
 	'Какое наибольшее число '+om.eda.rm[t]+' можно купить на '+c+'0 рублей?',
-	answers : (c*100/(10*a+b)).floor().ts(),
+
+	answers:(c*100/(10*a+b)).floor().ts(),
+
 });
 })();
 //Обзад 26616
