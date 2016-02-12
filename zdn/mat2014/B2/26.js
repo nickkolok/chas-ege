0fe2ee0625bb118e773f2545eff66deebe8061e8
Коновалов Игорь <crazy_vrn@mail.ru>
2016-02-12 13:40:47 +0300

    Рефакторинг mat2014-B2-26 перевод с window.vopr на setTask, mat2014/B2/26.js

--- zdn/mat2014/B2/26.js
+++ zdn/mat2014/B2/26.js
@@ -1,14 +1,19 @@
 (function(){'use strict';
+NAinfo.requireApiVersion(0, 0);
 
 var clothes = sklonlxkand(['футболка','майка'].iz());
 var oldprice = sluchch(600,1000,10);
 var newprice = sluchch(400,oldprice-100,10);
 
-window.vopr.txt = clothes.ie.toZagl() + " стоила " + chislitlx(oldprice,"рубль") + ". " +
-		"После снижения цены она стала стоить " + chislitlx(newprice,"рубль") + ". " +
-		"На сколько процентов была снижена цена на " + clothes.ve + "?";
+NAtask.setTask({
 
-window.vopr.ver = [(100-(newprice/(oldprice/100))).ceil()];
+    text: clothes.ie.toZagl() + " стоила " + chislitlx(oldprice,"рубль") + ". " +
+		"После снижения цены она стала стоить " + chislitlx(newprice,"рубль") + ". " +
+		"На сколько процентов была снижена цена на " + clothes.ve + "?",
+
+    answers: (100-(newprice/(oldprice/100))).ceil(),
+
+});
 })();
 
 //Обзад 26630
