61bce0002ccc7d93eb175a3a85b96e8e2aba8c2a
Коновалов Игорь <crazy_vrn@mail.ru>
2016-02-12 13:02:40 +0300

    Рефакторинг mat2014-B3-1 перевод с window.vopr на setTask, mat2014/B3/1.js

--- zdn/mat2014/B3/1.js
+++ zdn/mat2014/B3/1.js
@@ -1,16 +1,18 @@
 (function(){'use strict';
-	
-var c=1*(sluchch(0.1,0.5,0.1).toFixedLess(2));
-var a=sluchch(30,50,c);
-var d=sl1();
-var f=a+18*c;
-var b=[];
-b[0]=sluchch(a,f,c);
-for(var i=1;i<29;i++){
-	b[i]=1*(sluchch(Math.max(a,b[i-1]-5*c),Math.min(f,b[i-1]+5*c),c).toFixedLess(2));
-}
-b[0]=b[1];
-var g=[	'Каков минимальный курс за этот период?',
+NAinfo.requireApiVersion(0, 0);
+
+	var c=1*(sluchch(0.1,0.5,0.1).toFixedLess(2));
+	var a=sluchch(30,50,c);
+	var d=sl1();
+	var f=a+18*c;
+	var b=[];
+	b[0]=sluchch(a,f,c);
+	for(var i=1;i<29;i++){
+		b[i]=1*(sluchch(Math.max(a,b[i-1]-5*c),Math.min(f,b[i-1]+5*c),c).toFixedLess(2));
+	}
+	b[0]=b[1];
+	var g=[
+		'Каков минимальный курс за этот период?',
 		'Каков максимальный курс за этот период?',
 		'Найдите амплитуду изменения курса за этот период.',
 		'Какого числа был впервые достигнут минимальный курс?',
@@ -18,13 +20,22 @@ var g=[	'Каков минимальный курс за этот период?'
 		'Какого числа в последний раз достигнут минимальный курс?',
 		'Какого числа в последний раз достигнут максимальный курс?',
 		];
-var h=sluchch(0,6);
-var m=[b[b.min()],b[b.max()],b[b.max()]-b[b.min()],b.min(1),b.max(1),b.min(),b.max()];
-if(!m[h])
+	var h=sluchch(0,6);
+	var m=[b[b.min()],b[b.max()],b[b.max()]-b[b.min()],b.min(1),b.max(1),b.min(),b.max()];
+	if(!m[h])
 	m[h]=1;
-var slid=sl(1000000000);
+	var slid=sl(1000000000);
 
-window.vopr.dey=function(){
+
+NAtask.setTask({
+
+	text: '<canvas style="float:left;margin-right:1em;" width="600" height="400" id="ris'+slid+'" style="text-align:center" opozn="'+Math.random()+'"></canvas>'+
+	'На графике жирными точками, для наглядности соединёнными линией, показан курс '+sluchiz(window.valuta.re)+
+	', установленный неким банком в период с 1 по 28 '+sluchiz(window.mesiacy.re)+' '+sluchch(2001,2012)+' года.'+g[h],
+
+	answers: m[h].toFixedLess(2),
+
+	draw: function(){
 	var ris=document.getElementById('ris'+slid);
 	var ct = ris.getContext('2d');
 	var w=600;
@@ -52,18 +63,6 @@ window.vopr.dey=function(){
 		ct.fillKrug(i*s-s,-(b[i]-a)*s/c,5);
 	}
 	$('#ris').attr('id','');
-};
-
-
-
-window.vopr.txt='<canvas style="float:left;margin-right:1em;" width="600" height="400" id="ris'+slid+'" style="text-align:center" opozn="'+Math.random()+'"></canvas>';
-window.vopr.txt+='На графике жирными точками, для наглядности соединёнными линией, показан курс '+sluchiz(window.valuta.re);
-window.vopr.txt+=', установленный неким банком в период с 1 по 28 '+sluchiz(window.mesiacy.re)+' '+sluchch(2001,2012)+' года.';
-window.vopr.txt+=' '+g[h];
-window.vopr.ver=[''+m[h].toFixedLess(2)];
-
-window.vopr.kat['log']=0;
-window.vopr.kat['prz']=0;
-window.vopr.kat['drs']=0;
-window.vopr.kat['tri']=0;
+},
+});
 })();
