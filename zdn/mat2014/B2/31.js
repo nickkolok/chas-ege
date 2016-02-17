3f580620dbd32a4367dc0c2783359043b3abe712
Коновалов Игорь <crazy_vrn@mail.ru>
2016-02-17 20:54:43 +0300

    Рефакторинг mat2014-B2-31 перевод с window.vopr на setTask, mat2014/B2/31.js

--- zdn/mat2014/B2/31.js
+++ zdn/mat2014/B2/31.js
@@ -1,18 +1,25 @@
 (function(){'use strict';
+NAinfo.requireApiVersion(0, 0);
+
 
 // Чтобы не было бесконечных дробей в ответе
-do {
-    var students = sluchch(10, 50);
-    var percent = sluchch(20, 65, 15);
-} while (students * 100 % percent != 0);
+	do {
+		var students = sluchch(10, 50);
+		var percent = sluchch(20, 65, 15);
+	}
+	while (students * 100 % percent != 0);
 
-var school = sklonlxkand(['школа', 'лицей', 'гимназия'].iz());
-var vuz = ['военных', 'специальных', 'частных'].iz();
-           
-window.vopr.txt = chislitlx(students,'выпускник') +' '+ school.re +' собираются учиться в '+ vuz +' вузах. ' +
-	'Они составляют '+ percent +'% от числа выпускников. Сколько в '+ school.pe +' выпускников?';
-           
-window.vopr.ver = [(students / percent * 100).ts()];
+	var school = sklonlxkand(['школа', 'лицей', 'гимназия'].iz());
+	var vuz = ['военных', 'специальных', 'частных'].iz();
+
+NAtask.setTask({
+
+	text: chislitlx(students,'выпускник') +' '+ school.re +' собираются учиться в '+ vuz +' вузах. ' +
+	'Они составляют '+ percent +'% от числа выпускников. Сколько в '+ school.pe +' выпускников?',
+
+	answers: (students / percent * 100).ts(),
+
+});
 })();
 //Обзад 77341
 //ProgerFF
