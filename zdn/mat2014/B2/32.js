17004775a73faa0fab02c829a1e9547e886c00b0
Коновалов Игорь <crazy_vrn@mail.ru>
2016-02-17 20:59:15 +0300

    Рефакторинг mat2014-B2-32 перевод с window.vopr на setTask, mat2014/B2/32.js

--- zdn/mat2014/B2/32.js
+++ zdn/mat2014/B2/32.js
@@ -1,21 +1,27 @@
 (function(){'use strict';
+NAinfo.requireApiVersion(0, 0);
 
-var goods=sklonlxkand(['тетрадь','шуруп','карандаш','ручка'].iz());//товар
-var cost=sluchch(5,25);                                            //стоимость
-var sum=sluchch(20,70);                                            //количество
-var sumd=sluchch(30,50,10);                                        //количество,необходимое для скидки
-var discount=sluchch(5,12);                                        // скидка
-var client=['покупатель','школьник','ученик'].iz();                // покупатель
-var build=['киоск','ларёк','магазин'].iz()                         // заведение
-window.vopr.txt=goods.ie.toZagl()+' стоит '+chislitlx(cost,'рубль')+'. Сколько рублей заплатит '+
+	var goods=sklonlxkand(['тетрадь','шуруп','карандаш','ручка'].iz());//товар
+	var cost=sluchch(5,25);                                            //стоимость
+	var sum=sluchch(20,70);                                            //количество
+	var sumd=sluchch(30,50,10);                                        //количество,необходимое для скидки
+	var discount=sluchch(5,12);                                        // скидка
+	var client=['покупатель','школьник','ученик'].iz();                // покупатель
+	var build=['киоск','ларёк','магазин'].iz();                          // заведение
+	if (sumd<sum){
+		var m=(cost*sum*(1-discount/100)).ceil() ;   //чтобы лишний раз не пересчитывать
+	}else{
+		var m=(cost*sum).ceil();
+	}
+
+NAtask.setTask({
+
+	text: goods.ie.toZagl()+' стоит '+chislitlx(cost,'рубль')+'. Сколько рублей заплатит '+
 	client+' за '+chislitlx(sum,goods)+', если при покупке больше '+chislitlx(sumd,goods)+
-	' '+build+' делает скидку '+discount+'% от стоимости всей покупки?';
-if (sumd<sum){
-	window.vopr.ver=[(cost*sum*(1-discount/100)).ceil()];//чтобы лишний раз не пересчитывать
-}else{
-	window.vopr.ver=[(cost*sum).ceil()];
-}
+	' '+build+' делает скидку '+discount+'% от стоимости всей покупки?',
 
+	answers:  m,
+});
 })();
 // Обзад 77343
 // Павел Lopaste
