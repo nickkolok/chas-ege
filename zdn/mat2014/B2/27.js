5014a37e44884a915e6d656b9c1fd9dd18eb44c1
Коновалов Игорь <crazy_vrn@mail.ru>
2016-02-26 19:14:54 +0300

    Рефакторинг mat2014-B2-27 перевод с window.vopr на setTask, mat2014/B2/27.js

--- zdn/mat2014/B2/27.js
+++ zdn/mat2014/B2/27.js
@@ -1,28 +1,36 @@
-(function(){
-var G_IN_POUND = 400;
+(function(){'use strict';
+NAinfo.requireApiVersion(0, 0);
 
-var people_in_recipe;
-var filling_in_recipe_factor = sl(1, 3) * 10; 
-var filling_in_recipe_factor_a = sl(1, filling_in_recipe_factor-1);
-var required_people;
+	var G_IN_POUND = 400;
+	var people_in_recipe;
+	var filling_in_recipe_factor = sl(1, 3) * 10;
+	var filling_in_recipe_factor_a = sl(1, filling_in_recipe_factor-1);
+	var required_people;
 
-var answer;
+	var answer;
 
-do {
+	do
+	{
 	people_in_recipe = sl(5, 30);
 	required_people = slKrome(people_in_recipe, 3, 12);
 	
 	answer = G_IN_POUND / filling_in_recipe_factor * filling_in_recipe_factor_a / people_in_recipe * required_people;
-} while(!(answer * 100).isZ())
+	}
 
-var product = sklonlxkand([ 'чернослив', 'черника', 'земляника', 'ежевика' ].iz());
+	while(!(answer * 100).isZ());
 
-window.vopr.txt = 'Имеется рецепт пирога с ' + product.te + '. Для пирога на ' + people_in_recipe + ' человек следует взять ' +
+	var product = sklonlxkand([ 'чернослив', 'черника', 'земляника', 'ежевика' ].iz());
+
+NAtask.setTask({
+
+	text: 'Имеется рецепт пирога с ' + product.te + '. Для пирога на ' + people_in_recipe + ' человек следует взять ' +
                   '$'+filling_in_recipe_factor_a.frac(filling_in_recipe_factor) + '$ фунта ' + product.re +
                   '. Сколько граммов ' + product.re + ' следует взять для пирога, рассчитанного на ' + required_people +
-                  ' человек? Считайте, что 1 фунт равен 0,4 кг';
+                  ' человек? Считайте, что 1 фунт равен 0,4 кг',
 
-window.vopr.ver = [ answer ];
-})()
+	answers: answer,
+
+});
+})();
 //Обзад 318582
 // Sergey Naumov aka DrMGC
