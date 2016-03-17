c47cd8988a02cfd4b2b234e87a7a2b796423a1ad
KsenijaV <Ksenija.2017@mail.ru>
2016-03-17 23:12:43 +0300

    Рефакторинг zdn/tri/E3/7

--- zdn/tri/E3/7.js
+++ zdn/tri/E3/7.js
@@ -1,15 +1,17 @@
-(function(){'use strict';
-
-window.vopr.txt='$ \\sin{(2\\pi-a)}=$<br/>';
-window.vopr.ver=[
-	'$ -\\sin{a}  $',
-];
-window.vopr.nev=[
-	'$ \\cos{a}  $',
-	'$ -\\cos{a}  $',
-	'$ \\sin{a}  $',
-];
-AtoB();
+(function() {
+	'use strict';
+	chas2.task.setTask({
+		text: '$ \\sin{(2\\pi-a)}=$<br/>',
+		answers: [
+			'$ -\\sin{a}  $',
+		],
+		wrongAnswers: [
+			'$ \\cos{a}  $',
+			'$ -\\cos{a}  $',
+			'$ \\sin{a}  $',
+		],
+	});
+	AtoB();
 
 })();
-//Любовь Ерышова
+//Любовь 
