cecb52e5780c75633622b5d71c1429a080759bcd
KsenijaV <Ksenija.2017@mail.ru>
2016-03-17 14:15:30 +0300

    zdn/tri/E3/2.js

--- zdn/tri/E3/2.js
+++ zdn/tri/E3/2.js
@@ -1,16 +1,17 @@
-(function(){'use strict';
-
-window.vopr.txt='$ \\sin{(\\frac{\\pi}{2}+a)}=$<br/>';
-window.vopr.ver=[
-	'$ \\cos{a}  $',
-];
-window.vopr.nev=[
-	'$ \\sin{a}  $',
-	'$ \\mathrm{tg~}{a}  $',
-	'$ -\\cos{a}  $',
-	'$ -\\sin{a}  $',
-];
-AtoB();
-
+(function() {
+	'use strict';
+	chas2.task.setTask({
+		text: '$ \\sin{(\\frac{\\pi}{2}+a)}=$<br/>',
+		answers: [
+			'$ \\cos{a}  $',
+		],
+		wrongAnswers: [
+			'$ \\sin{a}  $',
+			'$ \\mathrm{tg~}{a}  $',
+			'$ -\\cos{a}  $',
+			'$ -\\sin{a}  $',
+		],
+	});
+	AtoB();
 })();
 //Любовь Ерышова
