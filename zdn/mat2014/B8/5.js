288876e3f8ae48764405788d5843adfc8df9fc90
dev-nulls <devilnulls@gmail.com>
2016-04-13 23:32:06 +0300

    eslint zdn/mat2014/B8/5.js

--- zdn/mat2014/B8/5.js
+++ zdn/mat2014/B8/5.js
@@ -8,7 +8,27 @@ var y1=b.isPolnKvadr();
 var v1=sl1();
 chas2.task.setTask({
 	text: 'В треугольнике $'+'ABC'.mesh()+'$ угол $A$ равен $90^\\circ$, $\\'+t1[0]+' B = '+
-		(y1?a:a+b.koren().esli(v1)).frac(y1?b.sqrt:(v1?b:b.koren()))+'$. Найдите $\\'+t1[1]+'$.',
+		(function()
+		{
+		   var res1, res2;
+		   res1 = res2 = null;
+		   if (y1){
+			   res1 = a;
+		   }else{
+			   res1 = a + b.koren().esli(v1);
+		   }
+		   if (y1){
+			   res2 = b.sqrt;
+		   }else{
+			   if (v1){
+				   res2 = b;
+			   }else{
+				   res2 = b.koren();
+			   }
+		   }
+		   return res1.frac(res2);
+		})() +
+	'$. Найдите $\\'+t1[1]+'$.',
 	answers: (a/c).ts(),
 	tags:{tri:1},
 });
