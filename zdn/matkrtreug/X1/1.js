c30bd45cf2b771edb8c453b891ed2e87e0fa75d2
dev-nulls <devilnulls@gmail.com>
2016-04-14 07:21:33 +0300

    spaces zdn/matkrtreug/X1/1.js

--- zdn/matkrtreug/X1/1.js
+++ zdn/matkrtreug/X1/1.js
@@ -14,33 +14,33 @@
 
 	switch(task)
 	{
-	case 0:
-		window.vopr.txt=(
-			'Катеты прямоугольного треугольника равны ' +
-			cat_a + ' и ' + cat_b + '. ' +
-			'Найдите гипотенузу.');
-		window.vopr.ver=[Math.sqrt(cat_a * cat_a + cat_b * cat_b)];
-		break;
-	case 1:
-		window.vopr.txt=(
-			'Катеты прямоугольного треугольника равны ' +
-			cat_a + ' и ' + cat_b + '. ' +
-			'Найдите площадь этого треугольника.');
-		window.vopr.ver=[0.5 * cat_a * cat_b];
-		break;
-	case 2:
-		window.vopr.txt=(
-			'В прямоугольном треугольнике один катет равен равен ' +
-			cat_a + ', а другой ' +
-			(cat_a == cat_b			 // is equal
-				? 'равен ему. '
-				: 'на ' + Math.abs(cat_b - cat_a) +
-				(cat_a < cat_b		  // not equal: cat_a != cat_b
-					? ' больше его. '
-					: ' меньше его. ')) +
-			'Найдите площадь треугольника.');
-		window.vopr.ver=[0.5 * cat_a * cat_b];
-		break;
+		case 0:
+			window.vopr.txt=(
+				'Катеты прямоугольного треугольника равны ' +
+				cat_a + ' и ' + cat_b + '. ' +
+				'Найдите гипотенузу.');
+			window.vopr.ver=[Math.sqrt(cat_a * cat_a + cat_b * cat_b)];
+			break;
+		case 1:
+			window.vopr.txt=(
+				'Катеты прямоугольного треугольника равны ' +
+				cat_a + ' и ' + cat_b + '. ' +
+				'Найдите площадь этого треугольника.');
+			window.vopr.ver=[0.5 * cat_a * cat_b];
+			break;
+		case 2:
+			window.vopr.txt=(
+				'В прямоугольном треугольнике один катет равен равен ' +
+				cat_a + ', а другой ' +
+				(cat_a == cat_b			 // is equal
+					? 'равен ему. '
+					: 'на ' + Math.abs(cat_b - cat_a) +
+					(cat_a < cat_b		  // not equal: cat_a != cat_b
+						? ' больше его. '
+						: ' меньше его. ')) +
+				'Найдите площадь треугольника.');
+			window.vopr.ver=[0.5 * cat_a * cat_b];
+			break;
 	}
 
 	window.vopr.kat['log'] = 0;
