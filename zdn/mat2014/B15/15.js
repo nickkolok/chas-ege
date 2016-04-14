698fb47d044aeb38f1035a020d85c84328f57f8b
dev-nulls <devilnulls@gmail.com>
2016-04-14 07:21:08 +0300

    eslint zdn/mat2014/B15/15.js | output(?)

--- zdn/mat2014/B15/15.js
+++ zdn/mat2014/B15/15.js
@@ -10,7 +10,7 @@ var fn=fn_zadan({
 		'-'+a.fracstr(36,'\\sqrt{3}\\pi'),
 	],
 	maxy: a/2,
-	prnz: v1==1 ? 1 .pina(6) : [sl(-4,0).pina(4),sl(-2,0).pina(3),sl(-6,-1).pina(6),sl(-5,-1).pina(5),1 .pm() .pina (sl(7,12))].iz(),
+	prnz: v1==1 ? 1 .pina(6) : [sl(-4,0).pina(4),sl(-2,0).pina(3),sl(-6,-1).pina(6),sl(-5,-1).pina(5),1 .pm().pina(sl(7,12))].iz(),
 	prnb: v1==1 ? 0 : sl(1),
 	prkz: v1==2 ? 1 .pina(6) : [1 .pina(2), sl(1,2).pina(3), sl(1,4).pina(5)].iz(),
 	prkb: v1==2 ? 0 : sl(1),
