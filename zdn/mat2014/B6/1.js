0819bef5f12d4edee27e56596a6696a68aada74f
dev-nulls <devilnulls@gmail.com>
2016-04-14 11:35:16 +0300

    eslint zdn/mat2014/B6/1.js

--- zdn/mat2014/B6/1.js
+++ zdn/mat2014/B6/1.js
@@ -4,7 +4,7 @@ function monet(r,n){
 	if(n>r)return 0;
 	if(n==r)return 1;
 	if(n==0)return 1;
-	
+
 	return monet(r-1,n-1)+monet(r-1,n);
 }
 var a=sluchch(2,4);
@@ -13,7 +13,7 @@ var c=sluchiz(window.moneta,1)[0];
 
 var d='В случайном эксперименте симметричную монету бросают '+a+' раза.';
 var f=' Какова вероятность того, что '+c+' выпадет '+window.razy[b]+'?';
-window.vopr.txt=d+f
+window.vopr.txt=d+f;
 window.vopr.ver=[''+(monet(a,b)/Math.pow(2,a))];
 
 window.vopr.kat['log']=0;
