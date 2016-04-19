49e9a608da03fd990419a6e48304744cc2e38c7d
KsenijaV <Kseniya.2017@mail.ru>
2016-04-19 18:01:00 +0300

    zdn/tri/E4/1

--- zdn/tri/E4/1.js
+++ zdn/tri/E4/1.js
@@ -1,17 +1,19 @@
-(function(){'use strict';
-
-window.vopr.txt='$2 \\sin{\\frac{a+b}{2}}\\cdot\\cos{\\frac{a-b}{2}} =$<br/>';
-window.vopr.ver=[
-  	'$ \\sin{a}+\\sin{b} $',
-];
-window.vopr.nev=[
-  	'$ \\sin{2a} $',
-  	'$ \\cos{2a} $',
-  	'$ \\sin{a}-\\sin{b} $',
-  	'$ \\cos{a}-\\cos{b} $',
-	'$ \\cos{a}+\\cos{b} $',
-];
-AtoB();
+(function() {
+	'use strict';
+	chas2.task.setTask({
+		text: '$2 \\sin{\\frac{a+b}{2}}\\cdot\\cos{\\frac{a-b}{2}} =$<br/>',
+		answers: [
+			'$ \\sin{a}+\\sin{b} $',
+		],
+		wrongAnswers: [
+			'$ \\sin{2a} $',
+			'$ \\cos{2a} $',
+			'$ \\sin{a}-\\sin{b} $',
+			'$ \\cos{a}-\\cos{b} $',
+			'$ \\cos{a}+\\cos{b} $',
+		],
+	});
+	AtoB();
 
 })();
 //Любовь Ерышова
