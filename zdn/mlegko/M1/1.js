4176d0533d6c22a326ed7d2048a2b988d4075c84
dev-nulls <devilnulls@gmail.com>
2016-06-02 18:17:20 +0400

    Рефакторинг zdn/rus2014/M1 (#595)

--- zdn/mlegko/M1/1.js
+++ zdn/mlegko/M1/1.js
@@ -1,8 +1,11 @@
-(function(){'use strict';
-var znam=[1,2,3,4,5,6,9,12,15,18,20].iz();
-var chis=sl(znam==1?0:1,znam).pm();
+(function() {
+	'use strict';
+	var znam = [1, 2, 3, 4, 5, 6, 9, 12, 15, 18, 20].iz();
+	var chis = sl(znam == 1 ? 0 : 1, znam).pm();
 
-window.vopr.txt='Переведите $'+chis.pina(znam)+'$ радиан в градусы.';
-window.vopr.ver=[ (180*chis/znam).ts() ];
+	chas2.task.setTask({
+		text: 'Переведите $' + chis.pina(znam) + '$ радиан в градусы.',
+		answers: [(180 * chis / znam).ts()],
+	});
 })();
 //http://matematikalegko.ru/priyomy/radiany-v-gradusy-gradusy-v-radiany.html
