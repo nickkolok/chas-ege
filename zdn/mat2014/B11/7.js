95f76ee4822da502a4965d932f7686f4978cbf2e
dev-nulls <devilnulls@gmail.com>
2016-06-28 05:23:15 +0300

    Рефакторинг zdn-mat2014-B11-7

--- zdn/mat2014/B11/7.js
+++ zdn/mat2014/B11/7.js
@@ -1,18 +1,25 @@
 (function() {
+	'use strict';
 
-var a=sluchch(2,9);
-var b=sluchch(2,4);
-var f=sluchch(1,b-1);
-var m=sluchch(1,9);
-var g=sluchch(1,9);
-var z=sluchch(1,4);
-var c=z*m-g;
-window.vopr.txt=('Найдите значение выражения $$'+a.pow(b)+'^{'+c.frac(b*m)+'}\\cdot'+
-	a.pow(f)+'^{'+g.frac(f*m)+'}$$').plusminus();
-window.vopr.ver=[''+a.pow(z)];
+	var a = sluchch(2, 9),
+		b = sluchch(2, 4),
+		f = sluchch(1, b - 1),
+		m = sluchch(1, 9),
+		g = sluchch(1, 9),
+		z = sluchch(1, 4),
+		c = z * m - g;
 
-window.vopr.kat['log']=0;
-window.vopr.kat['prz']=0;
-window.vopr.kat['drs']=1;
-window.vopr.kat['tri']=0;
+	chas2.task.setTask({
+		text: ('Найдите значение выражения $$'+a.pow(b)+'^{'+c.frac(b*m)+'}\\cdot'+
+				a.pow(f)+'^{'+g.frac(f*m)+'}$$').plusminus(),
+		answers: [''+a.pow(z),
+		],
+		tags: {
+			'log': 0,
+			'prz': 0,
+			'drs': 0,
+			'tri': 0,
+		},
+	});
+
 })();
