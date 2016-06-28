2001620c2c66efd8a811ed57eaefc8a056e91708
dev-nulls <devilnulls@gmail.com>
2016-06-28 06:20:42 +0300

    Рефакторинг zdn/mat2014/B11/11

--- zdn/mat2014/B11/11.js
+++ zdn/mat2014/B11/11.js
@@ -1,29 +1,42 @@
-(function(){'use strict';
+(function(){
+	'use strict';
 
-var a=slKrome([90,180,270],1,359);
-var at='{'+a.ts()+'^\\circ}';
-var b=sluchch(0.1,400,0.1).pm();
-var c=2 .pow(sl(-2,4))*5 .pow(sl(-2,4)) .pm();
-var f=[['\\sin','\\cos'],['\\mathrm{tg~}','\\cos^2'],['\\sin^2','\\mathrm{ctg~}']].iz().shuffle();
+	var a = slKrome([90, 180, 270], 1, 359),
+		at = '{' + a.ts() + '^\\circ}',
+		b = sluchch(0.1, 400, 0.1).pm(),
+		c = 2 .pow(sl(-2, 4)) * 5 .pow(sl(-2, 4)) .pm(),
+		f = [
+				['\\sin','\\cos'],
+				['\\mathrm{tg~}', '\\cos^2'],
+				['\\sin^2', '\\mathrm{ctg~}']
+		].iz().shuffle(),
 
-var vyr1=f[0]+at+'\\cdot'+f[1]+at;
-var vyr2='\\sin'+(2*a).ts()+'^\\circ';
+		vyr1 = f[0] + at+'\\cdot' + f[1] + at,
+		vyr2 = '\\sin' + (2 * a).ts() + '^\\circ',
 
-var y;
-if(sl1()){
-	y='\\frac{'+b.ts()+'~'+vyr1+'}{'+c.ts()+'~'+vyr2+'}';
-	window.vopr.ver=[(b/(2*c)).ts()];
-}else{
-	y='\\frac{'+b.ts()+'~'+vyr2+'}{'+c.ts()+'~'+vyr1+'}';
-	window.vopr.ver=[(b*2/c).ts()];
-}
+		y,
+		answer;
+	if(sl1()){
+		y = '\\frac{' + b.ts() + '~' + vyr1 +
+			'}{' + c.ts() + '~' + vyr2 + '}';
+		answer = [(b / (2 * c)).ts()];
+	}else{
+		y = '\\frac{' + b.ts() + '~' + vyr2 + '}{' +
+			c.ts() + '~' + vyr1 + '}';
+		answer = [(b * 2 / c).ts()];
+	}
 
-window.vopr.txt=('Найдите значение выражения $$'+y+'$$').plusminus().ts();
+	chas2.task.setTask({
+		text: ('Найдите значение выражения $$'+y+'$$').plusminus().ts(),
+		answers: answer,
+		tags: {
+			'log': 0,
+			'prz': 0,
+			'drs': 0,
+			'tri': 1,
+		},
+	});
 
-window.vopr.kat['log']=0;
-window.vopr.kat['prz']=0;
-window.vopr.kat['drs']=0;
-window.vopr.kat['tri']=1;
 })();
 //Обзад 26755
 //Николай Авдеев
