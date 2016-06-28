e8a544e0b841130f87291198d390f775d4194849
dev-nulls <devilnulls@gmail.com>
2016-06-28 04:27:57 +0300

    Рефакторинг zdn/mat2014/B11/3

--- zdn/mat2014/B11/3.js
+++ zdn/mat2014/B11/3.js
@@ -1,43 +1,49 @@
 (function() {
+	'use strict';
 
-var m=sluchch(1,37);
-var n=sluchch(1,m-1);
+	var m = sluchch(1, 37),
+		n = sluchch(1, m - 1),
 
-var a=m*m-n*n;
-var b=2*m*n;
-var c=m*m+n*n;
-var d='-';
-var f;
-var g;
-var h;
-var v=sluchch(1,3);
-switch(v){
-	case 1:{
-		h=a;
-		f=c;
-		g=b;
-		break;
+		a = m * m - n * n,
+		b = 2 * m * n,
+		c = m * m + n * n,
+		d = '-',
+		f,
+		g,
+		h,
+		v = sluchch(1, 3);
+
+	switch(v){
+		case 1:
+			h = a;
+			f = c;
+			g = b;
+			break;
+
+		case 2:
+			h = b;
+			f = c;
+			g = a;
+			break;
+
+		case 3:
+			d = '+';
+			h = c;
+			f = a;
+			g = b;
+			break;
 	}
-	case 2:{
-		h=b;
-		f=c;
-		g=a;
-		break;
-	}
-	case 3:{
-		d='+';
-		h=c;
-		f=a;
-		g=b;
-		break;
-	}
 
-}
-window.vopr.txt='Найдите значение выражения $$\\sqrt{'+f+'^{2}'+d+g+'{^2}}$$';
-window.vopr.ver=[''+h];
+	chas2.task.setTask({
+		text: 'Найдите значение выражения $$\\sqrt{'+f+'^{2}'+d+g+'{^2}}$$',
+		answers: [''+h,
+		],
+		tags: {
+			'log': 0,
+			'prz': 0,
+			'drs': 0,
+			'tri': 0,
+		},
+	});
 
-window.vopr.kat['log']=0;
-window.vopr.kat['prz']=0;
-window.vopr.kat['drs']=0;
-window.vopr.kat['tri']=0;
 })();
