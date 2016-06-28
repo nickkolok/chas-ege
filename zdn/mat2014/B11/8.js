69f5323cb08eb4d706eca87d7e096b0f084377b9
dev-nulls <devilnulls@gmail.com>
2016-06-28 05:35:59 +0300

    Рефакторинг zdn/mat2014/B11/8

--- zdn/mat2014/B11/8.js
+++ zdn/mat2014/B11/8.js
@@ -1,18 +1,28 @@
-(function(){'use strict';
+(function(){
+	'use strict';
 
-var a=sluchch(2,9);
-var c=slKrome(a,2,9);
-var b=slKrome(isZ,0.1,4.9,0.1);
-var f=sluchch(1,4);
-var d=slKrome(f,1,4);
-var g=sluchch(1,[f,d][[f,d].min()]-1);
-(a-1)*(a-2)*(a-4)||(f-=sluchch(1,4));
-(c-1)*(c-2)*(c-4)||(d-=sluchch(1,4));
-window.vopr.txt=('Найдите значение выражения $$\\frac{'+a+'^{'+(b+f).toFixedLess(6).toStandart()+'}\\cdot{'+c+'^{'+(b+d).toFixedLess(6).toStandart()+'}}}{'+(a*c)+'^{'+(b+g).toFixedLess(6).toStandart()+'}}$$').plusminus();
-window.vopr.ver=[''+(a.pow(f-g)*c.pow(d-g))];
+	var a = sluchch(2, 9),
+		c = slKrome(a, 2, 9),
+		b = slKrome(isZ, 0.1, 4.9, 0.1),
+		f = sluchch(1, 4),
+		d = slKrome(f, 1, 4),
+		g = sluchch(1, [f,d][[f,d].min()] - 1);
+	(a - 1) * (a - 2) * (a - 4)||(f -= sluchch(1, 4));
+	(c - 1) * (c - 2) * (c - 4)||(d -= sluchch(1, 4));
 
-window.vopr.kat['log']=0;
-window.vopr.kat['prz']=0;
-window.vopr.kat['drs']=1;
-window.vopr.kat['tri']=0;
+	chas2.task.setTask({
+		text: ('Найдите значение выражения $$\\frac{'+a+'^{'+
+			   (b+f).toFixedLess(6).toStandart()+'}\\cdot{'+c+'^{'+
+			   (b+d).toFixedLess(6).toStandart()+'}}}{'+(a*c)+'^{'+
+			   (b+g).toFixedLess(6).toStandart()+'}}$$').plusminus(),
+		answers: [''+(a.pow(f-g)*c.pow(d-g)),
+		],
+		tags: {
+				'log': 0,
+				'prz': 0,
+				'drs': 0,
+				'tri': 0,
+			},
+	});
+
 })();
