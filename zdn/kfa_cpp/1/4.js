0d301df3d744b7c0903003655a2d1fb9d2d68a1b
Nikolay Avdeev <nickkolok@mail.ru>
2017-04-13 21:46:31 +0300

    Первые наброски по С++

---
+++ zdn/kfa_cpp/1/4.js
@@ -0,0 +1,46 @@
+(function() {
+
+NAinfo.requireApiVersion(0, 2);
+
+var v1 = sl1();
+var right = ['cin >> a >> b;','cout << a << b;'];
+
+chas2.task.setTask({
+	text: 'Как правильно ' +
+		[
+			'ввести с клавиатуры',
+			'вывести на экран',
+		][v1] +
+		' значения двух переменных <b>a</b> и <b>b</b>:<br/>',
+	answers:  [
+		right[v1],
+	],
+	wrongAnswers: [
+		right[1-v1],
+		'cin >> a, b;',
+		'cin >> a && b;',
+		'cin >> a & b;',
+		'cin >> a >> b >> endl;',
+		'cin << a, b;',
+		'cin(a, b);',
+		'cin{a, b};',
+		'cin[a, b];',
+		'cin<a, b>;',
+		'cin << a && b;',
+		'cin << a & b;',
+		'cin << a << b << endl;',
+		'a ' + ['<<','>>'].iz() + ' ' + ['cin','cout'].iz() + ['<<','>>'].iz() + ' b;',
+		'cout >> a, b;',
+		'cout >> a && b;',
+		'cout >> a & b;',
+		'cout >> a >> b >> endl;',
+		'cout << a, b;',
+		'cout << a && b;',
+		'cout << a & b;',
+		'cout << a << b << endl;',
+	],
+});
+AtoB();
+
+})();
+
---
+++ zdn/kfa_cpp/1/5.js
@@ -0,0 +1,59 @@
+(function() {
+
+NAinfo.requireApiVersion(0, 2);
+
+var types = [
+	['int', 'целочисленный', function(){return sl(-200,200);}],
+	['char', 'символьный', function(){return '\'' + String.fromCharCode(sl('a'.charCodeAt(0),'z'.charCodeAt(0))) + '\'';}],
+	['bool', 'логический', function(){return ['true', 'false'].iz();}],
+	['double', 'вещественный', function(){return sl(-200,200,0.01).ts().replace(',','.');}],
+	['float', 'вещественный',  function(){return sl(-200,200,0.01).ts().replace(',','.');}],
+];
+
+var theType = types.iz();
+
+var wrongTypes = [];
+types.map(function(t) {
+	if (t[1] !== theType[1]) {
+		wrongTypes.push(t[1]);
+	}
+});
+
+var name = String.fromCharCode(sl('a'.charCodeAt(0),'z'.charCodeAt(0)));
+
+var taken = {name: true};
+
+var rows = [];
+//	theType[0] + ' ' + name + (' = ' + theType[2]()).esli(sl1()) + ';',
+//];
+
+for (var i = 0; i < sl(3,5); i++){
+	var anotherName1 = i ? String.fromCharCode(sl('a'.charCodeAt(0),'z'.charCodeAt(0))) : name;
+	var anotherName2 = String.fromCharCode(sl('a'.charCodeAt(0),'z'.charCodeAt(0)));
+	if (!taken[anotherName1]) {
+		taken[anotherName1] = true;
+		var t =  i ? types.iz() : theType;
+		rows.push(
+			t[0] + ' ' + anotherName1 + (' = ' + t[2]()).esli(sl1()) + 
+				(', '+anotherName2 + (' = ' + t[2]()).esli(sl1())).esli(sl1() && !taken[anotherName2]) +
+				';'
+		);
+		taken[anotherName2] = true;
+	}
+}
+
+rows.shuffle();
+
+
+chas2.task.setTask({
+	text: 'Какой тип имеет переменная ' + name + '?' + 
+		'<pre>' + rows.join('\n') + '</pre><br/>',
+	answers:  [
+	    theType[1],
+	],
+	wrongAnswers: wrongTypes,
+});
+AtoB();
+
+})();
+
