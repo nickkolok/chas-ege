638cef28a474da7a1547aa3af166524f13768ffc
Aisse-258 <y-kate@ukr.net>
2019-06-10 11:41:26 +0300

    Доработка "Рефакторинг zdn/mat2014/B11/3 #649"

--- zdn/mat2014/B11/3.js
+++ zdn/mat2014/B11/3.js
@@ -1,49 +1,22 @@
 (function() {
 	'use strict';
-
 	var m = sluchch(1, 37),
 		n = sluchch(1, m - 1),
-
 		a = m * m - n * n,
 		b = 2 * m * n,
 		c = m * m + n * n,
 		d = '-',
-		f,
-		g,
-		h,
-		v = sluchch(1, 3);
-
-	switch(v){
-		case 1:
-			h = a;
-			f = c;
-			g = b;
-			break;
-
-		case 2:
-			h = b;
-			f = c;
-			g = a;
-			break;
-
-		case 3:
-			d = '+';
-			h = c;
-			f = a;
-			g = b;
-			break;
-	}
-
+		variety = [a,b,c].shuffle(),
+        h = variety[0],
+        f = Math.max(variety[1],variety[2]),
+        g = Math.min(variety[1],variety[2]);
+        if(h == c){
+            d = '+';
+            f = variety[1];
+            g = variety[2];//если знак "+", числа в случайном порядке, если "-", большее стоит первым
+        }
 	chas2.task.setTask({
 		text: 'Найдите значение выражения $$\\sqrt{'+f+'^{2}'+d+g+'{^2}}$$',
-		answers: [''+h,
-		],
-		tags: {
-			'log': 0,
-			'prz': 0,
-			'drs': 0,
-			'tri': 0,
-		},
+		answers: ''+h,
 	});
-
 })();
