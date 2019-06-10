6b518c5e06462b5ee908de36b1587de4370b501d
Aisse-258 <y-kate@ukr.net>
2019-06-10 14:31:10 +0300

    Правка отступов

--- zdn/mat2014/B11/3.js
+++ zdn/mat2014/B11/3.js
@@ -7,14 +7,14 @@
 		c = m * m + n * n,
 		d = '-',
 		variety = [a,b,c].shuffle(),
-        h = variety[0],
-        f = Math.max(variety[1],variety[2]),
-        g = Math.min(variety[1],variety[2]);
-        if(h == c){
-            d = '+';
-            f = variety[1];
-            g = variety[2];//если знак "+", числа в случайном порядке, если "-", большее стоит первым
-        }
+		h = variety[0],
+		f = Math.max(variety[1],variety[2]),
+		g = Math.min(variety[1],variety[2]);
+		if(h == c){
+			d = '+';
+			f = variety[1];
+			g = variety[2];//если знак "+", числа в случайном порядке, если "-", большее стоит первым
+		}
 	chas2.task.setTask({
 		text: 'Найдите значение выражения $$\\sqrt{'+f+'^{2}'+d+g+'{^2}}$$',
 		answers: ''+h,
