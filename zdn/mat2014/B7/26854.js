efadcd587bd6d011850b6c3fdff3a54f3c80d711
aisse-258 <y-kate@ukr.net>
2020-08-02 14:35:24 +0300

    Правки №26854 (Математика профильного уровня на ОБЗад)

--- zdn/mat2014/B7/26854.js
+++ zdn/mat2014/B7/26854.js
@@ -1,22 +1,27 @@
-(function(){
-    NAinfo.requireApiVersion(0, 0);
-    var a = sluchch(2, 10);
-    var b = 1;
-    var c = 1;
-    var e = 1;
-    var d = 1;
-    var y = 1;
-    do {
-        b = sluchch(2, 20);
-        y = sluchch(2, 10);
-        d = sluchch(y+1, 10);
-        c = Math.pow(b, d);
-        e = Math.pow(b, y);
-    } while (c > 1000 || e > 1000);
+(function() {
+	NAinfo.requireApiVersion(0, 0);
+	var a = sluchch(2, 10); //основание степени
+	var b = 1; //основание логарифма
+	var c = 1; //подлогарифмическое числителя
+	var e = 1; //подлогарифмическое знаменателя
+	var d = 1;
+	do {
+		b = sluchch(2, 20);
+		d = sluchch(2, 10);
+		e = sluchch(2, 1000);
+		c = Math.pow(b, d) * e;
+	}
+	while (c > 1000 || e === b);
 	NAtask.setTask({
-	    text: 'Найдите значение выражения $$\\frac{{'+a+'}^{\\log_{'+b+'}{'+c+'}}}{{'+a+'}^{\\log_{'+b+'}{'+e+'}}} $$',
-	    answers: Math.pow(a, d-y),   
-	},{
-        tags: {log:1},
-    });
+		text: 'Найдите значение выражения $$\\frac{{' + a + '}^{\\log_{' + b + '}{' + c + '}}}{{' + a + '}^{\\log_{' + b +
+			'}{' + e + '}}} $$',
+		answers: Math.pow(a, d),
+	}, {
+		tags: {
+			log: 1,
+		},
+	});
 })();
+//https://math-ege.sdamgia.ru/problem?id=26854
+//Автор: Арахов Никита
+//Reviewed and commited by Aisse-258
