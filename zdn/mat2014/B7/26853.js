a1b05cf4403321ab78ace44e4d1cd2a6467a51a4
aisse-258 <y-kate@ukr.net>
2020-08-02 14:31:20 +0300

    Правки №26853 (Математика профильного уровня на ОБЗад)

--- zdn/mat2014/B7/26853.js
+++ zdn/mat2014/B7/26853.js
@@ -1,22 +1,29 @@
-(function(){
-    NAinfo.requireApiVersion(0, 0); // Не придумал, как исправить возникавшую роблему, теперь программа генерит немного другое задание, с той же сутью 
-    var b = sluchch(2, 10);
-    var y = [1, 3].iz();
-    var d = 1;
-    var a = 1;
-    var c = 1;
-    var e = 1;
-    do {
-        b = sluchch(2, 10);
-        a = sluchch(2, 10); 
-        c = sluchch(2, 10); 
-        e = Math.pow(a, c);
-        d = Math.pow(b, c);
-    } while (e > 100 || d > 100);
+(function() {
+	NAinfo.requireApiVersion(0, 0);
+	var a = 1; //основание первого логарифма
+	var b = 1; //основание второго логарифма
+	var c = 1;
+	var d = 1; //подлогарифмическое первого
+	var e = 1; //подлогарифмическое второго
+	var f = 1;
+	do {
+		a = sluchch(2, 10);
+		b = sluchch(2, 10);
+		c = sluchch(1, 10);
+		f = sluchch(1, 10);
+		e = Math.pow(a, c);
+		d = Math.pow(b, f);
+	}
+	while (e > 150 || d > 150 || a === b);
 	NAtask.setTask({
-	    text: 'Найдите значение выражения $$\\log_{'+a+'}{'+d+'}\\cdot\\log_{'+b+'}{'+e+'} $$',
-	    answers: c*c,   
-	},{
-        tags: {log:1},
-    });
-})();  
+		text: 'Найдите значение выражения $$\\log_{' + a + '}{' + d + '}\\cdot\\log_{' + b + '}{' + e + '} $$',
+		answers: c * f,
+	}, {
+		tags: {
+			log: 1,
+		},
+	});
+})();
+//https://math-ege.sdamgia.ru/problem?id=26853
+//Автор: Арахов Никита
+//Reviewed and commited by Aisse-258
