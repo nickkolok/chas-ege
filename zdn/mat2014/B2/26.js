5d6d24e645eec3c2c3b252effdc71f179605373c
aisse-258 <y-kate@ukr.net>
2020-08-05 20:56:34 +0300

    [fix #686] Условие на целочисленные ответы

--- zdn/mat2014/B2/26.js
+++ zdn/mat2014/B2/26.js
@@ -1,15 +1,20 @@
-(function(){'use strict';
+(function() {
+	'use strict';
 
-var clothes = sklonlxkand(['футболка','майка'].iz());
-var oldprice = sluchch(600,1000,10);
-var newprice = sluchch(400,oldprice-100,10);
-
-window.vopr.txt = clothes.ie.toZagl() + " стоила " + chislitlx(oldprice,"рубль") + ". " +
-		"После снижения цены она стала стоить " + chislitlx(newprice,"рубль") + ". " +
+	var clothes = sklonlxkand(['футболка', 'майка', 'толстовка', 'куртка', 'рубашка'].iz());
+	var oldprice, newprice, answer;
+	do {
+		oldprice = sluchch(600, 1000);
+		newprice = sluchch(400, oldprice - 100);
+		answer = 100 - newprice / oldprice / 100;
+	} while (answer % 1 !== 0)
+	window.vopr.txt = clothes.ie.toZagl() + " стоила " + chislitlx(oldprice, "рубль") + ". " +
+		"После снижения цены она стала стоить " + chislitlx(newprice, "рубль") + ". " +
 		"На сколько процентов была снижена цена на " + clothes.ve + "?";
 
-window.vopr.ver = [(100-(newprice/(oldprice/100))).ceil()];
+	window.vopr.ver = [answer];
 })();
 
 //Обзад 26630
 //Lincor
+//Fixed by Aisse-258
