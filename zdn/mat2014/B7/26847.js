849b3d7bd4f337ed8ebbc874efb8a96ced636334
aisse-258 <y-kate@ukr.net>
2020-08-08 16:34:51 +0300

    [#690] Правки №26847 (Математика профильного уровня на ОБЗад)

--- zdn/mat2014/B7/26847.js
+++ zdn/mat2014/B7/26847.js
@@ -1,15 +1,13 @@
 (function() {
 	NAinfo.requireApiVersion(0, 0);
-	var y = 1;
-	var a = 1;
-	var b = 1;
-	var c = [2, 5].iz();
-	var d = [1, 0.5].iz();
+	var y = 1; // Степень (ответ)
+	var a = 1; // Основание логарифма
+	var b = 1; // Подлогарифмическое
 	do {
-		a = sluchch(1, 25);
-		y = sluchch(2, 25, d);
+		a = sluchch(2, 25);
+		y = sluchch(2, 25, 0.5);
 		b = Math.pow(a, y);
-	} while (b > 100000 || a % c !== 0 || b % 1 !== 0);
+	} while (b > 10000 || b % 1 !== 0);
 	NAtask.setTask({
 		text: 'Найдите значение выражения $$ \\log_{' + a + '}' + b + ' $$',
 		answers: y,
