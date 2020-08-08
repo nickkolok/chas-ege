f6834fe1e52ae44d56e1f39c6a5b89eedfe5c5b4
aisse-258 <y-kate@ukr.net>
2020-08-08 20:53:01 +0300

    Деление на 2, знак ответа

--- zdn/mat2014/B7/282525.js
+++ zdn/mat2014/B7/282525.js
@@ -10,22 +10,25 @@
 		e = sluchch(1, 23, 2);
 		koef = [mn + '\\sqrt{2}', '\\frac{' + mn + '}{\\sqrt{2}}'].iz(); // умножить или поделить на sqrt{2}
 		if (koef == '\\frac{' + mn + '}{\\sqrt{2}}') { // ответ зависит от расположения sqrt{2}
-			answer = mn / 2;
+			answer = mn / 4;
 		}
 		else {
-			answer = mn;
+			answer = mn / 2;
 		}
 		break;
 	case 12: // углы, кратные п/6
 		e = [1, 5, 7, 8, 10, 11, 13, 14, 16, 17, 19, 20, 22, 26, 28].iz();
 		koef = e % 2 === 0 ? ['\\frac{' + mn + '}{\\sqrt{3}}', mn + '\\sqrt{3}'].iz() : mn.ts(); // если е чётно, умножаем или делим на sqrt{3}
 		if (koef == mn + '\\sqrt{3}') { // ответ зависит от расположения sqrt{3}
-			answer = mn / 2 * 3;
+			answer = mn / 4 * 3;
 		}
 		else {
-			answer = mn / 2;
+			answer = mn / 4;
 		}
 		break;
+	}
+	if (Math.sin(e * Math.PI / c * 2) < 0) {
+		answer = -answer;
 	}
 	var sincos = ['\\sin' + (e).texfracpi(c), '\\cos' + (e).texfracpi(c)].shuffle();
 	NAtask.setTask({
