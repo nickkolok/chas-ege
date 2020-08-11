e9f147e319da0cede34460d73f4d357d7b09ea82
NickKolok <nickkolok@mail.ru>
2020-08-12 00:52:36 +0300

    [fix] Endless generation

--- zdn/mat2014/B2/26.js
+++ zdn/mat2014/B2/26.js
@@ -6,7 +6,7 @@
 	do {
 		oldprice = sluchch(600, 1000);
 		newprice = sluchch(400, oldprice - 100);
-		answer = 100 - newprice / oldprice / 100;
+		answer = 100 - newprice / oldprice * 100;
 	} while (answer % 1 !== 0)
 	window.vopr.txt = clothes.ie.toZagl() + " стоила " + chislitlx(oldprice, "рубль") + ". " +
 		"После снижения цены она стала стоить " + chislitlx(newprice, "рубль") + ". " +
