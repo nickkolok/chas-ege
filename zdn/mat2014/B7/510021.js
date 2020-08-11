99f3f6dc3e625aa32530fbb111488c04eb3700f6
aisse-258 <y-kate@ukr.net>
2020-08-11 21:36:41 +0300

    [fix #692] Правки №510021 (Математика базового уровня на ОБЗад)

--- zdn/mat2014/B7/510021.js
+++ zdn/mat2014/B7/510021.js
@@ -3,14 +3,14 @@
 	var a = sluchch(1, 20);
 	var d = a;
 	var e = a;
-	while (d % 1 === 0 && e % 1 === 0) {
-		var c = [2, 5, 10, d].iz();
+	while (d % 1 === 0 && e % 1 === 0 || sl1()) {
+		var c = [2, 5, d].iz();
 		d = d / c;
 		e = e * c;
 	}
 	var f = [d, e].shuffle();
 	NAtask.setTask({
-		text: "Найдите значение выражения $$\\sqrt{" + f[0] + "} \\cdot \\sqrt{" + f[1] + "}$$",
+		text: "Найдите значение выражения $$\\sqrt{" + f[0].ts() + "} \\cdot \\sqrt{" + f[1].ts() + "}$$",
 		answers: a,
 	});
 })();
