4da1305541816a386e8fff368fa2e371db595a7a
NickKolok <nickkolok@mail.ru>
2020-11-25 18:13:38 +0300

    [fix #725] Multyply by the sign

--- zdn/mat2014/B11/12.js
+++ zdn/mat2014/B11/12.js
@@ -26,7 +26,7 @@ var vyr1=f[0];
 var vyr2=c.ts()+'\\cos'+(2*a).ts()+'^\\circ';
 
 var y='\\frac{'+(v1?vyr1:vyr2)+'}{'+(v1?vyr2:vyr1)+'}';
-window.vopr.ver=[(p1/p2).ts()];
+window.vopr.ver=[(p1/p2*f[1]).ts()];
 
 window.vopr.txt=('Найдите значение выражения $$'+y+'$$').plusminus().ts();
 
