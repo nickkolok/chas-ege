8b0a02187dd3d8f018ced8f3c54aebc2e416d29e
NickKolok <nickkolok@mail.ru>
2020-11-29 22:16:24 +0300

    [fix #727] Fix and improve the variety

--- zdn/mat2014/B11/26896.js
+++ zdn/mat2014/B11/26896.js
@@ -1,11 +1,21 @@
 (function(){
 	NAinfo.requireApiVersion(0, 0);
-	var a = sluchch(2, 10);
+	var base = slKrome([1], 0.1, 100, 0.1);
 	var c = sluchch(2, 20);
-	var b = c*c;
+	var argpower = [1, 2, 4].iz()
+	var b = c.pow(argpower);
+	var logbase = '\\log_{'+base.ts()+'}';
+	var sqrtpower = [2,4,5,10,20,25,100].iz();
+	var sqrttex = '\\sqrt' +
+		(sqrtpower == 2 ? '' : '['+sqrtpower+']') +
+		'{'+c+'}';
+
+
 	NAtask.setTask({
-		text: 'Найдите значение выражения $$\\frac{\\log_{'+a+'}{\\sqrt{'+c+'}}}{\\log_{'+a+'}{'+b+'}}$$',
-		answers: 1,
+		text: 'Найдите значение выражения $$' +
+			'\\frac{' + logbase + sqrttex + '}{' + logbase + '{' + b + '}}' +
+			'.$$',
+		answers: (1/sqrtpower/argpower).ts(),
 	},{
 		tags: {
 			log:1,
