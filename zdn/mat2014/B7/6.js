a00e70f011a5b2c45017fc79accb5e2975727dfc
NickKolok <nickkolok@mail.ru>
2020-12-15 02:36:01 +0300

    [zdn] Raise the variance of the template mat2014/B7/6

--- zdn/mat2014/B7/6.js
+++ zdn/mat2014/B7/6.js
@@ -4,11 +4,14 @@ var x=sluchch(-9,9);
 var b=sluchch(1,9);
 var c=sluchch(1,9);
 var x=sluchch(1,9);
-var a=c*c-b*x;
+var power = sl(2,5);
+var a = Math.pow(c,power)-b*x;
+var textpower = ('['+power+']').esli(power!=2);
 
 chas2.task.setEquationTask({
-	parts: [ '\\sqrt{'+[a,b+'x'].slag()+'}', c],
+	parts: [ '\\sqrt'+textpower+'{'+[a,b+'x'].slag()+'}', c],
 	roots: x,
 	enablePartsSubtraction: 1,
 });
 })();
+// В том числе РешуЕГЭ 501205
