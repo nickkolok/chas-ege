df2f613c40ef272ebb5ce4eb5350057d72793279
NickKolok <nickkolok@mail.ru>
2020-12-17 20:49:59 +0300

    [zdn] More variative task

--- zdn/mat2014/B14/1.js
+++ zdn/mat2014/B14/1.js
@@ -11,16 +11,38 @@ var g=sluchch(0,2);
 var h=['первого','второго','третьего'];
 var m=[x,x+c,2*x+c];
 
-window.vopr.txt='Первый раствор содержит '+a.toFixedLess(4)+'% соли, второй содержит '+b.toFixedLess(4)+'% соли.';
-window.vopr.txt+=' Масса второго раствора больше массы первого раствора на '+c.toFixedLess(4)+' г.';
-window.vopr.txt+=' Два раствора сливают и получают третий, содержащий '+d.toFixedLess(4)+'% соли.';
-window.vopr.txt+=' Найдите массу '+h[g]+' раствора.';
-window.vopr.txt+=' Ответ дайте в граммах.';
 
-window.vopr.ver=[''+m[g].toFixedLess(4)];
+if(sl1()){ // Растворы
+	var bulk = 'раствор';
+	var mixin = ['соль','щёлочь','кислота'].iz();
+	var juncture = 'Два раствора сливают и получают третий, содержащий';
+} else { //Сплавы
+	var bulk = 'сплав';
+	var mixin = ['медь','олово','серебро','золото','алюминий'].iz();
+	var juncture = 'Из этих двух сплавов получили третий сплав, содержащий';
+}
+
+bulk = sklonlxkand(bulk);
+mixin = sklonlxkand(mixin);
+
+var massUnits=[
+	[ 'г', 'в граммах'],
+	['кг', 'в килограммах'],
+].iz();
+
+window.vopr.txt=
+	('Имеется два ' + bulk.re + '. ').esli(sl1()) +
+	'Первый ' + bulk.ie + ' содержит '+a.ts()+'% ' + mixin.re + ', второй содержит '+b.ts()+'% ' + mixin.re + '.' +
+	' Масса второго ' + bulk.re + ' больше массы первого ' + bulk.re + ' на '+c.ts()+' ' + massUnits[0] + '.' +
+	' ' + juncture + ' '+d.toFixedLess(4)+'% ' + mixin.re + '.' +
+	' Найдите массу '+h[g]+' ' + bulk.re + '.' +
+	' Ответ дайте ' + massUnits[1] + '.';
+
+window.vopr.ver=[''+m[g].ts()];
 
 window.vopr.kat['log']=0;
 window.vopr.kat['prz']=0;
 window.vopr.kat['drs']=0;
 window.vopr.kat['tri']=0;
 })();
+// В том числе РешуЕГЭ 99575
