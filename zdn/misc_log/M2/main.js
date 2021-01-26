b9b981e79ce362e948ef29c0427aa3c9cb934e50
NickKolok <nickkolok@mail.ru>
2021-01-27 00:31:12 +0300

    [zdn][sym] New category for logs: non-natural powers

---
+++ zdn/misc_log/M2/main.js
@@ -0,0 +1,2 @@
+window.nomer=sl(1,14);
+window.comment='Дробные и отрицательные степени';
--- zdn/misc_log/misc_log.js
+++ zdn/misc_log/misc_log.js
@@ -1,7 +1,7 @@
 if(!window.nabor)
 	window.nabor={};
 window.nabor.importFrom({
-	nZad:1,
+	nZad:2,
 	adres:'../zdn/misc_log/',
 	name:'misc_log',
 	prefix:'M',
