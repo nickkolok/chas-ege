7353f6480053aacffd4e3326f9b1d1be6c9c2c90
NickKolok <nickkolok@mail.ru>
2021-01-27 16:06:53 +0300

    [zdn] Refactoring

--- zdn/mat2014/B15/13.js
+++ zdn/mat2014/B15/13.js
@@ -1,15 +1,15 @@
 (function() {
 
-var a=sl(1,19,0.5);
+var a=sl(1,19);
 var v1=sl(5);//0 - pi/3 не участвует, 1 - начало, 2 - конец, 3 и больше - не участвует.
 
 var fn=fn_zadan({
 	slag:[
-		(2*a+1)+'\\cos x',
-		(2*a+1).texfrac(2,'\\sqrt{3}')+'x',
-		!((2*a+1)%3) ? '-\\frac{'+(2*a+1)/3+'\\sqrt{3}\\pi}{2}' : '-'+(2*a+1)+'\\frac{\\sqrt{3}\\pi}{6}',
+		a + '\\cos x',
+		a.texfrac(2,'\\sqrt{3}') + 'x',
+		'-' + a.texfrac(6,['\\sqrt{3}','\\pi'].shuffle().soed()),
 	],
-	maxy: a+0.5,
+	maxy: a/2,
 	prnz: v1==1 ? 1 .pina(3) : [sl(-5,1).pina(4),sl(-2,0).pina(3),sl(-7,1).pina(6),sl(-5,1).pina(5),1 .pm().pina(sl(4,12))].iz(),
 	prnb: v1==1 ? 0 : sl(1),
 	prkz: v1==2 ? 1 .pina(3) : 1 .pina(2),
