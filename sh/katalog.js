266b97024d7bf4c5fb78ef07412535befc927f62
NickKolok <nickkolok@mail.ru>
2021-01-31 22:17:07 +0300

    [sh] Refactoring: separate HTML generation for a single task
    This is needed for ability to regenerate one single task
    (that is obviously faster than using the debug shell).

--- sh/katalog.js
+++ sh/katalog.js
@@ -1,5 +1,50 @@
 'use strict';
 
+
+function generateHtmlForTask(kat,zdn,masdey){
+	var rez='';
+	vopr.podg();
+	var currentTask = nabor.adres+kat+'/'+zdn+'.js';
+	rez+='<div class="task-wrapper">';
+	rez+=currentTask.vTag('h2');
+	console.log(currentTask);
+	try{
+		nabor.upak[kat][zdn]();
+		vopr.template = currentTask.replace(/^(\.\.\/)+/,'');
+		vopr.taskNumber = kat;
+		rez+=('<br/>'+vopr.txt.vTag('div')+'<br/>');
+		rez+=(
+			(
+				'<button class="copybutton" style="display:block; float:right;" title="Экспорт в РешуЕГЭ"'+
+				'data-task="' + encodeURIComponent(JSON.stringify(vopr)) + '"' +
+				'>' +
+					'&#x2398;' +
+				'</button>'
+
+				+
+				'Ответ: '+vopr.ver.join('или')
+			).vTag('div') +
+			'<br/>'
+		);
+		masdey.push(vopr.dey);
+		if(vopr.rsh){
+			rez+=(
+				('Показать решение ').vTag('button','class="spoiler-show"')+
+				('Скрыть   решение ').vTag('button','class="spoiler-hide"')+
+				'<div class="spoiler-body">'+
+				'Решение: '+'<br/>'+
+				vopr.rsh+
+				'</div>'+
+			'');
+
+		}
+	}catch(e){
+		console.log(e);
+	}
+	rez += '</div>';
+	return rez;
+}
+
 function generateKatalog(){
 	var rez='';
 	var toc='';
@@ -9,7 +54,9 @@ function generateKatalog(){
 		window.comment='';
 		try{
 				nabor.upak[kat].main()
-		}catch(e){}
+		}catch(e){
+			console.log(e);
+		}
 		rez+=(
 			('Показать категорию '+kat).vTag('button','class="spoiler-show"')+
 			('Скрыть   категорию '+kat).vTag('button','class="spoiler-hide"')+
@@ -23,45 +70,9 @@ function generateKatalog(){
 		'');
 		for(var zdn in nabor.upak[kat])
 			if(zdn!='main'){
-				vopr.podg();
-				var currentTask = nabor.adres+kat+'/'+zdn+'.js';
-				rez+='<div class="task-wrapper">';
-				rez+=currentTask.vTag('h2');
-				console.log(currentTask);
-				try{
-					nabor.upak[kat][zdn]();
-					vopr.template = currentTask.replace(/^(\.\.\/)+/,'');
-					vopr.taskNumber = kat;
-					rez+=(br+vopr.txt.vTag('div')+br);
-					rez+=(
-						(
-							'<button class="copybutton" style="display:block; float:right;" title="Экспорт в РешуЕГЭ"'+
-							'data-task="' + encodeURIComponent(JSON.stringify(vopr)) + '"' +
-							'>' +
-								'&#x2398;' +
-							'</button>'
-
-							+
-							'Ответ: '+vopr.ver.join('или')
-						).vTag('div') +
-						'<br/>'
-					);
-					masdey.push(vopr.dey);
-					if(vopr.rsh){
-						rez+=(
-							('Показать решение ').vTag('button','class="spoiler-show"')+
-							('Скрыть   решение ').vTag('button','class="spoiler-hide"')+
-							'<div class="spoiler-body">'+
-							'Решение: '+br+
-							vopr.rsh+
-							'</div>'+
-						'');
-
-					}
-				}catch(e){}
-				rez += '</div>';
+				rez += generateHtmlForTask(kat,zdn,masdey);
 			}
-			rez+=('</div>');
+			rez += '</div>';
 	}
 	$('#divrez').html(toc+br+rez);
 	MathJax.Hub.Typeset();
