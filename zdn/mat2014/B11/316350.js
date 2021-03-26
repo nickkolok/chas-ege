592fddc581d93a24daf46a39d70e3b538fe065d3
NickKolok <nickkolok@mail.ru>
2021-03-26 23:06:34 +0300

    [zdn] A new task by Arakhov

---
+++ zdn/mat2014/B11/316350.js
@@ -0,0 +1,13 @@
+(function() {
+	NAinfo.requireApiVersion(0, 0);
+	var a;
+	do {
+		a = sluchch(-50, 50);
+	} while (a === 0);
+	var b = sluchch(0.1, 0.9, 0.1);
+	var ans = a*(2*Math.pow(b, 2) - 1);
+	NAtask.setTask({
+		text: 'Вычислить значение выражения: $$ {'+a+'}\\cos2\\alpha, если \\cos\\alpha = {'+b.ts()+'}$$',
+		answers: ans.ts(),
+	});
+})();
