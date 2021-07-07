c9c5903d986774c1f3da04163d394a9d58337447
aisse-258 <y-kate@ukr.net>
2021-07-07 17:33:23 +0300

    Правки к задаче 319157 с РешуЕГЭ

--- zdn/mat2014/B8/319157.js
+++ zdn/mat2014/B8/319157.js
@@ -1,12 +1,19 @@
-(function() {'use strict';
+(function() {
+	'use strict';
 	NAinfo.requireApiVersion(0, 0);
 	let side = 'M';
-	let middle = ['AB', 'CD'].iz();
-	//TODO:рассмотреть случай с серединой на стороне BC
+	let lowSide = 'AD';
+	let middle = ['AB', 'CD', 'BC', 'AD'].iz(); 
 	let answer = sluchch(5, 69);
 	let S = 4 * answer;
+	if (middle == 'BC' || middle == 'AD') {
+		S = 2 * answer;
+		if (middle == 'AD')
+			lowSide = 'BC';
+	}
 	NAtask.setTask({
-		text: 'Площадь параллелограмма ABCD равна ' + S + '. Точка ' + side + ' - середина стороны ' + middle + '. Найдите площадь треугольника AD' + side + '',
+		text: 'Площадь параллелограмма ABCD равна ' + S + '. Точка ' + side + ' - середина стороны ' + middle +
+			'. Найдите площадь треугольника ' + lowSide + side + '.',
 		answers: answer,
 	});
 	NAtask.modifiers.variativeABC();//расставляем случайные буквы
