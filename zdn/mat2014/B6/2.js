e1f81237714f78c66b45dcc461737d7e1325e790
aezv <aezv.global@gmail.com>
2021-07-18 13:36:34 +0300

    Задание mat2014/B6/2 : Добавление модификатора округления ответа.

--- zdn/mat2014/B6/2.js
+++ zdn/mat2014/B6/2.js
@@ -5,8 +5,10 @@ var a=sluchch(1,20);
 var b=sluchch(2,3);
 
 window.vopr.txt='В случайном эксперименте бросают '+b+' игральные кости. '+
-				'Найдите вероятность того, что в сумме выпадет '+a+' '+chislit(a,'очко','очка','очков')+'. Результат округлите до сотых.';
-window.vopr.ver=[''+(om.igrkosti[b][a]/(6).pow(b)).toFixedLess(2).toStandart()];
+				'Найдите вероятность того, что в сумме выпадет '+a+' '+chislit(a,'очко','очка','очков')+'.';
+window.vopr.ver=[''+(om.igrkosti[b][a]/(6).pow(b))];
+
+NAtask.modifiers.roundUpTo(-2); //модификатор округления ответа
 
 window.vopr.kat['log']=0;
 window.vopr.kat['prz']=0;
