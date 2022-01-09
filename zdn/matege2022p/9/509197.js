f009a6503afa601c0fd26040931c3eb5c61677b1
SugarHedgehog <sneginca.cat@gmail.com>
2022-01-09 17:22:57 +0300

    [zdn] 509197 - дробные коэффициенты

--- zdn/matege2022p/9/509197.js
+++ zdn/matege2022p/9/509197.js
@@ -6,19 +6,21 @@ retryWhileUndefined(function () {
     }
 
     function f1(x) {
-        return kxb(k1, b1, x);
+        return kxb(k1.ch / k1.zn, b1, x);
     }
 
     function f2(x) {
-        return kxb(k2, b2, x);
+        return kxb(k2.ch / k2.zn, b2, x);
     }
-    let k1 = sluchch(0.1, 20, 0.1);
-    let k2 = slKrome(k1, k1 + 0.4, 10, 0.1);
-    k1 = k1.pm();
-    k2 = k2.pm();
+    let k1 = { ch: sluchch(1, 10), zn: sluchch(1, 10), };
+    let k2 = { ch: slKrome(k1.ch, 1, 10), zn: slKrome(k1.zn, 1, 10), };
+    k1.ch.pm();
+    k2.ch.pm();
+    if (k1.ch * k2.zn == k2.ch * k1.zn)
+        return;
     let b1 = sluchch(0, 10).pm();
     let b2 = sluchch(0, 10).pm();
-    let px = (b2 - b1) / (k1 - k2);
+    let px = k1.zn * k2.zn * (b2 - b1) / (k1.ch * k2.zn - k2.ch * k1.zn);
     if (!(px * 1000).isZ() || (px.abs() < 6 && px.isZ()))
         return;
     if (!(f1(px) * 1000).isZ() || (f1(px).abs() < 6 && f1(px).isZ()))
@@ -76,8 +78,8 @@ retryWhileUndefined(function () {
     NAtask.setTask({
         text: 'На рисунке изображёны графики двух линейных функций. Найдите ' + find + ' точки пересечения графиков.',
         answers: answ,
-        analys: '$f_1(x)=' + (k1.ts() + 'x+' + b1).replace('+0', '').plusminus() + '$<br>' +
-            '$f_2(x)=' + (k2.ts() + 'x+' + b2).replace('+0', '').plusminus() + '$',
+        analys: '$f_1(x)=' + (k1.ch.texfrac(k1.zn) + 'x+' + b1).replace('+0', '').plusminus() + '$<br>' +
+            '$f_2(x)=' + (k2.ch.texfrac(k2.zn) + 'x+' + b2).replace('+0', '').plusminus() + '$',
     });
     chas2.task.modifiers.addCanvasIllustration({
         width: 300,
