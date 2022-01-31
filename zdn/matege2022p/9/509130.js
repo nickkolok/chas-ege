27e198e3b099eab960269138a629bc62042c57fc
SugarHedgehog <sneginca.cat@gmail.com>
2022-01-31 20:49:40 +0500

    [zdn] [fix] 509130 - уборка

--- zdn/matege2022p/9/509130.js
+++ zdn/matege2022p/9/509130.js
@@ -1,36 +1,3 @@
-CanvasRenderingContext2D.prototype.drawСoordPlane = function (w, h, kl, text, mash) {
-	kl.hor *= mash;
-	kl.ver *= mash;
-	let width = (w / 2) % (kl.hor);
-	let height = (h / 2) % (kl.ver);
-	this.lineWidth = 0.5;
-	this.setkaVer2(w, h, kl.hor, kl.ver, 1);
-	this.translate(w / 2 - width, h / 2 - height);
-	//стрелки
-	this.lineWidth = 1.5;
-	this.drawArrow(0, h / 2 + height - 10, 0, -h / 2 + height + 10);
-	this.drawArrow(-w / 2 + width + 10, 0, w / 2 + width - 10, 0);
-	this.font = "12px liberation_sans";
-	this.fillText('x', w / 2 + width - 20, 15);
-	this.fillText('y', -15, -h / 2 + height + 20);
-	//единичные отрезки
-	this.font = "12px liberation_sans";
-	this.fillText('0', -10, 12);
-	this.drawLine(kl.hor, 5, kl.hor, -5);
-	this.drawLine(-5, -kl.ver, 5, -kl.ver);
-	this.font = '' + (text.sh1 || 12) + 'px liberation_sans';
-	this.fillText(text.x1, kl.hor, 15);
-	this.font = '' + (text.sh2 || 12) + 'px liberation_sans';
-	this.fillText(text.y1, -15, -kl.ver);
-};
-CanvasRenderingContext2D.prototype.setkaVer2 = function (h, w, hor, ver, mash) {
-	hor *= mash;
-	ver *= mash;
-	for (let i = 0; i < w; i += hor)
-		this.drawLine(i, 0, i, w);
-	for (let i = 0; i < h; i += ver)
-		this.drawLine(0, i, h, i);
-};
 retryWhileUndefined(function () {
 	NAinfo.requireApiVersion(0, 2);
 
