096f66af824734c39a442ef6efee95063b55060c
SugarHedgehog <sneginca.cat@gmail.com>
2022-02-07 12:32:04 +0500

    [zdn] [fix] разделение sin и tg

--- zdn/matege2022p/9/509130.js
+++ zdn/matege2022p/9/509130.js
@@ -2,8 +2,11 @@ retryWhileUndefined(function() {
 	NAinfo.requireApiVersion(0, 2);
 
 	function intPoints(f, o) {
-		o.minX = o.minX.ceil();
-		o.maxX = o.maxX.floor();
+		if (o.step === undefined) {
+			o.minX = o.minX.ceil();
+			o.maxX = o.maxX.floor();
+			o.step = 1;
+		}
 		let XY = [];
 		for (let i = o.minX; i < o.maxX; i += o.step) {
 			if (f(i).isZ() && f(i) <= o.maxY && f(i) >= o.minY) {
@@ -14,26 +17,13 @@ retryWhileUndefined(function() {
 	}
 
 	function variant(a, b, x) {
-		switch (trigfuncs) {
-		case 'sin':
-			return a * Math.sin(x * Math.PI / 6) + b;
-		case 'cos':
-			return a * Math.cos(x * Math.PI / 6) + b;
-		case 'tg':
-			return a * Math.tan(x * Math.PI / 6) + b;
-		case 'ctg':
-			return a * (1 / Math.tan(x * Math.PI / 6)) + b;
-		}
+		return a * (x)[trigfuncs]() + b;
 	}
 
 	function f(x) {
-		return 2 * variant(a, b, x);
-	}
-
-	function fp(x) {
 		return variant(a, b, x);
 	}
-	let trigfuncs = ['sin', 'cos', 'tg', 'ctg'].iz();
+	let trigfuncs = ['sin', 'cos'].iz();
 	let a = sluchch(1, 6).pm();
 	let b = sluchch(0, a).pm();
 	let find, answ;
@@ -44,49 +34,39 @@ retryWhileUndefined(function() {
 		find = 'b';
 		answ = b;
 	}
-	let points;
-	if (trigfuncs == 'sin' || trigfuncs == 'cos')
-		points = intPoints(f, {
-			minX: -6,
-			maxX: 5.5,
-			minY: -7.5,
-			maxY: 4.5,
-			step: 1,
-		});
-	else
-		points = intPoints(f, {
-			minX: -6,
-			maxX: 5.5,
-			minY: -7.5,
-			maxY: 4.5,
-			step: 0.5,
-		});
+	let points = intPoints(f, {
+		minX: -1.5 * Math.PI,
+		maxX: 2 * Math.PI,
+		minY: -5,
+		maxY: 5,
+		step: Math.PI / 2,
+	});
 	if (points.length < 2)
 		return;
 	let paint1 = function(ct) {
 		let h = 300;
 		let w = 300;
-		//Оси координат 
+		//Оси координат
 		ct.drawCoordPlane(w, h, {
-			hor: 3,
-			ver: 4
+			hor: Math.PI / 2,
+			ver: 1
 		}, {
-			x1: 'π/4',
+			x1: 'π/2',
 			y1: '1',
 			sh1: 13,
-		}, 10);
+		}, 20);
 		//График
 		ct.scale(20, -20);
 		ct.lineWidth = 0.1;
 		graph9AdrawFunction(ct, f, {
-			minX: -6.5,
-			maxX: 5,
-			minY: -8,
-			maxY: 5,
+			minX: -1.8 * Math.PI,
+			maxX: 2.5 * Math.PI,
+			minY: -7,
+			maxY: 6,
 			step: 0.05,
 		});
 		//точки
-		graph9AmarkCircles(ct, points, 2, 0.18);
+		graph9AmarkCircles(ct, points, 2, 0.2);
 	};
 	NAtask.setTask({
 		text: 'На рисунке изображён график функции $f(x)=a\\' + trigfuncs + ' x+b$. Найдите $' + find + '$.',
