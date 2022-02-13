235149bfdab10ffff792b1b7abf94345fb3d8833
SugarHedgehog <sneginca.cat@gmail.com>
2022-02-13 13:21:49 +0300

    [new] [zdn] 509271 - корень и прямая

---
+++ zdn/matege2022p/9/509271.js
@@ -0,0 +1,116 @@
+retryWhileUndefined(function() {
+	NAinfo.requireApiVersion(0, 2);
+
+	function pryam(k, b, x) {
+		return k * x + b;
+	}
+
+	function fp(x) {
+		return pryam(k, b, x);
+	}
+
+	function sq(a, c, x) {
+		return a * Math.sqrt(x) + c;
+	}
+
+	function fs(x) {
+		return sq(a, c, x);
+	}
+
+	let x1 = Math.pow(sluchch(1, 2), 2);
+	let y1 = sluchch(-7, 6);
+	let x2 = Math.pow(sluchch(3, 20), 2);
+	let y2 = sluchch(8, 20).pm();
+
+	let k = (y1 - y2) / (x1 - x2);
+	if (!k.isZ() || !k)
+		return;
+	let b = y1 - k * x1;
+
+	let a = (y1 - y2) / (Math.sqrt(x1) - Math.sqrt(x2));
+	let c = y1 - a * Math.sqrt(x1);
+
+	let pointss = intPoints(fs, {
+		minX: -5,
+		maxX: 5,
+		minY: -5.5,
+		maxY: 5.5,
+		step: 1,
+	});
+	if (pointss.length < 2)
+		return;
+	let pointsp = intPoints(fp, {
+		minX: -5,
+		maxX: 5,
+		minY: -5.5,
+		maxY: 5.5,
+		step: 1,
+	});
+	if (pointsp.length < 2)
+		return;
+	let find, answ;
+	if (sl1()) {
+		answ = x2;
+		find = 'абсциссу';
+	} else {
+		answ = y2;
+		find = 'ординату';
+	}
+	let paint1 = function(ct) {
+		let h = 300;
+		let w = 300;
+		//Оси координат
+		ct.drawCoordPlane(w, h, {
+			hor: 1,
+			ver: 1
+		}, {
+			x1: '1',
+			y1: '1',
+			sh1: 13,
+		}, 20);
+		//График
+		ct.scale(20, -20);
+		ct.lineWidth = 0.1;
+		graph9AdrawFunction(ct, fs, {
+			minX: -6.5,
+			maxX: 7,
+			minY: -7,
+			maxY: 6,
+			step: 0.01,
+		});
+		graph9AdrawFunction(ct, fp, {
+			minX: -6.5,
+			maxX: 7,
+			minY: -7,
+			maxY: 6,
+			step: 0.05,
+		});
+		//точки
+		graph9AmarkCircles(ct, pointss, 2, 0.15);
+		graph9AmarkCircles(ct, pointsp, 2, 0.15);
+		graph9AmarkCircles(ct, [
+			[x1, y1]
+		], 2, 0.15);
+		//буква
+		ct.fillStyle = "blue";
+		ct.font = "18px liberation_sans";
+		ct.scale(1 / 20, -1 / 20);
+		ct.fillText('A', 20 * x1 - 10, -20 * y1 - 10);
+	};
+	NAtask.setTask({
+		text: 'На рисунке изображены графики функций $f(x)=a\\sqrt{x} $ и $g(x)=kx+b$,' +
+			' которые пересекаются в точках $A$ и $B$. Найдите ' + find + ' точки $B$.',
+		answers: answ,
+		analys: '$f(x)=' + (a + '\\sqrt{x}+' + c + '$').plusminus() + '<br>' +
+			'$g(x)=' + (k + 'x+' + b).plusminus() + '$<br>' +
+			'$A(' + x1 + ';' + y1 + ')$<br>' +
+			'$B(' + x2 + ';' + y2 + ')$',
+	});
+	chas2.task.modifiers.addCanvasIllustration({
+		width: 300,
+		height: 300,
+		paint: paint1,
+	});
+	return true;
+}, 100000);
+//509271
