d3642b69bf45a66b3e84905c945903a02289d83f
warekeruz <maymus_013@mail.ru>
2022-02-20 21:28:06 +0300

    подредактирована задача №2, обзад №320210

--- zdn/mat2014/B6/320210.js
+++ zdn/mat2014/B6/320210.js
@@ -5,41 +5,43 @@
 //Найдите вероятность того, что обе батарейки окажутся исправными.
 
 (function () {
-	'use strict';
-	NAinfo.requireApiVersion(0, 0);
-	var predmet = sklonlxkand(['батарейка', 'сережка', 'насадка', 'шайба', 'лампочка'].iz());
-	var mesto;
-	if ((predmet.ie == 'батарейка') || (predmet == 'лампочка'))
-		mesto = ['магазине', 'супермаркете', 'торговом центре', 'киоске'].iz();
-	else
-		mesto = ['магазине', 'торговом центре'].iz();
-	var kolvo = ['две', 'три'].iz();
-	var mest;
-	if (kolvo == 'две')
-		mest = 'обе';
-	else
-		mest = 'все';
-	var nayti = ['исправными', 'неисправными'].iz();
-	var verbrak = (sluchch(1, 30)) / 100;
-	var verispr;
-	var answers;
-	if (nayti == 'исправными') {
-		verispr = 1 - verbrak;
-		if (kolvo == 'две')
-			answers = verispr * verispr;
-		else
-			answers = verispr * verispr * verispr;
-	} else {
-		if (kolvo == 'две')
-			answers = verbrak * verbrak;
-		else
-			answers = verbrak * verbrak * verbrak;
-	}
+    'use strict';
+    NAinfo.requireApiVersion(0, 0);
+    var predmet = sklonlxkand(['батарейка', 'сережка', 'насадка', 'шайба', 'лампочка'].iz());
+    var mesto;
+    if ((predmet.ie == 'батарейка') || (predmet == 'лампочка'))
+        mesto = ['магазине', 'супермаркете', 'торговом центре', 'киоске'].iz();
+    else
+        mesto = ['магазине', 'торговом центре'].iz();
+    var k = [2,3].iz();
+    var kolvo = [,,'две', 'три'][k];
+    var nayti = ['исправными', 'неисправными'].iz();
+    var verbrak = (sluchch(1, 30)) / 100;
+    var verispr;
+    var mest;
+    var answers;
 
-	NAtask.setTask({
-		text: 'Вероятность того, что ' + predmet.ie + ' бракованная, равна ' + verbrak + '. Покупатель в ' +
-			'' + mesto + ' выбирает случайную упаковку, в которой ' + kolvo + ' таких ' + predmet.im + '. ' +
-			'Найдите вероятность того, что ' + mest + ' ' + predmet.im + ' окажутся ' + nayti + '.',
-		answers,
-	});
+    if (kolvo == 'две')
+        mest = 'обе';
+    else
+        mest = 'все';
+    if (nayti == 'исправными') {
+        verispr = 1 - verbrak;
+        if (kolvo == 'две')
+            answers = verispr * verispr;
+        else
+            answers = verispr * verispr * verispr;
+    } else {
+        if (kolvo == 'две')
+            answers = verbrak * verbrak;
+        else
+            answers = verbrak * verbrak * verbrak;
+    }
+
+    NAtask.setTask({
+        text: 'Вероятность того, что ' + predmet.ie + ' бракованная, равна ' + verbrak + '. Покупатель в ' +
+            '' + mesto + ' выбирает случайную упаковку, в которой ' + kolvo + ' таких ' + predmet.im + '. ' +
+            'Найдите вероятность того, что ' + mest + ' ' + predmet.im + ' окажутся ' + nayti + '.',
+        answers,
+    });
 })();
