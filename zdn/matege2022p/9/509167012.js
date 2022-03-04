5a22a67e53fca01a88f9d09d97a6771e5345ae83
SugarHedgehog <sneginca.cat@gmail.com>
2022-03-04 17:42:59 +0300

    [new] [zdn] 50916702 - две гиперболы

---
+++ zdn/matege2022p/9/509167012.js
@@ -0,0 +1,118 @@
+retryWhileUndefined(function() {
+	NAinfo.requireApiVersion(0, 2);
+
+	function f2(x) {
+		return (a2 * x + d2) / (x + c2);
+	}
+
+	function f1(x) {
+		return (a1 * x + d1) / (x + c1);
+	}
+
+	let x1 = sluchch(1, 6).pm();
+	let x2 = sluchch(8, 20);
+	let y1 = sl(-6, 6);
+	let y2 = sl(1, 20).pm();
+	let c1 = sl(-5, 5);
+	let a1 = (y1 * x1 - y2 * x2 + c1 * (y1 - y2)) / (x1 - x2);
+	if (a1 == 1 || !a1)
+		return;
+	let d1 = y1 * (x1 + c1) - a1 * x1;
+	let k = (y1 - y2) / (x1 - x2);
+	let c2 = sl(-5, 5);
+	let a2 = (y1 * x1 - y2 * x2 + c2 * (y1 - y2)) / (x1 - x2);
+	if (a2 == 1 || !a2 || a1 == a2)
+		return;
+	let d2 = y1 * (x1 + c2) - a2 * x1;
+	if (d1 / a1 == c1)
+		return;
+	if (d2 / a2 == c2)
+		return;
+	if (!(a1 * 100).isZ() || !(d1 * 100).isZ())
+		return;
+	if (!(a2 * 100).isZ() || !(d2 * 100).isZ())
+		return;
+	let points1 = intPoints(f1, {
+		minX: -5,
+		maxX: 5,
+		minY: -5.5,
+		maxY: 5.5,
+		step: 1,
+	});
+	if (points1.length < 3)
+		return;
+	let points2 = intPoints(f2, {
+		minX: -5,
+		maxX: 5,
+		minY: -5.5,
+		maxY: 5.5,
+		step: 1,
+	});
+	if (points2.length < 3)
+		return;
+	let find, answ;
+	if (sl1()) {
+		answ = x2;
+		find1 = 'абсциссу';
+	} else {
+		answ = y2;
+		find1 = 'ординату';
+	}
+	let paint1 = function(ct) {
+		let h = 300;
+		let w = 300;
+		//Оси координат
+		ct.drawCoordPlane(w, h, {
+			hor: 1,
+			ver: 1
+		}, {
+			x1: '1',
+			y1: '1',
+			sh1: 13,
+		}, 20);
+		//График
+		ct.scale(20, -20);
+		ct.lineWidth = 0.1;
+		graph9AdrawFunction(ct, f1, {
+			minX: -6.5,
+			maxX: 7,
+			minY: -7,
+			maxY: 6,
+			step: 0.05,
+		});
+		graph9AdrawFunction(ct, f2, {
+			minX: -6.5,
+			maxX: 7,
+			minY: -7,
+			maxY: 6,
+			step: 0.05,
+		});
+		//точки
+		graph9AmarkCircles(ct, points1, 3, 0.15);
+		graph9AmarkCircles(ct, points2, 3, 0.15);
+		graph9AmarkCircles(ct, [
+			[x1, y1]
+		], 2, 0.15);
+		//буква
+		ct.fillStyle = "blue";
+		ct.font = "18px liberation_sans";
+		ct.scale(1 / 20, -1 / 20);
+		ct.fillText('A', 20 * x1 - 10, -20 * y1 - 10);
+	};
+	NAtask.setTask({
+		text: 'На рисунке изображены графики функций $f(x)=\\frac{a_1 x+d_1}{x+c_1}$ и $g(x)=\\frac{a_2 x+d_2}{x+c_2 }$,' +
+			' которые пересекаются в точках $A$ и $B$. Найдите ' + find1 + ' точки $B$.',
+		answers: answ,
+		analys: '$f(x)=\\frac{' + (a1 + 'x+' + d1).plusminus() + '}{' + ('x+' + c1).plusminus() + '}' + '$<br>' +
+			'$g(x)=\\frac{' + (a2 + 'x+' + d2).plusminus() + '}{' + ('x+' + c2).plusminus() + '}' + '$<br>' +
+			'$A(' + x1 + ';' + y1 + ')$<br>' +
+			'$B(' + x2 + ';' + y2 + ')$',
+	});
+	chas2.task.modifiers.addCanvasIllustration({
+		width: 300,
+		height: 300,
+		paint: paint1,
+	});
+	return true;
+}, 100000);
+//509167012
