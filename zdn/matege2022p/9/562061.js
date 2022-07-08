55e5d943a8600a1615c1cb81f30244e831efefd3
SugarHedgehog <sneginca.cat@gmail.com>
2022-07-08 11:50:50 +0300

    [zdn] [fix] - функциИ и упрощение вопроса

--- zdn/matege2022p/9/562061.js
+++ zdn/matege2022p/9/562061.js
@@ -34,11 +34,13 @@ retryWhileUndefined(function() {
 	case 3:
 		let num3 = sl(1, 10).pm();
 		let num4 = slKrome(num3.abs, 1, 10).pm();
-		question = 'значение $f(' + num3 + ') ' + ['+', '-'].iz() + ' f(' + num4 + ')$';
-		if (question.indexOf('+'))
-			answ = f(num3) + f(num4);
-		else
-			answ = f(num3) - f(num4);
+		let signs = [
+			['+', f(num3) + f(num4)],
+			['-', f(num3) - f(num4)],
+			['\\cdot', f(num3) * f(num4)],
+		].iz();
+		question = 'значение $f(' + num3 + ') ' + signs[0] + ' f(' + num4 + ')$';
+		answ = signs[1];
 		if ((answ * 1000).isZ())
 			break;
 	case 4:
@@ -76,7 +78,7 @@ retryWhileUndefined(function() {
 		graph9AmarkCircles(ct, points, 3, 0.15);
 	};
 	NAtask.setTask({
-		text: 'На рисунке изображен график функций $f(x)=\\frac{x^2}{a}+bx+c$, где $a, b$ и $c$ - целые. Найдите ' +
+		text: 'На рисунке изображен график функции $f(x)=\\frac{x^2}{a}+bx+c$, где $a, b$ и $c$ - целые. Найдите ' +
 			question + '.',
 		answers: answ,
 		analys: ('$f(x)=\\frac{' + 'x^2}{' + a + '}+' + b + 'x+' + c).plusminus() + '$'
