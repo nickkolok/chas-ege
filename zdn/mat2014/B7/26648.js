4465db0f462b23e9ce5de1294390da64bb2ff286
SugarHedgehog <sneginca.cat@gmail.com>
2022-07-12 11:18:10 +0300

    [new] [zdn] - 26648

---
+++ zdn/mat2014/B7/26648.js
@@ -0,0 +1,26 @@
+(function() {
+	'use strict';
+
+	var c = sluchch(1, 9).pm();
+	var d = sluchch(2, 99);
+	var o = sluchch(2, 99);
+	var x = sluchch(1, 9).pm();
+	var b = d - c * x;
+
+	chas2.task.setEquationTask({
+		parts: ['(' + b + '+' + c + 'x)', d],
+		wrapper: ['\\log_{' + o + '}', ''],
+		roots: x,
+		enablePartsSubtraction: 1,
+	}, {
+		tags: {
+			log: 1,
+		},
+
+		analys: 'Решение:' +
+			'$$\\log_{' + o + '}{(' + b + '+' + c + 'x)}=' + '\\log_{' + o + '}{' + d + '}\\ \\Rightarrow \\ ' + b + '+' + c +
+			'x=' + d + ' \\ \\Rightarrow \\ ' + c + 'x=' + (d - b) + ' \\ \\Rightarrow \\ x=' + (d - b) / c + '$$' +
+			'Ответ: $x=' + x +'.'+ '$',
+	});
+})();
+//26648 
