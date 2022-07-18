7bbcc3481b5fe22535709aeee57158605e00f196
NickKolok <nickkolok@mail.ru>
2022-07-18 21:17:00 +0300

    [zdn][fix] 77366 - more variabilty

--- zdn/mat2014/B7/77366.js
+++ zdn/mat2014/B7/77366.js
@@ -2,9 +2,10 @@
 
 var a=sluchch(1,9).pm();
 var b=slKrome(2*a.abs(),1,9).pm();
+var k=[1,sluchch(2,9)].iz();
 
 chas2.task.setEquationTask({
-	parts: [1, '\\frac{'+a*a+'}{'+'x^2+'+(-2*a*b-b*b)+'}'],
+	parts: [k, '\\frac{'+a*a*k+'}{'+'x^2+'+(-2*a*b-b*b)+'}'],
 	roots: [a+b,-1*(a+b)],
 	enablePartsSubtraction: 1,
     handleMultipleRoots: 'randomExceptList',
