c42e3e7753d6721a6eac2c01bca05b9f0f4f9b46
NickKolok <nickkolok@mail.ru>
2022-07-18 17:08:26 +0300

    [zdn] 282849 - more exponents

--- zdn/mat2014/B7/282849.js
+++ zdn/mat2014/B7/282849.js
@@ -2,20 +2,22 @@
 
 var a=sluchch(1,9).pm();
 var b=slKrome(a.abs(),1,9).pm();
-var n=sluchch(3,13,2);
+var q=[2,3,5,7].iz();
+
+var n = q==2? sluchch(3,13,2) : sluchch(3,5,2);
 
 var sgn=[-1,1].iz();
 var part;
 
 if((a*a-a*b+sgn*2)!=0)
-    part='('+(b-a)+'x+'+(a*a-a*b+sgn*2)+')^{'+n+'}';
+    part='('+(b-a)+'x+'+(a*a-a*b+sgn*q)+')^{'+n+'}';
 else if(b-a!=1)
     part='('+(b-a)+'x'+')^{'+n+'}';
 else
     part=(b-a)+'x^{'+n+'}';
 
 chas2.task.setEquationTask({
-    parts: [part, sgn*Math.pow(2,n)],
+    parts: [part, sgn*Math.pow(q,n)],
     roots: a,
     enablePartsSubtraction: 1,
 });
