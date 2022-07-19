01f30342b79fdc6e71244ae4c827c9017fd858c0
NickKolok <nickkolok@mail.ru>
2022-07-19 04:07:08 +0300

    [zdn][new] 525399 - equation by Belozorov

---
+++ zdn/mat2014/B7/525399.js
@@ -0,0 +1,14 @@
+(function(){'use strict';
+
+var n=sluchch(1,5);
+var x=sluchch(2,9);
+
+chas2.task.setEquationTask({
+	parts: ['\\log_{x}{'+x.pow(n)+'}', n,],
+	roots: x,
+	enablePartsSubtraction: 1,
+});
+})();
+
+//525399
+//Белозоров Никита
