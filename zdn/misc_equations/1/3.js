43afc96eb75dfe0efa65af318e3612fe3040fc67
Aleksandra Sumatokhina <SugarHedgehog@users.noreply.github.com>
2022-08-30 20:03:38 +0300

    [zdn] [misc] - Линейные, квадратные, кубические

---
+++ zdn/misc_equations/1/3.js
@@ -0,0 +1,14 @@
+(function(){'use strict';
+
+var a=sluchch(1,9).pm();
+var b=sluchch(1,9).pm();
+
+chas2.task.setEquationTask({
+	parts: ['('+b+'x+'+a*b+')^2', (4*a*b*b)+'x'],
+	roots: a,
+	enablePartsSubtraction: 1,
+});
+})();
+
+//77369
+//Белозоров Никита
