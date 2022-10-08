e40bca5390c84329e73fd38525c2f0797626ba14
Aleksandra Sumatokhina <SugarHedgehog@users.noreply.github.com>
2022-10-08 11:03:15 +0300

    [math23] - ещё косяки имён

--- zdn/matege2023p/matege2023p.js
+++ zdn/matege2023p/matege2023p.js
@@ -2,7 +2,7 @@ if(!window.nabor)
 	window.nabor={};
 window.nabor.importFrom({
 	nZad:11,
-	adres:'../zdn/matege2023/',
-	name:'matege2023',
+	adres:'../zdn/matege2023p/',
+	name:'matege2023p',
 	prefix:'',
 });
