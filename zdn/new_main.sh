de9581f4f262f68fdc4cb2d163cf6b102d71c8ff
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2022-11-23 07:38:29 +0300

    [script] [fix] - \t \n

--- zdn/new_main.sh
+++ zdn/new_main.sh
@@ -1,17 +1,17 @@
 i=1;
 touch main.js;
-printf "window.nomer=[\n" >> main.js;
+printf "window.nomer=[\n\t" >> main.js;
 for file in $(ls $1/* -v)
 do
 filename=$(basename -- "$file");
 extension="${filename##*.}";
 filename="${filename%.*}";
 if(($filename!="main")); then
-printf "%s,\n" $filename >> main.js
+printf "%s,\n\t" $filename >> main.js
 fi
 ((i++));
 done
-printf "].iz()\nwindow.comment='';" >> main.js;
+printf "].iz()\nwindow.comment='';\n" >> main.js;
 #cd {путь к папке, где будут лежать симлинки}
 #../.././symlinks.sh ../../matege2022p/9 <--папкка откуда будут браться файлы для симлинков
 #../.././new_main.sh ../../matege2022p/9 создаём main.js  в той же папке
