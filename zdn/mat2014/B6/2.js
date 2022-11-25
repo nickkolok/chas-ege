f5a87481dadf228b0a30e8484c9f0494d9fc5e2a
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2022-11-26 00:58:59 +0300

    [fix] [zdn] - переход на новый API

--- zdn/mat2014/B6/2.js
+++ zdn/mat2014/B6/2.js
@@ -1,17 +1,18 @@
 (function() {
-
-
-var a=sluchch(1,20);
-var b=sluchch(2,3);
-
-window.vopr.txt='В случайном эксперименте бросают '+b+' игральные кости. '+
-				'Найдите вероятность того, что в сумме выпадет '+a+' '+chislit(a,'очко','очка','очков')+'.';
-window.vopr.ver=[''+(om.igrkosti[b][a]/(6).pow(b))];
-
-NAtask.modifiers.roundUpTo(-2); //модификатор округления ответа
-
-window.vopr.kat['log']=0;
-window.vopr.kat['prz']=0;
-window.vopr.kat['drs']=0;
-window.vopr.kat['tri']=0;
+	retryWhileError(function() {
+		NAinfo.requireApiVersion(0, 2);
+		var b = sl(2, 3);
+		var a = 0;
+		for (let i = 0; i < b; i++)
+			a += sl(1, 6);
+		NAtask.setTask({
+			text: 'В случайном эксперименте бросают ' + b + ' игральные кости. ' +
+				'Найдите вероятность того, что в сумме выпадет ' + a + ' ' + chislit(a, 'очко', 'очка', 'очков') + '.',
+			answers: (om.igrkosti[b][a] / (6).pow(b)).ts(),
+			analys: '',
+		});
+		NAtask.modifiers.roundUpTo(-2); //модификатор округления ответа
+	});
 })();
+//fixed by SugarHedgehog
+//282853 283441 283461 283465 283443 283445 283447 283449 283451 283453 283455 283457 283459 283463 Все
