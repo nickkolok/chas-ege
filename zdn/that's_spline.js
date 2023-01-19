549997ac8c9fa93d226c40c0bdabdda84375e7b6
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2023-01-19 17:18:42 +0300

    теперь находит максимумы и минимумы

--- zdn/that's_spline.js
+++ zdn/that's_spline.js
@@ -1,42 +1,62 @@
-(function () {
-	retryWhileError(function () {
-		NAinfo.requireApiVersion(0, 2);
-		let xs = [];
-		let ys = [];
-		for (let i = -10; i < 10; i++) {
-			xs.push(i);
-			ys.push(sl(1, 4, 0.5) * (-1).pm());
-		}
-		// new a Spline object
-		let spline = new Spline(xs, ys);
+(function() {
+	retryWhileError(function() {
+		'use strict';
 
-		// interpolate a line at a higher resolution
-		let points = [];
-		for (let i = -10; i < 10; i += 0.1) {
-			points.push([i, spline.at(i)]);
+		function f(x) {
+			return spline.at(x);
 		}
-		let paint1 = function (ct) {
+		function maximum(){
+		let max=[];
+			for(let i=1;i<X.length;i++)
+				if(Y[i]>Y[i-1]&&Y[i]>Y[i+1])
+					max.push([X[i],Y[i]]);
+			return max;
+		}
+		function minimum(){
+		let min=[];
+			for(let i=1;i<X.length;i++)
+				if(Y[i]<Y[i-1]&&Y[i]<Y[i+1])
+					min.push([X[i],Y[i]]);
+			return min;
+		}
+		NAinfo.requireApiVersion(0, 2);
+		let X = [];
+		let Y = [];
+		for (let i = -10; i < 10; i += sl(1, 3, 0.1))
+			X.push(i);
+		Y.push(sl(1, 6).pm());
+		for (let i = 1; i < X.length; i++) {
+			do {
+				Y[i] = Y[i - 1] + sl(2, 10).pm();
+			} while (Y[i].abs() > 5||Y[i]==0);
+		}
+		let spline = new Spline(X, Y);
+		let paint1 = function(ct) {
 			let h = 500;
 			let w = 800;
-			ct.translate(400, 250);
-			ct.drawCoordPlane(w, h, {
-				hor: 1,
-				ver: 1
-			}, {
-				x1: '1',
-				y1: '1',
-				sh1: 13,
-			}, 20);
-
+			ct.translate(w/2, h/2);
+			/*ct.drawLine(-w/2,0,w/2,0);
+			ct.drawLine(0,-h/2,0,h/2);*/
 			ct.scale(30, -30);
 			ct.lineWidth = 0.1;
-			for (let i = 1; i < points.length; i++)
-				ct.drawLine(points[i - 1][0], points[i - 1][1], points[i][0], points[i][1]);
+
+			graph9AdrawFunction(ct, f, {
+				minX: -10,
+				maxX: 10,
+				minY: -8,
+				maxY: 8,
+				step: 0.01
+			});
+			graph9AmarkCircles(ct, [X,Y].T(), 20, 0.1);
+			ct.fillStyle = "red";
+			graph9AmarkCircles(ct, maximum(), 20, 0.1);
+			ct.fillStyle = "green";
+			graph9AmarkCircles(ct, minimum(), 20, 0.1);
 		};
 		NAtask.setTask({
-			text: '',
+			//text: Y + '<br>' + X,
 			answers: 0,
-			analys: '',
+			analys: 'Максимумов: '+maximum().length+'<br>'+'Минимумов: '+minimum().length,
 		});
 		chas2.task.modifiers.addCanvasIllustration({
 			width: 800,
