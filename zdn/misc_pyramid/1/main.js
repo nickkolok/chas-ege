d4279f63849c5569dc3d15e77c3596c857dc1d69
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2023-03-28 10:07:37 +0300

    [misc] [fix] - new main

---
+++ zdn/misc_pyramid/1/main.js
@@ -0,0 +1,7 @@
+window.nomer=[
+	8,
+	13,
+	905,
+	27069,
+].iz()
+window.comment='Пирамиды';
---
+++ zdn/misc_pyramid/misc_pyramid.js
@@ -0,0 +1,8 @@
+if(!window.nabor)
+	window.nabor={};
+window.nabor.importFrom({
+	nZad:1,
+	adres:'../zdn/misc_pyramid/',
+	name:'misc_pyramid',
+	prefix:'',
+});
