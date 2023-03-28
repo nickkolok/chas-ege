770d8440885c48c182b12d150c1bbc04e56e08a1
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2023-03-28 10:45:10 +0300

    [zdn] [fix] - fix draw pyramid

--- zdn/matege2023p/2/905.js
+++ zdn/matege2023p/2/905.js
@@ -11,7 +11,7 @@
 			['апофема', edgeOfBase / 2, 'равна'],
 			['высота', edgeOfBase / 3, 'равна'],
 		].iz(2);
-		let question = sklonlxkand((name).T()[0].shuffle());
+		let question = (name).T()[0].shuffle();
 		let numbers = (name).T()[1];
 		let verb = (name).T()[2];
 
@@ -19,12 +19,10 @@
 			genAssert((element).isZ(), 'Значение не целое');
 		});
 
-		genAssert((numbers[1] * 10).isZ() && numbers[1], 'плохой ответ');
-
-		let numbersView = [];
+		let numbersView = numbers.map((elem) => elem);
 		let ps;
 		for (let i = 0; i < 2; i++)
-			switch (question[i].ie) {
+			switch (question[i]) {
 			case 'объём':
 				if (!i)
 					numbersView[i] = numbers[i] + '\\sqrt{2}';
@@ -32,7 +30,7 @@
 					numbersView[i] = numbers[i] * 2;
 				ps = '\\sqrt{2}';
 				break;
-			case 'площадь поверхности':
+			case 'площадь полной поверхности':
 			case 'площадь боковой поверхности':
 			case 'площадь основания':
 			case 'апофема':
@@ -45,28 +43,29 @@
 			case 'высота':
 				if (!i)
 					numbersView[i] = numbers[i] + '\\sqrt{6}';
-				else
+				else {
 					numbersView[i] = numbers[i] * 6;
-				ps = '\\sqrt{6}';
+					ps = '\\sqrt{6}';
+				}
 				break;
 
 			}
 
-            let paint1 = function(ct) {
-                ct.translate(150, 100);
-                ct.scale(15, 15);
-                ct.lineWidth = 2 / 15;
-                let edgeOfBase = 10;
-                ct.drawTetrahedron({
-                    side: edgeOfBase,
-                    angle: -60.2
-                }, [1], [0.5, 0.2], true, true);
-            };
+		let paint1 = function(ct) {
+			ct.translate(10, 20);
+			ct.scale(10, -10);
+			ct.lineWidth = 2 / 15;
+			ct.rightPyramid3({
+				edge: 17,
+				angle: Math.PI / 8,
+				height: 11,
+			}, [1], [0.5, 0.2], question.includes('высота'), question.includes('апофема'));
+		};
 		NAtask.setTask({
-			text: 'В тетраэдре ' +
-				question[0].ie + ' ' + verb[0] + ' $' + numbers[0] + '$. Найдите ' +
-				question[1].ve + ' тетраэдра. Ответ умножьте на ' + '$' + ps + '$',
-			answers: '$' + numbersView[1].ts(1) + '$.',
+			text: 'В тетраэдре ' + question[0] + ' ' + verb[0] + ' $' + numbersView[0] + '$. Чему ' + verb[1] + ' ' +
+				question[1] + ' тетраэдра. Ответ умножьте на $' + ps + '$.',
+
+			answers: numbersView[1],
 		});
 		chas2.task.modifiers.addCanvasIllustration({
 			width: 240,
