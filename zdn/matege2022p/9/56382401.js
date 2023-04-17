75bec0fc932b33a2f1d4c568474c33b33e8a82d2
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2023-04-17 10:49:33 +0300

    [zdn] [fix] - fix question

--- zdn/matege2022p/9/56382401.js
+++ zdn/matege2022p/9/56382401.js
@@ -25,23 +25,16 @@ retryWhileUndefined(function() {
 		return;
 	for (let i = 0; i < p.length; i++)
 		p[i][0] = p[i][0].pm();
-	let find = 0,
-		answ = 0;
-	switch (sl(1, 3)) {
-	case 1:
-		find = 'произведение коэффициентов $k \\cdot b$';
-		answ = k * b;
-		break;
-	case 2:
-		find = 'сумму коэффициентов $k+b$';
-		answ = k + b;
-		break;
-	case 3:
-		let num1 = sl(8, 20, 0.5);
-		find = '$f(' + num1 + ')$';
-		answ = f(num1).ts();
-		break;
-	}
+		
+	let num1 = sl(6, 10, 05);
+	let variant = [
+		['произведение коэффициентов $k$ и $b$', k * b, sl1()],
+		['сумму коэффициентов $k$ и $b$', k + b, true],
+		['$f(' + num1 + ')$', f(num1).ts(), sl1()],
+		['коэффициент $k$', k, true],
+		['коэффициент $b$', b, true]
+	].iz();
+
 	let paint1 = function(ct) {
 		let h = 300;
 		let w = 300;
@@ -64,10 +57,11 @@ retryWhileUndefined(function() {
 		graph9AmarkCircles(ct, p, 4, 0.15);
 	};
 	NAtask.setTask({
-		text: 'На рисунке изображен график функций $f(x)=\\Bigl|k|x|+b\\Bigr|$, где $k' + sign[0] + '0$, $b' + sign[1] +
-			'0$. Найдите ' + find +
+		text: 'На рисунке изображен график функций $f(x)=\\Bigl|k|x|+b\\Bigr|$' + ', где $k' + sign[0] + '0$, $b' + sign[
+				1] +
+			'0$'.esli(variant[2]) + '. Найдите ' + variant[0] +
 			'.',
-		answers: answ,
+		answers: variant[1],
 		analys: ('$f(x)=\\Bigl|' + k.ts() + '|x|+' + b.ts() + '\\Bigr|').replace('+-', '-') + '$'
 	});
 	chas2.task.modifiers.addCanvasIllustration({
