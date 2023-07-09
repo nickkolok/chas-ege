b88d28c8b3d1bb372fa253f103dbc1026df80624
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2023-07-09 19:59:24 +0300

    [zdn] [fux] - fix scale

--- zdn/matege2023p/2/27187.js
+++ zdn/matege2023p/2/27187.js
@@ -2,28 +2,30 @@
 	retryWhileError(function() {
 			NAinfo.requireApiVersion(0, 2);
 			let a = sl(10, 17);
-			let b = sl(5, 14);
+			let b = sl(5, 10);
 			let c = sl(5, 10);
 			let d = sl(10, 16);
 			let k = slKrome(d, 5, 16);
 			let f = a + c;
-			
+
 			let rand = sl1();
 			let s = [2 * (f * k + (d + b) * f + (d + b) * k) - 2 * ((f - c) * b), k * (f * d + c * b)][rand];
 
 			let paint1 = function(ctx) {
 
-				a *= 13;
-				b *= 10;
+				a *= 10;
+				b *= 16;
 				c *= 10;
 				d *= 10;
+
+				let depth = 10;
 				ctx.translate(200, 200);
 				ctx.translate(-95, 0);
 
 				ctx.lineWidth = 2;
 				for (let i = 0; i < 2; i++) {
-					if (i){
-						ctx.translate(30, -b*0.2);
+					if (i) {
+						ctx.translate(30, -b * 0.2);
 					}
 					ctx.moveTo(-c, -b);
 					ctx.lineTo(c, -b);
@@ -31,7 +33,7 @@
 					ctx.lineTo(a, 0);
 					ctx.lineTo(a, d);
 					ctx.stroke();
-					if (i){
+					if (i) {
 						ctx.setLineDash([4, 3]);
 					}
 					ctx.lineTo(-c, d);
@@ -40,24 +42,24 @@
 				}
 				ctx.setLineDash([0, 0]);
 
-				ctx.translate(-30, b*0.2);
+				ctx.translate(-30, b * 0.2);
 
-				ctx.drawLine(-c, -b, -c + 30, -b - b*0.2);
-				ctx.drawLine(c, -b, c + 30, -b - b*0.2);
-				ctx.drawLine(c, 0, c + 30, -b*0.2);
-				ctx.drawLine(a, 0, a + 30, -b*0.2);
-				ctx.drawLine(a, d, a + 30, d - b*0.2);
+				ctx.drawLine(-c, -b, -c + 30, -b - b * 0.2);
+				ctx.drawLine(c, -b, c + 30, -b - b * 0.2);
+				ctx.drawLine(c, 0, c + 30, -b * 0.2);
+				ctx.drawLine(a, 0, a + 30, -b * 0.2);
+				ctx.drawLine(a, d, a + 30, d - b * 0.2);
 
 				ctx.setLineDash([4, 3]);
-				ctx.drawLine(-c, d, -c + 30, d - b*0.2);
+				ctx.drawLine(-c, d, -c + 30, d - b * 0.2);
 
 				//цифорки
 				ctx.font = "20px serif";
-				ctx.fillText((b / 10).toString(), c + 30 + 3, -b / 2, 18); //b
-				ctx.fillText((c / 10).toString(), 30, -b - b*0.2 - 5, 18); //c
+				ctx.fillText((b / 16).toString(), c + 30 + 3, -b / 2, 18); //b
+				ctx.fillText((c / 10).toString(), 30, -b - b * 0.2 - 5, 18); //c
 				ctx.fillText((d / 10).toString(), a + 30 + 5, d / 2, 18); //d
 				ctx.fillText(k.toString(), a + 10, d * 1.05, 18); //k
-				ctx.fillText(f.toString(), 0, d + 30+10, 18); //f
+				ctx.fillText(f.toString(), 0, d + 30 + 10, 18); //f
 
 
 			};
