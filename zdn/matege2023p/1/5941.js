e3cea8d3f29a45dc9a2a1407fe53cc0f4c918311
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2023-08-03 20:59:08 +0500

    [zdn] [fix] - no vertices

--- zdn/matege2023p/1/5941.js
+++ zdn/matege2023p/1/5941.js
@@ -4,14 +4,6 @@
 
 		let angle = sl(2, 44);
 
-		let vertices = [];
-		do {
-			let a = slLetter(vertices).toUpperCase();
-			if (!vertices.includes(a))
-				vertices.push(a);
-		}
-		while (vertices.length < 6);
-
 		let question = [
 			['больший', 180 + angle],
 			['меньший', 180 - angle]
@@ -23,12 +15,6 @@
 			ctx.drawLine(10, 320, 310, 320);
 			ctx.drawLine(50, 80, 10, 320);
 			ctx.drawLine(310, 320, 350, 80);
-
-			ctx.font = "23px liberation_sans";
-			ctx.fillText(vertices[0], 50, 80 - 10);
-			ctx.fillText(vertices[1], 350, 80 - 10);
-			ctx.fillText(vertices[2], 310, 320 + 20);
-			ctx.fillText(vertices[3], 10, 320 + 20);
 
 		};
 
