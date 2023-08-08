83671292c10c769047eb91b5b0819c49c8658697
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2023-08-08 18:45:31 +0500

    [zdn] [ref] - 4

--- zdn/mat2014/B8/4.js
+++ zdn/mat2014/B8/4.js
@@ -3,10 +3,31 @@
 var t1=[['sin','cos'],['tg','ctg']].iz().shuffle();
 var a=sluchch(0.01,0.99,0.01).ts();
 
-chas2.task.setTask({
+let paint1 = function(ctx) {
+	ctx.lineWidth = 2;
+
+	ctx.drawLine(10, 370, 390, 370);
+	ctx.drawLine(10, 370, 10, 50);
+	ctx.drawLine(10, 50, 390, 370);
+
+	//прямой угол
+	ctx.lineWidth = 1;
+
+	ctx.drawLine(10, 370 - 20, 10 + 20, 370 - 20);
+	ctx.drawLine(10 + 20, 370, 10 + 20, 370 - 20);
+
+};
+
+NAtask.setTask({
 	text: 'В треугольнике $'+'ABC'.mesh()+'$ угол $A$ равен $90^\\circ$, $\\'+t1[0]+' B = '+a+'$. Найдите $\\'+t1[1]+' C$.',
 	answers: a,
 	tags: {tri:1},
 });
-chas2.task.modifiers.variativeABC();
+NAtask.modifiers.variativeABC();
+
+NAtask.modifiers.addCanvasIllustration({
+	width: 400,
+	height: 400,
+	paint: paint1,
+});
 })();
