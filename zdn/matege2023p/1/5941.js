19054345356ca7af05e8bee80845bc7f43976d9b
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2023-08-08 16:32:00 +0500

    merge "newTask_5941"

---
+++ zdn/matege2023p/1/5941.js
@@ -0,0 +1,33 @@
+(function() {
+	retryWhileError(function() {
+		NAinfo.requireApiVersion(0, 2);
+
+		let angle = sl(2, 44);
+
+		let question = [
+			['больший', 180 + angle],
+			['меньший', 180 - angle]
+		].iz();
+
+		let paint1 = function(ctx) {
+			ctx.lineWidth = 2;
+			ctx.drawLine(50, 80, 350, 80);
+			ctx.drawLine(10, 320, 310, 320);
+			ctx.drawLine(50, 80, 10, 320);
+			ctx.drawLine(310, 320, 350, 80);
+
+		};
+
+		NAtask.setTask({
+			text: 'Один угол параллелограмма больше другого на $' + angle + '^\\circ$. Найдите ' + question[0] +
+				' угол. Ответ дайте в градусах.',
+			answers: 0.5 * question[1],
+		});
+		NAtask.modifiers.addCanvasIllustration({
+			width: 400,
+			height: 400,
+			paint: paint1,
+		});
+	}, 1000);
+})();
+//5941
