79e333b6218bdecba10c4e55e80a89d20737fec2
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2023-08-08 18:16:59 +0500

    [zdn] [fix] - paint

--- zdn/mat2014/B8/319157.js
+++ zdn/mat2014/B8/319157.js
@@ -11,12 +11,32 @@
 		if (middle == 'AD')
 			lowSide = 'BC';
 	}
+	let paint1 = function(ctx) {
+		ctx.lineWidth = 2;
+		ctx.drawLine(100, 80, 350, 80);
+		ctx.drawLine(10, 320, 270, 320);
+		ctx.drawLine(100, 80, 10, 320);
+		ctx.drawLine(270, 320, 350, 80);
+
+		ctx.drawLine(10, 320, 310, 200);
+
+		ctx.lineWidth = 1;
+		ctx.drawLine(300 + 15, 150, 320 + 10, 160);
+		ctx.drawLine(270 + 15, 250, 290 + 10, 260);
+
+	};
+
 	NAtask.setTask({
 		text: 'Площадь параллелограмма $ABCD$ равна ' + S + '. Точка $' + side + '$ – середина стороны $' + middle +
 			'$. Найдите площадь треугольника $' + lowSide + side + '$.',
 		answers: answer,
 	});
-	NAtask.modifiers.variativeABC();//расставляем случайные буквы
+	NAtask.modifiers.variativeABC(); //расставляем случайные буквы
+	NAtask.modifiers.addCanvasIllustration({
+		width: 400,
+		height: 400,
+		paint: paint1,
+	});
 })();
 //Обр.задания 319157
 //Антипова Татьяна
