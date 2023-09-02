4a9b388bfb7c1dceebd28754b5bc6174fb7e80ea
NickKolok <nickkolok@mail.ru>
2023-09-03 01:31:32 +0300

    [zdn][new] 245169

---
+++ zdn/matege2023p/6/245169.js
@@ -0,0 +1,22 @@
+(function() {
+	retryWhileError(function() {
+		'use strict';
+
+		let a = sl(3,29,2).pm();
+		let b = [6,8,12].iz();
+		genAssert(a.nod(b) == 1, 'Дробь должна быть несократима');
+
+		chas2.task.setEvaluationTask({
+			expr: '' +
+				('' + sl(2,100).pm()).esli(sl(3)) +
+				'sqrt(' + /*[2,3].iz()*/ 2 + ') ' +
+				[
+					'sin(' + a.pm() + 'pi/' + b + ')',
+					'cos(' + a.pm() + 'pi/' + b + ')',
+				].shuffle().join(''),
+			//forbiddenAnswers: [0],
+			authors: ['Николай Авдеев'],
+		});
+	},10000);
+})();
+//245169
