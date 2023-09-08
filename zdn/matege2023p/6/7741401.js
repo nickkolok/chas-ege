5be23908a2f5a8f9ae8275364b629db3cd50bce2
NickKolok <nickkolok@mail.ru>
2023-09-09 02:39:43 +0300

    [zdn] 7741401 chas2.task -> NAtask

--- zdn/matege2023p/6/7741401.js
+++ zdn/matege2023p/6/7741401.js
@@ -12,7 +12,7 @@
 		//let angle2 = sl(30,180,15).pm();
 		genAssert(func1 !== func2 || angle1 !== angle2, 'Множители должны различаться');
 
-		chas2.task.setEvaluationTask({
+		NAtask.setEvaluationTask({
 			expr: '' + sl(3,50).pm() + '' + func1 + '(' + angle1 + ')  ' + func2 + '(' + angle2 + ')' + (['+','-'].iz() + sl(1,1000)).esli(sl1()),
 			//forbiddenAnswers: [0],
 			authors: ['Николай Авдеев'],
