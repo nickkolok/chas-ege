4f01a8a7182818cf768e29f2bf14a4a25f86688f
SugarHedgehog <sneginca.cat@gmail.com>
2023-09-08 13:40:35 +0300

    [script] [fix] - new script for fast sets

---
+++ zdn/fast_set.sh
@@ -0,0 +1,31 @@
+array=()
+
+if [[ ${#array[@]} -eq 0 ]]; then
+     echo "change array in file"
+fi
+
+result=${PWD##*/}
+result=${result:-/} 
+touch $result.js
+printf "if (!window.nabor)\n\twindow.nabor = {};\nwindow.nabor.importFrom({\n\tnZad: "${#array[@]}",\n \tadres: '../zdn/"$result"',\n" >> $result.js
+printf "\tname: '"$result"',\n});\n" >> $result.js
+
+cd "./"
+i=1;
+for index in ${!array[*]}
+do
+mkdir $i;
+cd $i;
+ln -s $1/${array[$index]}.js ${array[$index]}.js;
+((i++));
+
+touch main.js
+printf "window.nomer=[\n" >> main.js;
+printf "\t%s,\n" ${array[$index]} >> main.js
+printf "].iz()\nwindow.comment='"${array[$index]}"';\n" >> main.js;
+cd ..;
+done
+
+#переходим в общую папку набора
+
+#.././fast_set.sh ../../matege2023p/5
