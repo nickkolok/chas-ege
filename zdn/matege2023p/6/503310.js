8d5011d138dabac01f1d5a3ab72b4d95868ed080
NickKolok <nickkolok@mail.ru>
2023-09-09 00:32:57 +0300

    [zdn] powdeg -> degpow

--- zdn/matege2023p/6/503310.js
+++ zdn/matege2023p/6/503310.js
@@ -4,7 +4,7 @@
 
 		let a = sl(1,89) + 90 * sl(-3,3);
 		let b = a + 90 * sl(1,3).pm();
-		let fn = ['sin','cos'].iz() + 'powdeg';
+		let fn = ['sin','cos'].iz() + 'degpow';
 
 		NAtask.setEvaluationTask({
 			expr: '' + sl(10,100).pm() + '/(' +
