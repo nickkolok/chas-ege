55e56c148da84d71bf5e8e6abad9f0369ce27435
NickKolok <nickkolok@mail.ru>
2023-09-09 02:38:30 +0300

    [zdn] 77394 chas2.task -> NAtask

--- zdn/matege2023p/6/77394.js
+++ zdn/matege2023p/6/77394.js
@@ -4,7 +4,7 @@
 
 		let base = sl(2,10);
 
-		chas2.task.setEvaluationTask({
+		NAtask.setEvaluationTask({
 			expr: 'divideColon(' + [
 				'(' + base.pow(sl(1,2)) + '^' + sl(2,20) + ')' + '^' + sl(2,20),
 				base.pow(sl(1,2)) + '^' + sl(2,100),
