45ce61b61efc83f677ababcac7b5e587ce7fa2a1
NickKolok <nickkolok@mail.ru>
2023-09-09 00:31:16 +0300

    [zdn] 26740 - no need to manually prevent reducible fractions anymore

--- zdn/matege2023p/6/26740.js
+++ zdn/matege2023p/6/26740.js
@@ -9,10 +9,8 @@
 		genAssert(a < 200 && b < 200, 'Числа не должны быть слишком большими');
 		let c1 = sl(2,9);
 		let c2 = sl(c1+1,10);
-		genAssertIrreducible(c1, c2);
 		let c3 = sl(2,9);
 		let c4 = sl(c3+1,10);
-		genAssertIrreducible(c3, c4);
 
 		NAtask.setEvaluationTask({
 			expr: '' + a + '^(' + c1 + '/' + c2 + ') * ' + b + '^(' + c3 + '/' + c4 + ')',
