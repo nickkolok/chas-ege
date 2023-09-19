257894b245676ef8864c66bccc4d1e99b4c7e3f9
SugarHedgehog <sneginca.cat@gmail.com>
2023-09-19 22:44:05 +0300

    [zdn] [new] - 64485006

---
+++ zdn/matege2024p/2/64485006.js
@@ -0,0 +1,50 @@
+(function() {
+	retryWhileError(function() {
+		NAinfo.requireApiVersion(0, 2);
+
+		let vectorsName = window.smallLatinLetters.iz(5);
+
+		let vectA = [sl(-20, 20), sl(-20, 20)];
+		let vectB = [slKrome(vectA[0], -20, 20), slKrome(vectA[1], -20, 20)];
+		let vectC = [slKrome([vectA[0], vectB[0]], -20, 20), slKrome([vectA[1], vectB[1]], -20, 20)];
+		let vectD = [slKrome([vectA[1], vectB[1], vectC[0]], -20, 20), slKrome([vectA[1], vectB[1], vectC[1]], -20, 20)];
+
+		let vectors = [vectA, vectB, vectC, vectD];
+		let vectorsView = vectors.map((elem, index) => '$\\vec{' + vectorsName[index] + '} = ( ' + elem.join(';') + ' )$');
+
+		let coeffs = generateMatrix(4, 2, -20, 20);
+		let finalyVector = [0, 0];
+		let condition = [];
+
+		for (let i = 0; i < vectors.length; i++) {
+			condition.push(coeffs[i][0].texfrac(coeffs[i][1]) + '\\vec{' + vectorsName[i] + '}');
+			finalyVector = objSum(finalyVector, objUmn(vectors[i], coeffs[i][0] / coeffs[i][1]));
+		}
+
+		let question = [
+			[
+				['первую координату', finalyVector[0]],
+				['вторую координату', finalyVector[1]]
+			].iz(), ['сумму координат', finalyVector.sum()],
+			['произведение координат', finalyVector.production()]
+		].iz();
+		let finalyVectorView = finalyVector.slice().map((elem) => elem.ts());
+
+		let answ = question.pop();
+
+		genAssertZ1000(finalyVector[0]);
+		genAssertZ1000(finalyVector[1]);
+
+		let resultExpr = condition.shuffle().slag().plusminus();
+
+		NAtask.setTask({
+			text: 'Даны векторы ' + vectorsView.joinWithConjunction() + '. ' +
+				'Найдите координаты вектора $\\vec{' + vectorsName[2] + '}=' + resultExpr.replace('+-', '-') +
+				'$. В ответе запишите ' + question +
+				' вектора $\\vec{' + vectorsName[2] + '}$.',
+			answers: answ,
+			analys: '$' + (resultExpr + '=(' + finalyVectorView.join('; ')).plusminus() + ')$',
+		});
+		NAtask.modifiers.multiplyAnswerBySqrt(13);
+	}, 1000);
+})();
