aae5554f04217d8e1ef71cc3a1b157de43f4b2d5
SugarHedgehog <sneginca.cat@gmail.com>
2023-10-05 21:16:56 +0300

    [zdn] [fix] - fix 26750

--- zdn/matege2024p/7/26750.js
+++ zdn/matege2024p/7/26750.js
@@ -1,11 +1,20 @@
 (function() {
 	retryWhileError(function() {
 		'use strict';
-		let num1 = sl(1.1, 10, 0.1);
-		let num2 = slKrome(num1, 1.1, 10, 0.1);
-		let num3 = num1 * num2;
+		let a = sl(2, 20);
+		let b = sl(2, 100).pm();
+		let c = slKrome(a, 2, 20);
+		let f = a * c;
+		let e = -sl(2, 10)*b/b.abs();
+		let d = (a+b.pow(2)*c)*e/(2*b);
+													
+		genAssert((d*100).isZ(), 'Такой себе множитель');
+		genAssert(!f.isPolnKvadr(), 'Корень извлекается');
+		genAssert(!a.isPolnKvadr(), 'Корень извлекается');
+		genAssert(!c.isPolnKvadr(), 'Корень извлекается');
 		NAtask.setEvaluationTask({
-			expr: '(sqrt(' + num1 + ')' + ['+', '-'].iz() + 'sqrt(' + num2 + '))^2/(' + sl(2, 10) + ['+', '-'].iz() + sl(2,	10) + 'sqrt(' + num3 + '))',
+			expr: ('(' + ['sqrt(' + a + ')', b + 'sqrt(' + c + ')'].shuffle().slag() + ')^2/(' + [d, e + 'sqrt(' + f + ')']
+				.shuffle().slag() + ')').plusminus(),
 			//forbiddenAnswers: [0],
 			authors: ['Суматохина Александра'],
 		});
