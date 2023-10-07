d1711030b3d4dc984dabfe5f22108613113c5ffe
NickKolok <nickkolok@mail.ru>
2023-10-08 00:29:28 +0300

    [sh][print] Better images

--- sh/pechmat.js
+++ sh/pechmat.js
@@ -486,7 +486,8 @@ function replaceCanvasWithImgInTask(element, text) {
 	for (var i = 0; i < canvases.length; i++) {
 		var imageName = canvases[i].getAttribute('data-nonce').substr(3) + "n" + i;
 		preparedImages[imageName] = canvases[i].toDataURL().replace('data:image/png;base64,','');
-		text = text.replace(/<canvas.*?<\/canvas>/, '\\addpictoright[0.25\\textwidth]{images/'+imageName+'}');
+		text = text.replace(/<canvas.*?<\/canvas>/, '\\addpictoright[0.4\\linewidth]{'+imageName+'}');
+		text+='\\ifdefined\\OnAfterIllustratedTask\\OnAfterIllustratedTask\\fi';
 	}
 	return text;
 }
