7677f0da65374fb0fad61fc28693e51dbc9a7fef
NickKolok <nickkolok@mail.ru>
2023-10-30 21:41:10 +0300

    [zdn][fix] mat2014/B15/7 - get rid of +0 - see #1248

--- zdn/mat2014/B15/7.js
+++ zdn/mat2014/B15/7.js
@@ -10,7 +10,7 @@ var t1=['наибольшее значение','наименьшее значе
 var tg='\\mathrm{tg} x';
 var t3=['-',''];
 
-var h=[t3[v2]+(4*a)+tg,t3[1-v2]+(4*a)+'x',c,t3[0+((v2!=v1)==v2)]+a.pina(1)].slag();
+var h=[t3[v2]+(4*a)+tg,t3[1-v2]+(4*a)+'x',c,t3[0+((v2!=v1)==v2)]+a.pina(1)].slag0();
 var p=(v1?-a:a)*4+c;
 
 window.vopr.txt=fn_txt(t1[v1],h,(-1).pina(4),(1).pina(4));
