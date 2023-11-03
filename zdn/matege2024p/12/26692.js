e74332b46bb55882fe4d0c9e23e6bfb5e0eb3106
NickKolok <nickkolok@mail.ru>
2023-11-04 02:59:14 +0300

    [zdn] 26692 - more variations

--- zdn/matege2024p/12/26692.js
+++ zdn/matege2024p/12/26692.js
@@ -6,7 +6,7 @@
 
 		NAtask.setMinimaxFunctionTask({
 			expr: '' + (a*6) + 'cos(x) + ' + (a*3) + ' sqrt(3) x - ' + a + ' sqrt(3) pi + ' + b,
-			leftEnd: '0',
+			leftEnd: ['0','pi/'+[3,4,6].iz()].iz(),
 			rightEnd: 'pi/'+sl(2,3),
 			authors: ['Николай Авдеев'],
 		});
