c0a51b9fbafb008e46980fa7570723a0ff46eb4e
SugarHedgehog <sneginca.cat@gmail.com>
2023-11-20 17:44:32 +0300

    Squashed commit of the following:
    commit 53e9f156b3db84796206347df0e704e3a9d7a57b
    Author: SugarHedgehog <sneginca.cat@gmail.com>
    Date:   Mon Nov 20 13:59:39 2023 +0300
    
        [zdn] [fix] - author
    
    commit c7d45e49a63518032982f7082aab8050329166f7
    Author: SugarHedgehog <sneginca.cat@gmail.com>
    Date:   Sun Nov 19 20:23:03 2023 +0300
    
        [zdn] [new] - newTask_168

---
+++ zdn/matege2024p/3/168.js
@@ -0,0 +1,59 @@
+(function() {
+	retryWhileError(function() {
+		lx_declareClarifiedPhrase('ребро', 'боковое');
+		lx_declareClarifiedPhrase('сторона', 'основания');
+		NAinfo.requireApiVersion(0, 2);
+		let a = sl(2, 20, 2);
+		let h = slKrome(a, 1, 20);
+		let b = 0.5 * a.pow(2) + h.pow(2);
+		let ap = (0.5 * a).pow(2) + h.pow(2);
+
+		let question = [
+			[sklonlxkand('боковое ребро'), b.texsqrt(sl1())],
+			[sklonlxkand('сторона основания'), a],
+			[sklonlxkand('высота'), h],
+			[sklonlxkand('апофема'), ap.texsqrt(sl1())],
+		].iz(3);
+
+		let answ;
+		switch (question[2][0].ie) {
+		case 'апофема':
+			answ = ap.sqrt();
+			break;
+		case 'боковое ребро':
+			answ = b.sqrt();
+			break;
+		default:
+			answ = question[2][1];
+		}
+
+		let paint1 = function(ctx) {
+			ctx.translate(0, 500);
+			ctx.scale(10, 10);
+			ctx.lineWidth = 2 / 15;
+			ctx.drawRightPyramid4({
+				edge: 21,
+				angle: Math.PI / 4,
+				height: -23,
+				scale: 10,
+			}, [0, 2, 5], [1, 0.5], true);
+		};
+
+		NAtask.setTask({
+			text: 'В правильной четырёхугольной пирамиде ' +
+				question[0][0].ie + ' рав' + ['ен', 'на', 'но'][question[0][0].rod] + ' $' + question[0][1] + '$, а ' +
+				question[1][0].ie + ' рав' + ['ен', 'на', 'но'][question[1][0].rod] + ' $' + question[1][1] + '$. ' +
+				'Найдите ' + question[2][0].ve + ' пирамиды.',
+			answers: answ,
+			author: ['Суматохина Александра'],
+		});
+		NAtask.modifiers.multiplyAnswerBySqrt(13);
+		NAtask.modifiers.addCanvasIllustration({
+			width: 400,
+			height: 400,
+			paint: paint1,
+		});
+	});
+})();
+//https://ege314.ru/8-stereometriya-ege/reshenie-168/
+//168
