57b4e3cb902b7a6bee0c4648b54b79dde85a81db
aisse_258 <winter.b258@yandex.ru>
2023-12-15 19:28:41 +0300

    Модификатор чисел, удаление промежуточных переменных склонения

--- zdn/matege2024p/9/27995.js
+++ zdn/matege2024p/9/27995.js
@@ -2,20 +2,17 @@
 	retryWhileError(function() {
 		NAinfo.requireApiVersion(0, 2);
 
-		let liquid = ['вода','масло','глицерин','пропиленгликоль'].iz();
-		let liquid_skl=sklonlxkand(liquid);
+		let liquid = sklonlxkand(['вода','масло','глицерин','пропиленгликоль'].iz());
 		let capacity={'вода':4200,'масло':1810,'глицерин':2350, 'пропиленгликоль':2480};
 		//TODO: добавить в склонялку слово узел
-		let room=['помещение','комната','магазин','здание','квартира','санузл','ангар'].iz();
-		let room_skl=sklonlxkand(room);
-		let heat_system=['радиатор','система', 'батарея','нагреватель','климат-контроль'].iz();
-		let heat_system_skl=sklonlxkand(heat_system);
+		let room=sklonlxkand(['помещение','комната','магазин','здание','квартира','санузл','ангар'].iz());
+		let heat_system=sklonlxkand(['радиатор','система', 'батарея','нагреватель','климат-контроль'].iz());
 
 		let log_zn = sl(1,6);//значение логарифма, x*j/a*c*m
 		let alpha = sl(0.1,1.9,0.01);//коэф альфа
 		let gamma = sl(10,50,0.01);//коэф гамма
 		let m = sl(0.1,0.9,0.01);//расход жидкости
-		let x = alpha*capacity[liquid]*m*log_zn/gamma;//длина трубы
+		let x = alpha*capacity[liquid.ie]*m*log_zn/gamma;//длина трубы
 		let T_R = sl(15,30,0.01);//температура помещения
 		let T_L = sl(T_R+10,99,0.01);//температура жидкости
 		let T = (T_L-T_R)/math.pow(2,log_zn)+T_R;
@@ -24,23 +21,24 @@
 		genAssertZ1000(T,'Ответ слишком дробный');
 
 		NAtask.setTask({
-			text: 'Для обогрева '+room_skl.re+', температура в котор'+['ом','ой','ом','ых'][room_skl.rod]+
-				' поддерживается на уровне $T_\\text{'+room[0].toUpperCase()+'}='+T_R.ts()+' {}^\\circ C$, через '+
-				heat_system_skl.ve+' отопления '+['пропускают','прокачивают'].iz()+' горяч'+['ий','ую','ее','ие'][liquid_skl.rod]+' '+liquid_skl.ve+'. '+
-				'Расход проходящ'+['его','ей','его','их'][liquid_skl.rod]+' через трубу '+heat_system_skl.re+' '+liquid_skl.re+' $m='+m.ts()+
-				' \\dfrac{\\mbox{кг}}{\\mbox{с}}$. Проходя по трубе расстояние $x~(\\mbox{м})$, '+liquid+' охлаждается от начальной температуры '+
-				'$T_\\text{'+liquid[0].toUpperCase()+'}='+T_L.ts()+
+			text: 'Для обогрева '+room.re+', температура в котор'+['ом','ой','ом','ых'][room.rod]+
+				' поддерживается на уровне $T_\\text{'+room.ie[0].toUpperCase()+'}='+T_R+' {}^\\circ C$, через '+
+				heat_system.ve+' отопления '+['пропускают','прокачивают'].iz()+' горяч'+['ий','ую','ее','ие'][liquid.rod]+' '+liquid.ve+'. '+
+				'Расход проходящ'+['его','ей','его','их'][liquid.rod]+' через трубу '+heat_system.re+' '+liquid.re+' $m='+m+
+				' \\dfrac{\\mbox{кг}}{\\mbox{с}}$. Проходя по трубе расстояние $x~(\\mbox{м})$, '+liquid.ie+' охлаждается от начальной температуры '+
+				'$T_\\text{'+liquid.ie[0].toUpperCase()+'}='+T_L+
 				' {}^\\circ C$, до температуры $T~({}^\\circ C)$, причём $x=\\alpha \\cdot \\dfrac{cm}{\\gamma}\\cdot \\log_2 \\dfrac{T_\\text{'+
-				liquid[0].toUpperCase()+'}-T_\\text{'+room[0].toUpperCase()+'}}{T-T_\\text{'+room[0].toUpperCase()+'}}$, где '+
-				'$c='+capacity[liquid]+'\\dfrac{\\mbox{Вт}\\cdot \\mbox{с}}{\\mbox{кг}\\cdot {}^\\circ C}$ — теплоёмкость '+liquid_skl.re+', $\\gamma='+
-				gamma.ts()+'\\dfrac{\\mbox{Вт}}{\\mbox{м}\\cdot {}^\\circ C}$ — коэффициент '+
-				'теплообмена, а $\\alpha='+alpha.ts()+'$ — постоянная. Найдите, до какой температуры (в градусах '+
-				'Цельсия) охладится '+liquid+', если длина трубы '+heat_system_skl.re+' '+
-				'равна $'+x.ts()+' ~\\mbox{м}$.',
+				liquid.ie[0].toUpperCase()+'}-T_\\text{'+room.ie[0].toUpperCase()+'}}{T-T_\\text{'+room.ie[0].toUpperCase()+'}}$, где '+
+				'$c='+capacity[liquid.ie]+'\\dfrac{\\mbox{Вт}\\cdot \\mbox{с}}{\\mbox{кг}\\cdot {}^\\circ C}$ — теплоёмкость '+liquid.re+', $\\gamma='+
+				gamma+'\\dfrac{\\mbox{Вт}}{\\mbox{м}\\cdot {}^\\circ C}$ — коэффициент '+
+				'теплообмена, а $\\alpha='+alpha+'$ — постоянная. Найдите, до какой температуры (в градусах '+
+				'Цельсия) охладится '+liquid.ie+', если длина трубы '+heat_system.re+' '+
+				'равна $'+x+' ~\\mbox{м}$.',
 			answers: T,
-			analys: '$\\log_2 \\dfrac{'+T_L.ts()+'-'+T_R.ts()+'}{T-'+T_R.ts()+'}='+log_zn+'$',
+			analys: '$\\log_2 \\dfrac{'+T_L+'-'+T_R+'}{T-'+T_R+'}='+log_zn+'$',
 			authors: ['Aisse-258'],
 		});
+		NAtask.modifiers.allDecimalsToStandard();
 	}, 20000);
 })();
 //Aisse-258
