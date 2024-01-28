75fb545da35513be1a8be44a50a642cc3102e88a
Алендарь Сергей <sergey.alendar@mail.ru>
2024-01-28 15:38:42 +0300

    Сделана задача №1, matege2024p/B15/1.js

---
+++ zdn/matege2024p/15/1.js
@@ -0,0 +1,14 @@
+﻿(function() {
+	retryWhileError(function() {
+		'use strict';
+		let a = sl(2, 5);
+		let b = sl(2, 5);
+		let c = a * b * b - sl(1, 7);
+		NAtask.setEvaluationTask({
+			expr: ' (' + b + 'sqrt(' + a + ')+sqrt(' + c + '))*(' + b + 'sqrt(' + a + ')-sqrt(' + c + '))',
+			//forbiddenAnswers: [0],
+			authors: ['Алендарь Сергей'],
+		});
+	}, 10000);
+})();
+
