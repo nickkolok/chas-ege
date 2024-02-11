9a13221dae700209d210ae031b570186ec052d61
SugarHedgehog <sneginca.cat@gmail.com>
2024-02-11 21:42:29 +0300

    [zdn] [fix] - angle on picture

--- zdn/matege2023p/1/27589.js
+++ zdn/matege2023p/1/27589.js
@@ -7,14 +7,14 @@
 		let paint1 = function(ctx) {
 			ctx.lineWidth = 2;
 
-			ctx.drawLineAtAngle(30, 300, -Math.PI / 3, 340);
-			ctx.drawLineAtAngle(370, 300, 4 * Math.PI / 3, 340);
-			ctx.drawLine(30, 300, 370, 300);
+			ctx.drawLineAtAngle(200, 10, 5*Math.PI/12, 350);
+			ctx.drawLineAtAngle(200, 10, -17*Math.PI/12, 350);
+			ctx.drawLine(108, 350-2, 292, 350-2);
 
 			//штрихи
 			ctx.lineWidth = 1;
-			ctx.strokeInMiddleOfSegment(30, 300, 200, 10, 10);
-			ctx.strokeInMiddleOfSegment(370, 300, 200, 10, 10);
+			ctx.strokeInMiddleOfSegment(108, 350-2, 200, 10, 10);
+			ctx.strokeInMiddleOfSegment(292, 350-2, 200, 10, 10);
 		};
 
 		NAtask.setTask({
