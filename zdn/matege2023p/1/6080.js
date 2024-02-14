ef0b9736910932ddae0e531a6aa36c015028fd5c
SugarHedgehog <sneginca.cat@gmail.com>
2024-02-14 11:56:40 +0300

    [zdn] [fix] - color&angle

--- zdn/matege2023p/1/6080.js
+++ zdn/matege2023p/1/6080.js
@@ -7,21 +7,28 @@
 		let paint1 = function(ctx) {
 			ctx.lineWidth = 2;
 
-			ctx.drawLine(10, 300, 390, 300);
-			ctx.drawLine(10, 300, 150, 100);
-			ctx.drawLine(150, 100, 390, 300);
-			ctx.drawLine(150, 100, 390 / 2, 300);
-			ctx.drawLine(150, 100, 150, 300);
+			let arc = Math.PI/2.9;
+			
+			ctx.strokeStyle = om.secondaryBrandColors.iz();
+			ctx.drawLine(10, 250, 390-8, 250);
+			let ver = ctx.drawLineAtAngle(10, 250, -arc, 200-25);
+			ctx.drawLineAtAngle(ver.x, ver.y, -arc+Math.PI/2, 350-20);
+			//штрихи
+			ctx.strokeInMiddleOfSegment(10, 250, (390-8)/2, 250, 10);
+			ctx.strokeInMiddleOfSegment(10, 250, 3*(390-8)/2, 250, 10);
+			
+			//медиана
+			ctx.strokeStyle = om.primaryBrandColors [0];
+			ctx.drawLine(ver.x, ver.y,(390-8)/2, 250);
+			
+			//высота
+			ctx.strokeStyle = om.primaryBrandColors [1];
+			ctx.drawLine(ver.x, ver.y,ver.x, 250);
 
 			//прямой угол
 			ctx.lineWidth = 1.2;
-			ctx.drawLine(150 - 20, 300, 150 - 20, 300 - 20);
-			ctx.drawLine(150, 300 - 20, 150 - 20, 300 - 20);
-
-			//штрихи
-			ctx.drawLine(390 / 4, 300 - 10, 390 / 4, 300 + 10);
-			ctx.drawLine(3 * 390 / 4, 300 - 10, 3 * 390 / 4, 300 + 10);
-			ctx.stroke();
+			ctx.drawLine(ver.x - 20, 250, ver.x - 20, 250 - 20);
+			ctx.drawLine(ver.x, 250 - 20, ver.x - 20, 250 - 20);
 
 		};
 
