eabc3d2ef4b6be97f029591e367d13020a15d178
SugarHedgehog <sneginca.cat@gmail.com>
2024-02-17 23:18:49 +0300

    [zdn] [fix] - color

--- zdn/matege2023p/1/5941.js
+++ zdn/matege2023p/1/5941.js
@@ -11,6 +11,7 @@
 
 		let paint1 = function(ctx) {
 			ctx.lineWidth = 2;
+			ctx.strokeStyle = om.secondaryBrandColors.iz();
 			ctx.drawLine(50, 80, 350, 80);
 			ctx.drawLine(10, 320, 310, 320);
 			ctx.drawLine(50, 80, 10, 320);
