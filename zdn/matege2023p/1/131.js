29fd0a006245d1f90122b93d6cbe9d40de7e7980
SugarHedgehog <sneginca.cat@gmail.com>
2024-02-18 17:28:01 +0300

    [zdn] [fix] - color&letter

--- zdn/matege2023p/1/131.js
+++ zdn/matege2023p/1/131.js
@@ -6,42 +6,36 @@
 		let a = sl(2, 89);
 		let b = sl(90, 179);
 
-		let vertices = [];
-		do {
-			let a = slLetter(vertices).toUpperCase();
-			if (!vertices.includes(a))
-				vertices.push(a);
-		}
-		while (vertices.length < 3);
+		let vertices = om.latbukv.iz(3);
 
 		let paint1 = function(ctx) {
 			ctx.lineWidth = 2;
-			ctx.beginPath();
-			ctx.arc(200, 200, 180, 0, 2 * Math.PI);
-			ctx.stroke();
+
+			ctx.strokeStyle = om.secondaryBrandColors.iz();
+			ctx.drawArc(200, 200, 170, 0, 2 * Math.PI);
 
 			//вписанный
-
-			ctx.drawLine(200 + 180, 200 + 10, 200 - 80, 200 - 160);
-			ctx.drawLine(200 - 180, 200 + 10, 200 - 80, 200 - 160);
+			ctx.strokeStyle = om.primaryBrandColors.iz();
+			ctx.drawLine(200 + 170, 200 + 10, 200 - 80, 200 - 150);
+			ctx.drawLine(200 - 170, 200 + 10, 200 - 80, 200 - 150);
 
 			ctx.drawFilledCircle(200, 200, 2);
 
 			ctx.font = "23px liberation_sans";
-			ctx.fillText(vertices[0], 200 - 180 - 20, 200 + 10);
-			ctx.fillText(vertices[1], 200 + 180 + 5, 200 + 10);
-			ctx.fillText(vertices[2], 200 - 90, 200 - 170);
+			ctx.fillText(vertices[0], 200 - 180 - 10, 200 + 10);
+			ctx.fillText(vertices[1], 200 + 170, 200 + 10);
+			ctx.fillText(vertices[2], 200 - 90, 200 - 160);
 
 		};
 
 		NAtask.setTask({
 			text: 'На окружности отмечены точки $' + vertices[0] + '$, $' + vertices[1] + '$ и $' + vertices[2] + '$. ' +
-				'Дуга окружности $' + [vertices[0], vertices[2]].shuffle().join('') + '$, не содержащая точку $' + vertices[1] +
+				'Дуга окружности $' + [vertices[0], vertices[2]].shuffleJoin() + '$, не содержащая точку $' + vertices[1] +
 				'$, составляет $' + a + '^\\circ$. ' +
-				'Дуга окружности $' + vertices.slice(1, 4).shuffle().join('') + '$, не содержащая точку $' + vertices[0] +
+				'Дуга окружности $' + vertices.slice(1, 4).shuffleJoin()  + '$, не содержащая точку $' + vertices[0] +
 				'$, составляет $' + b +
 				'^\\circ$. ' +
-				'Найдите вписанный угол $' + [vertices[0], vertices[2], vertices[1]].join('') + '$. Ответ дайте в градусах.',
+				'Найдите вписанный угол $' + [vertices[0], vertices[2], vertices[1]].randomReverse().join('') + '$. Ответ дайте в градусах.',
 			answers: (360 - a - b) / 2,
 		});
 		NAtask.modifiers.addCanvasIllustration({
