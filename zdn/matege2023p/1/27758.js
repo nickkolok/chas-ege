1e3b719fc22f12246baee9299050588603d1642c
SugarHedgehog <sneginca.cat@gmail.com>
2024-02-18 11:42:00 +0300

    [zdn] [fix] - beauty paint

--- zdn/matege2023p/1/27758.js
+++ zdn/matege2023p/1/27758.js
@@ -7,65 +7,37 @@
 
 		genAssert(180 - a - 2 * b > 0, 'Слишком большие углы')
 
-		let vertices = [];
-		do {
-			let a = slLetter(vertices).toUpperCase();
-			if (!vertices.includes(a))
-				vertices.push(a);
-		}
-		while (vertices.length < 4);
+		let vertices = om.latbukv.iz(4);
 
 		let paint1 = function(ctx) {
 			ctx.lineWidth = 2;
+			ctx.strokeStyle = om.secondaryBrandColors.iz();
 
-			ctx.drawLine(10, 370, 390, 370);
-			ctx.drawLine(10, 370, 150, 70);
-			ctx.drawLine(150, 70, 390, 370);
+			ctx.drawLine(10, 370, 380, 370);
+			let ver = ctx.drawLineAtAngle(10, 370, -Math.PI / 2.8, 325);
+			ctx.drawLine(ver.x, ver.y, 380, 370);
 
-			ctx.drawLine(10, 370, 270 - 2, 220 - 5);
-			//ctx.drawLine(79 + 4, 220 - 10, 390, 370);
+			ctx.strokeStyle = om.primaryBrandColors[0];
+			ctx.drawLineAtAngle(10, 370, -0.5 * Math.PI / 2.8, 300 - 6);
 
-			ctx.beginPath();
-			//ctx.arc(183, 270, 30, 0.10 * Math.PI, -1.13 * Math.PI);
-			ctx.stroke();
-
-			ctx.beginPath();
-			ctx.arc(10, 370, 30, 0, -0.17 * Math.PI, true);
-			ctx.stroke();
-			ctx.beginPath();
-			ctx.arc(10, 370, 25, -0.17 * Math.PI, -0.34 * Math.PI, true);
-			ctx.stroke();
-
-			ctx.beginPath();
-			//ctx.arc(390, 370, 30, -Math.PI, -0.85 * Math.PI);
-			ctx.stroke();
-			ctx.beginPath();
-			//ctx.arc(390, 370, 35, -Math.PI, -0.85 * Math.PI);
-			ctx.stroke();
-
-			ctx.beginPath();
-			//ctx.arc(390, 370, 40, -0.85 * Math.PI, -0.72 * Math.PI);
-			ctx.stroke();
-			ctx.beginPath();
-			//ctx.arc(390, 370, 45, -0.85 * Math.PI, -0.72 * Math.PI);
-			ctx.stroke();
-
+			ctx.strokeStyle = om.primaryBrandColors[1];
+			ctx.drawArc(10, 370, 40, 0, -0.5 * Math.PI / 2.8, true);
+			ctx.drawArc(10, 370, 30, -0.5 * Math.PI / 2.8, -Math.PI / 2.8, true);
 
 			ctx.font = "23px liberation_sans";
-			ctx.fillText(vertices[0], 150, 70 - 10);
+			ctx.fillText(vertices[0], ver.x - 5, ver.y - 5);
 			ctx.fillText(vertices[1], 10 - 5, 370 + 20);
-			ctx.fillText(vertices[2], 390 - 20, 370 + 20);
+			ctx.fillText(vertices[2], 380, 370 + 20);
 
-			ctx.fillText(vertices[3], 270 + 10, 220 + 5);
+			ctx.fillText(vertices[3], 270 - 2, 220 - 5);
 
 		};
 
 		NAtask.setTask({
-			text: 'В треугольнике $' + vertices.slice(0, 3).join('') + '$ $' + [vertices[1], vertices[3]].shuffle().join('') +
-				'$ – биссектриса, ' +
-				'угол $' + vertices[2] + '$ равен $' + a + '^\\circ$, угол $' + vertices.slice(1, 4).permuteCyclic(1).join('') +
-				'$ равен $' + b + '^\\circ$.' +
-				'Найдите угол $' + vertices[0] + '$. Ответ дайте в градусах.',
+			text: 'В треугольнике $' + vertices.slice(0, 3).shuffleJoin() + '$ $' + [vertices[1], vertices[3]].shuffleJoin() +
+				'$ – биссектриса, ' + 'угол $' + vertices[2] + '$ равен $' + a + 
+				'^\\circ$, угол $' + vertices.slice(1, 4).permuteCyclic(1).randomReverse().join('') +
+				'$ равен $' + b + '^\\circ$. Найдите угол $' + vertices[0] + '$. Ответ дайте в градусах.',
 			answers: 180 - a - 2 * b,
 			analys: '',
 		});
