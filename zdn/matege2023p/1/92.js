317764497832e094c0e1896ee730478c0281073a
SugarHedgehog <sneginca.cat@gmail.com>
2024-02-18 11:22:48 +0300

    [zdn] [fix] - beauty paint

--- zdn/matege2023p/1/92.js
+++ zdn/matege2023p/1/92.js
@@ -4,46 +4,40 @@
 
 		let a = sl(2, 89);
 
-		let vertices = [];
-		do {
-			let a = slLetter(vertices).toUpperCase();
-			if (!vertices.includes(a))
-				vertices.push(a);
-		}
-		while (vertices.length < 5);
+		let vertices = om.latbukv.iz(5);
 
-		let trapezh = [vertices[0], vertices[1], vertices[4], vertices[3]];
-
-		if (sl1())
-			trapezh = trapezh.reverse();
+		let trapezoid = [vertices[0], vertices[1], vertices[4], vertices[3]].randomReverse();
 
 		let paint1 = function(ctx) {
 			ctx.lineWidth = 2;
+			ctx.strokeStyle = om.secondaryBrandColors.iz();
+
 			ctx.drawLine(100, 80, 350, 80);
 			ctx.drawLine(10, 320, 270, 320);
 			ctx.drawLine(100, 80, 10, 320);
 			ctx.drawLine(270, 320, 350, 80);
 
-			ctx.drawLine(10, 320, 310, 200);
+			ctx.strokeStyle = om.primaryBrandColors[0];
+			let middle = coordinatesMiddleOfSegment(270, 320, 350, 80);
+			ctx.drawLine(10, 320, middle[0], middle[1]);
 
-			ctx.lineWidth = 1;
-			ctx.drawLine(300 + 15, 150, 320 + 10, 160);
-			ctx.drawLine(270 + 15, 250, 290 + 10, 260);
+			ctx.strokeStyle = om.primaryBrandColors[1];
+			ctx.strokeInMiddleOfSegment(350, 80, middle[0], middle[1], 10);
+			ctx.strokeInMiddleOfSegment(270, 320, middle[0], middle[1], 10);
 
 			ctx.font = "23px liberation_sans";
 			ctx.fillText(vertices[0], 100, 80 - 10);
 			ctx.fillText(vertices[1], 350, 80 - 10);
 			ctx.fillText(vertices[2], 270, 320 + 20);
 			ctx.fillText(vertices[3], 10, 320 + 20);
-			ctx.fillText(vertices[4], 310 + 10, 200 + 10);
+			ctx.fillText(vertices[4], middle[0] + 5, middle[1] + 5);
 
 		};
 
 		NAtask.setTask({
-			text: 'Площадь параллелограмма $' + vertices.slice(0, 4).linalRand(sl(1, 3)).join('') + '$ равна $' + a + '$. ' +
-				'Точка $' + vertices[4] + '$ – середина стороны $' + vertices.slice(1, 3).shuffle().join('') +
-				'$. Найдите площадь ' +
-				'трапеции $' + trapezh.linalRand(sl(0, 3)).join('') + '$.',
+			text: 'Площадь параллелограмма $' + vertices.slice(0, 4).permuteCyclic(sl(0, 3)).join('') + '$ равна $' + a +
+				'$. Точка $' + vertices[4] + '$ – середина стороны $' + vertices.slice(1, 3).shuffleJoin() +
+				'$. Найдите площадь трапеции $' + trapezoid.permuteCyclic(sl(0, 3)).join('') + '$.',
 			answers: a * 0.75,
 		});
 		NAtask.modifiers.addCanvasIllustration({
