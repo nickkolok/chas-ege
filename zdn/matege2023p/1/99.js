19020800d170d2c690f44934ac86a075a24091bc
SugarHedgehog <sneginca.cat@gmail.com>
2024-02-18 17:37:23 +0300

    [zdn] [fix] - shuffleJoin&symmetry

--- zdn/matege2023p/1/99.js
+++ zdn/matege2023p/1/99.js
@@ -14,38 +14,38 @@
 
 			ctx.strokeStyle = om.secondaryBrandColors.iz();
 			ctx.drawLine(10, 370, 390, 370);
-			ctx.drawLine(10, 370, 180, 50);
-			ctx.drawLine(180, 50, 390, 370);
+			ctx.drawLine(10, 370, 190, 50);
+			ctx.drawLine(190, 50, 390, 370);
 
 			ctx.strokeStyle = om.primaryBrandColors.iz();
-			ctx.drawLine(180, 50, 180, 370);
+			ctx.drawLine(190, 50, 190, 370);
 
 			//прямой угол
 			ctx.lineWidth = 1;
-			ctx.drawLine(180, 370 - 20, 180 + 20, 370 - 20);
-			ctx.drawLine(180 + 20, 370, 180 + 20, 370 - 20);
+			ctx.drawLine(190, 370 - 20, 190 + 20, 370 - 20);
+			ctx.drawLine(190 + 20, 370, 190 + 20, 370 - 20);
 
 			//штрихи
 
-			ctx.strokeInMiddleOfSegment(10, 370, 180, 50, 10);
-			ctx.strokeInMiddleOfSegment(180, 50, 390, 370, 10);
+			ctx.strokeInMiddleOfSegment(10, 370, 190, 50, 10);
+			ctx.strokeInMiddleOfSegment(190, 50, 390, 370, 10);
 
 			ctx.font = "23px liberation_sans";
 			ctx.fillText(vertices[0], 10 - 5, 370 + 25);
-			ctx.fillText(vertices[1], 180, 40);
+			ctx.fillText(vertices[1], 190-5, 40);
 			ctx.fillText(vertices[2], 390 - 10, 370 + 25);
-			ctx.fillText(vertices[3], 180, 370 + 25);
+			ctx.fillText(vertices[3], 190, 370 + 25);
 
 		};
 
 		NAtask.setTask({
-			text: 'В треугольнике  $' + vertices.slice(0, 3).shuffle().join('') + '$ $' +
-				vertices.slice(0, 2).shuffle().join('') +
-				'=' + vertices.slice(1, 3).shuffle().join('') + '$, высота $' + [vertices[1], vertices[3]].shuffle().join('') +
+			text: 'В треугольнике  $' + vertices.slice(0, 3).shuffleJoin() + '$ $' +
+				vertices.slice(0, 2).shuffleJoin() +
+				'=' + vertices.slice(1, 3).shuffleJoin() + '$, высота $' + [vertices[1], vertices[3]].shuffleJoin() +
 				'$ равна $' + a + '$, $\\cos{' + [vertices[0], vertices[2]].iz() + '} = ' + b.ts(1) +
 				' $. Найдите $' + [vertices.slice(0, 2),
 					vertices.slice(1, 3)
-				].iz().shuffle().join('') + '$.',
+				].iz().shuffleJoin() + '$.',
 			answers: a / (1 - b.pow(2)).sqrt(),
 			analys: '',
 		});
