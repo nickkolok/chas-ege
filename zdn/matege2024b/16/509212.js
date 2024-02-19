a4d1f54cfaeddb939e6dec064e3bfac5ed8c3a53
Алендарь Сергей <sergey.alendar@mail.ru>
2024-02-19 18:12:03 +0300

    Файл 509212.js изменён

--- zdn/matege2024b/16/509212.js
+++ zdn/matege2024b/16/509212.js
@@ -1,8 +1,8 @@
 ﻿(function() {
 	retryWhileError(function() {
 		'use strict';
-		let a = sl(2, 6);
-		let b = sl(2, 8);
+		let a = sl(2, 8);
+		let b = slKrome(a, 2, 8)
 		NAtask.setEvaluationTask({
 			expr: 'sqrt(' + a + '^' + sl(2, 6, 2) + '*' + b + '^' + sl(2, 6, 2) + ')',
 			//forbiddenAnswers: [0],
