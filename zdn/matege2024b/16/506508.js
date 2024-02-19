08b544922a97b6d82af52c1db0fc1223445e0d74
Алендарь Сергей <sergey.alendar@mail.ru>
2024-02-19 20:08:58 +0300

    Файл 506508.js  изменён

--- zdn/matege2024b/16/506508.js
+++ zdn/matege2024b/16/506508.js
@@ -1,11 +1,12 @@
-﻿(function() {
+﻿
+(function() {
 	retryWhileError(function() {
 		'use strict';
-		let a = sl(2, 6);
-		let b = sl(4, 10, 2);
+		let a = sl(2, 10);
+		let b = sl(3, 9);
 		let c = a * b * b;
 		NAtask.setEvaluationTask({
-			expr: (sl(-1, 3, 2) + b) + '/' + b / sl(1, 2) + 'sqrt(' + a + ')' + '*' + 'sqrt(' + c + ')',
+			expr: slKrome(b, 1, 5).pm() + '/' + b + ['sqrt(' + a + ')', 'sqrt(' + c + ')'].shuffle().join('*'),
 			//forbiddenAnswers: [0],
 			authors: ['Алендарь Сергей'],
 		});
