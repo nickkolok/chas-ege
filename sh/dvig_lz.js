4060408c88e8dfc518b1679e3dfcd0a28b37ac7b
NickKolok <nickkolok@mail.ru>
2024-02-28 23:15:35 +0300

    [parser] Preserve brackets

--- sh/dvig_lz.js
+++ sh/dvig_lz.js
@@ -1,9 +1,9 @@
 'use strict';
 
 //{{ Новый, даже работающий, парсер линейных задач
-let punctuation = [',', '.', ':', ';', '?', '!', '(', ')'];
+let punctuation = [',', '.', ':', ';', '?', '!', '(', ')', '[', ']'];
 function splitTextToLexems(text) {
-	var lexemArray = text.split(/[\[\]' '°]/);
+	var lexemArray = text.split(/[' '°]/);
 	//отделение от слов символов пунктуации
 	for (let i = 0; i < lexemArray.length; i++) {
 		if (lexemArray[i].length > 1 && punctuation.includes(lexemArray[i].slice(-1))) {
