09be65d3c903b57caaef509de584fef2249d2c77
NickKolok <nickkolok@mail.ru>
2024-03-02 00:42:19 +0300

    [zdn] setEvaluationTask supports sl() as a mathjs function - example

--- zdn/matoge2024/8/412184.js
+++ zdn/matoge2024/8/412184.js
@@ -3,7 +3,7 @@
 		'use strict';
 
 		NAtask.setEvaluationTask({
-			expr: 'a^' + sl(4,21) + ' * divideColon(a^' + sl(4,21).pm() + ',a^' + sl(4,21) + ')',
+			expr: 'a^sl(4,21) * divideColon(a^' + sl(4,21).pm() + ',a^sl(4,21))',
 			variables: {a : sl(sl(2,7),9)}, // Смещаемся от вечных двоек
 			//forbiddenAnswers: [0],
 			authors: ['Николай Авдеев'],
