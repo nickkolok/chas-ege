ea0703721a6d6a75afc98cf4ff007c34922e2a8d
Алендарь Сергей <sergey.alendar@mail.ru>
2024-03-05 19:43:06 +0300

    файл 1397.js изменён

--- zdn/matege2024b/16/1397.js
+++ zdn/matege2024b/16/1397.js
@@ -2,11 +2,12 @@
 	retryWhileError(function() {
 		'use strict';
 		NAtask.setEvaluationTask({
-			expr: 'divideColon(' + sl(0.01, 0.1, 0.09) + '^' + sl(2, 6) + '*10^' + sl(2, 6) + ',' + sl(2, 9) + '^' + sl(-1, -5) + ')',
+			expr: 'divideColon(' + [0.01, 0.1].iz() + '^' + sl(2, 6) + '*10^' + sl(2, 6) + ',' + sl(2, 9) + '^' + sl(-1, -5) +
+				')',
 			//forbiddenAnswers: [0],
 			authors: ['Алендарь Сергей'],
 		});
 	}, 10000);
 })();
-//1397
+//509707
 
