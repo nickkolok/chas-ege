0d7b532dc023a609bdd30fe3bcaa6c0e926911fb
Алендарь Сергей <sergey.alendar@mail.ru>
2024-03-10 12:30:28 +0300

    Файл 509607.js изменён

--- zdn/matege2024b/16/509607.js
+++ zdn/matege2024b/16/509607.js
@@ -4,7 +4,7 @@
 		let c = sl(2, 20);
 		let znak = (1).pm();
 		NAtask.setEvaluationTask({
-			expr: ['(' + c + '^' + znak * sl(1, 10) + '*' + c + '^' + -znak * sl(1, 10) + ')', c + '^' + sl(1, 10).pm()].shuffle()
+			expr: ['(' + c + '^' + znak * sl(1, 10) + '*' + c + '^' + (-znak) * sl(1, 10) + ')', c + '^' + sl(1, 10).pm()].shuffle()
 				.join('/'),
 			//forbiddenAnswers: [0],
 			authors: ['Алендарь Сергей'],
