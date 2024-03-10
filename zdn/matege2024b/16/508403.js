c779a625707304cad9a97dc33780f2cb92b12527
Алендарь Сергей <sergey.alendar@mail.ru>
2024-03-10 14:36:26 +0300

    Файл 508403.js изменён

--- zdn/matege2024b/16/508403.js
+++ zdn/matege2024b/16/508403.js
@@ -3,10 +3,10 @@
 		'use strict';
 		let a = sl(2, 9);
 		let b = sl(1, 9).pm();
-		let c = slKrome([b, 0, 1], 2, 9);
-		let d = slKrome([b, c, 0, 1], 2, 9);
+		let c = slKrome([b, 0, 1, -1], -9, 9);
+		let d = slKrome([b, c, 0, 1, -1], -9, 9);
 		NAtask.setEvaluationTask({
-			expr: a + '^' + b + '*(' + a + '^' + c + '/' + a + '^' + d + ')',
+			expr: [a + '^' + b, '(' + a + '^' + c + '/' + a + '^' + d + ')'].shuffle().join('*'),
 			//forbiddenAnswers: [0],
 			authors: ['Алендарь Сергей'],
 		});
