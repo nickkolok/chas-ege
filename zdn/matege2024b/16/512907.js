32e9bc53ee171879555b719a7f207f8b35d96293
Алендарь Сергей <sergey.alendar@mail.ru>
2024-03-10 14:58:01 +0300

    Файл 512907.js изменён

--- zdn/matege2024b/16/512907.js
+++ zdn/matege2024b/16/512907.js
@@ -2,7 +2,8 @@
 	retryWhileError(function() {
 		'use strict';
 		NAtask.setEvaluationTask({
-			expr: [0.01, 0.1].iz() + '^' + sl(2, 5) + '/' + '10^' + sl(-1, -5) + '*10^' + sl(2, 6),
+			expr: ['(' + [0.01, 0.1].iz() + '^' + sl(2, 5) + '/' + '10^' + sl(-1, -5) + ')', '10^' + sl(2, 6)].shuffle().join(
+				'*'),
 			//forbiddenAnswers: [0],
 			authors: ['Алендарь Сергей'],
 		});
