50ef1f859361fcfe7f704b379bf3e6e9ff4969f6
Алендарь Сергей <sergey.alendar@mail.ru>
2024-03-11 14:10:30 +0300

    Файл 1414.js изменён

--- zdn/matege2024p/12/1414.js
+++ zdn/matege2024p/12/1414.js
@@ -1,10 +1,10 @@
 (function() {
 	'use strict';
 
-	var arr1 = ['максимумa', 'минимумa']
-	var arr2 = ['положительный', 'отрицательный']
-	var maxmin1 = sl(0, 1)
-	var maxmin2 = (maxmin1 + 1) % 2
+	var arr1 = ['максимумa', 'минимумa'];
+	var arr2 = ['положительный', 'отрицательный'];
+	var maxmin1 = sl(0, 1);
+	var maxmin2 = (maxmin1 + 1) % 2;
 
 
 	var ans = sl(2, 14, 2) / 10;
@@ -19,28 +19,37 @@
 
 	function plusmin(member) {
 		var el = math.parse(member);
-		return math.simplify(el).toTex();
+		el = math.simplify(el);
+		el = math.simplify(el, [{
+			l: 'n1*n2',
+			r: 'n1 n2'
+		}]);
+		return el.toTex({
+			implicit: 'hide'
+		});
 	}
 
 
 	chas2.task.setTask({
 		text: 'Найдите точку ' + arr1[maxmin1] + ' функции:' +
 			'$$  y=(' + plusmin(b + 'x-' + a) + ')' + '\\cos ' + plusmin('x-' + b) + '\\sin ' + plusmin('x+' + d) + ', $$' +
-			'принадлежащую промежутку $(0, \\frac{\\pi}{2} )$',
-		analys: '$$ y^{\'}=(' + plusmin(b + 'x-' + a) + ')^{\'}\\cos x+(' + plusmin(b + 'x-' + a) + ')(\\cos x)^{\'}+(' + -b + '\\sin x)^{\'}$$' +
-			'$$ y^{\'}=' + b + '\\cos x-(' + plusmin(b + 'x-' + a) + ')' + '\\sin ' + plusmin('x-' + b) + '\\cos x' + ' \\Rightarrow\\ ' +
+			'принадлежащую промежутку $(0; \\frac{\\pi}{2} )$',
+		analys: '$$ y^{\'}=(' + plusmin(b + 'x-' + a) + ')^{\'}\\cos x+(' + plusmin(b + 'x-' + a) + ')(\\cos x)^{\'}+(' +
+			-b + '\\sin x)^{\'}$$' +
+			'$$ y^{\'}=' + b + '\\cos x-(' + plusmin(b + 'x-' + a) + ')' + '\\sin ' + plusmin('x-' + b) + '\\cos x' +
+			' \\Rightarrow\\ ' +
 			'y^{\'}=-(' + plusmin(b + 'x-' + a) + ')\\sin x$$' +
 			'$$ y^{\'}=0' + ' \\Rightarrow\\ ' + '(' + plusmin(b + 'x-' + a) + ')\\sin x=0$$' +
-			'$$1)' + plusmin(b + 'x-' + a) + '=0' + ' \\Rightarrow\\ ' +
+			'$$1) \\quad' + plusmin(b + 'x-' + a) + '=0' + ' \\Rightarrow\\ ' +
 			b + 'x=' + a + ' \\Rightarrow\\ ' + 'x=' + ans + '$$' +
-			'$$2)\\sin x=0' + ' \\Rightarrow\\ ' + 'x=0' + '$$' +
-			'Промежутку $(0,\\frac{\\pi}{2}  )$ принадлежит только точка: "$' + ans +
-			'$". Подставляя любые значения из промежутка $(0,' + ans +
+			'$$2) \\quad \\sin x=0' + ' \\Rightarrow\\ ' + 'x=0' + '$$' +
+			'Промежутку $(0;\\frac{\\pi}{2}  )$ принадлежит только точка $' + ans +
+			'$. Подставляя любые значения из промежутка $(0;' + ans +
 			')$ в найденную производную, мы узнаём, что она имеет ' + arr2[maxmin1] +
-			' знак, а на промежутке $(' + ans + ', \\frac{\\pi}{2} )$ ' + arr2[maxmin2] +
+			' знак, а на промежутке $(' + ans + '; \\frac{\\pi}{2} )$ ' + arr2[maxmin2] +
 			'. Производная функции меняет знак с ' +
-			sklonlxkand(arr2[maxmin1]).re + ', на ' + arr2[maxmin2] + ',' + ' значит, "$' + ans +
-			'$" - это искомая точка ' + arr1[maxmin1] + '.',
+			sklonlxkand(arr2[maxmin1]).re + ' на ' + arr2[maxmin2] + ',' + ' значит $' + ans +
+			'$ - это искомая точка ' + arr1[maxmin1] + '.',
 
 		answers: ans,
 	});
