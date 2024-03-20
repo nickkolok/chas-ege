589994df9453682ff18fc1175360891ff53cd279
SugarHedgehog <sneginca.cat@gmail.com>
2024-03-20 14:17:26 +0300

    [zdn] [new] - newTask_4504

---
+++ zdn/matege2024p/2/4504.js
@@ -0,0 +1,19 @@
+(function() {
+	retryWhileError(function() {
+		NAinfo.requireApiVersion(0, 2);
+		
+		let lengthA = sl(1,100);
+		let lengthB = sl(1,100);
+		
+		let angle = [30,45,60,120,135,150].iz();
+		
+		NAtask.setTask({
+			text: 'Длины векторов $\\vec{a}$ и $\\vec{b}$ равны $'+lengthA.texsqrt(1)+'$ и $'+lengthB.texsqrt(1)+'$, а угол между ними равен $'+angle+'^\\circ$. Найдите скалярное произведение $\\vec{a}\\cdot\\vec{b}$.',
+			answers: (lengthA*lengthA).sqrt()*cosdeg(angle),
+			analys: '',
+		});
+	}, 1000);
+	NAtask.modifiers.multiplyAnswerBySqrt(13);
+})();
+//https://ege314.ru/2-vektory/reshenie-4504/
+//4504
