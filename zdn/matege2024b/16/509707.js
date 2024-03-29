6fe3ab1c38215140e674bcbcbd98ca726fbc09ca
Алендарь Сергей <sergey.alendar@mail.ru>
2024-03-29 12:37:44 +0300

    Файл 509707.js изменён

--- zdn/matege2024b/16/509707.js
+++ zdn/matege2024b/16/509707.js
@@ -2,8 +2,9 @@
 	retryWhileError(function() {
 		'use strict';
 		NAtask.setEvaluationTask({
-			expr: 'forceBrackets(' + [0.01, 0.1].iz() + ')^' + sl(2, 6) + '*' + 'divideColon(10^' + sl(2, 6) + ',' + sl(2,
-				9) + '^' + sl(-1, -5) + ')',
+			expr: 'forceBrackets(' + [0.01, 0.1].iz() + ')^' + sl(2, 6) + '*' +
+				'divideColon(10^' + sl(2, 6) + ',' +
+				sl(2, 9) + ' ^ ' + sl(-1, -5) + ')',
 			//forbiddenAnswers: [0],
 			authors: ['Алендарь Сергей'],
 		});
