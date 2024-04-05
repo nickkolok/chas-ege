07bcefc82f34d8c8f1ae978186e8d129d30cfe5e
Алендарь Сергей <sergey.alendar@mail.ru>
2024-04-05 15:39:51 +0300

    Файл 99585.js изменён

--- zdn/matege2023p/9/99585.js
+++ zdn/matege2023p/9/99585.js
@@ -1,20 +1,30 @@
-let a = sluchch(2, 10);
-let b = sluchch(2, 4);
-let n = sluchch(10, 24);
-let n2 = n - sluchch(5, 7);
-var arr = ['Лена', 'Ира', 'Настя', 'Маша', 'Катя', 'Вера', 'Саша', 'Оля', 'Даша', 'Тоня', 'Аня', 'Ира', 'Женя'];
-var rand = Math.floor(Math.random() * arr.length);
+(function() {
+	retryWhileError(function() {
+		'use strict';
 
-let progression = new ArithmeticProgression(a, b);
+		let a = sluchch(2, 10);
+		let b = sluchch(2, 4);
+		let n = sluchch(10, 24);
+		let n2 = n - sluchch(5, 7);
+		var arr = ['Лена', 'Ира', 'Настя', 'Маша', 'Катя', 'Вера', 'Саша', 'Оля', 'Даша', 'Тоня', 'Аня', 'Ира', 'Женя'].iz();
 
-NAtask.setTask({
-	text: sklonlxkand(arr[rand]).de + ' надо подписать ' + chislitlx(progression.sum(n), 'открытка', 'v') + 
-		'. Ежедневно она подписывает на одно и то же количество открыток больше по сравнению с предыдущим днем. ' +
-		'Известно, что за первый день ' + arr[rand] + ' подписала ' + chislitlx(a, 'открытка', 'v') + '. ' +
-		'Определите, сколько открыток было подписано за ' + om.porchisl[n2].i[0] + 
-		' день, если вся работа была выполнена за ' + chislitlx(n, 'день') + 
-		'.',
+		let progression = new ArithmeticProgression(a, b);
 
-	answers: progression.member(n2),
-});
+		function summa(n) {
+			return progression.sum(n);
+		}
+
+		NAtask.setTask({
+			text: sklonlxkand(arr).de + ' надо подписать ' + chislitlx(summa(n), 'открытка', 'v') +
+				'. Ежедневно она подписывает на одно и то же количество открыток больше по сравнению с предыдущим днем. ' +
+				'Известно, что за первый день ' + arr + ' подписала ' + chislitlx(a, 'открытка', 'v') + '. ' +
+				'Определите, сколько открыток было подписано за ' + om.porchisl[n2].i[0] +
+				' день, если вся работа была выполнена за ' + chislitlx(n, 'день') +
+				'.',
+
+			answers: progression.member(n2),
+
+		});
+	}, 10000);
+})();
 //Сергей Алендарь
