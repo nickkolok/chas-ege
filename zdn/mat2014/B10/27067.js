f446d852a215a4fe5125bcdd6b5cd45b1b57dc11
SugarHedgehog <sneginca.cat@gmail.com>
2024-04-17 11:08:12 +0300

    [zdn] [up] - refactoring

--- zdn/mat2014/B10/27067.js
+++ zdn/mat2014/B10/27067.js
@@ -1,147 +1,100 @@
 (function() {
-	'use strict';
-	NAinfo.requireApiVersion(0, 2);
+	retryWhileError(function() {
+		'use strict';
+		NAinfo.requireApiVersion(0, 2);
 
-	function toStandatForSqrt(a) {
-		do {
-			a.int *= a.sq.polnKvadrMnozh();
-			a.sq /= (a.sq).polnKvadrMnozh().pow(2);
-		} while (!a.sq.polnKvadrMnozh == 1);
-	}
-	let a = {
-		sq: sl(1, 50),
-		int: sl(1, 100)
-	};
+		let cube = new Cube(sl(1, 100));
+		let sphere = new Sphere(0.5 * cube.baseSide);
 
-	let informAboutCircle = ['радиусом', 'диаметром', 'объёмом', 'площадью поверхности'].iz();
-	let informAboutCube = ['объём', 'диагональ', 'площадь поверхности', 'площадь основания'];
-	if (informAboutCircle != 'диаметром' || informAboutCircle != 'радиусом')
-		informAboutCube.push('сторона');
-	informAboutCube = sklonlxkand(informAboutCube.iz());
-	let variableH = {
-		sq: 1,
-		int: 1
-	};
-	switch (informAboutCircle) {
-	case 'радиусом':
-		variableH.int = a.int / 2;
-		variableH.sq = a.sq;
-		break;
-	case 'диаметром':
-		variableH.int = a.int;
-		variableH.sq = a.sq;
-		break;
-	case 'объёмом':
-		variableH.int = (a.int.pow(3)) / 6;
-		variableH.sq = a.sq.pow(3);
-		break;
-	case 'площадью поверхности':
-		variableH.int = a.int.pow(2);
-		variableH.sq = a.sq.pow(2);
-		break;
-	}
-	let variableF = {
-		sq: 0,
-		int: 0
-	};
-	switch (informAboutCube.ie) {
-	case 'объём':
-		variableF.int = a.int.pow(3);
-		variableF.sq = a.sq.pow(3);
-		break;
-	case 'диагональ':
-		variableF.int = a.int;
-		variableF.sq = a.sq * 3;
-		break;
-	case 'сторона':
-		variableF.int = a.int;
-		variableF.sq = a.sq;
-		break;
-	case 'площадь поверхности':
-		variableF.int = 6 * a.int;
-		variableF.sq = a.sq;
-		break;
-	case 'площадь основания':
-		variableF.int = a.int.pow(2);
-		variableF.sq = a.sq.pow(2);
-		break;
-	}
-	if (!(1000 * variableH.int).isZ())
-		return;
-	toStandatForSqrt(a);
-	toStandatForSqrt(variableH);
-	toStandatForSqrt(variableF);
-	let answ = [
-		['целую часть', variableF.int]
-	];
-	if (variableF.sq != 1)
-		answ.push(['подкоренную часть', variableF.sq]);
-	answ = answ.iz();
-	let paint1 = function(ct) {
-		//график
-		ct.scale(20, 20);
-		ct.lineWidth = 0.1;
-		ct.translate(1, -1);
-		//шар
-		ct.strokeStyle = ' #0077b3';
-		for (let i = 0; i < 2 * Math.PI; i += 2 * Math.PI / 50) {
-			ct.beginPath();
-			ct.arc(7, 7, 4.5, i, i + Math.PI / 50, false);
-			ct.stroke();
-		}
-		for (let i = 0; i < 2 * Math.PI; i += 2 * Math.PI / 50) {
-			ct.beginPath();
-			ct.ellipse(7, 7, 4.5, 1, 0, i, i + Math.PI / 50);
-			ct.stroke();
-		}
-		ct.strokeStyle = 'black';
-		//вертикальные
-		ct.drawLine(0, 2, 0, 10);
-		ct.drawLine(3, 4, 3, 12);
-		ct.drawLine(13, 4, 13, 12);
-		ct.setLineDash([1, 0.5]);
-		ct.drawLine(10, 2, 10, 10);
-		ct.setLineDash([]);
-		//горизонтальные
-		ct.drawLine(0, 2, 10, 2);
-		ct.drawLine(3, 4, 13, 4);
-		ct.drawLine(3, 8, 13, 8);
-		ct.drawLine(3, 12, 13, 12);
-		ct.setLineDash([1, 0.5]);
-		ct.drawLine(0, 10, 10, 10);
-		ct.drawLine(0, 6, 10, 6);
-		ct.setLineDash([]);
-		//диагональные
-		ct.drawLine(0, 10, 3, 12);
-		ct.drawLine(0, 6, 3, 8);
-		ct.drawLine(0, 2, 3, 4);
-		ct.drawLine(10, 2, 13, 4);
-		ct.drawLine(10, 6, 13, 8);
-		ct.setLineDash([1, 0.5]);
-		ct.drawLine(10, 10, 13, 12);
-	};
-	
-	NAtask.setTask({
-		text: ' Прямоугольный параллелепипед описан около сферы с ' + 
-		informAboutCircle +
-			' $' + variableH.int.ts() + 
-			('\\sqrt{' + variableH.sq + '}').esli(variableH.sq != 1) +
-			('\\pi').esli(informAboutCircle == 'объёмом' || 
-			informAboutCircle == 'площадью поверхности') +
-			 '$.' +
-			' Найдите его ' + (informAboutCube.ve).replace('основанию', 
-			'основания') +
-			'. Если ответ содержит неизвлекаемый корень, запишите его ' 
-			+ answ[0] + '.',
-		answers: answ[1],
-		analys: '$ a= ' + a.int + '\\sqrt{' + a.sq + '} $ <br>' +
-			'$ answ= ' + variableF.int + '\\sqrt{' + variableF.sq + 
-			'} $ <br>',
-	});
-	chas2.task.modifiers.addCanvasIllustration({
-		width: 300,
-		height: 300,
-		paint: paint1,
-	});
+		let strok = [5, 4];
+
+		let matrixCube = [
+			[strok],
+			[0, 1],
+			[strok, 0, 1],
+			[0, 0, 0, 1],
+			[strok, 0, 0, 0, 1],
+			[0, 1, 0, 0, 0, 1],
+			[0, 0, 1, 0, 1, 0, 1],
+		];
+
+		let camera = {
+			x: 0,
+			y: 0,
+			z: 0,
+			scale: 1,
+
+			rotationX: -Math.PI / 2 + Math.PI / 15,
+			rotationY: 0,
+			rotationZ: 2 * Math.PI / 3,
+		};
+
+		let point2DCube = cube.verticesOfFigure.map((coord3D) => project3DTo2D(coord3D, camera));
+
+		autoScale(cube, camera, point2DCube, {
+			startX: -180,
+			finishX: 160,
+			startY: -160,
+			finishY: 160,
+			maxScale: 100,
+			step: 0.5,
+		});
+
+		point2DCube = cube.verticesOfFigure.map((coord3D) => project3DTo2D(coord3D, camera));
+
+		let paint1 = function(ctx) {
+			let h = 400;
+			let w = 400;
+			ctx.translate(h / 2, w / 2);
+			ctx.lineWidth = 2;
+			ctx.strokeStyle = om.secondaryBrandColors;
+			ctx.drawFigure(point2DCube, matrixCube);
+
+			ctx.strokeStyle = om.primaryBrandColors.iz();
+			ctx.setLineDash([4, 5]);
+			ctx.drawArc(0, 0, point2DCube[1].y, 0, Math.PI * 2, true); // Внешняя окружность
+
+			ctx.beginPath();
+			ctx.ellipse(0, 0, point2DCube[1].y, 20, 0, Math.PI * 2, 0); // Внешняя окружность
+			ctx.stroke();
+		};
+
+		NAtask.setTask({
+			text: 'Прямоугольный параллелепипед описан около сферы c ' + [
+				['радиусом', sphere.radius],
+				['диаметром', sphere.diameter],
+				['объёмом', sphere.volume],
+				['площадью поверхности', sphere.surfaceArea],
+				['площадью большого круга', sphere.areaGreatCircle],
+			].iz().join(' $') + '$. Найдите его ',
+			questions: [{
+				text: 'площадь поверхности',
+				answers: cube.surfaceArea,
+			}, {
+				text: 'объём',
+				answers: cube.volume,
+			}, {
+				text: 'диагональ',
+				answers: cube.mainDiagonal
+			}, {
+				text: 'диагональ его грани',
+				answers: cube.DWDiagonal
+			}, {
+				text: 'площадь основания',
+				answers: cube.baseArea,
+			}, ],
+			postquestion: '.',
+			analys: '',
+			author: ['Суматохина Александра']
+		});
+		NAtask.modifiers.multiplyAnswerBySqrt(12);
+		NAtask.modifiers.allDecimalsToStandard(true);
+		NAtask.modifiers.assertSaneDecimals();
+		NAtask.modifiers.addCanvasIllustration({
+			width: 400,
+			height: 400,
+			paint: paint1,
+		});
+	}, 100000);
 })();
 // 27067 5065
