fd358d77638bb0322c3bb0755f634c45c7aa3ece
SugarHedgehog <sneginca.cat@gmail.com>
2024-05-02 19:08:04 +0300

    Squashed commit of the following:
    commit 85c4b8e3f70ca0d2833113029314c69a3dc0ed02
    Author: SugarHedgehog <sneginca.cat@gmail.com>
    Date:   Fri Apr 19 17:00:29 2024 +0300
    
        [zdn] [fix] - update
    
    commit a3d6a37385e6cb86e4abe02cf68667e87ad34baf
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Wed Mar 29 11:54:45 2023 +0300
    
        [misc] [fix] - apothem &verbs
    
    commit 319a538c99e6e20f470a0e2c6e77cab48d5528b3
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Tue Mar 28 09:57:32 2023 +0300
    
        [zdn] [fix] - main and fipi
    
    commit aa75ea132c32e3ef878369296b378a283ab895e7
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Tue Mar 28 09:56:47 2023 +0300
    
        [zdn] [new] - newTask_27069

--- zdn/matege2023p/2/27069.js
+++ zdn/matege2023p/2/27069.js
@@ -1,71 +1,72 @@
 (function() {
 	retryWhileError(function() {
 		NAinfo.requireApiVersion(0, 2);
-		let edgeOfBase = sl(1, 20);
-		let height = sl(1, 20);
-		let numberOfEdge = [3, 3, 4, 6].iz();
-		let apothem = [(height.pow(2) + edgeOfBase.pow(2) / 12).sqrt(), (height.pow(2) + 0.25 * edgeOfBase.pow(2)).sqrt(),
-			0, (height.pow(2) + 0.75 * (edgeOfBase).pow(2)).sqrt()
-		][numberOfEdge - 3];
+
+		let pyr = new RegularPyramid({
+			height: sl(30, 50),
+			baseSide: sl(20, 50),
+			numberSide: sl(3, 10)
+		});
 
 		let question = [
-			['площадь основания', 0.25 * numberOfEdge * edgeOfBase.pow(2) * (Math.PI / numberOfEdge).ctg()],
-			['площадь боковой поверхности', 0.5 * numberOfEdge * edgeOfBase * apothem],
-			['апофема', apothem],
-			['высота', height],
-			['сторона основания', edgeOfBase],
+			['площадь основания', pyr.baseArea],
+			['площадь боковой поверхности', pyr.sideSurfaceArea],
+			['апофема', pyr.apothem],
+			['высота', pyr.height],
+			['сторона основания', pyr.baseSide],
 		].iz(3);
 
 		let name = question.T()[0];
 		let number = question.T()[1];
 
-		number.forEach(element => {
-			genAssert((element * 1000).isZ(), 'Значение не целое');
+		let camera = {
+			x: 0,
+			y: 0,
+			z: 0,
+			scale: 1,
+
+			rotationX: -Math.PI / 2 + Math.PI / 13,
+			rotationY: 0,
+			rotationZ: -2 * Math.PI / (pyr.numberSide - 2),
+		};
+
+		let point2DPyr = pyr.verticesOfFigure.map((coord3D) => project3DTo2D(coord3D, camera));
+
+		autoScale(pyr, camera, point2DPyr, {
+			startX: -390 / 2,
+			finishX: 390 / 2,
+			startY: -390 / 2,
+			finishY: 390 / 2,
+			maxScale: 50,
 		});
-		let paint1 = function(ct) {
-			ct.translate(30, 60);
-			ct.scale(15, -15);
-			ct.lineWidth = 2 / 20;
-			ct.strokeStyle = "#809DF2";
-			switch (numberOfEdge) {
-			case 3:
-				ct.rightPyramid3({
-					edge: 17,
-					angle: Math.PI / 8,
-					height: 13,
-				}, [1], [0.5, 0.2], name.includes('высота'), name.includes('апофема'));
-				break;
-			case 4:
-				ct.rightPyramid4({
-					edge: 13,
-					angle: Math.PI / 4,
-					height: 14,
-				}, [1, 3], [0.5, 0.2], name.includes('высота'), name.includes('апофема'));
-				break;
-			case 6:
-				ct.translate(1, -13);
-				ct.rightPyramid6({
-					edge: 7,
-					angle: Math.PI / 3,
-					height: 14,
-				}, [0, 2, 4, 7, 9], [0.5, 0.2], name.includes('высота'), name.includes('апофема'));
-				break;
-			}
+
+		point2DPyr = pyr.verticesOfFigure.map((coord3D) => project3DTo2D(coord3D, camera));
+
+		let paint1 = function(ctx) {
+			let h = 400;
+			let w = 400;
+
+			ctx.translate(w / 2, h / 2);
+			ctx.lineWidth = 2;
+			ctx.strokeStyle = om.secondaryBrandColors;
+
+			ctx.drawFigure(point2DPyr, pyr.connectionMatrix);
+
 		};
 
 		NAtask.setTask({
-			text: 'В правильной ' + ['тре', 'четырёх', '', 'шести'][numberOfEdge - 3] +
-				'угольной пирамиде ' + name[0] + [' составляет ', ' равна '].iz() + number[0] + '; ' + name[1] + [
-					' составляет ', ' равна '
-				].iz() + number[1] +
-				'. Чему равна ' + name[2] + ' пирамиды?',
+			text: 'В правильной ' + ['тре', 'четырёх', 'пяти', 'шести', 'семи', 'восьми', 'девяти', 'десяти'][pyr.numberSide - 3] +
+				'угольной пирамиде ' + name[0] + [' составляет ', ' равна '].iz() + '$' + number[0] + '$; ' + name[1] + 
+				[' составляет ', ' равна '].iz() + '$' + number[1] +
+				'$. Чему равна ' + name[2] + ' пирамиды?',
 			answers: number[2],
 		});
+		NAtask.modifiers.allDecimalsToStandard(true);
+		NAtask.modifiers.assertSaneDecimals();
 		NAtask.modifiers.addCanvasIllustration({
 			width: 400,
 			height: 400,
 			paint: paint1,
 		});
 	}, 10000);
-
 })();
