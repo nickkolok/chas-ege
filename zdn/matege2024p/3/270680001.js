0efaf5c0128cf63c6d40e6a9525029faa61074b2
SugarHedgehog <sneginca.cat@gmail.com>
2024-05-09 14:34:52 +0300

    [zdn] [up] - 27068

--- zdn/matege2024p/3/270680001.js
+++ zdn/matege2024p/3/270680001.js
@@ -2,56 +2,90 @@
 	retryWhileError(function() {
 		lx_declareClarifiedPhrase('площадь', 'боковой поверхности');
 		NAinfo.requireApiVersion(0, 2);
-		
-		let v1 = 0;
-		let rand = 0;
-		
+
+		let prism = new IrregularTriangularPrism({
+			height: sl(10, 20),
+			sideA: sl(10, 20),
+			sideB: sl(15, 20),
+			sideC: sl(10, 20),
+		});
+
+		let rand1 = 0;
+		let rand2 = 0;
 		let question = [
 			['площадь боковой поверхности', 0.5],
 			['объём', 0.25]
-		][v1];
+		][rand1];
 		let ratio = question.pop();
 		question = sklonlxkand(question[0]);
-		let number = sl(5, 100);
-		let answ = !rand ? number * ratio : number / ratio;
+		let number = [prism.sideSurfaceArea, prism.volume][rand1];
 
-		let paint1 = function(ctx) {
-			ctx.translate(110, 50);
-			let edge = 26;
-			ctx.scale(10, 10);
-			ctx.lineWidth = 0.2;
-			ctx.font = "3px liberation_sans";
-			ctx.drawPrism({
-				width: edge,
-				height: edge * 0.8,
-				depth: edge * 0.8,
-				angle: Math.PI / 1.7,
-				scale: 10,
-			}, [0, 3, 4], [0.4, 0.5]);
-			ctx.drawPrism({
-				width: edge / 2,
-				height: edge * 0.8,
-				depth: edge * 0.8 / 2,
-				angle: Math.PI / 1.7,
-				scale: 10,
-				strokeStyle: om.primaryBrandColors.iz(),
-			}, [0, 3, 4, 5, 6, 8], [0.4, 0.5]);
+		prism.verticesOfFigure.push(coordinatesMiddleOfSegment3D(prism.verticesOfFigure[0], prism.verticesOfFigure[2]),
+			coordinatesMiddleOfSegment3D(prism.verticesOfFigure[1], prism.verticesOfFigure[2]),
+			coordinatesMiddleOfSegment3D(prism.verticesOfFigure[4], prism.verticesOfFigure[5]),
+			coordinatesMiddleOfSegment3D(prism.verticesOfFigure[3], prism.verticesOfFigure[5]));
+
+		let camera = {
+			x: 0,
+			y: 0,
+			z: 0,
+			scale: 5,
+
+			rotationX: -Math.PI / 2 + Math.PI / 10,
+			rotationY: 0,
+			rotationZ: -Math.PI / 3,
 		};
 
+		let point2DPrism = prism.verticesOfFigure.map((coord3D) => project3DTo2D(coord3D, camera));
+
+		autoScale(prism, camera, point2DPrism, {
+			startX: -180,
+			finishX: 160,
+			startY: -160,
+			finishY: 160,
+			maxScale: 100,
+		});
+
+		point2DPrism = prism.verticesOfFigure.map((coord3D) => project3DTo2D(coord3D, camera));
+		
+		genAssert((point2DPrism[1].x-point2DPrism[6].x).abs()>20, 'Сечение слилось с ребром');
+
+		let strok = [5, 4];
+
+		let paint1 = function(ctx) {
+			let h = 400;
+			let w = 400;
+			ctx.translate(w/2, h/2);
+			ctx.lineWidth = 2;
+			ctx.strokeStyle = om.secondaryBrandColors;
+			ctx.drawFigure(point2DPrism, [
+				[strok],
+				[1, strok],
+				[1, 0, 0],
+				[0, strok, 0, 1],
+				[0, 0, 1, 1, 1],
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+				[0, 0, 0, 0, 0, 0, strok, 0, strok],
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+			]);
+		};
 
 		NAtask.setTask({
 			text: 'Через среднюю линию основания треугольной призмы проведена плоскость, параллельная боковому ребру. ' +
 				question.ie.toZagl() +
-				' отсечённой'.esli(rand) + ' призмы рав' + ['ен', 'на'][question.rod] + ' ' + number +
-				'. Найдите  '+ question.ve + ' ' + ['отсечённой', 'исходной'][rand] + ' треугольной призмы.',
-			answers: answ,
+				' отсечённой'.esli(rand2) + ' призмы рав' + ['ен', 'на'][question.rod] + ' $' + number +
+				'$. Найдите  ' + question.ve + ' ' + ['отсечённой', 'исходной'][rand2] + ' треугольной призмы.',
+			answers: [number * ratio, number / ratio][rand2],
 			analys: '',
 		});
+		NAtask.modifiers.multiplyAnswerBySqrt(13);
+		NAtask.modifiers.allDecimalsToStandard(true);
+		NAtask.modifiers.assertSaneDecimals();
 		NAtask.modifiers.addCanvasIllustration({
 			width: 400,
 			height: 400,
 			paint: paint1,
 		});
-	});
+	}, 1000);
 })();
 //27068 73157 509838 73111 73113 73115 73117 73119 73121 73123 73125 73127 73129 73131 73133 73135 73137 73139 73141 73143 73145 73147 73149 73151 73153 73155
