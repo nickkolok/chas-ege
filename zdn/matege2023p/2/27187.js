f5d2c4d8f88418dd9581f58f90af3c13c5d19b53
SugarHedgehog <sneginca.cat@gmail.com>
2024-05-14 23:01:45 +0300

    [zdn] [up] - newrail

--- zdn/matege2023p/2/27187.js
+++ zdn/matege2023p/2/27187.js
@@ -1,75 +1,92 @@
 (function() {
 	retryWhileError(function() {
 			NAinfo.requireApiVersion(0, 2);
-			let a = sl(10, 17);
-			let b = sl(5, 9);
-			let c = sl(5, 10);
-			let d = sl(3, 10);
-			let k = slKrome(d, 5, 16);
-			let f = a + c;
 
+			let stroke = [4, 5];
+
+			let matrixConnections = {
+				0: [1, [3, stroke], 13],
+				2: [1, [3, stroke], 7],
+				6: [1, 7, 9],
+				10: [7, 9, 15],
+				12: [
+					[3, stroke], 13, 15
+				],
+				14: [9, 13, 15],
+			};
+
+			let par1 = new Parallelepiped({
+				depth: sl(10, 20),
+				height: sl(5, 20),
+				width: sl(10, 20),
+			});
+
+			let par2 = new Parallelepiped({
+				depth: par1.depth,
+				height: sl(5, 20),
+				width: slKrome(par1.width, 5, par1.width - 5),
+			});
+
+			let vertex3D = par1.verticesOfFigure.concat(par2.verticesOfFigure.map((elem) => shiftCoordinate3D(elem, {
+				x: 0.5 * (par1.width - par2.width),
+				y: 0,
+				z: -0.5 * (par1.height + par2.height),
+			})));
+
+			vertex3D = vertex3D.map((elem) => shiftCoordinate3D(elem, {
+				x: 0,
+				y: 0,
+				z: 0.5 * par2.height,
+			}));
+
+			let camera = {
+				x: 0,
+				y: 0,
+				z: 0,
+				scale: 1,
+
+				rotationX: -Math.PI / 2 + Math.PI / 14,
+				rotationY: 0,
+				rotationZ: Math.PI / sl(10, 14),
+			};
+
+			let point2D = vertex3D.map((coord3D) => project3DTo2D(coord3D, camera));
+
+			autoScale(vertex3D, camera, point2D, {
+				startX: -180,
+				finishX: 160,
+				startY: -160,
+				finishY: 160,
+				maxScale: 100,
+			});
+
+			point2D = vertex3D.map((coord3D) => project3DTo2D(coord3D, camera));
+			genAssert(distanceFromPointToSegment(point2D[9], point2D[3],point2D[12]) > 10);
+			genAssert(distanceFromPointToSegment(point2D[9], point2D[3],point2D[2]) > 10);
+			
+			genAssert([point2D[1], point2D[2]].mt_rasst()>40);
+			
 			let rand = sl1();
-			let s = [2 * (f * k + (d + b) * f + (d + b) * k) - 2 * ((f - c) * b), k * (f * d + c * b)][rand];
 
 			let paint1 = function(ctx) {
-
-				a *= 10;
-				b *= 16;
-				c *= 10;
-				d *= 16;
-
-				let depth = 10;
-				ctx.translate(200, 200);
-				ctx.translate(-95, 0);
-
+				let h = 400;
+				let w = 400;
+				ctx.translate(w / 2, h / 2);
 				ctx.lineWidth = 2;
-				for (let i = 0; i < 2; i++) {
-					if (i) {
-						ctx.translate(30, -b * 0.2);
-					}
-					ctx.moveTo(-c, -b);
-					ctx.lineTo(c, -b);
-					ctx.lineTo(c, 0);
-					ctx.lineTo(a, 0);
-					ctx.lineTo(a, d);
-					ctx.stroke();
-					if (i) {
-						ctx.setLineDash([4, 5]);
-						ctx.lineWidth = 0.8;
-					}
-					ctx.lineTo(-c, d);
-					ctx.lineTo(-c, -b);
-					ctx.stroke();
-				}
-				ctx.lineWidth = 2;
-				ctx.setLineDash([0, 0]);
+				ctx.strokeStyle = om.secondaryBrandColors;
+				ctx.drawFigureVer2(point2D, matrixConnections);
 
-				ctx.translate(-30, b * 0.2);
-
-				ctx.drawLine(-c, -b, -c + 30, -b - b * 0.2);
-				ctx.drawLine(c, -b, c + 30, -b - b * 0.2);
-				ctx.drawLine(c, 0, c + 30, -b * 0.2);
-				ctx.drawLine(a, 0, a + 30, -b * 0.2);
-				ctx.drawLine(a, d, a + 30, d - b * 0.2);
-
-				ctx.lineWidth = 1;
-				ctx.setLineDash([4, 3]);
-				ctx.drawLine(-c, d, -c + 30, d - b * 0.2);
-
-				//цифорки
-				ctx.font = "20px serif";
-				ctx.fillText((b / 16).toString(), c + 30 + 3, -b / 2, 18); //b
-				ctx.fillText((c / 10).toString(), 30, -b - b * 0.2 - 5, 18); //c
-				ctx.fillText((d / 16).toString(), a + 30 + 5, d / 2, 18); //d
-				ctx.fillText(k.toString(), a + 10, d * 1.05, 18); //k
-				ctx.fillText(f.toString(), 0, d + 30 + 10, 18); //f
-
-
+				ctx.font = "20px liberation_sans";
+				ctx.signSegmentInMiddle(point2D[2].x, point2D[2].y, point2D[7].x, point2D[7].y, par1.height, 10, 20);
+				ctx.signSegmentInMiddle(point2D[10].x, point2D[10].y, point2D[15].x, point2D[15].y, par2.height, 10, 20);
+				ctx.signSegmentInMiddle(point2D[12].x, point2D[12].y, point2D[15].x, point2D[15].y, par2.width, -10, 20);
+				ctx.signSegmentInMiddle(point2D[0].x, point2D[0].y, point2D[1].x, point2D[1].y, par1.width, 18, 20);
+				ctx.signSegmentInMiddle(point2D[1].x, point2D[1].y, point2D[2].x, point2D[2].y, par1.depth, 18, 20);
 			};
 			NAtask.setTask({
 				text: 'Найдите ' + ['площадь поверхности', 'объём'][rand] +
 					' многогранника, изображённого на рисунке (все двугранные углы – прямые).',
-				answers: s,
+				answers: [par1.surfaceArea + par2.surfaceArea - 2 * par2.baseArea, par1.volume + par2.volume][rand],
 			});
 			NAtask.modifiers.addCanvasIllustration({
 				width: 400,
@@ -80,4 +97,4 @@
 		1000);
 
 })();
-// 27187 25531 25539 639857 639908 25533 25535 25537 27188 25551 25559 25553 25555 25557
+//27193 25671 25673 25675 25677 25679
