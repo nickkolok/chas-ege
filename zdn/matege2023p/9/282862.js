b5d15390493ba98d184fee348e2558b1a803c91e
Алендарь Сергей <sergey.alendar@mail.ru>
2024-05-21 21:24:41 +0300

    Файл 282862.js изменён

--- zdn/matege2023p/9/282862.js
+++ zdn/matege2023p/9/282862.js
@@ -2,19 +2,19 @@
 	retryWhileError(function() {
 		'use strict';
 		let a = sl(1, 30);
-		let b = sl(1, 30);
+		let b = slKrome(a, 1, 30);
 		let c = sl(-15, 15, 0.5);
 		let d = c + sl(3, 30, 0.5);
 		let arr = ['+', '-'];
 		NAtask.setMinimaxFunctionTask({
-			expr: '(x' + arr.iz() + a + ')^2(' + ' x ' + arr.iz() + b + ')' + arr.iz() + sl(1, 10),
+			expr: ['(x' + arr.iz() + a + ')^2','(' + ' x ' + arr.iz() + b + ')'].shuffle().join('') + arr.iz() + sl(1, 10),
 			leftEnd: '' + c,
 			rightEnd: '' + d,
 			primaryStep: 0.1,
 			secondaryStep: 0.001,
 			authors: ['Алендарь Сергей'],
 		});
-	}, 100);
+	}, 10000);
 })();
 //282862
 
