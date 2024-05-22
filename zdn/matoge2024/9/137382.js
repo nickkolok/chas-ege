2a7dd8d74f05795d71a9f17b1ade1fee0846f03a
VeronikaKit <166473321+VeronikaKit@users.noreply.github.com>
2024-05-22 17:42:33 +0300

    [zdn] [new] - newTask_137382 - close #1584

---
+++ zdn/matoge2024/9/137382.js
@@ -0,0 +1,14 @@
+(function() { retryWhileError(function() {
+    let d=sluchch(1,20).pm();
+    let e=slKrome(d,1,20).pm();
+    let c=-d*e;
+    let b=-d-e;
+    genAssert(b**2-4*(-c)>0,'Нужно два решения');
+    NAtask.setEquationTask({
+    	parts: ['x^2+'+b+'x', c],
+        roots: [d,e],
+	handleMultipleRoots: 'randomExceptList',
+    });
+}, 20);})();
+//Решу ОГЭ 137382
+//VeronikaKit
