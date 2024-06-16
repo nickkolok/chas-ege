8b83ddfb0c6ca00cf0a54d946dd2c4820090d6a9
Алендарь Сергей <sergey.alendar@mail.ru>
2024-06-16 23:35:59 +0300

    Создан файл 26717.js

---
+++ zdn/matege2023p/9/26717.js
@@ -0,0 +1,21 @@
+(function() {
+	retryWhileError(function() {
+		'use strict';
+		let a = sl(1, 20);
+		let b = sl(1, 15);
+		let c = sl(1, 20).pm();
+		let d = sl(0, 10).pm();
+		let l = sl(-20.5, 1, 0.5);
+		let arr1 = ['+', '-'];
+		let maxmin1 = sl1();
+		let maxmin2 = (maxmin1 + 1) % 2;
+		NAtask.setMinimaxFunctionTask({
+			expr: a+'log(x+'+b+')+'+c+'x'+d,
+			leftEnd: ''+l,
+			rightEnd: '0',
+			primaryStep: 0.001,
+			secondaryStep: 0.0001,
+			authors: ['Алендарь Сергей'],
+		});
+	}, 1000);
+})();
