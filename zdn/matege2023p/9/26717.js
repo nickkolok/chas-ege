305c3f7cbab8dd5a03771f302529c6787be7ff64
Алендарь Сергей <sergey.alendar@mail.ru>
2024-06-17 00:45:23 +0300

    Файл 26717.js изменён

--- zdn/matege2023p/9/26717.js
+++ zdn/matege2023p/9/26717.js
@@ -6,9 +6,6 @@
 		let c = sl(1, 20).pm();
 		let d = sl(0, 10).pm();
 		let l = sl(-20.5, 1, 0.5);
-		let arr1 = ['+', '-'];
-		let maxmin1 = sl1();
-		let maxmin2 = (maxmin1 + 1) % 2;
 		NAtask.setMinimaxFunctionTask({
 			expr: a+'log(x+'+b+')+'+c+'x'+d,
 			leftEnd: ''+l,
@@ -19,3 +16,5 @@
 		});
 	}, 1000);
 })();
+//26717
+
