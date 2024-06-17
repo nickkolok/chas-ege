04f09322c9788dc028db6f490b961d2fb67e63ae
Алендарь Сергей <sergey.alendar@mail.ru>
2024-06-17 23:25:30 +0300

    Файл 26691.js изменён

--- zdn/matege2023p/9/26691.js
+++ zdn/matege2023p/9/26691.js
@@ -4,16 +4,17 @@
 		let a = sl(1, 3);
 		let b = sl(1, 50);
 		let c = sl(1, 15);
-		let d = c + sl(1, 2);
-		let e = c - sl(1, 2);
+		let d = b / a + a + sl(1, 10);
+		let e = -b / a - a - sl(1, 10);
 		let arr1 = ['+', '-'];
 		let arr2 = ['-', '+'];
 		let maxmin = sl1();
 		NAtask.setMinimaxFunctionTask({
-			expr: '' + '(' + a + 'x' + arr1[sl1()] + b + ')' + 'e^(' + arr1[sl1()] + 'x' + arr2[sl1()] + c + ')',
+			expr: '' + '(' + a + 'x' + arr1[sl1()] + b + ')' + 'e^(' + arr1[maxmin] + 'x' + arr2[sl1()] + c + ')',
 			leftEnd: '' + e,
 			rightEnd: '' + d,
 			primaryStep: 1 / 6,
+			forbiddenAnswers: [0,'-0'],
 			authors: ['Алендарь Сергей'],
 		});
 	}, 10000);
