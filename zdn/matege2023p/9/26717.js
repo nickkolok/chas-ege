9f07bbce2180353e4ff377abee8c9e61801cba77
Алендарь Сергей <sergey.alendar@mail.ru>
2024-06-17 21:56:53 +0300

    Файл 26717.js изменён

--- zdn/matege2023p/9/26717.js
+++ zdn/matege2023p/9/26717.js
@@ -1,21 +1,26 @@
+
 (function() {
 	retryWhileError(function() {
 		'use strict';
 		let a = sl(1, 20);
 		let b = sl(1, 15);
-		let c = sl(1, 20).pm();
+		let c = a;
 		let d = sl1();
 		let k = sl(0, 10).pm();
 		let g = 1;
 		if (d == 0) {
-			g = sl(2, 5);
+			g = sl(2, 9);
 			k = 0;
 			a = 1;
+			c = g;
 		}
-		let l = sl(-20.5, 1, 0.5);
+		let l = -b-1-sl(0, 1.5, 0.5);
+		let arr1 = ['+', '-'];
+		let arr2 = ['-', '+'];
+		let maxmin = sl1();
 		NAtask.setMinimaxFunctionTask({
-			expr: a + 'log(x+' + b + ')^' + g + '+' + c + 'x+' + k,
-			leftEnd: '' + l,
+			expr: arr1[maxmin]+a + 'log(x+' + b + ')^' + g + '+'+arr2[maxmin] + c + 'x+' + k,
+			leftEnd: '' +l,
 			rightEnd: '0',
 			primaryStep: 0.01,
 			secondaryStep: 0.001,
