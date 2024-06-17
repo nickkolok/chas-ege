7cb76829685a42f2e9c1ff35fe685a69bf3b728d
Алендарь Сергей <sergey.alendar@mail.ru>
2024-06-17 20:46:49 +0300

    Файл 26719.js изменён

--- zdn/matege2023p/9/26719.js
+++ zdn/matege2023p/9/26719.js
@@ -2,16 +2,22 @@
 	retryWhileError(function() {
 		'use strict';
 		let a = sl(1, 20);
-		let b = sl(1, 15);
+		let b = sl(7, 19);
 		let l = sl(-20.5, 1, 0.5);
 		let r = sl(-20.5, 1, 0.5);
 		let arr1 = ['+', '-'];
 		let arr2 = ['-', '+'];
 		let maxmin = sl1();
+		let b1 = [3, 4, 5, 7, 11, 13, b * 2].iz();
+		let a1 = sl(2, 7);
+		if (2 * b > b1) {
+			a1 = 1;
+		}
+		genAssertIrreducible(a1, b1);
 		NAtask.setMinimaxFunctionTask({
 			expr: arr1[maxmin] + 'log(' + b + 'x)+' + arr2[maxmin] + b + 'x+' + a,
 			leftEnd: '1/' + b * 2,
-			rightEnd: '' + sl(3, 5) + '/' + b * 2,
+			rightEnd: '' + a1 + '/' + b1,
 			primaryStep: 0.01,
 			secondaryStep: 0.001,
 			authors: ['Алендарь Сергей'],
@@ -19,3 +25,4 @@
 	}, 1000);
 })();
 //26719
+
