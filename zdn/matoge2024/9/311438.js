75a84469806da0b9ace0951f5b4973e197d7d0b1
VeronikaKit <166473321+VeronikaKit@users.noreply.github.com>
2024-07-16 14:50:44 +0300

    [zdn][new]-equation-fix#1585

---
+++ zdn/matoge2024/9/311438.js
@@ -0,0 +1,15 @@
+(function() { retryWhileError(function() {
+    let d=sluchch(1,30).pm();
+    let e=slKrome(d,1,30).pm();
+    let b=-(d+e);
+    let c=d*e;
+    genAssert(b!=0, 'Нужно два решения');
+    genAssert(c!=0, 'Нужно два решения');
+    genAssertZ1000(d,'Корень не должен быть слишком дробным');
+    genAssertZ1000(e,'Корень не должен быть слишком дробным');
+    NAtask.setEquationTask({
+    	    parts: ['x^2+'+b+'x+'+c, 0],
+	    roots: [d,e],
+	    handleMultipleRoots: 'randomExceptList',
+    });
+}, 20);})();
