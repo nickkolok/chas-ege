bf867767c1fd49bdffcd00fbff7c9eef080a1800
VeronikaKit <166473321+VeronikaKit@users.noreply.github.com>
2024-07-17 21:25:27 +0300

    [zdn][new] 357173 - OGE equation - fix #1575 (#1627)

---
+++ zdn/matoge2024/9/357173.js
@@ -0,0 +1,12 @@
+(function() { retryWhileError(function() {
+    let d=sluchch(1,50);
+    let a=d**2;
+    let e=-d;
+    NAtask.setEquationTask({
+    	    parts: ['x^2-'+a, 0],
+            roots: [d,e],
+	    handleMultipleRoots: 'randomExceptList',
+    });
+}, 20);})();
+//VeronikaKit
+//РешуОГЭ 311447
