888ee215449e388f7103d3fc97ca5f0fa79c0d91
SugarHedgehog <sneginca.cat@gmail.com>
2024-08-27 16:30:10 +0500

    update

--- zdn/matege2023p/2/905.js
+++ zdn/matege2023p/2/905.js
@@ -1,47 +1,94 @@
 (function() {
 	retryWhileError(function() {
 		NAinfo.requireApiVersion(0, 2);
-		let edgeOfBase = sl(2, 10);
+
+		lx_declareClarifiedPhrase('площадь', 'боковой поверхности');
+		lx_declareClarifiedPhrase('площадь', 'полной поверхности');
+		lx_declareClarifiedPhrase('площадь', 'основания');
+
+		let a = sl(20, 100);
+		let pyr = new RegularPyramid({
+			baseSide: a,
+			height: a * (6).sqrt() / 3,
+			numberSide: 3
+		});
+
 		let name = [
-			['площадь полной поверхности', 3 * edgeOfBase.pow(4), 'равна'],
-			['площадь боковой поверхности', 3 * (0.75 * edgeOfBase.pow(2)).pow(2), 'равна'],
-			['площадь основания', 3 * (0.25 * edgeOfBase.pow(2)).pow(2), 'равна'],
-			['объём', 2 * (edgeOfBase.pow(3) / 12).pow(2), 'равен'],
-			['апофема', 3 * (0.5 * edgeOfBase).pow(2), 'равна'],
-			['высота', 6 * (edgeOfBase / 3).pow(2), 'равна'],
+			['площадь полной поверхности', pyr.surfaceArea],
+			['площадь боковой поверхности', pyr.sideSurfaceArea],
+			['площадь основания', pyr.baseArea],
+			['объём', pyr.volume],
+			['апофема', pyr.apothem],
+			['высота', pyr.height],
 		].iz(2);
 
 		let question = (name).T()[0];
 		let numbers = (name).T()[1];
-		let verb = (name).T()[2];
 
-		numbers.forEach(element => {
-			genAssert((element * 10).isZ(), 'Значение не целое');
+		let strok = [5, 4];
+
+		let matrixPyr = [
+			[1],
+			[strok, strok],
+			[1, 1, strok, 0, [0, strok][Number(question.includes('высота'))],
+				[0, strok][Number(question.includes('апофема'))]
+			],
+		];
+
+		let camera = {
+			x: 0,
+			y: 0,
+			z: 0,
+			scale: 5,
+
+			rotationX: -Math.PI / 2 + Math.PI / 14,
+			rotationY: 0,
+			rotationZ: Math.PI / slKrome(6, 4, 8),
+		};
+
+		let vertex3D = pyr.verticesOfFigure.concat([{
+			x: 0,
+			y: 0,
+			z: -pyr.height / 2,
+		}, {
+			x: -pyr.radiusOfInscribedCircle,
+			y: 0,
+			z: -pyr.height / 2,
+		}]);
+
+		let point2DPyr = vertex3D.map((coord3D) => project3DTo2D(coord3D, camera));
+
+		autoScale(vertex3D, camera, point2DPyr, {
+			startX: -180,
+			finishX: 180,
+			startY: -180,
+			finishY: 180,
+			maxScale: 50,
 		});
 
-		let paint1 = function(ct) {
-			ct.translate(10, 20);
-			ct.scale(10, -10);
-			ct.lineWidth = 2 / 15;
-			ct.rightPyramid3({
-				edge: 17,
-				angle: Math.PI / 8,
-				height: 11,
-			}, [1], [0.5, 0.2], question.includes('высота'), question.includes('апофема'));
+		point2DPyr = vertex3D.map((coord3D) => project3DTo2D(coord3D, camera));
+
+		let paint1 = function(ctx) {
+			let h = 400;
+			let w = 400;
+			ctx.translate(h / 2, w / 2);
+			ctx.lineWidth = 2;
+			ctx.strokeStyle = om.secondaryBrandColors;
+			ctx.drawFigure(point2DPyr, matrixPyr);
 		};
 		NAtask.setTask({
-			text: 'В правильном тетраэдре ' + question[0] + ' ' + verb[0] + ' $' + numbers[0].texsqrt(1) + '$. Чему ' +
-				verb[1] + ' ' +
-				question[1] + ' тетраэдра?',
-
-			answers: numbers[1].sqrt(),
+			text: 'В правильном тетраэдре ' + question[0] + ' ' + ['равен', 'равна'][sklonlxkand(question[0]).rod] + ' $' +
+				numbers[0].pow(2).texsqrt(sl1()) +
+				'$. Чему ' + ['равен', 'равна'][sklonlxkand(question[1]).rod] + ' ' + question[1] + ' тетраэдра?',
+			answers: numbers[1],
 		});
 		NAtask.modifiers.multiplyAnswerBySqrt(6);
-		chas2.task.modifiers.addCanvasIllustration({
-			width: 240,
-			height: 240,
+		NAtask.modifiers.allDecimalsToStandard(true);
+		NAtask.modifiers.assertSaneDecimals();
+		NAtask.modifiers.addCanvasIllustration({
+			width: 400,
+			height: 400,
 			paint: paint1,
 		});
 	}, 10000);
-
 })();
