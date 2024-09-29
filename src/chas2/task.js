e17702daf7051f4e90cfa02d61dc222be27c8a8a
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2024-09-29 12:55:20 +0500

    test

--- src/chas2/task.js
+++ src/chas2/task.js
@@ -500,7 +500,7 @@ chas2.task = {
 			rootsIsInteger: rootsIsInteger
 		});	
 
-		console.log(findExtremumsOfFunctionSort(func, minX, maxX))
+		console.log('Экстремумы',findExtremumsOfFunctionSort(func, minX, maxX))
 
 		task.text = [];
 		task.text.push(`На рисунке изображён график`);
