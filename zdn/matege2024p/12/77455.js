ef6d173ec462bab8610fe9188cb6a5a77d57c07a
NickKolok <nickkolok@mail.ru>
2024-10-22 19:51:46 +0300

    [zdn] 77455 - sqrt switcher

--- zdn/matege2024p/12/77455.js
+++ zdn/matege2024p/12/77455.js
@@ -1,9 +1,13 @@
 (function() {
 	retryWhileError(function() {
 		'use strict';
+		let xsqrtx = ['x^(3/2)', 'x sqrt(x)'];
+		if (nabor.preferences && "77455" in nabor.preferences){
+			xsqrtx = [xsqrtx[nabor.preferences["77455"]]];
+		}
 		NAtask.setLocalExtremumTask({
 			expr: [
-				'' + sl(1,5) + ['x^(3/2)', 'x sqrt(x)'].iz(),
+				'' + sl(1,5) + xsqrtx.iz(),
 				sl(1,20) + 'x',
 				sl(0,100)
 			].joinPlusMinus(),
