d78d0896e1033d9fba108c9385d0f659b4bb3158
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2024-10-24 19:44:26 +0500

    fix

--- zdn/misc_largest_smallest_values_of_function_fipi_2024/6/main.js
+++ zdn/misc_largest_smallest_values_of_function_fipi_2024/6/main.js
@@ -2,6 +2,6 @@ window.nomer=[
 	77460,
 ].iz()
 window.nabor.preferences["77460"] = 0;
-indow.comment='77460 (корень)';
+window.comment='77460 (корень)';
 chas2.task.setMinimaxFunctionTask.forbidOpenEnds = true;
 
--- zdn/misc_largest_smallest_values_of_function_fipi_2024/7/main.js
+++ zdn/misc_largest_smallest_values_of_function_fipi_2024/7/main.js
@@ -2,5 +2,5 @@ window.nomer=[
 	77460,
 ].iz()
 window.nabor.preferences["77460"] = 1;
-indow.comment='77460 (3/2)';
+window.comment='77460 (3/2)';
 chas2.task.setMinimaxFunctionTask.forbidOpenEnds = true;
