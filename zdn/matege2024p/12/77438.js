7c7b3f28f6c2a20438b30cd96a841f1dc2dfcf07
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2024-10-30 13:45:28 +0500

    usePreference - fix

--- zdn/matege2024p/12/77438.js
+++ zdn/matege2024p/12/77438.js
@@ -7,21 +7,6 @@
 		let arr1 = [' ', '-'];
 		let arr2 = ['+', '-'];
 
-		let preference = [
-			[{
-				preference: 'maximum',
-				preferenceValue: true,
-				defaultValue: false
-			}],
-			[{
-				preference: 'minimum',
-				preferenceValue: true,
-				defaultValue: false
-			},]
-		];
-
-		let pref = usePreference("77438", preference);
-
 		NAtask.setMinimaxFunctionTask({
 			expr: [arr1.iz() + 'x^3', arr1.iz() + a + 'x^2', arr1.iz() + b + 'x'].iz(sl(2, 3)).join('+') + arr2.iz() + c,
 			leftEnd: '-' + sl(1, 9, 0.5),
@@ -29,8 +14,16 @@
 			primaryStep: 1,
 			secondaryStep: 0.001,
 			authors: ['Алендарь Сергей'],
-			forbidMinY: pref[0],
-			forbidMaxY: pref[1],
+			forbidMinY: usePreference("77438", {
+				preference: 'maximum',
+				preferenceValue: true,
+				defaultValue: false
+			}),
+			forbidMaxY: usePreference("77438", {
+				preference: 'minimum',
+				preferenceValue: true,
+				defaultValue: false
+			}),
 		});
 	}, 10);
 })();
