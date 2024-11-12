d55261f761f582f3d093919f3682dbd73adf9f49
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2024-11-12 12:30:44 +0300

    [katalog] - Использование let и const: Заменены var на let и const для блочной области видимости. Шаблонные литералы: Использованы шаблонные литералы для интерполяции строк. Стрелочные функции: Использованы стрелочные функции для коллбэков. Описание функций: Добавлены комментарии на русском языке для описания функций.

--- sh/katalog.js
+++ sh/katalog.js
@@ -1,154 +1,153 @@
 'use strict';
 
-
-function generateHtmlForTask(kat,zdn,masdey){
-	var rez='';
-	vopr.podg();
-	var currentTask = nabor.adres+kat+'/'+zdn+'.js';
-	rez+='<div class="task-wrapper" data-category="'+kat+'" data-tasknumber="'+zdn+'">';
-	rez+=currentTask.vTag('h2');
-	console.log(currentTask);
-	try{
-		nabor.upak[kat][zdn]();
-		vopr.template = currentTask.replace(/^(\.\.\/)+/,'');
-		vopr.taskNumber = kat;
-		rez+=('<br/>'+vopr.txt.vTag('div')+'<br/>');
-		rez+=(
-			(
-				'<button class="copybutton" style="display:block; float:right;" title="Экспорт в РешуЕГЭ"'+
-				'data-task="' + encodeURIComponent(JSON.stringify(vopr)) + '"' +
-				'>' +
-					'&#x2398;' +
-				'</button>'+
-
-				'<button class="renewbutton" style="display:block; float:right; margin-right:1.46em;" title="Заменить задание на похожее"'+
-				'>' +
-					'&#x27F3;' +
-				'</button>'+
-
-				'<button class="addbutton" style="display:block; float:right; margin-right:1.46em;" title="Добавить похожее задание"'+
-				'>' +
-					'+' +
-				'</button>'+
-
-				'Ответ: '+vopr.ver.join('или')
-			).vTag('div') +
-			'<br/>'
-		);
-		masdey.push(vopr.dey);
-		if(vopr.rsh){
-			rez+=(
-				('Показать решение ').vTag('button','class="spoiler-show"')+
-				('Скрыть   решение ').vTag('button','class="spoiler-hide"')+
-				'<div class="spoiler-body">'+
-				'Решение: '+'<br/>'+
-				vopr.rsh+
-				'</div>'+
-			'');
-
-		}
-		if(vopr.authors && vopr.authors.length){
-			rez+=(
-				'<br/>' +
-				'<div class="katalog-authors">' +
-						'Автор' + ('ы').esli(vopr.authors.length > 1) + ': &nbsp;' +
-						vopr.authors.join(', ') +
-				'</div>'+
-				'<br/>'+
-			'');
-		}
-	}catch(e){
-		console.log(e);
-	}
-	rez += '</div>';
-	return rez;
+/**
+ * Генерирует HTML для задания.
+ * @param {string} kat - Категория задания.
+ * @param {string} zdn - Номер задания.
+ * @param {Array} masdey - Массив действий.
+ * @returns {string} - HTML-код задания.
+ */
+function generateHtmlForTask(kat, zdn, masdey) {
+    let rez = '';
+    vopr.podg();
+    const currentTask = `${nabor.adres}${kat}/${zdn}.js`;
+    rez += `<div class="task-wrapper" data-category="${kat}" data-tasknumber="${zdn}">`;
+    rez += currentTask.vTag('h2');
+    console.log(currentTask);
+    try {
+        nabor.upak[kat][zdn]();
+        vopr.template = currentTask.replace(/^(\.\.\/)+/, '');
+        vopr.taskNumber = kat;
+        rez += `<br/>${vopr.txt.vTag('div')}<br/>`;
+        rez += (
+            `<button class="copybutton" style="display:block; float:right;" title="Экспорт в РешуЕГЭ" data-task="${encodeURIComponent(JSON.stringify(vopr))}">&#x2398;</button>` +
+            `<button class="renewbutton" style="display:block; float:right; margin-right:1.46em;" title="Заменить задание на похожее">&#x27F3;</button>` +
+            `<button class="addbutton" style="display:block; float:right; margin-right:1.46em;" title="Добавить похожее задание">+</button>` +
+            `Ответ: ${vopr.ver.join('или')}`
+        ).vTag('div') + '<br/>';
+        masdey.push(vopr.dey);
+        if (vopr.rsh) {
+            rez += (
+                'Показать решение '.vTag('button', 'class="spoiler-show"') +
+                'Скрыть   решение '.vTag('button', 'class="spoiler-hide"') +
+                `<div class="spoiler-body">Решение: <br/>${vopr.rsh}</div>`
+            );
+        }
+        if (vopr.authors && vopr.authors.length) {
+            rez += (
+                '<br/>' +
+                '<div class="katalog-authors">' +
+                `Автор${'ы'.esli(vopr.authors.length > 1)}: &nbsp;` +
+                vopr.authors.join(', ') +
+                '</div><br/>'
+            );
+        }
+    } catch (e) {
+        console.log(e);
+    }
+    rez += '</div>';
+    return rez;
 }
 
-function generateKatalog(){
-	var rez='';
-	var toc='';
-	var masdey=[];
-	var br='<br/>';
-	for(var kat in nabor.upak){
-		window.comment='';
-		window.availableTaskNumbers = null;
-		try{
-				nabor.upak[kat][nabor.scheduler]()
-		}catch(e){
-			console.log(e);
-		}
-		rez+=(
-			('Показать категорию '+kat).vTag('button','class="spoiler-show"')+
-			('Скрыть   категорию '+kat).vTag('button','class="spoiler-hide"')+
-			'<div class="spoiler-body">'+
-			('Категория '+kat).vTag('h1','id="'+kat+'"')+
-			window.comment+
-		'');
-		toc+=(
-			(kat+'. '+window.comment).vTag('a','href="#'+kat+'"')+
-			br+
-		'');
-		var tasksToList = window.availableTaskNumbers || Object.keys(nabor.upak[kat]);
+/**
+ * Генерирует каталог заданий.
+ */
+function generateKatalog() {
+    let rez = '';
+    let toc = '';
+    const masdey = [];
+    const br = '<br/>';
+    for (const kat in nabor.upak) {
+        window.comment = '';
+        window.availableTaskNumbers = null;
+        try {
+            nabor.upak[kat][nabor.scheduler]();
+        } catch (e) {
+            console.log(e);
+        }
+        rez += (
+            `Показать категорию ${kat}`.vTag('button', 'class="spoiler-show"') +
+            `Скрыть   категорию ${kat}`.vTag('button', 'class="spoiler-hide"') +
+            `<div class="spoiler-body">` +
+            `Категория ${kat}`.vTag('h1', `id="${kat}"`) +
+            window.comment
+        );
+        toc += (
+            `${kat}. ${window.comment}`.vTag('a', `href="#${kat}"`) +
+            br
+        );
+        const tasksToList = window.availableTaskNumbers || Object.keys(nabor.upak[kat]);
 
-		for(var zdn of tasksToList)
-			if(zdn!='main' && zdn!='fipi'){
-				rez += generateHtmlForTask(kat,zdn,masdey);
-			}
-			rez += '</div>';
-	}
-	$('#divrez').html(toc+br+rez);
-	var len=masdey.length;
-	for(var i=0;i<len;i++){
-		try{
-			masdey[i]();
-		}catch(e){
-			console.log(e);
-		}
-	}
-	MathJax.Hub.Typeset();
-	afterTasksGenerated();
-	$('.spoiler-show').click();
+        for (const zdn of tasksToList) {
+            if (zdn !== 'main' && zdn !== 'fipi') {
+                rez += generateHtmlForTask(kat, zdn, masdey);
+            }
+        }
+        rez += '</div>';
+    }
+    $('#divrez').html(toc + br + rez);
+    const len = masdey.length;
+    for (let i = 0; i < len; i++) {
+        try {
+            masdey[i]();
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    MathJax.Hub.Typeset();
+    afterTasksGenerated();
+    $('.spoiler-show').click();
 }
 
-function afterTasksGenerated(){
-	spoiler();
-	$( 'button.copybutton[data-already-inited!=true]').click( copyTask).attr('data-already-inited', true);
-	$('button.renewbutton[data-already-inited!=true]').click(renewTask).attr('data-already-inited', true);
-	$(  'button.addbutton[data-already-inited!=true]').click(  addTask).attr('data-already-inited', true);
+/**
+ * Выполняет действия после генерации заданий.
+ */
+function afterTasksGenerated() {
+    spoiler();
+    $('button.copybutton[data-already-inited!=true]').click(copyTask).attr('data-already-inited', true);
+    $('button.renewbutton[data-already-inited!=true]').click(renewTask).attr('data-already-inited', true);
+    $('button.addbutton[data-already-inited!=true]').click(addTask).attr('data-already-inited', true);
 }
 
-
-function copyTask(){
-	console.log(this);
-	//var theTask = this.getElementsByTagName('span')[0].innerHTML;
-	var theTask = decodeURIComponent(this.getAttribute('data-task'));
-	console.log(theTask);
-	theTask = JSON.parse(theTask);
-	console.log(theTask);
-	replaceCanvasWithImgInTaskAndHTML($(this).parents('div.task-wrapper')[0], theTask, function(){
-		var fillerCode = createFiller(theTask);
-		copyToClipboard(fillerCode)
-	});
+/**
+ * Копирует задание в буфер обмена.
+ */
+function copyTask() {
+    console.log(this);
+    let theTask = decodeURIComponent(this.getAttribute('data-task'));
+    console.log(theTask);
+    theTask = JSON.parse(theTask);
+    console.log(theTask);
+    replaceCanvasWithImgInTaskAndHTML($(this).parents('div.task-wrapper')[0], theTask, () => {
+        const fillerCode = createFiller(theTask);
+        copyToClipboard(fillerCode);
+    });
 }
 
-function renewTask(){
-	console.log(this);
-	var wrapper = $(this).parents('div.task-wrapper')[0];
-	var actions = [];
-	var taskHtml = $(generateHtmlForTask(wrapper.getAttribute('data-category'),wrapper.getAttribute('data-tasknumber'),actions));
-	$(wrapper).replaceWith(taskHtml);
-	actions[0]();
-	MathJax.Hub.Typeset(taskHtml[0]);
-	afterTasksGenerated();
+/**
+ * Обновляет задание на новое.
+ */
+function renewTask() {
+    console.log(this);
+    const wrapper = $(this).parents('div.task-wrapper')[0];
+    const actions = [];
+    const taskHtml = $(generateHtmlForTask(wrapper.getAttribute('data-category'), wrapper.getAttribute('data-tasknumber'), actions));
+    $(wrapper).replaceWith(taskHtml);
+    actions[0]();
+    MathJax.Hub.Typeset(taskHtml[0]);
+    afterTasksGenerated();
 }
 
-function addTask(){
-	console.log(this);
-	var wrapper = $(this).parents('div.task-wrapper')[0];
-	var actions = [];
-	var taskHtml = $(generateHtmlForTask(wrapper.getAttribute('data-category'),wrapper.getAttribute('data-tasknumber'),actions));
-	taskHtml.insertAfter(wrapper);
-	actions[0]();
-	MathJax.Hub.Typeset(taskHtml[0]);
-	afterTasksGenerated();
+/**
+ * Добавляет новое задание после текущего.
+ */
+function addTask() {
+    console.log(this);
+    const wrapper = $(this).parents('div.task-wrapper')[0];
+    const actions = [];
+    const taskHtml = $(generateHtmlForTask(wrapper.getAttribute('data-category'), wrapper.getAttribute('data-tasknumber'), actions));
+    taskHtml.insertAfter(wrapper);
+    actions[0]();
+    MathJax.Hub.Typeset(taskHtml[0]);
+    afterTasksGenerated();
 }
