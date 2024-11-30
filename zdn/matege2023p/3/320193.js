a3bcf4711479e9e9bc2e34f767d516bce081dd6b
SugarHedgehog <sneginca.cat@gmail.com>
2024-11-30 12:37:19 +0300

    [zdn] [fix] - answer

--- zdn/matege2023p/3/320193.js
+++ zdn/matege2023p/3/320193.js
@@ -18,35 +18,37 @@
 			],
 			['улитка', 'приползёт', 'усами']
 		].iz();
-		
+
 		let difference = transport.pop();
 		let move = transport.pop();
 		transport = sklonlxkand(transport);
-		
-		let colors = sklonlxkand(['темно-бордовый', 'темно-красный', 'лососевый', 'томатный', 'коралловый',
-			'оранжево-красный',
-			'шоколадный', 'песочно-коричневый', 'темно-оранжевый', 'оранжевый', 'золотарниковый', 'оливковый',
-			'желтый', 'зелёно-желтый', 'зеленый', 'лаймовый', 'бирюзовый', 'темно-бирюзовый',
-			'сине-фиолетовый'
+
+		let colors = sklonlxkand(['бордовый', 'лососевый', 'томатный', 'коралловый', 'шоколадный', 'оранжевый',
+			'золотарниковый', 'оливковый', 'желтый', 'зеленый', 'лаймовый', 'бирюзовый'
 		]).iz(4);
 		let numberOfTransprt = sl(10, 100);
-		let number = numberOfTransprt.nod(slKrome([numberOfTransprt,1], 1, numberOfTransprt - 1));
-		let dop = 0;
-		if (sl1()) {
-			number = numberOfTransprt - number;
-			dop = 1;
+		let number1 = numberOfTransprt.nod(slKrome([numberOfTransprt, 1], 1, numberOfTransprt - 1));
+		let number2 = numberOfTransprt - number1;
+		let answer =  number1 / numberOfTransprt;
+		
+		let dop=0;
+		
+		if(sl1()){
+			answer =  number2 / numberOfTransprt;
+			dop=2;
 		}
-		genAssertZ1000(number / numberOfTransprt, "Кривая вероятность");
+		
+		genAssertZ1000(answer, "Кривая вероятность");
 		NAtask.setTask({
 			text: 'В фирме такси в наличии ' + numberOfTransprt + ' ' + transport[0].rm.replace('Конёк', 'Коньков').replace(
-				'ковёр', 'ковров') +'; ' + number + ' из них ' + colors[0].ve + ' цвета с ' + colors[1].tm + ' ' + difference + ', ' +
-				'остальные — ' + colors[2].ve + ' цвета с ' + colors[3].tm + ' ' + difference + '. Найдите вероятность того, ' +
+				'ковёр', 'ковров') + '; ' + number1 + ' из них ' + colors[0].ve + ' цвета с ' + colors[1].tm + ' ' +
+				difference + ', ' + 'остальные — ' + colors[2].ve + ' цвета с ' + colors[3].tm + ' ' + difference + '. Найдите вероятность того, ' +
 				'что на случайный вызов ' + move + ' ' + transport[0].ie + ' ' + colors[0 + dop].ve + ' цвета с ' + colors[1 + dop].tm +
 				' ' + difference + '.',
-			answers: (number / numberOfTransprt).ts(),
+			answers: answer,
 		});
 
-	});
+	}, 100);
 })();
 // 320193 525368
 //SugarHedgehog
