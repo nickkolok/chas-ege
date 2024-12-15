bf229222bb424265535443b063e6f637bb47ef90
VeronikaKit <166473321+VeronikaKit@users.noreply.github.com>
2024-12-15 18:03:30 +0300

    [zdn] [new] - 511648.js

---
+++ zdn/matege2024b/16/511648.js
@@ -0,0 +1,15 @@
+(function() { 'use strict'; retryWhileError(function() {
+	let a=sl(1, 30);
+	let b=slKrome(a, 1, 30);
+	let c=slKrome(b, 1, 30);
+	let x=2*(a*b+a*c+b*c);
+  NAtask.setTask({
+		text:
+		    'Площадь поверхности прямоугольного параллелепипеда с ребрами $a$, $b$ и $c$ вычисляется по формуле $S=2(ab+ac+bc)$. Найдите площадь поверхности прямоугольного параллелепипеда с ребрами ' + a + ' ,' + b + ' и ' + c + '.',
+		answers: x,
+		authors: ['VeronikaKit'],
+	});
+	NAtask.modifiers.allDecimalsToStandard(/*true*/);
+}, 2000);})();
+//VeronikaKit
+//Решу ЕГЭ (база) 511648
