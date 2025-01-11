525633f705ac0e638c3cffc3af5a2646cd7db39c
Колесникова Елена <luly3751@gmail.com>
2025-01-11 18:04:39 +0300

    issue 1872 edit 1

--- zdn/matege2024b/21/2434.js
+++ zdn/matege2024b/21/2434.js
@@ -7,12 +7,12 @@
         let firstTapeLength = sluchch(5, 200, 1);
         let tapeColor = ['красн', 'жёлт', 'зелён', 'фиолетов', 'черн', 'оранжев', 'голуб'].iz(2);
         let typeOfTapes = ['тонкие', 'незаметные', 'полупрозранчые', 'маленькие'].iz();
-        let unitsOfLength = sklonlxkand(['сантиметр', 'дециметр', 'метр', 'миллиметр','дюйм'].iz());
+        let unitsOfLength = sklonlxkand(['сантиметр', 'дециметр', 'метр', 'миллиметр', 'дюйм'].iz());
         NAtask.setTask({
             text: 'На ' + tapeName.de + ' по разные стороны от середины отмечены две ' + typeOfTapes + ' поперечные полоски: ' +
                 tapeColor[0] + 'ая' + ' и ' + tapeColor[1] + 'ая' + '. Если разрезать ' + tapeName.ve + ' по ' + tapeColor[1] + 'ой' + ' полоске, то одна часть' +
-                ' будет на ' + chislitlx(secondTapeLength,unitsOfLength,'$') + ' длиннее другой. Если разрезать ' + tapeName.ve + ' по ' + tapeColor[0] + 'ой' + ' полоске, ' +
-                ' то одна часть будет на ' + chislitlx(firstTapeLength, unitsOfLength,'$') + ' длиннее другой.' +
+                ' будет на ' + chislitlx(secondTapeLength, unitsOfLength, '$') + ' длиннее другой. Если разрезать ' + tapeName.ve + ' по ' + tapeColor[0] + 'ой' + ' полоске, ' +
+                ' то одна часть будет на ' + chislitlx(firstTapeLength, unitsOfLength, '$') + ' длиннее другой.' +
                 ' Найдите расстояние в ' + unitsOfLength.pm + ' между ' + tapeColor[1] + 'ой' + ' и ' + tapeColor[0] + 'ой' + ' полосками.',
             answers: (secondTapeLength + firstTapeLength) / 2,
         });
