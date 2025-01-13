8217fba8b212f20afca8ee9c56e11efba4bb7e3d
Колесникова Елена <luly3751@gmail.com>
2025-01-13 11:57:07 +0300

    issue 514910 edit 1

--- zdn/matege2024b/21/514910.js
+++ zdn/matege2024b/21/514910.js
@@ -2,13 +2,10 @@
     'use strict';
     retryWhileError(function () {
         NAinfo.requireApiVersion(0, 2);
-        let numberOfCut, numberPiecesOfWood;
-        do {
-            numberOfCut = sluchch(3, 200, 1);
-            numberPiecesOfWood = sluchch(3, 200, 1);
-        } while (numberOfCut >= numberPiecesOfWood);
 
-        let begginingAdverbs = ['изначально', 'в начале', ' сначало', ' '].iz(2);
+        let numberOfCut = sluchch(3, 200, 1);
+        let numberPiecesOfWood = sluchch(numberOfCut + 1, 200, 1);
+        let begginingAdverbs = ['изначально', 'в начале', 'сначала', ' '].iz(2);
         let verbTakeVariants = ['взяли', 'было', 'имелось'].iz(2);
         let woodName = ['досок', 'брусьев', 'брёвен'].iz();
         let nameTypeOfCutFirst = ['распили', 'разреза', 'разруби', 'рассек'].iz();
@@ -16,15 +13,13 @@
         let numerAdverbs = ['несколько', 'некоторое количество', 'немало', 'сколько-то'].iz();
         let questionNumerAdverbs = ['Сколько', 'Какое количество'].iz();
 
-        let startText;
-        if (begginingAdverbs[0] === ' ') {
-            startText = verbTakeVariants[0].toZagl();
-        } else {
-            startText = begginingAdverbs[0].toZagl();
-        }
+        let startText = begginingAdverbs[0] === ' '
+            ? verbTakeVariants[0]
+            : begginingAdverbs[0] + ' ' + verbTakeVariants[0];
+
 
         NAtask.setTask({
-            text: startText + ' ' + numerAdverbs + ' ' + woodName + ' и их ' + nameTypeOfCutFirst + 'ли. Всего сделали ' + chislitlx(numberOfCut, 'поперечных ' + nameTypeOfCutSecond, '$') +
+            text: startText.toZagl() + ' ' + numerAdverbs + ' ' + woodName + ' и их ' + nameTypeOfCutFirst + 'ли. Всего сделали ' + chislitlx(numberOfCut, 'поперечных ' + nameTypeOfCutSecond, '$') +
                 ', в итоге получилось ' + chislitlx(numberPiecesOfWood, 'кусок', '$') + '. ' + questionNumerAdverbs + ' ' + woodName + ' ' + begginingAdverbs[1] + ' ' + verbTakeVariants[1] + '?',
             answers: numberPiecesOfWood - numberOfCut,
         });
