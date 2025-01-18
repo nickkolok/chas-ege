91b36f137127510a2d5b6a1549cc73c8fcf4d2ec
SugarHedgehog <sneginca.cat@gmail.com>
2025-01-18 19:10:51 +0300

    [zdn] [fix] - 348885

--- zdn/matoge2024/15/348885.js
+++ zdn/matoge2024/15/348885.js
@@ -1,7 +1,7 @@
-(function() {
-	retryWhileError(function() {
+(function () {
+	retryWhileError(function () {
 		NAinfo.requireApiVersion(0, 2);
-		
+
 		let triangle = new Triangle({
 			lengths: {
 				lengthAB: sl(5, 10),
@@ -12,7 +12,7 @@
 		genAssert(![triangle.lengthAB.round(), triangle.lengthBC.round(), triangle.lengthCA.round()].hasDubl(),
 			'Все стороны треугольника должны быть разными');
 
-		let paint1 = function(ctx) {
+		let paint1 = function (ctx) {
 			let h = 400;
 			let w = 400;
 
@@ -31,6 +31,7 @@
 			authors: ['Александра Суматохина'],
 		});
 
+		NAtask.modifiers.allDecimalsToStandard(/*true*/);
 		NAtask.modifiers.addCanvasIllustration({
 			width: 400,
 			height: 400,
