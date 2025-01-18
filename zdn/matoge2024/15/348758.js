3ff7c809711b250e6d158c65ef545ff68809de42
SugarHedgehog <sneginca.cat@gmail.com>
2025-01-18 19:10:36 +0300

    [zdn] [fix] - 348758

--- zdn/matoge2024/15/348758.js
+++ zdn/matoge2024/15/348758.js
@@ -1,5 +1,5 @@
-(function() {
-	retryWhileError(function() {
+(function () {
+	retryWhileError(function () {
 		NAinfo.requireApiVersion(0, 2);
 
 		let triangle = new Triangle({
@@ -20,7 +20,7 @@
 		let points = autoScale(triangle.vertices);
 		let letters = latbukv.slice(0, 3);
 
-		let paint1 = function(ctx) {
+		let paint1 = function (ctx) {
 			let h = 400;
 			let w = 400;
 
@@ -47,6 +47,7 @@
 		});
 		NAtask.modifiers.variativeABC(letters);
 
+		NAtask.modifiers.allDecimalsToStandard(/*true*/);
 		NAtask.modifiers.addCanvasIllustration({
 			width: 400,
 			height: 400,
