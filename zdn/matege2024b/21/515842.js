2f705ee1e2ac0a70fb32aebff3a7c733e86ae4e1
Колесникова Елена <luly3751@gmail.com>
2025-01-20 18:01:47 +0300

    issue 515842 edit 1

--- zdn/matege2024b/21/515842.js
+++ zdn/matege2024b/21/515842.js
@@ -3,14 +3,11 @@
     retryWhileError(function () {
         NAinfo.requireApiVersion(0, 2);
         let vaseColor = ['красн', 'жёлт', 'зелён', 'фиолетов', 'черн', 'оранжев', 'голуб', 'изумруд', 'бежев', 'бирюзов', 'ал', 'лилов', 'голуб'].iz(3);
-        let firstVaseCountFlower = sluchch(1, 30, 1);
-        let secondVaseCountFlower = sluchch(1, 30, 1);
-        let thirdVaseCountFlower = sluchch(1, 30, 1);
+        let typeOfFlowerInVases = sklonlxkand(['роза', 'гвоздника', 'ромашка', 'лилия', 'мак', 'ирис', 'лаванда', 'мимоза'].iz());
 
-        genAssert(firstVaseCountFlower !== secondVaseCountFlower &&
-            firstVaseCountFlower !== thirdVaseCountFlower &&
-            secondVaseCountFlower !== thirdVaseCountFlower,
-            "Значения ваз уникальны!");
+        let firstVaseCountFlower = sluchch(1, 30, 1);
+        let secondVaseCountFlower = slKrome([firstVaseCountFlower], 1, 30, 1);
+        let thirdVaseCountFlower = slKrome([firstVaseCountFlower, secondVaseCountFlower], 1, 30, 1);
 
         let leftOfThirdVase = firstVaseCountFlower + secondVaseCountFlower;
         let righttOfFirstVase = thirdVaseCountFlower + secondVaseCountFlower;
@@ -18,11 +15,11 @@
 
         NAtask.setTask({
             text:
-                'На прилавке цветочного магазина стоят три вазы с розами: ' + vaseColor[0] + 'ая, ' +
-                vaseColor[1] + 'ая, ' + vaseColor[2] + 'ая.' + ' Слева от ' + vaseColor[2] + 'ой ' + ' вазы ' + chislitlx(leftOfThirdVase, 'роза', '$') +
-                ', справа от ' + vaseColor[0] + 'ой ' + ' вазы ' + chislitlx(righttOfFirstVase, 'роза', '$') + '. ' +
-                'Всего в вазах ' + chislitlx(allFlowerInVases, 'роза', '$') + '. ' +
-                'Сколько роз в ' + vaseColor[1] + 'ой ' + 'вазе?',
+                'На прилавке цветочного магазина стоят три вазы с ' + typeOfFlowerInVases.tm + ': ' + vaseColor[0] + 'ая, ' +
+                vaseColor[1] + 'ая, ' + vaseColor[2] + 'ая.' + ' Слева от ' + vaseColor[2] + 'ой ' + ' вазы ' + chislitlx(leftOfThirdVase, typeOfFlowerInVases, '$') +
+                ', справа от ' + vaseColor[0] + 'ой ' + ' вазы ' + chislitlx(righttOfFirstVase, typeOfFlowerInVases, '$') + '. ' +
+                'Всего в вазах ' + chislitlx(allFlowerInVases, typeOfFlowerInVases, '$') + '. ' +
+                'Сколько ' + typeOfFlowerInVases.rm + ' в ' + vaseColor[1] + 'ой ' + 'вазе?',
             answers: secondVaseCountFlower,
         });
 
