c763f1e4cf9b5a2cb72ab10816f971ed73b7281f
Колесникова Елена <luly3751@gmail.com>
2025-01-22 14:44:06 +0300

    issue 512414 edit 1

--- zdn/matege2024b/1/512414.js
+++ zdn/matege2024b/1/512414.js
@@ -2,26 +2,24 @@
     'use strict';
     retryWhileError(function () {
         NAinfo.requireApiVersion(0, 2);
-        let speed = sl(0.5, 4, 0.5);
-        let driver = ["мотоцикл", "мотоциклист", "автомобиль", "автомобилист"].iz();
-        let firstTimePass = sl(15, 40, 1);
-        let minutes = sklonlxkand(['минута'].iz());
 
-        let firstDistance = speed * firstTimePass;
+        let theVehicle = sklonlxkand(['автомобиль', 'мотоцикл', 'велосипед', 'электросамокат', 'гироскутер', 'мотоциклист', 'велосипедист', 'гонщик', 'грузовик', 'автомобилист'].iz());
+        let speedInterval = window.adequateSpeed.vehicle[theVehicle.ie];
+        let speed = sluchch(speedInterval[0], speedInterval[1], 1);
+
+        let firstTimePass = sl(15, 40, 1);
+        let firstDistance = (speed / 60).ceil() * firstTimePass;
         let secondTimePass = firstTimePass + sl(5, 30, 3);
 
         NAtask.setTask({
             text:
-                driver.toZagl() + ' проехал ' + chislitlx(firstDistance, 'километр', '$') + ' за ' + chislitlx(firstTimePass, minutes, '$') + '. ' +
-                'Сколько километров он проедет за ' + chislitlx(secondTimePass, minutes, '$') + ', ' +
+                theVehicle.ie.toZagl() + ' проехал ' + chislitlx(firstDistance, 'километр', '$') + ' за ' + chislitlx(firstTimePass, 'минута', 'v$') + '. ' +
+                'Сколько километров он проедет за ' + chislitlx(secondTimePass, 'минута', 'v$') + ', ' +
                 'если будет ехать с той же скоростью?',
             answers: '$' + (secondTimePass * firstDistance) / firstTimePass + '$',
         });
 
-    }, 100);
+    }, 10);
 })();
-
 //https://mathb-ege.sdamgia.ru/test?likes=512414
 //zer00player
-
-
