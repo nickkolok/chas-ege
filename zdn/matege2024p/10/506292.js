d18fe0f0f9f9b3b47e79832ff0863cfbbe8b4b59
Алендарь Сергей <sergey.alendar@mail.ru>
2025-01-22 23:07:13 +0300

    Файл 506292.js изменён

--- zdn/matege2024p/10/506292.js
+++ zdn/matege2024p/10/506292.js
@@ -2,29 +2,30 @@ let basePrice = sluchch(1000, 5000, 100);
 let difference = sluchch(200, 900, 100);
 let measurement = sluchch(4, 15);
 let slOwner = sluchch(0, 4);
-let slObject = sluchch(0, 5);
+let slWell = sluchch(0, 5);
 let slWorker = sluchch(0, 3);
 let measurements = ' глубиной ';
 
 let progression = new ArithmeticProgression(basePrice, difference);
 
-if (slObject > 0) {
-	measurements = sl1() === 0 ? ' шириной ' : ' длиной ';
+if (slWell > 0) {
+	measurements = [' шириной ', ' длиной '].iz();
 }
 
-let Worker = [' рабочий', ' трудящийся', 'работник', 'сотрудник'];
+let Worker = ['рабочий', 'землекоп', 'работник', 'сотрудник'];
 let Owner = ['хозяин', 'фермер', 'землевладелец', 'собственник', 'владелец'];
-let Object = ['колодец', 'скважину', 'яму', 'шахту', 'траншею', 'канаву'];
+let Well = ['колодец', 'скважину', 'яму', 'шахту', 'траншею', 'канаву'];
+let sklonWorker = sklonlxkand(Worker[slWorker]);
 
 NAtask.setTask({
-	text: Owner[slOwner].toZagl() + ' договорился с ' + sklonlxkand(Worker[slWorker]).tm + ', что они выкопают ему ' +
-		Object[slObject] +
+	text: Owner[slOwner].toZagl() + [' договорился', ' условился'] + ' с ' + sklonWorker.tm + ', что они ' + 
+		['выкопают', 'откопают', 'сделают'].iz() + ' ему ' + Well[slWell] +
 		' на следующих условиях: за первый метр он заплатит им ' +
 		chislitlx(basePrice, 'рубль') +
-		', а за каждый следующий метр  — на ' + chislitlx(difference, 'рубль') +
+		', а за каждый ' + ['следующий', 'последующий'].iz() + ' метр  — на ' + chislitlx(difference, 'рубль') +
 		'  больше, чем за предыдущий. Сколько рублей ' + Owner[slOwner] + ' должен будет ' + 
-		['заплатить ', 'выплатить ', 'отдать '].iz() + sklonlxkand(Worker[slWorker]).dm +
-		', если они ' + ['выкопают ', 'откопают ', 'сделают '].iz() + Object[slObject] + measurements +
+		['заплатить ', 'выплатить ', 'отдать '].iz() + sklonWorker.dm +
+		', если они ' + ['выкопают ', 'откопают ', 'сделают '].iz() + Well[slWell] + measurements +
 		chislitlx(measurement, 'метр') + ' ?',
 	answers: progression.sum(measurement),
 });
