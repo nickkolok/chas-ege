34856eb8b2cea4e8346fb105007a01b3b2bc2447
Колесникова Елена <luly3751@gmail.com>
2025-01-23 16:14:08 +0300

    [zdn][new] 282848 -fix #1814 edit 2

--- zdn/matege2024b/1/282848.js
+++ zdn/matege2024b/1/282848.js
@@ -5,34 +5,37 @@
         NAinfo.requireApiVersion(0, 2);
 
         let personPayment = sl(1000, 5000, 1000);
-        let dopPriceInPennies = sl(10, 90, 10);
+        let dopPriceInKopeiki = sl(10, 90, 10);
         let priceInRuble = sl(25, 70, 1);
         let numberOfLiters = sl(20, 70, 1);
 
-        let pricePerLiter = (priceInRuble + dopPriceInPennies / 100).toFixed(1);
-        pricePerLiter = parseFloat(pricePerLiter);
-        let paymentForPurchase = pricePerLiter * numberOfLiters;
+        let paymentForPurchase = (priceInRuble + dopPriceInKopeiki / 100) * numberOfLiters;
         genAssert(personPayment > paymentForPurchase, "У клиента должно хватать денег на оплату");
 
         let change = personPayment - paymentForPurchase;
         let changeRubles = Math.floor(change);
-        let changePennies = Math.round((change - changeRubles) * 100);
+        let changeKopeiki = Math.round((change - changeRubles) * 100);
 
-        if (changePennies === 100) {
+        if (changeKopeiki === 100) {
             changeRubles++;
-            changePennies = 0;
+            changeKopeiki = 0;
         }
 
         let nameOfPerson = om.maleNames.iz();
-        let gasStationName = ['ФастОйл', 'Лукойл', 'Топлайн', 'Standart oil', 'Понедельник начинается в субботу', 'Трудно быть дальнобойщиком', 'Shell', 'ГазON', 'Кафе у уставшего таксиста', 'Сплин', 'Дайте бензин(!)', 'Пикник на обочине'].iz();
+        let gasStationName = ['ФастОйл', 'Лукойл', 'Топлайн', 'Standart oil', 'Понедельник начинается в субботу', 'Трудно быть дальнобойщиком', 'Shell',
+            'ГазON', 'Кафе у уставшего таксиста', 'Сплин', 'Дайте бензин(!)', 'Пикник на обочине'].iz();
+
+        let changeText = 'Сдачи клиент получил ' + '$' + changeRubles + '$' + ' руб.';
+        if (changeKopeiki > 0) {
+            changeText += ' и ' + '$' + changeKopeiki + '$' + ' коп.';
+        }
 
         NAtask.setTask({
             text:
                 'На автозаправке «' + gasStationName + '» ' + nameOfPerson + ' отдал кассиру ' + chislitlx(personPayment, 'рубль', '$') +
                 ' и попросил залить бензин до полного бака. ' +
-                'Цена бензина ' + priceInRuble + ' руб. ' + dopPriceInPennies + ' коп. за литр. ' +
-                'Сдачи клиент получил ' + changeRubles + 'руб. и ' + changePennies + ' коп. ' +
-                'Сколько литров бензина было залито в бак?',
+                'Цена бензина ' + '$' + priceInRuble + '$' + ' руб. ' + dopPriceInKopeiki + ' коп. за литр. ' +
+                changeText + ' ' + 'Сколько литров бензина было залито в бак?',
             answers: numberOfLiters,
         });
 
