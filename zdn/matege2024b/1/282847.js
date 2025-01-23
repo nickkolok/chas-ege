409e1ae90c1a4b3a7351496078a96bc52f80aa88
Колесникова Елена <luly3751@gmail.com>
2025-01-23 16:00:17 +0300

    [zdn][new] 282847 -fix #1812 edit 1

--- zdn/matege2024b/1/282847.js
+++ zdn/matege2024b/1/282847.js
@@ -9,20 +9,17 @@
         let priceInRuble = sl(25, 70, 1);
         let numberOfLiters = sl(20, 70, 1);
 
-        let pricePerLiter = (priceInRuble + dopPriceInPennies / 100).toFixed(1);
-        pricePerLiter = parseFloat(pricePerLiter);
-        let paymentForPurchase = pricePerLiter * numberOfLiters;
+        let paymentForPurchase = (priceInRuble + dopPriceInPennies / 100) * numberOfLiters;
         genAssert(personPayment > paymentForPurchase, "У клиента должно хватать денег на оплату");
 
         let nameOfPerson = om.maleNames.iz();
         let gasStationName = ['Опытный Гонщик', 'Лукойл', 'НеГаз', 'Дальнобойщик', 'Верный выбор', 'Big Boss', 'Shell', 'ГазON', 'AvtoFood'].iz();
 
-
         NAtask.setTask({
             text:
                 'На автозаправке «' + gasStationName + '» ' + nameOfPerson + ' отдал кассиру ' + chislitlx(personPayment, 'рубль', '$') +
                 ' и залил в бак ' + chislitlx(numberOfLiters, 'литр', '$') +
-                ' бензина по цене ' + priceInRuble + ' руб. и ' + dopPriceInPennies + 'коп. за литр. ' +
+                ' бензина по цене ' + '$' + priceInRuble + '$' + ' руб. и ' + '$' + dopPriceInPennies + '$' + ' коп. за литр. ' +
                 'Сколько рублей сдачи он должен получить у кассира?',
             answers: (personPayment - paymentForPurchase),
         });
