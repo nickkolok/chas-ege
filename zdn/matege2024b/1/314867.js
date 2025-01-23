a4022e463a696222455f9e3a77e189128fced837
Колесникова Елена <luly3751@gmail.com>
2025-01-23 17:22:11 +0300

    [zdn][new] 314867 -fix #1815 edit 3

--- zdn/matege2024b/1/314867.js
+++ zdn/matege2024b/1/314867.js
@@ -4,25 +4,20 @@
     retryWhileError(function () {
         NAinfo.requireApiVersion(0, 2);
 
-        let firstMonthConsumption = sl(100, 1200, 1);
-        let secondMonthConsumption = firstMonthConsumption + sl(10, 120, 1);
-
-        let numberOfLiters = secondMonthConsumption - firstMonthConsumption;
-
-        function getSequentialMonths() {
-            let months = om.months;
-            let firstIndex = sl(0, months.length - 1, 1);
-            let secondIndex = (firstIndex + 1) % months.length;
-            return [months[firstIndex], months[secondIndex]];
-        }
-        let [firstMonth, secondMonth] = getSequentialMonths();
+        let firstMonthConsumption = sl(12, 1000, 1);
+        let secondMonthConsumption = firstMonthConsumption + sl(5, 120, 1);
 
         let dopPriceInKopeki = sl(10, 90, 10);
         let priceInRuble = sl(50, 150, 1);
 
-        let pricePerLiter = (priceInRuble + dopPriceInKopeki / 100).toFixed(1);
-        pricePerLiter = parseFloat(pricePerLiter);
-        let paymentForPurchase = pricePerLiter * numberOfLiters;
+        let numberOfLiters = secondMonthConsumption - firstMonthConsumption;
+        let paymentForPurchase = (priceInRuble + dopPriceInKopeki / 100) * numberOfLiters;
+
+        let months = om.months;
+        let firstIndex = sl(0, months.length - 1, 1);
+        let secondIndex = (firstIndex + 1) % months.length;
+        let firstMonth = months[firstIndex];
+        let secondMonth = months[secondIndex];
 
         let nameOfPerson = om.maleNames.iz();
         let typeOfWater = ['горячей', 'холодной'].iz();
@@ -34,7 +29,8 @@
                 ' счётчик показывал расход ' + '$' + firstMonthConsumption + '$' + ' куб.м воды, ' +
                 'а ' + chislitlx(1, secondMonth, 'r$') + ' — ' + '$' + secondMonthConsumption + '$' + ' куб.м. ' +
                 'Какую сумму должен заплатить ' + nameOfPerson + ' за ' + firstMonth + ', ' +
-                'если цена одного куб.м. ' + typeOfWater + ' воды составляет ' + '$' + priceInRuble + '$' + ' руб. и ' + '$' + dopPriceInKopeki + '$' + ' коп.? Ответ дайте в рублях.',
+                'если цена одного куб.м. ' + typeOfWater + ' воды составляет ' + '$' + priceInRuble + '$' + ' руб. и ' +
+                '$' + dopPriceInKopeki + '$' + ' коп.? Ответ дайте в рублях.',
             answers: paymentForPurchase,
         });
 
