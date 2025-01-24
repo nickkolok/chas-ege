41f728fbea6cb575568665f5d24da31381171f21
VeronikaKit <166473321+VeronikaKit@users.noreply.github.com>
2025-01-24 20:39:00 +0300

    Update 311381.js

--- zdn/matoge2024/9/311381.js
+++ zdn/matoge2024/9/311381.js
@@ -1,8 +1,8 @@
 (function() { retryWhileError(function() {
     let c=sluchch(1,30).pm();
-    let a=slKrome(c,1,30).pm();
+    let a=slKrome(Math.abs(c),1,30).pm();
     let b=sluchch(1,30).pm();
-    let e=slKrome(b,1,30).pm();
+    let e=slKrome(Math.abs(b),1,30).pm();
     let d=(b*c-e*a)/(a-c);
     genAssertZ1000(d, 'Корень не должен быть слишком дробным');
     NAtask.setEquationTask({
