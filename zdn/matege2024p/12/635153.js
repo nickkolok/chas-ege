05f18ea3822d61f577edcd310c035aa4446aec80
Алендарь Сергей <sergey.alendar@mail.ru>
2025-01-26 19:45:29 +0300

    В файле 635153.js изменена генерация интервалов и добавлены функции tg и ctg

--- zdn/matege2024p/12/635153.js
+++ zdn/matege2024p/12/635153.js
@@ -1,14 +1,17 @@
 (function() {
 	retryWhileError(function() {
 		'use strict';
-		let a = sl(1, 9);
-		let b = sl(1, 9);
-		let arr1 = ['+', '-'];
-		lett sincos = ['sin','cos'].iz();
+		let a = sl(2, 20, 2);
+		let b = sl(1, 25);
+		let sign = ['+', '-'];
+		let triangleSideRatios = ['sin', 'cos', 'tg', 'ctg'].iz();
+		let start = ['-2pi', '-pi', '-pi/2', '-pi/3', '-pi/4', '0'].iz();
+		let end = ['2pi', 'pi', 'pi/2', 'pi/3', 'pi/4', '0'].iz();
 		NAtask.setMinimaxFunctionTask({
-			expr: '1/((' + sincos + '(x))^2' + arr1.iz() + a + sincos + '(x)' + arr1.iz() + b + ')',
-			leftEnd: '-pi',
-			rightEnd: 'pi',
+			expr: '1/((' + triangleSideRatios + '(x))^2' + sign.iz() +
+				a + triangleSideRatios + '(x)' + sign.iz() + b + ')',
+			leftEnd: start,
+			rightEnd: end,
 			primaryStep: 0.1,
 			secondaryStep: 0.0001,
 			forbidAnalys: true,
@@ -16,5 +19,7 @@
 		});
 	}, 1000);
 })();
+
 //635153
+
 
