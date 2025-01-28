26bed4dfe7a8cd9a399c61e7eb1065804ceeeb42
Колесникова Елена <luly3751@gmail.com>
2025-01-28 15:13:51 +0300

    [zdn][new] 531736 -fix #1820 edit 1

--- zdn/matege2024b/1/531736.js
+++ zdn/matege2024b/1/531736.js
@@ -4,61 +4,28 @@
         NAinfo.requireApiVersion(0, 2);
 
 
-        let useGigabytes = Math.random() < 0.5;
-        let useMbyteMinutes = !useGigabytes && Math.random() < 0.5;
+        let rand = sl(0, 2);
+        let timeUnits = sklonlxkand(['минута', 'минута', 'секунда']);
+        let dataUnits = ['Гбайт', 'Мбайт', 'Мбайт'];
+        let firstTimeFunction = [sl(5, 30, 1), sl(5, 30, 1), sl(30, 120, 1)];
+        let secondTimeFunction = [sl(10, 60, 1), sl(10, 60, 1), sl(50, 300, 1)];
+        let ratioFunction = [parseFloat((Math.random() * (0.5 - 0.1) + 0.1).toFixed(1)), sl(20, 750, 1), sl(2, 12, 1)];
 
-        if (useGigabytes) {
+        let firstTime = firstTimeFunction[rand];
+        let secondTime = secondTimeFunction[rand];
+        let ratio = ratioFunction[rand];
+        let firstData = parseFloat((firstTime * ratio).toFixed(1));
+        let secondData = parseFloat((secondTime * ratio).toFixed(1));
 
-            let firstMinutesForGbyte = sl(5, 30, 1);
-            let secondMinutesForGbyte = sl(10, 60, 1);
-            let ratioToGbyte = parseFloat((Math.random() * (0.5 - 0.1) + 0.1).toFixed(1));
-            let firstGbyte = parseFloat((firstMinutesForGbyte * ratioToGbyte).toFixed(1));
-            let secondGbyte = parseFloat((secondMinutesForGbyte * ratioToGbyte).toFixed(1));
+        genAssert(firstTime !== secondTime, "Время скачивания не должно совпадать");
 
-            genAssert(firstMinutesForGbyte !== secondMinutesForGbyte, "Время скачивания не должно совпадать");
-
-            NAtask.setTask({
-                text:
-                    'Файл размером ' + chislitlx(firstGbyte, 'Гбайт', '$') + ' скачался за ' + chislitlx(firstMinutesForGbyte, 'минута', 'v$') +
-                    '(скорость загрузки считайте постоянной).' +
-                    'За сколько минут скачается файл размером ' + chislitlx(secondGbyte, 'Гбайт', '$') + ', если скорость загрузки останется прежней?',
-                answers: secondMinutesForGbyte,
-            });
-        } else if (useMbyteMinutes) {
-
-            let firstMinutesForMbyte = sl(5, 30, 1);
-            let secondMinutesForMbyte = sl(10, 60, 1);
-            let ratioToMbyte = sl(20, 750, 1);
-            let firstMbyte = firstMinutesForMbyte * ratioToMbyte;
-            let secondMbyte = secondMinutesForMbyte * ratioToMbyte;
-
-            genAssert(firstMinutesForMbyte !== secondMinutesForMbyte, "Время скачивания не должно совпадать");
-
-            NAtask.setTask({
-                text:
-                    'Файл размером ' + chislitlx(firstMbyte, 'Мбайт', '$') + ' скачался за ' + chislitlx(firstMinutesForMbyte, 'минута', 'v$') +
-                    '(скорость загрузки считайте постоянной).' +
-                    'За сколько минут скачается файл размером ' + chislitlx(secondMbyte, 'Мбайт', '$') + ', если скорость загрузки останется прежней?',
-                answers: secondMinutesForMbyte,
-            });
-        } else {
-
-            let firstSeconds = sl(30, 120, 1);
-            let secondSeconds = sl(50, 300, 1);
-            let ratioToMbyte = sl(2, 12, 1);
-            let firstMbyte = firstSeconds * ratioToMbyte;
-            let secondMbyte = secondSeconds * ratioToMbyte;
-
-            genAssert(firstSeconds !== secondSeconds, "Время скачивания не должно совпадать");
-
-            NAtask.setTask({
-                text:
-                    'Файл размером ' + chislitlx(firstMbyte, 'Мбайт', '$') + ' скачался за ' + chislitlx(firstSeconds, 'секунда', 'v$') +
-                    '(скорость загрузки считайте постоянной).' +
-                    'За сколько секунд скачается файл размером ' + chislitlx(secondMbyte, 'Мбайт', '$') + ', если скорость загрузки останется прежней?',
-                answers: secondSeconds,
-            });
-        }
+        NAtask.setTask({
+            text:
+                'Файл размером ' + chislitlx(firstData, dataUnits[rand], '$') + ' скачался за ' + chislitlx(firstTime, timeUnits[rand], 'v$') +
+                '(скорость загрузки считайте постоянной).' +
+                'За сколько ' + timeUnits[rand].rm + ' скачается файл размером ' + chislitlx(secondData, dataUnits[rand], '$') + ', если скорость загрузки останется прежней?',
+            answers: secondTime,
+        });
     }, 100);
 })();
 //https://mathb-ege.sdamgia.ru/test?likes=531736
