a53ffa1157a25ae8cb916175d1495fcdeb2eba01
SugarHedgehog <sneginca.cat@gmail.com>
2025-01-30 00:14:30 +0300

    [zdn] [fix] - remove brackets & minE

--- zdn/mat2014/B11/8.js
+++ zdn/mat2014/B11/8.js
@@ -6,7 +6,7 @@
 		b = slKrome(isZ, 0.1, 4.9, 0.1),
 		f = sluchch(1, 4),
 		d = slKrome(f, 1, 4),
-		g = sluchch(1, [f,d][[f,d].min()] - 1);
+		g = sluchch(1, [f,d].minE() - 1);
 	(a - 1) * (a - 2) * (a - 4)||(f -= sluchch(1, 4));
 	(c - 1) * (c - 2) * (c - 4)||(d -= sluchch(1, 4));
 
@@ -15,12 +15,11 @@
 			   (b+f).toFixedLess(6).toStandart()+'}\\cdot{'+c+'^{'+
 			   (b+d).toFixedLess(6).toStandart()+'}}}{'+(a*c)+'^{'+
 			   (b+g).toFixedLess(6).toStandart()+'}}$$').plusminus(),
-		answers: [''+(a.pow(f-g)*c.pow(d-g)),
-		],
+		answers: a.pow(f-g)*c.pow(d-g),
 		tags: {
 				'log': 0,
 				'prz': 0,
-				'drs': 0,
+				'drs': 1,
 				'tri': 0,
 			},
 	});
