2a1b98b43dad92d2b697c9936ef49361fc2b0a45
SugarHedgehog <sneginca.cat@gmail.com>
2025-01-30 00:48:09 +0300

    [zdn] [fix] - remove brackets and tags

--- zdn/mat2014/B11/10.js
+++ zdn/mat2014/B11/10.js
@@ -5,17 +5,8 @@
 	var b=sluchch(1,20);
 	var c=sluchch(1,20);
 
-	var y='{\\frac{\\sqrt{'+(a*b*0.1).ts()+'}\\cdot\\sqrt{'+(a*c*0.1).ts()+'}}{\\sqrt{'+(c*b*0.01).ts()+'}}}';
 	chas2.task.setTask({
-		text: ('Найдите значение выражения $$'+y+'$$').plusminus(),
-		answers: [''+a,
-			],
-		tags: {
-			'log': 0,
-			'prz': 0,
-			'drs': 0,
-			'tri': 0,
-
-		},
+		text: ('Найдите значение выражения $${\\frac{\\sqrt{'+(a*b*0.1).ts()+'}\\cdot\\sqrt{'+(a*c*0.1).ts()+'}}{\\sqrt{'+(c*b*0.01).ts()+'}}}$$').plusminus(),
+		answers: a,
 	});
 })();
