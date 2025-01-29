3ba6df132a245b520298d86a49dab04d29bcbd3a
SugarHedgehog <sneginca.cat@gmail.com>
2025-01-30 01:14:35 +0300

    [zdn] [fix] - ship

--- zdn/mat2014/B1/3.js
+++ zdn/mat2014/B1/3.js
@@ -4,13 +4,16 @@
 	var c=sluchch(10,100);
 	var a=sluchch(100,999);
 	var b=sluchch(10,100);
+	
+	var ship = sklonlxkand(chaslib.sets.ships.iz());
 
 	NAtask.setTask({
-		text : chaslib.sets.ships[t].toZagl()+' перевозит '+a+' пассажиров и '+b+' членов экипажа. '+
-		'В целях безопасности на '+chaslib.sets.shipsPrepositional[t]+
+		text : ship.ie.toZagl()+' перевозит '+a+' пассажиров и '+b+' членов экипажа. '+
+		'В целях безопасности на '+ship.pe+
 		' размещены спасательные шлюпки, каждая из которых вмещает '+c+' человек. '+
-		'Какое наименьшее количество шлюпок должно быть на '+chaslib.sets.shipsPrepositional[t]+'?',
+		'Какое наименьшее количество шлюпок должно быть на '+ship.pe+'?',
 		answers : ((a+b)/c).ceil(),
+		authors: ['Николай Авдеев', 'dev-null', 'Суматохина Александра'],
 	});
 })();
 //Обзад 26617
