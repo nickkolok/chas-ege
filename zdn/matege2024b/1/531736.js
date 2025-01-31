df9e5f5a495914048a07c25c7377f8c165b80c24
Колесникова Елена <luly3751@gmail.com>
2025-01-31 18:09:36 +0300

    [zdn][new] 531736 -fix #1820 edit 4

--- zdn/matege2024b/1/531736.js
+++ zdn/matege2024b/1/531736.js
@@ -5,8 +5,8 @@
 
 
         let rand = sl(0, 2);
-        let timeUnits = sklonlxkand(['минута', 'минута', 'секунда']);
-        let dataUnits = ['Гбайт', 'Мбайт', 'Мбайт'];
+        let timeUnits = sklonlxkand(['минута', 'минута', 'секунда'])[rand];
+        let dataUnits = ['Гбайт', 'Мбайт', 'Мбайт'][rand];
         let firstTime = [sl(5, 30, 1), sl(5, 30, 1), sl(30, 120, 1)][rand];;
         let secondTime = [sl(10, 60, 1), sl(10, 60, 1), sl(50, 300, 1)][rand];
         let ratio = [sl(0.1, 0.5, 0.1), sl(20, 750, 1), sl(2, 12, 1)][rand];
@@ -17,9 +17,9 @@
 
         NAtask.setTask({
             text:
-                'Файл размером ' + chislitlx(firstData, dataUnits[rand], '$') + ' скачался за ' + chislitlx(firstTime, timeUnits[rand], 'v$') +
+                'Файл размером ' + chislitlx(firstData, dataUnits, '$') + ' скачался за ' + chislitlx(firstTime, timeUnits, 'v$') +
                 '(скорость загрузки считайте постоянной).' +
-                'За сколько ' + timeUnits[rand].rm + ' скачается файл размером ' + chislitlx(secondData, dataUnits[rand], '$') + ', если скорость загрузки останется прежней?',
+                'За сколько ' + timeUnits.rm + ' скачается файл размером ' + chislitlx(secondData, dataUnits, '$') + ', если скорость загрузки останется прежней?',
             answers: secondTime,
         });
     }, 100);
