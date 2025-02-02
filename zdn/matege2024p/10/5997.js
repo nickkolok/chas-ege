71b72a0dc43a18a34300a9cf9313092aea2f16dd
SugarHedgehog <sneginca.cat@gmail.com>
2025-02-02 21:27:08 +0300

    [zdn] [fix] - 5997

--- zdn/matege2024p/10/5997.js
+++ zdn/matege2024p/10/5997.js
@@ -2,13 +2,11 @@
 	let s=sl(20, 300, 1);
 	let n=sl(1, 5, 1);
 	let x=sl(1,15,1);
-	let v=slKrome(x, 3, 40);
-	let b=sl(1, 23, 1);
-        let a=b-n-s/(v+x)-s/(v-x);
-	genAssert(b>a,'Время отправления не должно быть больше времени прибытия');
-	genAssert(Number.isInteger(a),'Время не может быть дробным');
-	genAssert(a>0,'Время отправления не может быть отрицательным');
-	genAssert(a<15,'Время отправления не может быть слишком большим');
+	let v=sl(x+1, 40);
+	let a=sl(1, 12, 1);
+    let b=a+n+s/(v+x)+s/(v-x);
+	genAssert(Number.isInteger(b),'Время не может быть дробным');
+	genAssert(b<24,'Время отправления не может быть слишком большим');
 	let the_activeFloatingVehicle = sklonlxkand( ["лодка","байдарка","баржа","яхта","моторная лодка"].iz());
 	let the_humanSettlementDestination = sklonlxkand(["пункт","город","село"].iz()); 
 	let the_orderToFind = decor.orderToFind.iz(); 
