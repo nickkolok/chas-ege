1ea2ff34245e7267e593956e4bfcc613f3eba314
SugarHedgehog <sneginca.cat@gmail.com>
2025-02-05 19:33:51 +0300

    replace list

--- zdn/misc_text_tasks_movement_on_water/misc_text_tasks_movement_on_water.js
+++ zdn/misc_text_tasks_movement_on_water/misc_text_tasks_movement_on_water.js
@@ -5,3 +5,17 @@ window.nabor.importFrom({
  	adres: '../zdn/misc_text_tasks_movement_on_water/',
 	name: 'misc_text_tasks_movement_on_water',
 });
+
+/*"9 скорость_первого_теплохода speed_of_first"
+27482
+"7 суммарное_расстояние total_distance"
+"7 скорость_течения river_speed"
+"7 скорость_транспорта boat_speed"
+"8 скорость_течения river_speed"
+"8 собственная_скорость boat_speed"
+5997
+523375
+5999
+525375
+99602  
+26590*/
