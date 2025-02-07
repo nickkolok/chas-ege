25a401d6134186dc37b8252a6b6c4c17cf7ccaf2
SugarHedgehog <sneginca.cat@gmail.com>
2025-02-07 14:29:36 +0300

    [zdn] [up] - use mathjs

--- zdn/matege2024p/2/64485003.js
+++ zdn/matege2024p/2/64485003.js
@@ -1,105 +1,55 @@
-(function() {
-	retryWhileError(function() {
+(function () {
+	retryWhileError(function () {
 		NAinfo.requireApiVersion(0, 2);
-		let letter = om.latbukv.iz(3);
-		letter = letter.map((element) => element.toLowerCase());
+		let letter = om.smallLatinLetters.iz(3);
 
-		let vectorA = generateMatrix(1, 4, 1, 13).iz();
-		let vectorB = generateMatrix(1, 4, 1, 13).iz();
-		let vectorC = generateMatrix(1, 4, 1, 13).iz();
+		let coordVectorA = generateMatrix(1, 4, 1, 13).iz();
+		let coordVectorB = generateMatrix(1, 4, 1, 13).iz();
+		let coordVectorC = generateMatrix(1, 4, 1, 13).iz();
 
-		let check1 = [{
-			x: vectorA[0],
-			y: vectorA[1]
-		}, {
-			x: vectorA[2],
-			y: vectorA[3]
-		}, {
-			x: vectorB[0],
-			y: vectorB[1]
-		}, {
-			x: vectorB[2],
-			y: vectorB[3]
-		}].mt_otrPeres();
+		genAssert(![{ x: coordVectorA[0], y: coordVectorA[1] }, { x: coordVectorA[2], y: coordVectorA[3] }, { x: coordVectorB[0], y: coordVectorB[1] }, { x: coordVectorB[2], y: coordVectorB[3] }].mt_otrPeres(), 'Вектора A и B пересекаются');
+		genAssert(![{ x: coordVectorA[0], y: coordVectorA[1] }, { x: coordVectorA[2], y: coordVectorA[3] }, { x: coordVectorC[0], y: coordVectorC[1] }, { x: coordVectorC[2], y: coordVectorC[3] }].mt_otrPeres(), 'Вектора A и C пересекаются');
+		genAssert(![{ x: coordVectorB[0], y: coordVectorB[1] }, { x: coordVectorB[2], y: coordVectorB[3] }, { x: coordVectorC[0], y: coordVectorC[1] }, { x: coordVectorC[2], y: coordVectorC[3] }].mt_otrPeres(), 'Вектора B и C пересекаются');
 
-		genAssert(!check1, 'Вектора A и B пересекаются');
+		//Вычисление координат векторов
+		let vectorA = math.matrix(math.subtract([coordVectorA[2], coordVectorA[3]], [coordVectorA[0], coordVectorA[1]]));
+		let vectorB = math.matrix(math.subtract([coordVectorB[2], coordVectorB[3]], [coordVectorB[0], coordVectorB[1]]));
+		let vectorC = math.matrix(math.subtract([coordVectorC[2], coordVectorC[3]], [coordVectorC[0], coordVectorC[1]]));
 
-		let check2 = [{
-			x: vectorA[0],
-			y: vectorA[1]
-		}, {
-			x: vectorA[2],
-			y: vectorA[3]
-		}, {
-			x: vectorC[0],
-			y: vectorC[1]
-		}, {
-			x: vectorC[2],
-			y: vectorC[3]
-		}].mt_otrPeres();
+		// Проверка совпадения векторов
+		genAssert(!math.deepEqual(vectorA, vectorB), 'Вектора A и B совпадают');
+		genAssert(!math.deepEqual(vectorA, vectorC), 'Вектора A и C совпадают');
 
-		genAssert(!check2, 'Вектора A и C пересекаются');
+		// Проверка длин векторов
+		genAssert(math.norm(vectorA) > 3, 'Вектор A слишком маленький');
+		genAssert(math.norm(vectorB) > 3, 'Вектор B слишком маленький');
+		genAssert(math.norm(vectorC) > 3, 'Вектор C слишком маленький');
 
-		let check3 = [{
-			x: vectorB[0],
-			y: vectorB[1]
-		}, {
-			x: vectorB[2],
-			y: vectorB[3]
-		}, {
-			x: vectorC[0],
-			y: vectorC[1]
-		}, {
-			x: vectorC[2],
-			y: vectorC[3]
-		}].mt_otrPeres();
-
-		genAssert(!check3, 'Вектора B и C пересекаются');
-
-		let coordVect = (V) => [V[2] - V[0], V[3] - V[1]];
-
-		let coordA = coordVect(vectorA);
-		let coordB = coordVect(vectorB);
-		let coordC = coordVect(vectorC);
-
-		genAssert(!coordA.equalAr(coordB), 'Вектора A и B совпадают');
-		genAssert(!coordA.equalAr(coordC), 'Вектора A и C совпадают');
-
-		let lengthA = coordA.map((elem) => elem.pow(2)).sum();
-		let lengthB = coordB.map((elem) => elem.pow(2)).sum();
-		let lengthC = coordC.map((elem) => elem.pow(2)).sum();
-
-		genAssert(lengthA.sqrt() > 3, 'Вектор A слишком маленький');
-		genAssert(lengthB.sqrt() > 3, 'Вектор B слишком маленький');
-		genAssert(lengthC.sqrt() > 3, 'Вектор C слишком маленький');
-
+		// Создание коэффициентов
 		let coeffs = [
 			[sl(1, 10).pm(), sl(2, 20)],
 			[sl(1, 10).pm(), sl(2, 20)],
 			[sl(1, 10).pm(), sl(2, 20)]
 		];
-		let vectors = [coordA, coordB, coordC];
-		vectors = vectors.map((elem, index) => objUmn(elem, coeffs[index][0] / coeffs[index][1]));
+		
+		let vectors = [vectorA, vectorB, vectorC].map((elem, index) =>
+			math.multiply(elem, coeffs[index][0] / coeffs[index][1])
+		);
 
-		let answ = objSum(vectors[1], vectors[2]);
-
-		answ = vectors[0][0] * answ[0] + vectors[0][1] * answ[1];
-
+		// Вычисление скалярного произведения
+		let answ = math.multiply(vectors[0], math.add(vectors[1], vectors[2]));
 		genAssertZ1000(answ, 'Нецелое скалярное произведение');
 
+		let condition = (coeffs[0][0].texrndfrac(coeffs[0][1]) + '\\vec{' + letter[0] + '}\\cdot(') + (coeffs[1][0].texrndfrac(coeffs[1][1]) + '\\vec{' + letter[1] + '}' + '+'.esli(coeffs[2][0] > 0) + coeffs[2][0].texrndfrac(coeffs[2][1]) + '\\vec{' + letter[2] + '})');
 
-		let condition = (coeffs[0][0].texrndfrac(coeffs[0][1]) + '\\vec{' + letter[0] + '}\\cdot(');
-		condition += (coeffs[1][0].texrndfrac(coeffs[1][1]) + '\\vec{' + letter[1] + '}' + '+'.esli(coeffs[2][0] > 0) +
-			coeffs[2][0].texrndfrac(coeffs[2][1]) + '\\vec{' + letter[2] + '})');
-
-		let paint1 = function(ctx) {
+		let paint1 = function (ctx) {
 			ctx.translate(-320, 0);
 
 			let scale = 25;
 
-			vectorA = vectorA.map((num) => num * scale);
-			vectorB = vectorB.map((num) => num * scale);
-			vectorC = vectorC.map((num) => num * scale);
+			coordVectorA = coordVectorA.map((num) => num * scale);
+			coordVectorB = coordVectorB.map((num) => num * scale);
+			coordVectorC = coordVectorC.map((num) => num * scale);
 			let h = 700;
 			let w = 700;
 			//Оси координат
@@ -111,35 +61,31 @@
 				y1: '1',
 				sh1: 16,
 			}, 25);
-
+			
 			ctx.scale(1, -1);
-
 			ctx.lineWidth = 3;
 			ctx.strokeStyle = om.secondaryBrandColors;
-			ctx.drawArrow(vectorA[0], vectorA[1], vectorA[2], vectorA[3]);
+			ctx.drawArrow(coordVectorA[0], coordVectorA[1], coordVectorA[2], coordVectorA[3]);
 			ctx.strokeStyle = om.primaryBrandColors[0];
-			ctx.drawArrow(vectorB[0], vectorB[1], vectorB[2], vectorB[3]);
+			ctx.drawArrow(coordVectorB[0], coordVectorB[1], coordVectorB[2], coordVectorB[3]);
 			ctx.strokeStyle = om.primaryBrandColors[1];
-			ctx.drawArrow(vectorC[0], vectorC[1], vectorC[2], vectorC[3]);
-
+			ctx.drawArrow(coordVectorC[0], coordVectorC[1], coordVectorC[2], coordVectorC[3]);
 
 			ctx.font = "22px liberation_sans";
-			ctx.scale(1, -1);
-			ctx.signSegmentInMiddle(vectorA[0], -vectorA[1], vectorA[2], -vectorA[3], letter[0] + "⃗", 15, 15);
-			ctx.signSegmentInMiddle(vectorB[0], -vectorB[1], vectorB[2], -vectorB[3], letter[1] + "⃗", 15, 15);
-			ctx.signSegmentInMiddle(vectorC[0], -vectorC[1], vectorC[2], -vectorC[3], letter[2] + "⃗", 15, 15);
-
+			ctx.signSegmentInMiddle(coordVectorA[0], -coordVectorA[1], coordVectorA[2], -coordVectorA[3], letter[0] + "⃗", 15, 15);
+			ctx.signSegmentInMiddle(coordVectorB[0], -coordVectorB[1], coordVectorB[2], -coordVectorB[3], letter[1] + "⃗", 15, 15);
+			ctx.signSegmentInMiddle(coordVectorC[0], -coordVectorC[1], coordVectorC[2], -coordVectorC[3], letter[2] + "⃗", 15, 15);
 		};
 
 		NAtask.setTask({
 			text: 'На координатной плоскости изображены векторы ' + ['$\\overrightarrow{' + letter[0] + '}$',
-					'$\\overrightarrow{' + letter[1] + '}$', '$\\overrightarrow{' + letter[2] + '}$'
-				].shuffle().joinWithConjunction() +
+			'$\\overrightarrow{' + letter[1] + '}$', '$\\overrightarrow{' + letter[2] + '}$'
+			].shuffle().joinWithConjunction() +
 				'. Найдите скалярное произведение $' + condition + '$.',
-			answers: answ,
-			analys: '$\\overrightarrow{' + letter[0] + '}=\\{' + coordA.join(' ;') + '\\}$' + '<br>' +
-				'$\\overrightarrow{' + letter[1] + '}=\\{' + coordB.join(' ;') + '\\}$' + '<br>' +
-				'$\\overrightarrow{' + letter[2] + '}=\\{' + coordC.join(' ;') + '\\}$',
+			answers: 0,
+			analys: '$\\overrightarrow{' + letter[0] + '}=\\{' + vectorA.toArray().join(' ;') + '\\}$' + '<br>' +
+				'$\\overrightarrow{' + letter[1] + '}=\\{' + vectorB.toArray().join(' ;') + '\\}$' + '<br>' +
+				'$\\overrightarrow{' + letter[2] + '}=\\{' + vectorC.toArray().join(' ;') + '\\}$',
 			author: 'Суматохина Александра',
 		});
 		NAtask.modifiers.addCanvasIllustration({
