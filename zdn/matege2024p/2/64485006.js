b5de119a081561fc0e8c15cc69e22df8b3ce4050
SugarHedgehog <sneginca.cat@gmail.com>
2025-02-07 14:54:12 +0300

    [zdn] [up] - use mathjs

--- zdn/matege2024p/2/64485006.js
+++ zdn/matege2024p/2/64485006.js
@@ -6,46 +6,51 @@
 		
 		let vectorForQuestion = vectorsName.pop();
 
-		let vectA = [sl(-20, 20), sl(-20, 20)];
-		let vectB = [slKrome(vectA[0], -20, 20), slKrome(vectA[1], -20, 20)];
-		let vectC = [slKrome([vectA[0], vectB[0]], -20, 20), slKrome([vectA[1], vectB[1]], -20, 20)];
-		let vectD = [slKrome([vectA[1], vectB[1], vectC[0]], -20, 20), slKrome([vectA[1], vectB[1], vectC[1]], -20, 20)];
+		let vectA = math.matrix([sl(-20, 20), sl(-20, 20)]);
+		let vectB = math.matrix([sl(-20, 20), sl(-20, 20)]);
+		let vectC = math.matrix([sl(-20, 20), sl(-20, 20)]);
+		let vectD = math.matrix([sl(-20, 20), sl(-20, 20)]);
 
 		let vectors = [vectA, vectB, vectC, vectD];
-		let vectorsView = vectors.map((elem, index) => '$\\vec{' + vectorsName[index] + '} = ( ' + elem.join(';') + ' )$');
+		let vectorsView = vectors.map((elem, index) => '$\\vec{' + vectorsName[index] + '} = ( ' + elem.toArray().join(';') + ' )$');
 
-		let coeffs = generateMatrix(4, 2, -20, 20);
-		let finalVector = [0, 0];
+		let coeffs = generateMatrix(4, 2, 1, 20).map(c1=>c1.map(c2=>c2.pm()));
+		let finalVector = math.zeros(2);  
 		let condition = [];
 
 		for (let i = 0; i < vectors.length; i++) {
 			condition.push(coeffs[i][0].texfrac(coeffs[i][1]) + '\\vec{' + vectorsName[i] + '}');
-			finalVector = objSum(finalVector, objUmn(vectors[i], coeffs[i][0] / coeffs[i][1]));
+			finalVector = math.add(finalVector, math.multiply(vectors[i], coeffs[i][0] / coeffs[i][1]));
 		}
 
-		let question = [
-			[
-				['первую координату', finalVector[0]],
-				['вторую координату', finalVector[1]]
-			].iz(), ['сумму координат', finalVector.sum()],
-			['произведение координат', finalVector.production()]
-		].iz();
-		let finalVectorView = finalVector.slice().map((elem) => elem.ts());
-
-		let answ = question.pop();
-
-		genAssertZ1000(finalVector[0]);
-		genAssertZ1000(finalVector[1]);
+		genAssertZ1000(finalVector.subset(math.index(0)));
+		genAssertZ1000(finalVector.subset(math.index(1)));
 
 		let resultExpr = condition.shuffle().slag().plusminus();
 
 		NAtask.setTask({
 			text: 'Даны векторы ' + vectorsView.joinWithConjunction() + '. ' +
 				'Найдите координаты вектора $\\vec{' + vectorForQuestion + '}=' + resultExpr.replace('+-', '-') +
-				'$. В ответе запишите ' + question +
-				' вектора $\\vec{' + vectorForQuestion + '}$.',
-			answers: answ,
-			analys: '$' + (resultExpr + '=(' + finalVectorView.join('; ')).plusminus() + ')$',
+				'$. В ответе запишите ',
+				questions:[
+					{
+						text: 'первую координату',
+						answers: finalVector.subset(math.index(0)),
+					},{
+						text: 'вторую координату',
+						answers: finalVector.subset(math.index(1)),
+					},{
+						text: 'сумму координат',
+						answers: math.sum(finalVector),
+					},{
+						text: 'произведение координат',
+						answers: math.prod(finalVector),
+					},
+					],
+				postquestion: ' вектора $\\vec{' + vectorForQuestion + '}$.',
+			
+			analys: '$' + (resultExpr + '=(' + finalVector.toArray().join('; ')).plusminus() + ')$',
 		});
 	}, 1000);
+	NAtask.modifiers.allDecimalsToStandard();
 })();
