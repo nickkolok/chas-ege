260531cc04ce0cf3440800e2f71b5bd99d11a65d
nickkolok <nickkolok@mail.ru>
2025-02-10 16:16:24 +0300

    [zdn][fix] 316350 - allDecimalsToStandard

--- zdn/mat2014/B11/316350.js
+++ zdn/mat2014/B11/316350.js
@@ -4,7 +4,8 @@
 	var b = sluchch(0.1, 0.9, 0.1);
 	var ans = a*(2*Math.pow(b, 2) - 1);
 	NAtask.setTask({
-		text: 'Вычислить значение выражения: $$ {'+a+'}\\cos2\\alpha\\mbox{, если }\\cos\\alpha = {'+b.ts(1)+'}$$',
+		text: 'Вычислить значение выражения: $$ '+a+'\\cos2\\alpha\\mbox{, если }\\cos\\alpha = '+b+'$$',
 		answers: ans.ts(),
 	});
+	NAtask.modifiers.allDecimalsToStandard(true);
 })();
