575cd488360ab6e8e2355630cde99efdb0dcbdc6
Колесникова Елена <luly3751@gmail.com>
2025-02-10 14:32:18 +0300

    [zdn][new] 510679 -fix #1924 edit 1

--- zdn/matege2024b/15/510679.js
+++ zdn/matege2024b/15/510679.js
@@ -2,13 +2,21 @@
     'use strict';
     retryWhileError(function () {
         NAinfo.requireApiVersion(0, 2);
-        let howMuchIncreased = sl(2, 10, 1);
+
+        let rand = sl(0, 1);
+        let howMuchIncreased = sl(2, 10, 0.1);
+        let howMuchDecreased = [2, 4, 5, 10, 20, 25, 50].iz();
+
+        let increasedOrDecreased = ['увеличилось', 'уменьшилось'][rand];
+        let ByHowMuchGet = [howMuchIncreased, howMuchDecreased][rand];
+
+        let result = [100 * howMuchIncreased - 100, 100 - 100 / howMuchDecreased][rand];
 
         NAtask.setTask({
             text:
-                'Число посетителей сайта увеличилось за месяц в ' + chislitlx(howMuchIncreased, 'раз', '$') +
-                '. На сколько процентов увеличилось число посетителей сайта за этот месяц?',
-            answers: 100 * howMuchIncreased - 100,
+                'Число посетителей сайта ' + increasedOrDecreased + ' за месяц в ' + chislitlx(ByHowMuchGet, 'раз', '$') +
+                '. На сколько процентов ' + increasedOrDecreased + ' число посетителей сайта за этот месяц?',
+            answers: result,
         });
     }, 100);
 })();
