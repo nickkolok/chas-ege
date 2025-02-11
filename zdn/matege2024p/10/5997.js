636823a5dcecab289b30458e5dc0f2297909abd3
Aleksandra Sumatokhina <88662554+SugarHedgehog@users.noreply.github.com>
2025-02-11 16:20:00 +0300

    Update 5997.js

--- zdn/matege2024p/10/5997.js
+++ zdn/matege2024p/10/5997.js
@@ -1,20 +1,20 @@
 (function () {
 	'use strict'; retryWhileError(function () {
-		let s = sl(20, 300, 1);
-		let n = sl(1, 5, 1);
-		let x = sl(1, 15, 1);
+		let s = sl(20, 300);
+		let n = sl(1, 5);
+		let x = sl(1, 15);
 		let v = sl(x + 1, 40);
-		let a = sl(1, 12, 1);
+		let a = sl(1, 12);
 		let b = a + n + s / (v + x) + s / (v - x);
 		genAssert(b.isAlmostInteger(), 'Время не может быть дробным');
 		genAssert(b < 24, 'Время отправления не может быть слишком большим');
-		let the_activeFloatingVehicle = sklonlxkand(["лодка", "байдарка", "баржа", "яхта", "моторная лодка"].iz());
-		let the_humanSettlementDestination = sklonlxkand(["пункт", "город", "село"].iz());
+		let the_activeFloatingVehicle = sklonlxkand(decor.activeFloatingVehicleF.iz());
+		let the_humanSettlementDestination = sklonlxkand(decor.humanSettlementDestination.iz());
 		let the_orderToFind = decor.orderToFind.iz();
 		NAtask.setTask({
 			text:
 				'' + the_activeFloatingVehicle.ie.toZagl() + ' в ' + a + ':00 вышла из ' + the_humanSettlementDestination.re +
-				' A в ' + the_humanSettlementDestination.ie + ' B, расположенный в ' + s +
+				' A в ' + the_humanSettlementDestination.ie + ' B, расположенн'+['ый', 'ая', 'ое'][the_humanSettlementDestination.rod]+' в ' + s +
 				' км от A. Пробыв в ' + the_humanSettlementDestination.pe + ' B ' + chislitlx(n, 'час') + ', ' +
 				the_activeFloatingVehicle.ie + ' отправилась назад и вернулась в ' + the_humanSettlementDestination.ie +
 				' А в ' + b + ':00 того же дня. ' + the_orderToFind.toZagl() +
