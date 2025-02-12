3d63b18f4069f6ccdac2d75783b711f062f0ca1c
Колесникова Елена <luly3751@gmail.com>
2025-02-12 10:35:48 +0300

    [zdn][new] 506736 -fix #1925 edit 1

--- zdn/matege2024b/15/506736.js
+++ zdn/matege2024b/15/506736.js
@@ -5,7 +5,10 @@
 
         let post = ['председателя', 'заместителя председателя', 'секретаря'].iz();
         let educationPlace = ['школьного', 'университетского'].iz();
+
         let rand = sl(0, 1);
+        let randUpgrade = sl(0, 1);
+        
         let winOrLose = ['победитель', 'проигравший'][rand];
         let numberOfParticipants = sl(70, 120, 1);
         let loserRatio = sl(1, 4, 1);
@@ -18,9 +21,9 @@
 
         NAtask.setTask({
             text:
-                'На пост ' + post + ' ' + educationPlace + ' совета претендовали два кандидата. В голосовании приняли участие ' + chislitlx(numberOfParticipants, 'выборщик', 'v$') +
-                '. Голоса между кандидатами распределились в отношении ' + loserRatio + ':' + winnerRatio + '. Сколько голосов получил ' + winOrLose + '?',
-            answers: numberOfPeopleWhoVotedThatWeNeed,
+                'На пост ' + post + ' ' + educationPlace + ' совета претендовали два кандидата. В голосовании приняли участие ' + chislitlx([numberOfParticipants, 'n'][randUpgrade], 'выборщик', 'v$') +
+                '. Голоса между кандидатами распределились в отношении ' + loserRatio + ':' + winnerRatio + ['. Сколько голосов получил ' + winOrLose + '?', '. Сколько голосов, было изначально, если ' + winOrLose + ' получил ' + chislitlx(numberOfPeopleWhoVotedThatWeNeed, 'голос', '$') + '?'][randUpgrade],
+            answers: [numberOfPeopleWhoVotedThatWeNeed, numberOfParticipants][randUpgrade],
         });
     }, 100);
 })();
