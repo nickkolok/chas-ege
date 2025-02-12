32dbef3d271e947f9a716b715ff71be7a8bf0898
Колесникова Елена <luly3751@gmail.com>
2025-02-12 10:00:27 +0300

    [zdn][new] 515778 -fix #1926 edit 1

--- zdn/matege2024b/15/515778.js
+++ zdn/matege2024b/15/515778.js
@@ -6,23 +6,18 @@
         let rand = sl(0, 1);
         let govOrPriShare = ['государству', 'частным акционерам'][rand];
         let nameOfCompany = ['Что за люди', 'Одичалая ягода', 'Гелий', 'Бра и Тришка', 'Винни и Пух', '1 раз отрежь - 8 раз переделывай', 'Стул да стол', 'Папины сапожки', 'Юпитерианцы', 'Вершки да Корешки'].iz();
-        let companyProfit = sl(10, 200, 1);
         let government = sl(1, 10, 1);
-        let privateSharehorders = sl(1, 10, 1);
+        let privateSharehorders = slKrome([government], 1, 10, 1);
+        let companyProfit = (government + privateSharehorders) * sl(2, 200, 1);
 
-        genAssert(government !== privateSharehorders, 'части отношения у государства и частных акционеров совпадает');
         genAssert(government.nod(privateSharehorders) == 1, 'части отношения у государства и частных акционеров имеют общие множители по мимо 1');
-        genAssert(companyProfit.kratno(government + privateSharehorders), 'прибыль не кратка сумме отношения');
-
-        let theAmountThatWillBeRecived = companyProfit / (government + privateSharehorders) * [government, privateSharehorders][rand];
-
 
         NAtask.setTask({
             text:
                 'Акции предприятия "' + nameOfCompany + '" распределены между государством и частными акционерами в отношении ' + government + ':' + privateSharehorders + ' соответственно. ' +
                 'Общая прибыль предприятия после уплаты налогов за год составила ' + companyProfit + ' млн рублей. ' +
                 'Какая сумма из этой прибыли должна пойти на выплату ' + govOrPriShare + '? Ответ дайте в миллионах рублей.',
-            answers: theAmountThatWillBeRecived,
+            answers: companyProfit / (government + privateSharehorders) * [government, privateSharehorders][rand],
         });
     }, 100);
 })();
