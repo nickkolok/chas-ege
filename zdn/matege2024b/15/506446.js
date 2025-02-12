90f17e684e0f65fa877f795a5bbac1715ac23c59
Колесникова Елена <luly3751@gmail.com>
2025-02-12 10:10:15 +0300

    [zdn][new] 506446 -fix #1927 edit 1

--- zdn/matege2024b/15/506446.js
+++ zdn/matege2024b/15/506446.js
@@ -5,17 +5,12 @@
 
         let rand = sl(0, 1);
         let nameOfCompany = ['Вершки да Корешки', 'Одичалая Ягода', 'Вкуснее некуда', 'по Ведёрочку', 'Винни и Бинни', 'Сладкая Жизнь', 'Как у Бабушки', 'Счастливый Дачник', 'Крестьянская Революция', 'Как в Союзе!', 'Новые Технологии', 'Вегатерианец', 'Сто Пудов'].iz();
-        let nameOfCulture = sklonlxkand(['пшеница', 'рожь', 'кукуруза', 'овёс', 'ячмень', 'горох', 'фасоль', 'свекла', 'овощи'].iz(2));
-        let howManyHectares = sl(10, 200, 1);
+        let nameOfCulture = sklonlxkand(['пшеница', 'рожь', 'кукуруза', 'овёс', 'ячмень', 'горох', 'фасоль', 'свекла'].iz(2));
         let firstCulture = sl(1, 10, 1);
-        let secondCulture = sl(1, 10, 1);
+        let secondCulture = slKrome([firstCulture], 1, 10, 1);
+        let howManyHectares = (firstCulture + secondCulture) * sl(2, 200, 1);
 
-        genAssert(firstCulture !== secondCulture, 'части отношения у одного посева совпадает со вторым');
         genAssert(firstCulture.nod(secondCulture) == 1, 'части отношения у первого посева и второго посева имеют общие множители по мимо 1');
-        genAssert(howManyHectares.kratno(firstCulture + secondCulture), 'количество гектар не кратко сумме отношения');
-
-        let result = howManyHectares / (firstCulture + secondCulture) * [secondCulture, firstCulture][rand];
-
 
         NAtask.setTask({
             text:
@@ -23,7 +18,7 @@
                 'составляет ' + howManyHectares + ' га и распределена между ' + nameOfCulture[0].te + ' и ' + nameOfCulture[1].te +
                 '  в отношении ' + firstCulture + ':' + secondCulture + ' соответственно.' +
                 ' Сколько гектаров занимает ' + nameOfCulture[1].ie + '?',
-            answers: result,
+            answers: howManyHectares / (firstCulture + secondCulture) * [secondCulture, firstCulture][rand],
         });
     }, 100);
 })();
