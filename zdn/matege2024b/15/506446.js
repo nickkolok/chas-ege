a88b314c45bf227e74c0411cb8c121cb0d2ea9df
Колесникова Елена <luly3751@gmail.com>
2025-02-12 12:01:24 +0300

    506446 edit 3

--- zdn/matege2024b/15/506446.js
+++ zdn/matege2024b/15/506446.js
@@ -4,21 +4,24 @@
         NAinfo.requireApiVersion(0, 2);
 
         let rand = sl(0, 1);
+        let randUpgrade = sl(0, 1);
         let nameOfCompany = ['Вершки да Корешки', 'Одичалая Ягода', 'Вкуснее некуда', 'по Ведёрочку', 'Винни и Бинни', 'Сладкая Жизнь', 'Как у Бабушки', 'Счастливый Дачник', 'Крестьянская Революция', 'Как в Союзе!', 'Новые Технологии', 'Вегатерианец', 'Сто Пудов'].iz();
         let nameOfCulture = sklonlxkand(['пшеница', 'рожь', 'кукуруза', 'овёс', 'ячмень', 'горох', 'фасоль', 'свекла'].iz(2));
         let firstCulture = sl(1, 10, 1);
         let secondCulture = slKrome([firstCulture], 1, 10, 1);
         let howManyHectares = (firstCulture + secondCulture) * sl(2, 200, 1);
+        let result = (howManyHectares / (firstCulture + secondCulture)) * [secondCulture, firstCulture][rand];
 
         genAssertIrreducible(firstCulture, secondCulture, 'части отношения у первого посева и второго посева имеют общие множители по мимо 1');
 
         NAtask.setTask({
             text:
                 'Площадь земель фермерского хозяйства "' + nameOfCompany + '", отведённых под посадку сельскохозяйственных культур, ' +
-                'составляет ' + howManyHectares + ' га и распределена между ' + nameOfCulture[0].te + ' и ' + nameOfCulture[1].te +
+                ['составляет ' + howManyHectares + ' га и ', ''][randUpgrade] + 'распределена между ' + nameOfCulture[0].te + ' и ' + nameOfCulture[1].te +
                 '  в отношении ' + firstCulture + ':' + secondCulture + ' соответственно.' +
-                ' Сколько гектаров занимает ' + nameOfCulture[1].ie + '?',
-            answers: howManyHectares / (firstCulture + secondCulture) * [firstCulture, secondCulture][rand],
+                [' Сколько гектаров занимает ' + nameOfCulture[1].ie + '?', ' Сколько гектаров занимает поле, если площадь земель, отведённых под посадку '
+                    + nameOfCulture[1].re + ', составляет ' + result + 'га ?'][randUpgrade],
+            answers: [result, howManyHectares][randUpgrade],
         });
     }, 100);
 })();
