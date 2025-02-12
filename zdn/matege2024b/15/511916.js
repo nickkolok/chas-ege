c2f7dbfa564d5b204aee73f4b55da7e49009e94e
Колесникова Елена <luly3751@gmail.com>
2025-02-12 10:19:18 +0300

    [zdn][new] 511916 -fix #1923 edit 2

--- zdn/matege2024b/15/511916.js
+++ zdn/matege2024b/15/511916.js
@@ -7,26 +7,24 @@
         let randOfSickness = sl(0, 1);
 
         let theGetSickAtSchool = ['школе', 'университете', 'ВУЗе', 'детском садике'].iz();
-        let theySickinHospital = ['больнице', 'клинике', 'лечебнице', 'госпитале', 'стационаре', 'лазарете'].iz();
-        let whereTheyGetSick = [theGetSickAtSchool, theySickinHospital][randOfSickness];
+        let theyGetSickInHospital = ['больнице', 'клинике', 'лечебнице', 'госпитале', 'стационаре', 'лазарете'].iz();
+        let whereTheyGetSick = [theGetSickAtSchool, theyGetSickInHospital][randOfSickness];
 
-        let illnessInSchool = ['гриппом', 'корью', 'ОРВИ', 'гриппом', 'ангиной', 'гепатитом А', 'простудой', 'дифтерией', 'простудой'].iz();
-        let illnessesInTheHospital = ['туберкулёзом', 'гепатитом В', 'бронхитом', 'COVID-19', 'золотистым стафилококком'].iz();
-        let typeOfSickness = [illnessInSchool, illnessesInTheHospital][randOfSickness];
+        let illnessInSchool = ['гриппом', 'корью', 'ОРВИ', 'ангиной', 'гепатитом А', 'простудой', 'дифтерией',].iz();
+        let illnessInTheHospital = ['туберкулёзом', 'гепатитом В', 'бронхитом', 'COVID-19', 'золотистым стафилококком'].iz();
+        let typeOfSickness = [illnessInSchool, illnessInTheHospital][randOfSickness];
 
         let decreasedByHowMuchGet = [2, 4, 5, 10, 20, 25, 50].iz();
         let increasedByHowMuchGet = sl(2, 5, 0.1);
 
         let increasedOrDecreased = ['увеличилось', 'уменьшилось'][rand];
-        let ByHowMuchGet = [increasedByHowMuchGet, decreasedByHowMuchGet][rand];
-
-        let result = [100 * increasedByHowMuchGet - 100, 100 - 100 / decreasedByHowMuchGet][rand];
+        let byHowMuchGet = [increasedByHowMuchGet, decreasedByHowMuchGet][rand];
 
         NAtask.setTask({
             text:
-                'Число больных ' + typeOfSickness + ' в ' + whereTheyGetSick + ' ' + increasedOrDecreased + ' за месяц в ' + chislitlx(ByHowMuchGet, 'раз', '$') + '.' +
+                'Число больных ' + typeOfSickness + ' в ' + whereTheyGetSick + ' ' + increasedOrDecreased + ' за месяц в ' + chislitlx(byHowMuchGet, 'раз', '$') + '.' +
                 ' На сколько процентов ' + increasedOrDecreased + ' число больных ' + typeOfSickness + '?',
-            answers: result,
+            answers: [100 * increasedByHowMuchGet - 100, 100 - 100 / decreasedByHowMuchGet][rand],
         });
     }, 100);
 })();
