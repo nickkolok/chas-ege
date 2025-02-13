04f3db464a4e30e2e2ec456d6e790b02eb3c6e75
Колесникова Елена <luly3751@gmail.com>
2025-02-13 15:14:13 +0300

    [zdn][new] 512711 -fix #1928 edit 1

--- zdn/matege2024b/15/512711.js
+++ zdn/matege2024b/15/512711.js
@@ -3,18 +3,20 @@
     retryWhileError(function () {
         NAinfo.requireApiVersion(0, 2);
 
-        let rand = sl(0, 1);
+        let rand = sl1();
         let randPartOf100 = sl(0, 5);
         let coniferousOrDeciduous = ['хвойные', 'лиственные'][rand];
-        let deciduousTrees = [100 - sl(1, 99, 1), 50 - sl(1, 49, 1), 25 - sl(1, 24, 1), 20 - sl(1, 19, 1), 10 - sl(1, 9, 1), 5 - sl(1, 4, 1)][randPartOf100];
-        let coniferousTrees = [100 - deciduousTrees, 50 - deciduousTrees, 25 - deciduousTrees, 20 - deciduousTrees, 10 - deciduousTrees, 5 - deciduousTrees][randPartOf100];
-
+        let trees = [100, 50, 25, 20, 10, 5][randPartOf100];
+        let deciduousTrees = sl(1, trees - 1);
+        let coniferousTrees = trees - deciduousTrees;
+        
         genAssertIrreducible(deciduousTrees, coniferousTrees, 'части отношения у хвойных и лиственных имеют общие множители по мимо 1');
         let percent = ([coniferousTrees, deciduousTrees][rand] / (coniferousTrees + deciduousTrees)) * 100;
 
         NAtask.setTask({
             text:
-            'Число хвойных деревьев в парке относится к числу лиственных как ' + coniferousTrees + ':' + deciduousTrees + '. Других деревьев в парке нет. Сколько процентов деревьев в парке составляют ' + coniferousOrDeciduous + 'ые?',
+                'Число хвойных деревьев в парке относится к числу лиственных как ' + coniferousTrees + ':' + deciduousTrees +
+                '. Других деревьев в парке нет. Сколько процентов деревьев в парке составляют ' + coniferousOrDeciduous + '?',
             answers: percent,
         });
     }, 100);
