6f3604fbbb9ffc367c8b20a66cb2568cc7eaddfc
Колесникова Елена <luly3751@gmail.com>
2025-02-13 15:05:25 +0300

    [zdn][new] 506446 - fix #1927 edit 4

--- zdn/matege2024b/15/506446.js
+++ zdn/matege2024b/15/506446.js
@@ -3,16 +3,16 @@
     retryWhileError(function () {
         NAinfo.requireApiVersion(0, 2);
 
-        let rand = sl(0, 1);
-        let randUpgrade = sl(0, 1);
+        let rand = sl1();
+        let randUpgrade = sl1();
         let nameOfCompany = ['Вершки да Корешки', 'Одичалая Ягода', 'Вкуснее некуда', 'по Ведёрочку', 'Винни и Бинни', 'Сладкая Жизнь', 'Как у Бабушки', 'Счастливый Дачник', 'Крестьянская Революция', 'Как в Союзе!', 'Новые Технологии', 'Вегатерианец', 'Сто Пудов'].iz();
         let nameOfCulture = sklonlxkand(['пшеница', 'рожь', 'кукуруза', 'овёс', 'ячмень', 'горох', 'фасоль', 'свекла'].iz(2));
         let firstCulture = sl(1, 10, 1);
         let secondCulture = slKrome([firstCulture], 1, 10, 1);
         let howManyHectares = (firstCulture + secondCulture) * sl(2, 200, 1);
-        let result = (howManyHectares / (firstCulture + secondCulture)) * [secondCulture, firstCulture][rand];
 
         genAssertIrreducible(firstCulture, secondCulture, 'части отношения у первого посева и второго посева имеют общие множители по мимо 1');
+        let result = (howManyHectares / (firstCulture + secondCulture)) * secondCulture;
 
         NAtask.setTask({
             text:
@@ -20,7 +20,7 @@
                 ['составляет ' + howManyHectares + ' га и ', ''][randUpgrade] + 'распределена между ' + nameOfCulture[0].te + ' и ' + nameOfCulture[1].te +
                 '  в отношении ' + firstCulture + ':' + secondCulture + ' соответственно.' +
                 [' Сколько гектаров занимает ' + nameOfCulture[1].ie + '?', ' Сколько гектаров занимает поле, если площадь земель, отведённых под посадку '
-                    + nameOfCulture[1].re + ', составляет ' + result + 'га ?'][randUpgrade],
+                    + nameOfCulture[1].re + ', составляет ' + result + ' га?'][randUpgrade],
             answers: [result, howManyHectares][randUpgrade],
         });
     }, 100);
