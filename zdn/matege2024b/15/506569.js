4067ec009fe0e03d6e488532ebabbdff4c9a8921
Колесникова Елена <luly3751@gmail.com>
2025-02-13 15:31:16 +0300

    [zdn][new] 506569 -fix #1930 edit 1

--- zdn/matege2024b/15/506569.js
+++ zdn/matege2024b/15/506569.js
@@ -11,12 +11,12 @@
             'Исповедь сценариста', 'СПГС', 'Спортивный центр', 'Юный физик', 'Шабаш тимлида', 'Фото-охота', 'Бета-тестеры', 'Любители чтения', 'Музыкант', 'Юный химик', 'Во все тяжкие'].iz();
 
         let rand = sl(0, 1);
-        let randUpgrade = sl(0, 1);
         let randPartOf100 = sl(0, 5);
 
         let winOrLose = ['победитель', 'проигравший'][rand];
-        let loserRatio = [100 - sl(51, 99, 1), 50 - sl(26, 49, 1), 25 - sl(13, 24, 1), 20 - sl(11, 19, 1), 10 - sl(6, 9, 1), 5 - sl(3, 4, 1)][randPartOf100];
-        let winnerRatio = [100 - loserRatio, 50 - loserRatio, 25 - loserRatio, 20 - loserRatio, 10 - loserRatio, 5 - loserRatio][randPartOf100];
+        let ratio = [100, 50, 25, 20, 10, 5][randPartOf100];
+        let winnerRatio = sl((ratio / 2).ceil(), ratio - 1, 1);
+        let loserRatio = ratio - winnerRatio;
 
         genAssertIrreducible(winnerRatio, loserRatio, 'части отношения у победителя и проигравшего имеют общие множители по мимо 1');
 
@@ -24,10 +24,11 @@
 
         NAtask.setTask({
             text:
-                'В выборах на пост ' + post + ' ' + educationPlace + ' клуба "' + clubName + '" претендовали два кандидата. ' + ['Голоса избирателей распредилилсь между ними в отношении ' +
-                    + loserRatio + ':' + winnerRatio + '. Сколько голосов получил ' + winOrLose + ' в процентах?',
-                winOrLose.toZagl() + ' набрал ' + percent + '% голосов. Какое соотношение голосов проигравшего к голосам победителя?'][randUpgrade],
-            answers: [percent, loserRatio + ':' + winnerRatio][randUpgrade],
+                'В выборах на пост ' + post + ' ' + educationPlace + ' клуба "' + clubName + '" претендовали два кандидата. ' +
+                'Голоса избирателей распредилилсь между ними в отношении ' + '$'
+                + loserRatio + ':' + winnerRatio + '$' + '. Сколько голосов получил ' + winOrLose + ' в процентах?',
+
+            answers: percent,
         });
     }, 100);
 })();
