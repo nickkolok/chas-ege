3939a3b62667d7cc76247f8458a165daee5b3840
SugarHedgehog <sneginca.cat@gmail.com>
2025-02-13 10:57:24 +0300

    [zdn] [up] - getListedPreference waterVolume

--- zdn/matege2024p/10/99571.js
+++ zdn/matege2024p/10/99571.js
@@ -2,15 +2,25 @@
     'use strict';
     retryWhileError(function () {
         /* В сосуд, содержащий 5 литров 12−процентного водного раствора некоторого вещества, добавили 7 литров воды. Сколько процентов составляет концентрация получившегося раствора? */
-
+		let key = '99572';
+        let rand = getListedPreference(key, [{
+			preference: 'final_concentration',
+			preferenceValue: 0,
+		}, {
+			preference: 'water_volume',
+			preferenceValue: 1,
+		}, {
+			preference: 'first_concentration',
+			preferenceValue: 2,
+		}, {
+			preference: 'first_volume',
+			preferenceValue: 3,
+		}], sl(0, 3));
+		
         let firstVolume = sl(5, 50, 0.01);
-        let addVolume = slKrome(firstVolume, 5, 50, 0.01);
+        let waterVolume = slKrome(firstVolume, 5, 50, 0.01);
         let procent = sl(10, 80);
-        let finalProcent = procent * firstVolume / (firstVolume + addVolume);
-
-        let textAddVolume = chislitlx(addVolume, 'литр', 'v');
-
-        let rand = sl(0, 3);
+        let finalProcent = procent * firstVolume / (firstVolume + waterVolume);
 
         let dano = [chislitlx(firstVolume, 'литр', 'v'), procent + '−процентного'];
 
@@ -31,14 +41,14 @@
 
         NAtask.setTask({
             text: 'В сосуд, содержащий ' + dano + ' водного раствора некоторого вещества, ' +
-                'добавили ' + [textAddVolume, 'несколько литров'][Number(rand == 1)] +
+                'добавили ' + [chislitlx(waterVolume, 'литр', 'v'), 'несколько литров'][Number(rand == 1)] +
                 ' воды' + [' так, что получился ' + finalProcent + '−процентный раствор', ''][Number(rand == 0)] + '. ',
             questions: [[{
                 text: 'Сколько процентов составляет концентрация получившегося раствора',
                 answers: finalProcent,
             }, {
                 text: 'Сколько литров воды добавили в раствор',
-                answers: addVolume,
+                answers: waterVolume,
             }, {
                 text: 'Сколько процентов составляла концентрация изначального раствора',
                 answers: procent,
