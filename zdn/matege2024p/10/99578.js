84115abd88f6a4f4ec80b0a1f0109886b853cd7e
SugarHedgehog <sneginca.cat@gmail.com>
2025-02-13 11:55:41 +0300

    [zdn] [up] - getListedPreference & indent

--- zdn/matege2024p/10/99578.js
+++ zdn/matege2024p/10/99578.js
@@ -1,49 +1,63 @@
-(function() { 'use strict'; retryWhileError(function() {
-	/* Имеются два сосуда. Первый содержит 30 кг, а второй — 15 кг раствора кислоты различной концентрации. Если эти растворы смешать, то получится раствор, содержащий 34% кислоты. Если же смешать равные массы этих растворов, то получится раствор, содержащий 46% кислоты. Сколько килограммов кислоты содержится в первом сосуде? */
+(function () {
+	'use strict'; retryWhileError(function () {
+		/* Имеются два сосуда. Первый содержит 30 кг, а второй — 15 кг раствора кислоты различной концентрации. Если эти растворы смешать, то получится раствор, содержащий 34% кислоты. Если же смешать равные массы этих растворов, то получится раствор, содержащий 46% кислоты. Сколько килограммов кислоты содержится в первом сосуде? */
 
-	let firstMass=sl(1, 50);
-	let secondMass=slKrome(firstMass, 1, 50);
-	let finalMass = firstMass+secondMass;
-	
-	let firstProcent=slKrome(firstMass, 1, 50);
-	let secondProcent=slKrome([firstProcent, secondMass], 1, 50);
-	let firstFinalProcent = (firstMass*firstProcent+secondMass*secondProcent)/finalMass;
-	let secondFinalProcent = (firstProcent+secondProcent)/2;
-	
-	let mixin = sklonlxkand(['соль', 'щёлочь', 'кислота'].iz());
+		let key = '99578';
+		let decorRand = sl1();	//0 - концентрация в %, 1 - процентный раствор
+		let rand = getListedPreference(key, [{
+			preference: 'mass_of_mixin_in_first',
+			preferenceValue: 0,
+		}, {
+			preference: 'mass_of_mixin_in_second',
+			preferenceValue: 1,
+		}, {
+			preference: 'procent_of_mixin_in_first',
+			preferenceValue: 2,
+		}, {
+			preference: 'procent_of_mixin_in_second',
+			preferenceValue: 3,
+		}], sl(0, 3));
 
-	genAssertZ1000(firstFinalProcent,'Процент концентрации при смешении обоих растворов слишком дробный'); 
-	genAssertZ1000(secondFinalProcent,'Процент концентрации при смешении обоих растворов при равной массе слишком дробный'); 
+		let firstMass = sl(1, 50);
+		let secondMass = slKrome(firstMass, 1, 50);
+		let finalMass = firstMass + secondMass;
 
+		let firstProcent = slKrome(firstMass, 1, 50);
+		let secondProcent = slKrome([firstProcent, secondMass], 1, 50);
+		let firstFinalProcent = (firstMass * firstProcent + secondMass * secondProcent) / finalMass;
+		let secondFinalProcent = (firstProcent + secondProcent) / 2;
 
-	let rand = sl1();
-	let rand2 = sl(0,3);
+		let mixin = sklonlxkand(['соль', 'щёлочь', 'кислота'].iz());
 
-	NAtask.setTask({
-		text:
-			'Имеются два сосуда. '+
-			'Первый содержит ' + firstMass + ' кг, '+
-			'а второй — ' + secondMass + ' кг раствора '+mixin.re+' различной концентрации. '+
-			'Если эти растворы смешать, '+
-			'то ' + ['получится раствор, содержащий '+firstFinalProcent+'%', 'получили бы '+firstFinalProcent+'-процентный раствор'][rand]+' '+mixin.re+'. '+
-			'Если же смешать равные массы этих растворов, '+
-			'то '+['получится раствор, содержащий '+secondFinalProcent+'%', 'получили бы '+secondFinalProcent+'-процентный раствор'][rand]+' '+mixin.re+'. ',
-		questions: [[{
-			text: 'Сколько килограммов '+mixin.re+' содержится в первом сосуде?',
-			answers: firstMass,
-			},{
-			text: 'Сколько килограммов '+mixin.re+' содержится во втором сосуде?',
-			answers: secondMass,
-			},{
-			text: 'Сколько процентов '+mixin.re+' содержится в первом сосуде?',
-			answers: firstProcent,
-			},{
-			text: 'Сколько процентов '+mixin.re+' содержится во втором сосуде?',
-			answers: secondProcent,
-			}][rand2]],
-		authors: ['Александра Суматохина'],
-	});
-	NAtask.modifiers.allDecimalsToStandard(/*true*/);
-}, 2000);})();
+		genAssertZ1000(firstFinalProcent, 'Процент концентрации при смешении обоих растворов слишком дробный');
+		genAssertZ1000(secondFinalProcent, 'Процент концентрации при смешении обоих растворов при равной массе слишком дробный');
+
+		NAtask.setTask({
+			text:
+				'Имеются два сосуда. ' +
+				'Первый содержит ' + firstMass + ' кг, ' +
+				'а второй — ' + secondMass + ' кг раствора ' + mixin.re + ' различной концентрации. ' +
+				'Если эти растворы смешать, ' +
+				'то получится ' + ['раствор, содержащий ' + firstFinalProcent + '%', firstFinalProcent + '-процентный раствор'][decorRand] + ' ' + mixin.re + '. ' +
+				'Если же смешать равные массы этих растворов, ' +
+				'то получится ' + ['раствор, содержащий ' + secondFinalProcent + '%', secondFinalProcent + '-процентный раствор'][decorRand] + ' ' + mixin.re + '. ',
+			questions: [[{
+				text: 'Сколько килограммов ' + mixin.re + ' содержится в первом сосуде?',
+				answers: firstMass * firstProcent / 100,
+			}, {
+				text: 'Сколько килограммов ' + mixin.re + ' содержится во втором сосуде?',
+				answers: secondMass * secondProcent / 100,
+			}, {
+				text: 'Сколько процентов составляла концентрация раствора в первом сосуде?',
+				answers: firstProcent,
+			}, {
+				text: 'Сколько процентов составляла концентрация раствора во втором сосуде?',
+				answers: secondProcent,
+			}][rand]],
+			authors: ['Александра Суматохина'],
+		});
+		NAtask.modifiers.allDecimalsToStandard(/*true*/);
+	}, 2000);
+})();
 // РешуЕГЭ: 
 // https://mathb-ege.sdamgia.ru/problem?id=99578
