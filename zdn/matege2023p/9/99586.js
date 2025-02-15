7abc35e6aeb30f2afa19161b4c40b97d49a5e234
Aleksandra Sumatokhina <88662554+SugarHedgehog@users.noreply.github.com>
2025-02-15 18:03:26 +0300

    Update 99586.js

--- zdn/matege2023p/9/99586.js
+++ zdn/matege2023p/9/99586.js
@@ -1,40 +1,27 @@
-﻿class GeometricProgression {
-	constructor(first, factor) {
-		this.first = first;
-		this.factor = factor;
-		this.nmember = 0;
+(function() {
+	NAinfo.requireApiVersion(0, 2);
 
+	let b = sluchch(3, 25);
+	let a = sluchch(500000, 1500000, 100000);
+
+	if (a < 1000000) {
+		n = 3;
+	} else {
+		b = sluchch(10, 20, 10);
+		n = sluchch(4, 5);
 	}
 
-	member(nmember) {
-		this.nmember = nmember;
-		return (this.first * Math.pow(this.factor, this.nmember - 1)).toFixed(0);
-	}
+	let с = 1 + b / 100;
+	let progression = new GeometricProgression(a, с);
+	var arr = ['Печенов', 'Оладьев', 'Коржов', 'Плюшкин', 'Прянников', 'Баранкин', 'Бубликов', 'Батонов', 'Ватрушкин',
+		'Булкин'
+	].iz();
 
-}
-
-let b = sluchch(3, 25);
-let c;
-let a = sluchch(500000, 1500000, 100000);
-
-if (a < 1000000) {
-	n = 3;
-} else {
-	b = sluchch(10, 20, 10);
-	n = sluchch(4, 5);
-}
-
-let с = 1 +  b / 100;
-с = с.toFixed(2);
-let progression = new GeometricProgression(a, с);
-var arr = ['Печенов', 'Оладьев', 'Коржов', 'Плюшкин', 'Прянников', 'Баранкин', 'Бубликов', 'Батонов', 'Ватрушкин','Булкин'];
-var rand = Math.floor(Math.random() * arr.length);
-
-NAtask.setTask({
-	text: 'Бизнесмен ' + arr[rand] + ' получил в 2000 году прибыль в размере ' + chislitlx(a, 'рубль') +  
-		'. Каждый следующий год его прибыль увеличивалась на ' + b +
-		'% по сравнению с предыдущим годом. Сколько рублей заработал ' + arr[rand] + ' за 200' + (n - 1) +  ' год?',
-
-	answers: progression.member(n),
-});
-//Сергей Алендарь
+	NAtask.setTask({
+		text: 'Бизнесмен ' + arr + ' получил в 2000 году прибыль в размере ' + chislitlx(a, 'рубль') +
+			'. Каждый следующий год его прибыль увеличивалась на ' + b +
+			'% по сравнению с предыдущим годом. Сколько рублей заработал ' + arr + ' за 200' + (n - 1) + ' год?',
+		answers: progression.member(n),
+		authors: ['Сергей Алендарь', 'Александра Суматохина'],
+	});
+})();
