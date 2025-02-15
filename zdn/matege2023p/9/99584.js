8ebb6dcfb0f8f9bfa0414bed03dbf62d7dab537f
Aleksandra Sumatokhina <88662554+SugarHedgehog@users.noreply.github.com>
2025-02-15 17:39:22 +0300

    Update 99584.js

--- zdn/matege2023p/9/99584.js
+++ zdn/matege2023p/9/99584.js
@@ -1,38 +1,21 @@
-﻿class ArithmeticProgression {
-	constructor(first, difference) {
-		this.first = first;
-		this.difference = difference;
-		this.nmember = 0;
+(function() {
+	NAinfo.requireApiVersion(0, 2);
 
-	}
+	let a = sluchch(1, 5);
+	let b = sluchch(2, 4);
+	let n = sluchch(3, 11);
 
-	member(nmember) {
-		this.nmember = nmember;
-		return this.first + this.difference * (this.nmember - 1);
-	}
+	let progression = new ArithmeticProgression(a, b);
 
-	summa(nmember) {
-		this.nmember = nmember;
-		return ((2 * this.first + this.difference * (this.nmember - 1)) / 2) * this.nmember;
-	}
+	let q = a + progression.member(n);
 
-}
-
-let a = sluchch(1, 5);
-let b = sluchch(2, 4);
-let n = sluchch(3, 11);
-
-let progression = new ArithmeticProgression(a, b);
-
-let q = a + progression.member(n);
-
-NAtask.setTask({
-	text: ' Улитка ползет от одного дерева до другого.' +
-		' Каждый день она проползает на одно и то же расстояние больше, чем в предыдущий день.' +
-		' Известно, что за первый и последний дни улитка проползла в общей сложности ' + chislitlx(q, 'метр') +
-		'. Определите, сколько дней улитка потратила на весь путь, если расстояние между деревьями равно ' +
-		chislitlx(progression.summa(n), 'метр') + '.',
-
-	answers: n,
-});
-//Сергей Алендарь
+	NAtask.setTask({
+		text: ' Улитка ползет от одного дерева до другого.' +
+			' Каждый день она проползает на одно и то же расстояние больше, чем в предыдущий день.' +
+			' Известно, что за первый и последний дни улитка проползла в общей сложности ' + chislitlx(q, 'метр') +
+			'. Определите, сколько дней улитка потратила на весь путь, если расстояние между деревьями равно ' +
+			chislitlx(progression.sum(n), 'метр') + '.',
+		answers: n,
+		authors: ['Сергей Алендарь', 'Александра Суматохина'],
+	});
+})();
