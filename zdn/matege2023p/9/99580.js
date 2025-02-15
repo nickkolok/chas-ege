da493719f7c2c11f59ca387945155b85efbb76e3
Aleksandra Sumatokhina <88662554+SugarHedgehog@users.noreply.github.com>
2025-02-15 18:15:38 +0300

    Update 99580.js

--- zdn/matege2023p/9/99580.js
+++ zdn/matege2023p/9/99580.js
@@ -1,38 +1,19 @@
-﻿class ArithmeticProgression {
-	constructor(first, difference) {
-		this.first = first;
-		this.difference = difference;
-		this.nmember = 0;
+(function() {
+	NAinfo.requireApiVersion(0, 2);
 
-	}
+	let a = sluchch(3, 11);
+	let b = sluchch(2, 4);
+	let n = sluchch(4, 11);
 
-	member(nmember) {
-		this.nmember = nmember;
-		return this.first + this.difference * (this.nmember - 1);
-	}
+	let progression = new ArithmeticProgression(a, b);
 
-	summa(nmember) {
-		this.nmember = nmember;
-		return ((2 * this.first + this.difference * (this.nmember - 1)) / 2) * this.nmember;
-	}
-
-}
-
-
-
-let a = sluchch(3, 11);
-let b = sluchch(2, 4);
-let n = sluchch(4, 11);
-
-let progression = new ArithmeticProgression(a, b);
-
-NAtask.setTask({
-	text: ' Рабочие прокладывают тоннель длиной ' + chislitlx(progression.summa(n), 'метр') +  
-		', ежедневно увеличивая норму прокладки на одно и то же число метров.' +
-		' Известно, что за первый день рабочие проложили ' + chislitlx(a, 'метр')+  ' туннеля. ' +
-		' Определите, сколько метров туннеля проложили рабочие в последний день,' +  
-		' если вся работа была выполнена за ' + chislitlx(n, 'день') + '.',
-
-	answers: progression.member(n),
-});
-//Сергей Алендарь
+	NAtask.setTask({
+		text: ' Рабочие прокладывают тоннель длиной ' + chislitlx(progression.sum(n), 'метр') + 
+			', ежедневно увеличивая норму прокладки на одно и то же число метров.' +
+			' Известно, что за первый день рабочие проложили ' + chislitlx(a, 'метр') + ' туннеля. ' +
+			' Определите, сколько метров туннеля проложили рабочие в последний день,' + 
+			' если вся работа была выполнена за ' + chislitlx(n, 'день') + '.',
+		answers: progression.member(n),
+		authors: ['Сергей Алендарь', 'Александра Суматохина'],
+	});
+})();
