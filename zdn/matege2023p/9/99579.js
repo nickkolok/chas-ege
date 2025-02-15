e87b36f2b190bae7a53b2306217374f389142145
Aleksandra Sumatokhina <88662554+SugarHedgehog@users.noreply.github.com>
2025-02-15 18:05:39 +0300

    Update 99579.js

--- zdn/matege2023p/9/99579.js
+++ zdn/matege2023p/9/99579.js
@@ -1,37 +1,20 @@
-﻿class ArithmeticProgression {
-	constructor(first, difference) {
-		this.first = first;
-		this.difference = difference;
-		this.nmember = 0;
+(function() {
+	NAinfo.requireApiVersion(0, 2);
 
-	}
+	let a = sluchch(8, 30);
+	let b = sluchch(2, 4);
+	let n = sluchch(4, 11);
 
-	member(nmember) {
-		this.nmember = nmember;
-		return this.first + this.difference * (this.nmember - 1);
-	}
+	let progression = new ArithmeticProgression(a, b);
 
-	summa(nmember) {
-		this.nmember = nmember;
-		return ((2 * this.first + this.difference * (this.nmember - 1)) / 2) * this.nmember;
-	}
+	let q = a + progression.member(n);
 
-}
-
-let a = sluchch(8, 30);
-let b = sluchch(2, 4);
-let n = sluchch(4, 11);
-
-let progression = new ArithmeticProgression(a, b);
-
-let q = a + progression.member(n);
-
-NAtask.setTask({
-	text: ' Бригада маляров красит забор длиной ' + chislitlx(progression.summa(n), 'метр') +
-		', ежедневно увеличивая норму покраски на одно и то же число метров.' +
-		' Известно, что за первый и последний день в сумме бригада покрасила ' + chislitlx(q, 'метр') + ' забора. ' +
-		' Определите, сколько дней бригада маляров красила весь забор. ',
-
-	answers: n,
-});
-//Сергей Алендарь
+	NAtask.setTask({
+		text: ' Бригада маляров красит забор длиной ' + chislitlx(progression.sum(n), 'метр') +
+			', ежедневно увеличивая норму покраски на одно и то же число метров.' +
+			' Известно, что за первый и последний день в сумме бригада покрасила ' + chislitlx(q, 'метр') + ' забора. ' +
+			' Определите, сколько дней бригада маляров красила весь забор. ',
+		answers: n,
+		authors: ['Сергей Алендарь', 'Александра Суматохина'],
+	});
+})();
