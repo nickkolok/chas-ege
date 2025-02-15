df3ef9e171804ce18cfc0de53b7b5c3584bcb96d
Aleksandra Sumatokhina <88662554+SugarHedgehog@users.noreply.github.com>
2025-02-15 18:12:35 +0300

    Update 99581.js

--- zdn/matege2023p/9/99581.js
+++ zdn/matege2023p/9/99581.js
@@ -1,40 +1,20 @@
-﻿class ArithmeticProgression {
-	constructor(first, difference) {
-		this.first = first;
-		this.difference = difference;
-		this.nmember = 0;
+(function() {
+	NAinfo.requireApiVersion(0, 2);
 
-	}
+	let a = sluchch(1, 15);
+	let b = sluchch(2, 4);
+	let n = sluchch(4, 23);
+	var arr = om.childMaleNames.iz();
 
-	member(nmember) {
-		this.nmember = nmember;
-		return this.first + this.difference * (this.nmember - 1);
-	}
+	let progression = new ArithmeticProgression(a, b);
 
-	summa(nmember) {
-		this.nmember = nmember;
-		return ((2 * this.first + this.difference * (this.nmember - 1)) / 2) * this.nmember;
-	}
-
-}
-
-
-
-let a = sluchch(1, 15);
-let b = sluchch(2, 4);
-let n = sluchch(4, 23);
-var arr = ['Петя', 'Вася', 'Миша', 'Олег', 'Федя', 'Руслан', 'Саша', 'Игорь', 'Артур', 'Толя', 'Борис', 'Гриша', 'Женя'];
-var rand = Math.floor(Math.random() * arr.length);
-
-let progression = new ArithmeticProgression(a, b);
-
-NAtask.setTask({
-	text: sklonlxkand(arr[rand]).de + ' надо решить ' + chislitlx(progression.summa(n), 'задача', 'v') +  
-		'. Он решает на одно и то же количество задач больше по сравнению с предыдущим днем.' +
-		' Известно, что за первый день ' + arr[rand] + ' решил ' + chislitlx(a, 'задача', 'v') +
-		'. Определите, сколько задач решил ' + arr[rand] + ' в последний день,' +  
-		' если со всеми задачами он справился за ' + chislitlx(n, 'день') + '.',
-
-	answers: progression.member(n),
-});
-//Сергей Алендарь
+	NAtask.setTask({
+		text: sklonlxkand(arr).de + ' надо решить ' + chislitlx(progression.sum(n), 'задача', 'v') + 
+			'. Он решает на одно и то же количество задач больше по сравнению с предыдущим днем.' +
+			' Известно, что за первый день ' + arr + ' решил ' + chislitlx(a, 'задача', 'v') +
+			'. Определите, сколько задач решил ' + arr + ' в последний день,' + 
+			' если со всеми задачами он справился за ' + chislitlx(n, 'день') + '.',
+		answers: progression.member(n),
+		authors: ['Сергей Алендарь', 'Александра Суматохина'],
+	});
+})();
