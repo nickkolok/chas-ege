7e22663c5900f07e8e99580b7a1f909703251120
Колесникова Елена <luly3751@gmail.com>
2025-02-18 19:48:06 +0300

    [zdn][new] 513810 - fix #1968 and - fix #1969 edit 1

--- zdn/matege2024b/15/513810.js
+++ zdn/matege2024b/15/513810.js
@@ -2,41 +2,43 @@
     'use strict';
     retryWhileError(function () {
         NAinfo.requireApiVersion(0, 2);
-    
+
         let rand = sl1();
-        let randUpgrade = sl(0, 2);
         let randMoreOrLess = sl1();
         let studentBoy = sklonlxkand(['мальчик', 'юноша', 'парень', 'ученик', 'студент'].iz());
         let studentGirl = sklonlxkand(['девочка', 'девушка', 'ученица', 'студентка'].iz());
+        let boyOrGirl = [studentBoy, studentGirl][rand];
+        let girlOrBoy = [studentBoy, studentGirl][1 - rand];
         let educationInstitution = sklonlxkand(['школа', 'колледж', 'университет', 'ВУЗ', 'техникум', 'лицей'].iz());
         let firstHalfOfStudent = 100 - sl(20, 49, 1);
         let differenceInNumberOfStudent = sl(4, 100, 1);
 
         genAssert(differenceInNumberOfStudent.kratno(firstHalfOfStudent - (100 - firstHalfOfStudent)), "разница учеников в количестве не кратка разнице учеников в %");
 
+        let result = differenceInNumberOfStudent / (firstHalfOfStudent - (100 - firstHalfOfStudent));
 
         NAtask.setTask({
             text:
-                'В ' + educationInstitution.pe + ' ' + [studentBoy, studentGirl][rand].im + ' составляют ' + firstHalfOfStudent +
-                '% числа всех учащихся.' + ' Сколько всего ' +
-                ['учащихся в ' + educationInstitution.pe + ', если ' + [[studentBoy, studentGirl][rand], [studentGirl, studentBoy][rand]][randMoreOrLess].rm +
-                    ' там на ' + differenceInNumberOfStudent + ' учащихся ' + ['больше', 'меньше'][randMoreOrLess] + ', чем ' +
-                    [[studentGirl, studentBoy][rand], [studentBoy, studentGirl][rand]][randMoreOrLess].rm,
+                'В ' + educationInstitution.pe + ' ' + boyOrGirl.im + ' составляют ' + firstHalfOfStudent +
+                '% числа всех учащихся.' + ' Сколько всего ',
 
-                [studentBoy, studentGirl][rand].rm + ' в ' + educationInstitution.pe + ', если ' +
-                [[studentBoy, studentGirl][rand], [studentGirl, studentBoy][rand]][randMoreOrLess].rm + ' там на ' +
-                differenceInNumberOfStudent + ' учащихся ' + ['больше', 'меньше'][randMoreOrLess] + ', чем ' +
-                [[studentGirl, studentBoy][rand], [studentBoy, studentGirl][rand]][randMoreOrLess].rm,
-
-                [studentGirl, studentBoy][rand].rm + ' в ' + educationInstitution.pe + ', если ' +
-                [[studentBoy, studentGirl][rand], [studentGirl, studentBoy][rand]][randMoreOrLess].rm + ' там на ' +
-                differenceInNumberOfStudent + ' учащихся ' + ['больше', 'меньше'][randMoreOrLess] + ', чем ' +
-                [[studentGirl, studentBoy][rand], [studentBoy, studentGirl][rand]][randMoreOrLess].rm][randUpgrade] + '?',
-            answers: [differenceInNumberOfStudent / (firstHalfOfStudent - (100 - firstHalfOfStudent)) * 100,
-            differenceInNumberOfStudent / (firstHalfOfStudent - (100 - firstHalfOfStudent)) * firstHalfOfStudent,
-            differenceInNumberOfStudent / (firstHalfOfStudent - (100 - firstHalfOfStudent)) * (100 - firstHalfOfStudent)][randUpgrade],
+            questions: [
+                {
+                    text: 'учащихся ',
+                    answers: result * 100,
+                },
+                {
+                    text: boyOrGirl.rm,
+                    answers: result * firstHalfOfStudent,
+                },
+                {
+                    text: girlOrBoy.rm,
+                    answers: result * (100 - firstHalfOfStudent),
+                },
+            ],
+            postquestion: ' в ' + educationInstitution.pe + ', если ' + [boyOrGirl, girlOrBoy][randMoreOrLess].rm +
+                ' там на ' + differenceInNumberOfStudent + ' учащихся ' + ['больше', 'меньше'][randMoreOrLess] + ', чем ' +
+                [girlOrBoy, boyOrGirl][randMoreOrLess].rm + '?',
         });
     }, 100);
 })();
-//https://mathb-ege.sdamgia.ru/test?likes=513810
-//zer00player
