a011c3d980f27b92be3efb6620b1839995855b32
Колесникова Елена <luly3751@gmail.com>
2025-02-18 19:02:59 +0300

    [zdn][new] 137247 - fix #1937

---
+++ zdn/matege2024b/15/137247.js
@@ -0,0 +1,35 @@
+(function () {
+    'use strict';
+    retryWhileError(function () {
+        NAinfo.requireApiVersion(0, 2);
+        let rand = sl1();
+        let randUpgrade = sl1();
+        let govOrPriShare = ['государству', 'частным акционерам'].iz(2);
+        let nameOfCompany = ['Что за люди', 'Одичалая ягода', 'Гелий', 'Бра и Тришка', 'Винни и Пух', '1 раз отрежь - 8 раз переделывай', 'Стул да стол', 'Папины сапожки', 'Юпитерианцы', 'Вершки да Корешки'].iz();
+        let persent = sl(20, 90, 1);
+        let companyProfit = sl(40, 500, 1);
+        let onePresent = companyProfit * 10000;
+
+        NAtask.setTask({
+            text:
+                govOrPriShare[0].toZagl() + ' принадлежит ' + persent + '% акций предприятия "' + nameOfCompany + '", ' +
+                'остальные акции принадлежат ' + govOrPriShare[1] + '. ',
+            questions: [
+                {
+                    text: 'Общая прибыль предприятия после уплаты налогов за год составила ' + companyProfit + ' млн. р. ' +
+                        'Какая сумма в рублях из этой прибыли должна пойти на выплату ' + [govOrPriShare[0], govOrPriShare[1]][rand],
+                    answers: [onePresent * persent, onePresent * (100 - persent)][rand],
+                },
+                {
+                    text: 'Прибыль, которая досталась ' + [govOrPriShare[0], govOrPriShare[1]][rand] + ' составляет ' +
+                        [onePresent * persent, onePresent * (100 - persent)][rand] +
+                        ' рублей. Чему была равна общая прибыль предприяти в млн. р.',
+                    answers: companyProfit,
+                },
+            ],
+            postquestion: '?',
+        });
+    }, 100);
+})();
+//https://oge.sdamgia.ru/test?likes=137247
+//zer00player
