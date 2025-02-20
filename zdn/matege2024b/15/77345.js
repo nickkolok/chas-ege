820552a089a41cddd96faae30796f6c29affbd20
Колесникова Елена <luly3751@gmail.com>
2025-02-20 15:23:21 +0300

    [zdn][new] 77345 - fix #1934 and - fix #1936 edit 1

--- zdn/matege2024b/15/77345.js
+++ zdn/matege2024b/15/77345.js
@@ -9,43 +9,47 @@
         let persent = sl(10, 90, 1);
         let taskNumber = sl(1, 20, 1);
         let gender = ['выпускников', 'выпускниц'].iz();
-        let educationInstitution = ['школ', 'университетов', 'колледжей', 'техникумов', 'вечерних школ',].iz();
+        let educationInstitution = ['школ', 'лицеев', 'гимназий', '', 'вечерних школ',].iz();
         let сorrectIncorrect = ['правильно', 'не правильно'][rand];
-        let IncorrectCorrect = ['правильно', 'не правильно'][1 - rand];
+        let incorrectCorrect = ['правильно', 'не правильно'][1 - rand];
 
         let result = students * persent / 100;
+        let answer = [result, students - result][randUpgrade];
+        let checkForCorrect = [сorrectIncorrect, incorrectCorrect][randUpgrade];
+        let typeOfPersent = [persent, 100 - persent][randUpgrade];
+
         NAtask.setTask({
             text: '',
             questions: [
                 {
                     text: 'Из ' + '$' + students + '$' + ' ' + gender + ' ' + educationInstitution + ' города ' + '$' + persent + '$' +
-                        '% ' + сorrectIncorrect + ' решили задачу №' + '$' + taskNumber + '$' + '. Сколько из ' + '$' + students + '$' + ' ' + gender + ' ' + educationInstitution +
-                        ' города ' + [сorrectIncorrect, IncorrectCorrect][randUpgrade] + ' решили задачу №' + '$' + taskNumber + '$',
-                    answers: [result, students - result][randUpgrade],
+                        '% ' + сorrectIncorrect + ' решили задачу №' + '$' + taskNumber + '$' + '. Сколько из ' + gender +
+                        ' города ' + checkForCorrect + ' решили задачу №' + '$' + taskNumber + '$',
+                    answers: answer,
                 },
                 {
-                    text: '$' + [result, students - result][randUpgrade] + '$' + ' из ' + '$' + students + '$' + ' ' + gender + ' ' + educationInstitution + ' города ' +
-                        ' ' + сorrectIncorrect + ' решили задачу №' + '$' + taskNumber + '$' + '. Сколько процентов ' + gender + ' ' + educationInstitution +
-                        ' города ' + [сorrectIncorrect, IncorrectCorrect][randUpgrade] + ' решили задачу №' + '$' + taskNumber + '$',
+                    text: '$' + answer + '$' + ' из ' + '$' + students + '$' + ' ' + gender + ' ' + educationInstitution + ' города ' +
+                        ' ' + сorrectIncorrect + ' решили задачу №' + '$' + taskNumber + '$' + '. Сколько процентов ' + gender + ' ' +
+                        checkForCorrect + ' решили задачу №' + '$' + taskNumber + '$',
                     answers: persent,
                 },
                 {
-                    text: '$' + [result, students - result][randUpgrade] + '$' + ' ' + gender + ' ' + educationInstitution + ' города ' +
-                        ' ' + сorrectIncorrect + ' решили задачу №' + '$' + taskNumber + '$' + '. Что составило ' + '$' + [persent, 100 - persent][randUpgrade] + '$' +
-                        '% от всех решивших' + '. Сколько всего ' + gender + ' ' + educationInstitution + ' попробовали решить задачу №' + '$' + taskNumber + '$',
+                    text: '$' + answer + '$' + ' ' + gender + ' ' + educationInstitution + ' города ' +
+                        ' ' + сorrectIncorrect + ' решили задачу №' + '$' + taskNumber + '$' + '. Что составило ' + '$' + typeOfPersent + '$' +
+                        '% от всех решивших' + '. Сколько всего ' + gender + ' попробовали решить задачу №' + '$' + taskNumber + '$',
                     answers: students,
                 },
                 {
-                    text: '$' + [result, students - result][randUpgrade] + '$' + ' ' + gender + ' ' + educationInstitution + ' города ' +
-                        ' ' + сorrectIncorrect + ' решили задачу №' + '$' + taskNumber + '$' + '. Что составило ' + '$' + [persent, 100 - persent][randUpgrade] + '$' +
-                        '% от всех решивших' + '. Сколько ' + gender + ' ' + educationInstitution + '  решили задачу №' + '$' + taskNumber + '$' + ' ' + IncorrectCorrect,
+                    text: '$' + answer + '$' + ' ' + gender + ' ' + educationInstitution + ' города ' +
+                        ' ' + сorrectIncorrect + ' решили задачу №' + '$' + taskNumber + '$' + '. Что составило ' + '$' + typeOfPersent + '$' +
+                        '% от всех решивших' + '. Сколько ' + gender + '  решили задачу №' + '$' + taskNumber + '$' + ' ' + incorrectCorrect,
                     answers: [students - result, result][randUpgrade],
                 },
                 {
                     text: 'Только ' + '$' + persent + '$' + '% из ' + '$' + students + '$' + ' ' + gender + ' ' +
                         educationInstitution + ' города ' + ' ' + сorrectIncorrect + ' решили задачу №' + '$' + taskNumber + '$' +
-                        '. Сколько ' + gender + ' ' + educationInstitution + '  решили задачу №' + '$' + taskNumber + '$' + ' ' + [сorrectIncorrect, IncorrectCorrect][randUpgrade],
-                    answers: [result, students - result][randUpgrade],
+                        '. Сколько ' + gender + '  решили задачу №' + '$' + taskNumber + '$' + ' ' + checkForCorrect,
+                    answers: answer,
                 },
             ],
             postquestion: '?',
