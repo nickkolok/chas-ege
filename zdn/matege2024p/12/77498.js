8ec2c0ef73b712432242bc76ba5681b0b3ce139f
Алендарь Сергей <sergey.alendar@mail.ru>
2025-03-06 22:24:52 +0300

    Файл 77498.js изменён. Изменена генерация занчений под корнем и самих уравнений

--- zdn/matege2024p/12/77498.js
+++ zdn/matege2024p/12/77498.js
@@ -1,24 +1,27 @@
 (function() {
 	retryWhileError(function() {
 		'use strict';
-		let a1 = sl(1, 50).pm();
-		let a2 = sl(1, 19);
-		let b1 = sl(1, 50);
+		let a1 = sl(1, 9, 0.5).pm();
+		let a2 = a1 * sl(2, 6);
+		let a3 = slKrome(5, 1, 6);
 		let b2 = sl(1, 19);
-		let c = sl(1, 19);
+		let c = slKrome(5, 1, 6);
 		let d = sl(1, 99);
-		let plusminus = ['+', '-'];
+		let sign = ['+', '-'];
 		let sincos = ['sin', 'cos'];
 		NAtask.setMinimaxFunctionTask({
-			expr: [a1, a1 + 'sqrt(' + a2 + ')'].iz() + sincos.iz() + '(x)' + plusminus.iz() + [c, b1 + 'sqrt(' + c + ')'].iz() +
-				'x' + plusminus.iz() + [c + 'pi/' + b2, 'sqrt(' + c + ')' + 'pi'].iz() + plusminus.iz() + d,
+			expr: [a1, a2 * sl(2, 3) + ['sqrt(' + a3 + ')', ''].iz()].iz() + sincos.iz() + 
+				'(x)' + sign.iz() + [a2, a2 + ['sqrt(' + c + ')', ''].iz()].iz() + 
+				'x' + sign.iz() + a1 + ['sqrt(' + c + ')', ''].iz() + 'pi' + [sign.iz() + d, ''].iz(),
 			leftEnd: '0',
 			rightEnd: 'pi/2',
 			primaryStep: 0.1,
 			secondaryStep: 0.0001,
 			authors: ['Алендарь Сергей'],
 		});
-	}, 1000);
+	}, 10000);
 })();
+
 //77498
+//77499
 
