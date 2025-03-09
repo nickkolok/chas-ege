d47f44b0b7f66f562c68c49855c420db2f1c80db
Колесникова Елена <luly3751@gmail.com>
2025-03-09 19:41:45 +0300

    [zdn][new] 509648- fix #1962

---
+++ zdn/matege2024b/15/509648.js
@@ -0,0 +1,45 @@
+(function () {
+    'use strict';
+    retryWhileError(function () {
+        NAinfo.requireApiVersion(0, 2);
+
+        let rand = sl1();
+        let studentsFirst = sl(200, 10000, 10);
+        let studentsArrived = sl(20, 200, 10);
+        let studentsSecond = studentsFirst + studentsArrived;
+
+        let result = studentsArrived / studentsFirst * 100;
+        genAssertZ1000(result, "слишком много знаков после запятой");
+
+        let educationInstitution = sklonlxkand(['школа', 'лицей', 'гимназия', 'академия', 'колледж', 'университет', 'институт', 'ВУЗ', 'техникум', 'училище'].iz());
+
+        NAtask.setTask({
+            text:
+                'В начале учебного года в ' + educationInstitution.pe,
+
+            questions: [
+                {
+                    text: ' было ' + students + ' учащихся, ' +
+                        'а к концу года их стало ' + studentsSecond + '. ' +
+                        'На сколько процентов увеличилось за учебный год число учащихся',
+                    answers: result,
+                },
+                {
+                    text: ' было ' + studentsFirst + ' учащихся, ' +
+                        'а к концу года их стало на ' + result + '% больше. ' +
+                        ' ' + ['На сколько человек увеличилось число учащихся за учебный год', 'Сколько всего теперь учащихся'][rand],
+                    answers: [studentsArrived, studentsSecond][rand],
+                },
+                {
+                    text: ' было на ' + result + '% меньше учащихся, чем в конце года. ' +
+                        'Известно, что к весне стало ' + studentsSecond + ' учащихся. ' +
+                        'Сколько человек обучались в ' + educationInstitution.pe + ' в начале года',
+                    answers: studentsFirst,
+                },
+            ],
+            postquestion: '?',
+        });
+    }, 100);
+})();
+//https://mathb-ege.sdamgia.ru/test?likes=509648
+//zer00player
