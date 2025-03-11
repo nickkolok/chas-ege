12160c6e55dd2db48d5ef2a75ea1f38e21858e75
Колесникова Елена <luly3751@gmail.com>
2025-03-11 09:40:47 +0300

    [zdn][new] 506426 - fix #1957 edit 1

--- zdn/matege2024b/15/506426.js
+++ zdn/matege2024b/15/506426.js
@@ -2,9 +2,9 @@
     'use strict';
     retryWhileError(function () {
         NAinfo.requireApiVersion(0, 2);
-    
+
         let rand = sl1();
-       
+
         let clotherMR = sklonlxkand(['свитер', 'халат', 'плащ', 'дождевик', 'товар'].iz());
         let clotherWR = sklonlxkand(['рубашка', 'футболка', 'куртка', 'кофта'].iz());
         let clothers = [clotherMR, clotherWR][rand];
@@ -15,8 +15,7 @@
         let prise = sl(300, 5000, 10);
         let percent = sl(10, 80, 1);
 
-        let result = ((100 - percent) * 0.01 * prise);
-        result = result % 1 === 0 ? Math.round(result) : parseFloat(result.toFixed(1));
+        let result = (100 - percent) * 0.01 * prise;
 
 
         NAtask.setTask({
@@ -24,24 +23,25 @@
                 clothers.ve.toZagl() + ' на распродаже уценили ',
             questions: [
                 {
-                    text: 'на ' + '$' + percent + '$' + '% при этом он' + mw + ' ' + hasBecome + ' стоить ' + '$' + result + '$' + ' р. ' +
+                    text: 'на ' + '$' + percent + '$' + '%, при этом он' + mw + ' ' + hasBecome + ' стоить ' + '$' + result + '$' + ' р. ' +
                         'Сколько рублей ' + cost + ' ' + clothers.ie + ' до распродажи',
                     answers: prise,
                 },
                 {
-                    text: ' при этом он' + mw + ' ' + hasBecome + ' стоить ' + '$' + result + '$' + ' р. ' + 'До распродажи ' + clothers.ie + ' ' + cost +
+                    text: ', при этом он' + mw + ' ' + hasBecome + ' стоить ' + '$' + result + '$' + ' р. ' + 'До распродажи ' + clothers.ie + ' ' + cost +
                         ' ' + '$' + prise + '$' + ' р. Сколько процентов составляет скидка',
                     answers: percent,
                 },
                 {
                     text: 'на ' + '$' + percent + '$' + '%.' + ' До распродажи ' + clothers.ie + ' ' + cost + ' ' + '$' + prise + '$' +
-                        ' р. Сколько ' + cost + ' ' + clothers.ie + ' во время распродажи',
+                        ' р. Сколько рублей стоит ' + clothers.ie + ' во время распродажи',
                     answers: result,
                 },
 
             ],
             postquestion: '?',
         });
+        NAtask.modifiers.allDecimalsToStandard();
     }, 100);
 })();
 //https://mathb-ege.sdamgia.ru/test?likes=506426
