ed19d29d0ee541e1df6e4944421176033094acc7
Алендарь Сергей <sergey.alendar@mail.ru>
2025-03-15 13:00:14 +0300

    Изменены названия переменных, расширена генерация значений, Добавлены новые варианты слов в тексте, удалён лишний текст из-за которого задача не запускалась

--- zdn/matege2024p/10/99579.js
+++ zdn/matege2024p/10/99579.js
@@ -1,36 +1,19 @@
-class ArithmeticProgression {
-	constructor(first, difference) {
-		this.first = first;
-		this.difference = difference;
-		this.nmember = 0;
-	}
+(function() {
+	let firstDay = sl(5, 30);
+	let difference = sl(2, 6);
+	let days = sl(5, 15);
+	let progression = new ArithmeticProgression(firstDay, difference);
+	let firstAndLastSum = firstDay + progression.member(days);
+	let people = ['маляров', 'рабочих', 'мастеров', 'работников'][sl(0, 3)];
+	let group = ['бригада', 'группа', 'команда', 'отряд', 'коллектив'][sl(0, 4)];
 
-	member(nmember) {
-		this.nmember = nmember;
-		return this.first + this.difference * (this.nmember - 1);
-	}
+	NAtask.setTask({
+		text: group.toZagl() + ' ' + people + ' красит забор длиной ' + chislitlx(progression.sum(days), 'метр') +
+			' , ежедневно увеличивая норму покраски на одно и то же число метров.' +
+			' Известно, что за первый и последний день в сумме ' + group + ' покрасила ' + chislitlx(firstAndLastSum, 'метр') + ' забора. ' +
+			' Определите, сколько дней ' + group + ' ' + people + ' красила весь забор.',
+		answers: days,
+	});
+})();
 
-	summa(nmember) {
-		this.nmember = nmember;
-		return ((2 * this.first + this.difference * (this.nmember - 1)) / 2) * this.nmember;
-	}
-}
-
-let a = sluchch(8, 30);
-let b = sluchch(2, 4);
-let n = sluchch(4, 11);
-let progression = new ArithmeticProgression(a, b);
-let q = a + progression.member(n);
-
-NAtask.setTask({
-	text: ' Бригада маляров красит забор длиной ' + chislitlx(progression.summa(n), 'метр') +
-		' , ежедневно увеличивая норму покраски на одно и то же число метров.' +
-		' Известно, что за первый и последний день в сумме бригада покрасила ' + chislitlx(q, 'метр') + ' забора. ' +
-		' Определите, сколько дней бригада маляров красила весь забор.
-	text: sklonlxkand(arr[rand]).de + ' надо решить ' + chislitlx(progression.summa(n), 'задача', 'v') +  
-		'. Он решает на одно и то же количество задач больше по сравнению с предыдущим днем.' +
-		' Известно, что за первый день ' + arr[rand] + ' решил ' + chislitlx(a, 'задача', 'v') +
-		'. Определите, сколько задач решил ' + arr[rand] + ' в последний день,' +  
-		' если со всеми задачами он справился за ' + chislitlx(n, 'день') + '.', ',
-	answers: n,
-});
+//99579
