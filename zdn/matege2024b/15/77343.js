48597051256bb6f4b9022f699669fa7c0d9644a1
Колесникова Елена <luly3751@gmail.com>
2025-03-16 13:46:39 +0300

    [zdn][new] 77343 - fix #1949 edit 1

--- zdn/matege2024b/15/77343.js
+++ zdn/matege2024b/15/77343.js
@@ -3,15 +3,20 @@
     retryWhileError(function () {
         NAinfo.requireApiVersion(0, 2);
 
+        let rand = sl1();
+        let priseExpensive = sl(60, 200, 1);
+        let priseCheap = sl(3, 50, 1);
+        let prise = [priseExpensive, priseCheap][rand];
 
-        let prise = sl(3, 100, 1);
-        let persent = sl(10, 50, 1);
+        let itemCheap = sklonlxkand(['тетрадь', 'ручка', 'карандаш', 'ластик', 'файл'].iz());
+        let itemExpensive = sklonlxkand(['скотч', 'фломастер', 'маркер', 'папка'].iz());
+        let item = [itemExpensive, itemCheap][rand];
+
+        let perсent = sl(10, 90, 1);
         let countNotebooksBeforeDsicount = sl(10, 100, 10);
         let numberOfNotebooks = countNotebooksBeforeDsicount + sl(10, 200, 5);
 
-        let item = sklonlxkand(['тетрадь', 'ручка', 'карандаш', 'ластик', 'скотч', 'фломастер', 'маркер', 'файл', 'папка'].iz());
-
-        let result = numberOfNotebooks * prise - persent * 0.01 * (numberOfNotebooks * prise);
+        let result = prise * (numberOfNotebooks - perсent * 0.01 * numberOfNotebooks);
 
         NAtask.setTask({
             text: '',
@@ -20,24 +25,24 @@
                     text: item.ie.toZagl() + ' стоит ' + prise + ' р. ' +
                         'Сколько рублей заплатит покупатель за ' + chislitlx(numberOfNotebooks, item) + ', ' +
                         'если при покупке больше ' + chislitlx(countNotebooksBeforeDsicount, item) + ' магазин делает скидку ' +
-                        persent + '% от стоимости всей покупки',
+                        perсent + '% от стоимости всей покупки',
                     answers: result,
                 },
                 {
                     text: item.ie.toZagl() + ' стоит ' + prise + ' р. ' +
                         'Магазин делает скидку от стоимости всей покупки, если купить больше ' + chislitlx(countNotebooksBeforeDsicount, item) +
                         '. Сколько процентов составляет скидка, если покупатель заплатил за ' + chislitlx(numberOfNotebooks, item) + ' ' + result + ' р. ',
-                    answers: persent,
+                    answers: perсent,
                 },
                 {
                     text: item.ie.toZagl() + ' стоит ' + prise + ' р. ' +
-                        'Магазин делает скидку ' + persent + '% от стоимости всей покупки, если купить больше ' + chislitlx(countNotebooksBeforeDsicount, item) +
+                        'Магазин делает скидку ' + perсent + '% от стоимости всей покупки, если купить больше ' + chislitlx(countNotebooksBeforeDsicount, item) +
                         '. Покупатель заплатил за покупку ' + result + ' р. с учётом скидки. Сколько ' + item.rm +
-                        ' было куплено, если известно, что условия скидки были соблюдены',
+                        ' было куплено, если известно, что условия предоставления скидки были соблюдены',
                     answers: numberOfNotebooks,
                 },
                 {
-                    text: 'Магазин делает скидку ' + persent + '% от стоимости всей покупки, если купить больше ' + chislitlx(countNotebooksBeforeDsicount, item) +
+                    text: 'Магазин делает скидку ' + perсent + '% от стоимости всей покупки, если купить больше ' + chislitlx(countNotebooksBeforeDsicount, item) +
                         '. Покупатель заплатил за покупку ' + chislitlx(numberOfNotebooks, item) + ' ' + result + ' р. Сколько стоит 1 ' + item.ie,
                     answers: prise,
                 },
