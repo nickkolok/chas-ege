2d8561a9bc1a5bc906026ea168f3d4e4e629883b
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-03-17 10:37:43 +0300

    fix

--- sh/pechmat.js
+++ sh/pechmat.js
@@ -134,7 +134,7 @@ function konecSozd() {
 			).
 			 // Escape LaTeX comments,
 			 // but don't ruin if they've been already escaped!
-			 replace(/\\?%/g, '\\%').replace(/<br>/g, '\\\\').replace(/<br\/>/g, '\\\\').replace(/<b>/g, '\\textbf{').replace(/<\/b>/g, '}');
+			 replace(/\\?%/g, '\\%').replace(/<br>/g, '\\\\').replace(/<br\/>/g, '\\\\').replace(/<b>/g, '\\textbf{').replace(/<\/b>/g, '}').replace(/\" /g, '"\\space ');
 		}
 	}
 
