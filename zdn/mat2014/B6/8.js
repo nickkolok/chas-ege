b5efb5d4a4b87db9a00e625c7fb2d279e2ac1e4f
VeronikaKit <166473321+VeronikaKit@users.noreply.github.com>
2025-03-20 15:54:54 +0300

    8 refactoring (#2135)
    * 8 refactoring
    
    * Update 8.js
    
    ---------
    
    Co-authored-by: VeronikaKit <veroninika.ushakova.dm@mail.ru>

--- zdn/mat2014/B6/8.js
+++ zdn/mat2014/B6/8.js
@@ -1,24 +1,23 @@
-(function() {
-	var all_sports, rus_sports, answ, is_rus;
+(function() { 'use strict'; retryWhileError(function() {
+	let all_sports, rus_sports, answ, is_rus;
 	do {
 		all_sports = sluchch(100, 900, 2);
 		rus_sports = sluchch(10, all_sports - 50);
 		is_rus = ['', 'не '].iz();
 		answ = is_rus === '' ? (rus_sports - 1) / (all_sports - 1) : (all_sports - rus_sports) / (all_sports - 1);
 	} while (answ.ts().length > 4)
-	var name = om.imenaj.ie.iz();
-	var game = om.sportparn.pe.iz();
-	var country = om.strany.re.iz();
-	window.vopr.txt = 'Перед началом первого тура чемпионата по ' + game +
+	let name = om.imenaj.ie.iz();
+	let game = om.sportparn.pe.iz();
+	let country = om.strany.re.iz();
+	NAtask.setTask({
+		text: 'Перед началом первого тура чемпионата по ' + game +
 		' участниц разбивают на игровые пары случайным образом ' +
-		'с помощью жребия. Всего в чемпионате участвует ' + chislitM(all_sports, 'спортсменка', 'спортсменки', 'спортсменок') +
-		', среди которых ' + chislitM(rus_sports, 'участница', 'участницы', 'участниц') + ' из ' + country + ', ' +
+		'с помощью жребия. Всего в чемпионате участвует ' + chislitlx(all_sports, 'спортсменка') +
+		', среди которых ' + chislitlx(rus_sports, 'участница') + ' из ' + country + ', ' +
 		'в том числе ' + name + '. Найдите вероятность того, что в первом туре ' + name +
-		' будет играть с какой-либо спортсменкой ' + is_rus + 'из ' + country + '.';
-	window.vopr.ver = [answ];
-	window.vopr.kat['log'] = 0;
-	window.vopr.kat['prz'] = 0;
-	window.vopr.kat['drs'] = 0;
-	window.vopr.kat['tri'] = 0;
-})();
+		' будет играть с какой-либо спортсменкой ' + is_rus + 'из ' + country + '.',
+	        answers: answ,
+	});
+	NAtask.modifiers.allDecimalsToStandard(/*true*/);
+}, 2000);})();
 //fixed by Aisse-258
