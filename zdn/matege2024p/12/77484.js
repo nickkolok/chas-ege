aa4aff1fc9bfcf685636da125567e150306c63a9
Алендарь Сергей <sergey.alendar@mail.ru>
2025-03-27 20:31:43 +0300

    Файл main.js добавлен в соответсвующую папку

---
+++ zdn/matege2024p/12/77484.js
@@ -0,0 +1,19 @@
+(function() {
+	retryWhileError(function() {
+		'use strict';
+		let a = sl(1, 99).pm();
+		let b = a + sl(1, 5);
+		let sign = ['+', '-'];
+		NAtask.setMinimaxFunctionTask({
+			expr: '(x + ' + b + ')^2*e^(-' + a + '-x)',
+			leftEnd: (-a - sl(0.5, 2, 0.5)) + '',
+			rightEnd: (-a + sl(0.5, 2, 0.5)) + '',
+			primaryStep: 0.01,
+			secondaryStep: 0.0001,
+			authors: ['Алендарь Сергей'],
+		});
+	}, 10000);
+})();
+
+//77484 
+//77485
