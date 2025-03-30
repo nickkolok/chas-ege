b99da6546f2886a0eac9b648d8855c96d86820e5
Колесникова Елена <luly3751@gmail.com>
2025-03-30 13:55:48 +0300

    [zdn][new] 512925 - fix #1977

---
+++ zdn/matege2024b/21/512925.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+    retryWhileError(function () {
+        NAinfo.requireApiVersion(0, 2);
+        let countPillars = sl(3, 20, 1);
+        let countWires = sluchch(3, 20, 1);
+        genAssert((countWires * countPillars).kratno(2), "Количество соединений не кратно двум ")
+        NAtask.setTask({
+            text: chislitlx(countPillars, 'столб', 'v$') + ' соединены между собой проводами так, ' +
+                'что от каждого столба отходит ровно ' + chislitlx(countWires, 'провод', 'r$') + '. ' +
+                'Сколько всего проводов протянуто между этими столбами?',
+            answers: (countWires * countPillars) / 2,
+        });
+    });
+})();
+//https://mathb-ege.sdamgia.ru/test?likes=512925
+//zer00player
