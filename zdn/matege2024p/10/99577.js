5773e4986f84ffac2f4e123435e557693f8ecde4
Aleksandra Sumatokhina <88662554+SugarHedgehog@users.noreply.github.com>
2025-03-31 12:41:57 +0300

    Update 99577.js

--- zdn/matege2024p/10/99577.js
+++ zdn/matege2024p/10/99577.js
@@ -2,19 +2,11 @@
     'use strict'; retryWhileError(function () {
         /* Смешав 30 -процентный и 60 -процентный растворы '+mixin.re+' и добавив 10 кг чистой воды, получили 36 -процентный раствор '+mixin.re+'. Если бы вместо 10 кг воды добавили 10 кг 50 -процентного раствора той же '+mixin.re+', то получили бы 41 -процентный раствор '+mixin.re+'. Сколько килограммов 30 -процентного раствора использовали для получения смеси? */
         let key = '99577';
-        let rand = getListedPreference(key, [{
-            preference: 'first_mass',
-            preferenceValue: 0,
-        }, {
-            preference: 'second_mass',
-            preferenceValue: 1,
-        }, {
-            preference: 'mass_of_mixin_in_first',
-            preferenceValue: 2,
-        }, {
-            preference: 'mass_of_mixin_in_second',
-            preferenceValue: 3,
-        }], sl1());
+        let preference = ['first_mass', 'second_mass', 'mass_of_mixin_in_first', 'mass_of_mixin_in_second'];
+        let rand = getListedPreference(key, preference.map((pref, index) => ({
+			preference: pref,
+			preferenceValue: index
+		})), sl(preference.length - 1));
 
         let firstProcent = sl(1, 60);
         let firstMass = slKrome(firstProcent, 60);
