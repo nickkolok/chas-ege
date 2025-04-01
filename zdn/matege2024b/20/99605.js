11a316c9121a38d95961c11ebb88c879dce682ba
VeronikaKit <166473321+VeronikaKit@users.noreply.github.com>
2025-04-01 18:41:20 +0300

    Update 99605.js

--- zdn/matege2024b/20/99605.js
+++ zdn/matege2024b/20/99605.js
@@ -1,11 +1,10 @@
 (function() { 'use strict'; retryWhileError(function() {
 	let a=sl(10, 150, 1);
 	let b=slKrome(a, 10, 150);
-	let c=slKrome(b, 10, 150);
+	let c=slKrome([a,b], 10, 150);
         let v=3*a*b*c/(a*b+b*c+a*c);
         let d=['пути', 'трассы'].iz();
 	genAssert(v.isAlmostInteger(),'Скорость не может быть дробной');
-	genAssert(a!=c, 'Скорости не могут равняться')
 	NAtask.setTask({
 		text:'Первую треть '+d+' автомобиль ехал со скоростью '+a+' км/ч, вторую треть - со скоростью '+b+' км/ч, а последнюю - со скоростью '+c+' км/ч. Найдите среднюю скорость автомобиля на протяжении всего пути. Ответ дайте в км/ч.',
 		answers: v,
