a07978a8ce2ad43b33256f0280247158bff68914
VeronikaKit <166473321+VeronikaKit@users.noreply.github.com>
2025-04-01 19:45:17 +0300

    Update 99621.js

--- zdn/matege2024b/20/99621.js
+++ zdn/matege2024b/20/99621.js
@@ -3,13 +3,14 @@
 	let b=sl(a+1, 50, 1);
 	let c=sl(1, 59, 1);
         let v=(a*b*c)/(60*(b-a));
+        let rebenok=sklonlxkand(om.childMaleNames.iz());
+        let rebenoka=sklonlxkand(om.childMaleNames.iz());
 	genAssert(v.isAlmostInteger(),'Колличество вопросов не может быть дробным');
 	NAtask.setTask({
-		text: 'Петя и Ваня выполняют одинаковый тест. Петя отвечает за час на '+chislitlx(a, 'вопрос')+' теста, а Ваня - на '+
-		b+'. Они одновременно начали отвечать на вопросы теста, и Петя закончил свой тест позже Вани на '+chislitlx(c,'минута')+'. Сколько вопросов содержит тест?',
+		text: rebenok.ie.toZagl()+' и '+rebenoka.ie+' выполняют одинаковый тест. '+rebenok.ie+' отвечает за час на '+chislitlx(a, 'вопрос')+' теста, а '+rebenoka.ie+' - на '+
+		b+'. Они одновременно начали отвечать на вопросы теста, и '+rebenok.ie+' закончил свой тест позже '+rebenoka.ie+' на '+chislitlx(c,'минута')+'. Сколько вопросов содержит тест?',
 		answers: v,
 		authors: ['VeronikaKit'],
 	});
 	NAtask.modifiers.allDecimalsToStandard(/*true*/);
 }, 20000);})();
-//РешуЕГЭ-99621
