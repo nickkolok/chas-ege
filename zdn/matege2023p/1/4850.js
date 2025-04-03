5220827d678cda5db042f029b8ed95722989333a
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-04-03 11:53:52 +0300

    [zdn] [up] - ЕГЭ База - 12 - 115-118 #2236

--- zdn/matege2023p/1/4850.js
+++ zdn/matege2023p/1/4850.js
@@ -2,50 +2,71 @@
 	retryWhileError(function() {
 		NAinfo.requireApiVersion(0, 2);
 
-		let a = sl(2, 50);
-		let b = slKrome(a, 2, 50);
+		let rand = sl1(); //AB < BC = 0, AB > BC = 1
 
-		let rand = sl1();
-		let h = sl(1, !rand ? [a, b].minE() - 1 : [a, b].maxE() - 1);
+		let a = sl(4, 48);
 
-		let answ = !rand ? [a, b].minE() * h / [a, b].maxE() : [a, b].maxE()* h / [a, b].minE();
+		let par = new Parallelogram({
+			lengths: {
+				lengthAB: a,
+				lengthBC: [sl(a + 1, 50), sl(2, a - 1)][rand]
+			},
+			angles: {
+				angle: {
+					angleA: Math.PI * [1 / 6, 2 / 3].iz()
+				},
+			},
+			supplementary: {
+				calculateHeights: true,
+			}
+		});
 
-		genAssertZ1000(answ, 'Кривой ответ');
+
+		genAssert(par.heightDAB.pe.x + 5 < par.pointB.x, 'Высота из вершины D к стороне AB выходит за пределы фигуры');
+		genAssert(par.heightDBC.pe.x > par.pointB.x, 'Высота из вершины D к стороне BC выходит за пределы фигуры');
+
+		let height = [par.lengthHeightDAB, par.lengthHeightDBC];
+		height.forEach((h) => genAssert(h.isAlmostInteger(), 'Высоты не целые'));
+
+		par.addVertexToConnectionMatrix([par.heightDAB.pe, par.heightDBC.pe], 'E');
+		par.connectVerticesInConnectionMatrix([
+			[3, 4],
+			[3, 5]
+		]);
+
+		let points = autoScale(par.vertices);
 
 		let paint1 = function(ctx) {
+			let h = 400;
+			let w = 400;
+
+			ctx.translate(w / 2, h / 2);
+
+			ctx.scale(1, -1);
+			ctx.strokeStyle = om.secondaryBrandColors;
+
 			ctx.lineWidth = 2;
+			ctx.drawFigure(points, par.connectionMatrix);
 
-			ctx.strokeStyle = om.secondaryBrandColors.iz();
-
-			let angle = Math.PI / 2.5;
-
-			let ver1 = ctx.drawLineAtAngle(20, 300, -angle, 250);
-			ctx.drawLine(ver1.x, ver1.y, 340, ver1.y);
-			ctx.drawLine(20, 300, 300 - 40, 300);
-			ctx.drawLine(300 - 40, 300, 340, ver1.y);
-
-			//высоты
-			ctx.strokeStyle = om.primaryBrandColors[0];
-			ctx.drawLine(ver1.x, ver1.y, ver1.x, 300);
-			let ver2 = ctx.drawLineAtAngle(ver1.x, ver1.y, -angle - 1.5 * Math.PI, 230);
-
-			//прямые углы
 			ctx.strokeStyle = om.primaryBrandColors[1];
-			ctx.arcBetweenSegments([ver1.x, ver1.y, ver1.x, 300, 300 - 40, 300], 20);
-			ctx.arcBetweenSegments([340, ver1.y, ver2.x, ver2.y, ver1.x, ver1.y], 20);
+			ctx.arcBetweenSegments([points[3].x, points[3].y, points[4].x, points[4].y, points[0].x, points[0].y], 15);
+			ctx.arcBetweenSegments([points[3].x, points[3].y, points[5].x, points[5].y, points[2].x, points[2].y], 15);
 		};
 
 		NAtask.setTask({
-			text: 'Стороны параллелограмма равны $' + [a, b].shuffleJoin('$ и $') + '$. Высота, ' +
-				'опущенная на ' + ['меньшую', 'большую'][rand] + ' из этих сторон, ' +
-				'равна $' + h + '$. Найдите высоту, опущенную на '+['меньшую', 'большую'][1-rand]+' сторону параллелограмма.',
-			answers: answ,
+			text: 'Стороны параллелограмма равны $' + [par.lengthAB, par.lengthBC].shuffleJoin('$ и $') + '$. Высота, ' +
+				'опущенная на ' + ['большую', 'меньшую'][rand] + ' из этих сторон, ' +
+				'равна $' + height[rand] + '$. Найдите высоту, опущенную на ' + ['меньшую', 'большую'][1 - rand] +
+				' сторону параллелограмма.',
+			answers: height[1 - rand],
+			authors: ['Александра Суматохина'],
 		});
+		NAtask.modifiers.allDecimalsToStandard(true);
 		NAtask.modifiers.addCanvasIllustration({
 			width: 400,
 			height: 400,
 			paint: paint1,
 		});
-	}, 1000);
+	}, 2000);
 })();
 //4850
