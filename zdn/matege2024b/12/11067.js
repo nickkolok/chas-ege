4e9b86b646133b42b865d061444d018d73843f8c
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-04-04 12:11:24 +0300

    add task

---
+++ zdn/matege2024b/12/11067.js
@@ -0,0 +1,55 @@
+(function() {
+	retryWhileError(function() {
+		NAinfo.requireApiVersion(0, 2);
+
+		let a = sl(1, 50);
+
+		let triangle = new Triangle({
+			lengths: {
+				lengthAB: a,
+				lengthBC: slKrome(a, 1, 50),
+			},
+			angles: {
+				angle: Math.PI / 2,
+			},
+			supplementary: {
+				calculateHeights: true,
+			}
+		});
+
+		genAssertZ1000(triangle.heightBLength, 'Высота не целая');
+
+		let points = autoScale(triangle.vertices);
+
+		let paint1 = function(ctx) {
+			let h = 400;
+			let w = 400;
+
+			ctx.translate(w / 2, h / 2);
+
+			ctx.scale(1, -1);
+			ctx.strokeStyle = om.secondaryBrandColors;
+
+			ctx.lineWidth = 2;
+			ctx.drawFigure(points, triangle.connectionMatrix);
+
+			ctx.strokeStyle = om.primaryBrandColors.iz();
+			ctx.arcBetweenSegments([points[0].x, points[0].y, points[1].x, points[1].y, points[2].x, points[2].y], 20);
+		};
+
+		NAtask.setTask({
+			text: `В прямоугольном треугольнике катеты равны $${triangle.lengthAB}$ и $${triangle.lengthBC}$. Найдите высоту, опущенную на гипотенузу.`,
+			answers: triangle.heightBLength,
+			authors: ['Александра Суматохина'],
+		});
+		NAtask.modifiers.allDecimalsToStandard(true);
+
+		NAtask.modifiers.addCanvasIllustration({
+			width: 400,
+			height: 400,
+			paint: paint1,
+		});
+	}, 2000);
+
+})();
+// https://base.mathege.ru/clones/?position=16&parent=11067
