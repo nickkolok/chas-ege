8b8a7c07f5cb0f77322ababb1eba50a1a8798df5
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-04-09 15:00:33 +0300

    fix sides

--- zdn/matege2024b/12/10188.js
+++ zdn/matege2024b/12/10188.js
@@ -1,24 +1,26 @@
 (function() {
 	retryWhileError(function() {
 		NAinfo.requireApiVersion(0, 2);
-		let letters = latbukv.slice(0, 3).concat('H');
-
-		let a = sl(1, 50);
+		let letters = latbukv.slice(0, 3);
+		
+		let angleB = Math.PI / [6, 3].iz();
+		let AC = sl(1,50);
+		let sinB = angleB.sin();
+		let sinA = {n:sl(1,6), d:sl(7,10)};
+		
 		let triangle = new Triangle({
 			lengths: {
-				lengthCA: a,
-				lengthBC: slKrome(a, 1, 50),
+				lengthCA: AC,
+				lengthBC: (sinA.n*AC)/(sinB*sinA.d),
 			},
 			angles: {
-				angle: Math.PI / 2,
+				angle: Math.PI - angleB - (sinA.n/sinA.d).asin(),
 			},
-			supplementary: {
-				calculateHeights: true,
-			}
 		});
 
 		genAssertZ1000(triangle.sinA.pow(2));
-		genAssert((triangle.sinB - (0.75).sqrt()).abs() < 0.1 || (triangle.sinB - 0.5).abs() < 0.1, 'Угол B не равен 30');
+		genAssert((triangle.sinA-0.5)>0.1, 'Задача теряет смысл с sinA=1/2');
+		genAssertZ1000(triangle.lengthBC.pow(2));
 
 		let points = autoScale(triangle.vertices, {
 			x: 0,
@@ -58,7 +60,7 @@
 		};
 
 		NAtask.setTask({
-			text: ` В треугольнике $ABC$ известно, что $BC=${triangle.lengthAB.pow(2).texsqrt(1)}$, $\\sin A=${triangle.sinA.pow(2).texsqrtfrac(1)}$, внешний угол при вершине $B$ равен $${[`150 `, `120 `][(triangle.sinB - 0.5).abs() < 0.1? 0: 1]}^\\circ$. Найдите $AC$.`,
+			text: ` В треугольнике $ABC$ известно, что $BC=${triangle.lengthBC.pow(2).texsqrtfrac(1)}$, $\\sin A=${triangle.sinA.pow(2).texsqrtfrac(1)}$, внешний угол при вершине $B$ равен $${180 - triangle.angleBInDegrees}^\\circ$. Найдите $AC$.`,
 			answers: triangle.lengthCA,
 			authors: ['Александра Суматохина'],
 		});
