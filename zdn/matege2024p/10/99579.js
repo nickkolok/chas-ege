ffeda873707ed4013c026389ed4e4c4273a148fd
Алендарь Сергей <sergey.alendar@mail.ru>
2025-04-13 15:35:39 +0300

    Убран лишний пробел, и добавлено склонение по мужскому и женскому роду

--- zdn/matege2024p/10/99579.js
+++ zdn/matege2024p/10/99579.js
@@ -5,12 +5,14 @@
 	let progression = new ArithmeticProgression(firstDay, difference);
 	let firstAndLastSum = firstDay + progression.member(days);
 	let people = ['маляров', 'рабочих', 'мастеров', 'работников'][sl(0, 3)];
-	let group = ['бригада', 'группа', 'команда', 'отряд', 'коллектив'][sl(0, 4)];
+	let slChislo = sl1();
+	let rod = ['покрасила', 'покрасил'];
+	let group = ['бригада', 'группа', 'команда', 'отряд', 'коллектив', 'персонал'][slChislo * 3 + sl(0, 2)];
 
 	NAtask.setTask({
 		text: group.toZagl() + ' ' + people + ' красит забор длиной ' + chislitlx(progression.sum(days), 'метр') +
-			' , ежедневно увеличивая норму покраски на одно и то же число метров.' +
-			' Известно, что за первый и последний день в сумме ' + group + ' покрасила ' + chislitlx(firstAndLastSum, 'метр') + ' забора. ' +
+			', ежедневно увеличивая норму покраски на одно и то же число метров.' +
+			' Известно, что за первый и последний день в сумме ' + group + ' ' + rod[slChislo] + ' ' + chislitlx(firstAndLastSum, 'метр') + ' забора. ' +
 			' Определите, сколько дней ' + group + ' ' + people + ' красила весь забор.',
 		answers: days,
 	});
