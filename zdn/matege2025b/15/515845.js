07c8b140ef2231e0ce61972f8bcb29b219b2b660
Алендарь Сергей <sergey.alendar@mail.ru>
2025-04-13 13:48:35 +0300

    Убран вариант с увеличением статистики

--- zdn/matege2025b/15/515845.js
+++ zdn/matege2025b/15/515845.js
@@ -1,18 +1,14 @@
 (function() {
 	let statisticForSummer = sluchch(0.38, 0.98, 0.01);
 	let answer = (1 - statisticForSummer) * 100;
-	let time = ['летом', 'зимой'];
-	let difference = ['уменьшилось', 'увеличилось'];
-	let slTime1 = sl1();
-	let slTime2 = 1 - slTime1;
 	let quantity = ['число', 'количество'].iz();
 	let events = ['дорожно-транспортных происшествий (ДТП)', 'переломов', 'повреждений кровли', 'заболевших', 'отключений электроэнергии',
 			'повреждений заборов', 'повреждений линий электропередач', 'покупок витамин', 'покупок лекарств',  'покупок лопат'].iz();
 	NAtask.setTask({
 		text: quantity.toZagl() + ' ' + events + ' в летний период составило ' + statisticForSummer.ts() +
-			' от числа в  зимний период. На сколько процентов ' + difference[slTime1] + ' ' +
-			quantity + ' ' + events + ' ' + time[slTime1] + [' по сравнению ', ' в сравнении '].iz() +
-			'с ' + time[slTime2] + '?',
+			' от числа в  зимний период. На сколько процентов уменьшилось ' +
+			quantity + ' ' + events + ' летом' + [' по сравнению ', ' в сравнении '].iz() +
+			'с зимой?',
 		answers: answer,
 	});
 })();
