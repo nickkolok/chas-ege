c019dd66f11d74c1f0f3a9c6cf95eb23dc88cd26
Алендарь Сергей <sergey.alendar@mail.ru>
2025-05-25 21:34:09 +0300

    Файл 77476.js изменён. Убрана лишняя строка, изменена генерация интервалов

--- zdn/matege2024p/12/77476.js
+++ zdn/matege2024p/12/77476.js
@@ -2,13 +2,11 @@
 	retryWhileError(function() {
 		'use strict';
 
-		let taskType = ['наименьшее', 'наибольшее'].iz();
-
 		let a = sl(0, 20); 
 		let b = sl(0, 20); 
 
-		let intervalStart = sl(-10, 10);
-		let intervalEnd = intervalStart + sl(1, 25);
+		let intervalStart = b - sl(1, 15, 0.5);
+		let intervalEnd = b  + sl(1, 15, 0.5);;
 
 		NAtask.setMinimaxFunctionTask({
 			expr: '(' + [a, 'x'].shuffle().join('-') + ')*e^(' + [b, 'x'].shuffle().join('-') + ')',
@@ -25,4 +23,3 @@
 //77475
 //77476
 //77477
-
