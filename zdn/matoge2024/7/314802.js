2c58b5616de060ca805bba192df85ccd4d1c2b15
NickKolok <nickkolok@mail.ru>
2025-05-26 02:48:29 +0300

    [zdn] Use coordAxis_drawAuto

--- zdn/matoge2024/7/314802.js
+++ zdn/matoge2024/7/314802.js
@@ -13,14 +13,17 @@
 		let label2 = ['y', 'b'][randAlfabel];
 
 		let paint1 = function (ct) {
-			coordAxis_prepare(ct, { width: 400, height: 100 });
-			const w = ct.__coordAxisW;
-			const mid = w / 2;
-			const scale = 15;
-
-			coordAxis_drawMarkPoint(ct, mid, "0", "line", "underAxis");
-			coordAxis_drawMarkPoint(ct, mid + x * scale, label1, "dot", "overAxis");
-			coordAxis_drawMarkPoint(ct, mid + y * scale, label2, "dot", "overAxis");
+			coordAxis_drawAuto(ct, { points: [
+				// Выставляем правый край насильно, чтобы при авторасчёте масштаба не терялась вариативность
+				{ value:  10, mark: "nothing" },
+				// Левый тоже
+				{ value: -10, mark: "nothing" },
+				// Нолик с чёрточкой!
+				{ value:   0, mark: "line",  label:    "0", labelPos: "underAxis" },
+				// Сами точки
+				{ value:   x, mark: "dot",   label: label1, labelPos: "overAxis"  },
+				{ value:   y, mark: "dot",   label: label2, labelPos: "overAxis"  },
+			] });
 		};
 
 		// строки + логическая проверка
