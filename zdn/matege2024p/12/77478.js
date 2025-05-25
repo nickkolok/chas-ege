e886995bf873b4077678b331da5ef738cb125b58
Алендарь Сергей <sergey.alendar@mail.ru>
2025-05-25 22:18:45 +0300

    Убрана лишняя строка

--- zdn/matege2024p/12/77478.js
+++ zdn/matege2024p/12/77478.js
@@ -2,16 +2,14 @@
 	retryWhileError(function() {
 		'use strict';
 
-		let taskType = ['наименьшее', 'наибольшее'].iz();
-
 		let a = sl(1, 5);
 		let b = sl(-50, 50);
 		let c = sl(-50, 50);
 		let d = sl(1, 20);
 		let sign = ['-', '+'].iz();
 
-		let intervalStart = sl(-20, 20);
-		let intervalEnd = intervalStart + sl(1, 5);
+		let intervalStart = d - sl(1, 15, 0.5);
+		let intervalEnd = d + sl(1, 15, 0.5);
 
 		NAtask.setMinimaxFunctionTask({
 			expr: '(' + a + 'x^2 + ' + b + 'x + ' + c + ')*e^(' + [['x', d].shuffle().join('-'), 'x'].iz() + ')',
