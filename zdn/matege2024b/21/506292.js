28cd84bce0064df8b54c2c3e4badbaf840d49b7e
Алендарь Сергей <sergey.alendar@mail.ru>
2025-05-27 12:25:38 +0300

    slWorker,SlOwner замененены на iz(), расширен массив Owner,массив Well теперь в именительном падеже, убраны пробелы из массивов

--- zdn/matege2024b/21/506292.js
+++ zdn/matege2024b/21/506292.js
@@ -2,9 +2,7 @@
 	let basePrice = sluchch(1000, 5000, 100);
 	let difference = sluchch(200, 900, 100);
 	let measurement = sluchch(4, 15);
-	let slOwner = sluchch(0, 4);
 	let slWell = sluchch(0, 5);
-	let slWorker = sluchch(0, 3);
 	let measurements = ' глубиной ';
 
 	let progression = new ArithmeticProgression(basePrice, difference);
@@ -13,23 +11,23 @@
 		measurements = [' шириной ', ' длиной '].iz();
 	}
 
-	let Worker = ['рабочий', 'землекоп', 'работник', 'сотрудник'];
-	let Owner = ['хозяин', 'фермер', 'землевладелец', 'собственник', 'владелец'];
-	let Well = ['колодец', 'скважину', 'яму', 'шахту', 'траншею', 'канаву'];
-	let sklonWorker = sklonlxkand(Worker[slWorker]);
+	let Worker = ['рабочий', 'землекоп', 'работник', 'сотрудник'].iz();
+	let Owner = ['хозяин', 'фермер', 'землевладелец', 'собственник', 'владелец', 'лесник', 'дачник', 'огородник'].iz();
+	let Well = ['колодец', 'скважина', 'яма', 'шахта', 'траншея', 'канава'];
+	let sklonWorker = sklonlxkand(Worker);
+	let sklonWell = sklonlxkand(Well[slWell]);
 
 	NAtask.setTask({
-		text: Owner[slOwner].toZagl() + [' договорился', ' условился'].iz() + ' с ' + sklonWorker.tm + ', что они ' + 
-			['выкопают', 'откопают', 'сделают'].iz() + ' ему ' + Well[slWell] +
+		text: Owner.toZagl() + ' ' + ['договорился', 'условился'].iz() + ' с ' + sklonWorker.tm + ', что они ' + 
+			['выкопают', 'откопают', 'сделают'].iz() + ' ему ' + sklonWell.ve +
 			' на следующих условиях: за первый метр он заплатит им ' + chislitlx(basePrice, 'рубль') +
 			', а за каждый ' + ['следующий', 'последующий'].iz() + ' метр  — на ' + chislitlx(difference, 'рубль') +
-			'  больше, чем за предыдущий. Сколько рублей ' + Owner[slOwner] + ' должен будет ' + 
-			['заплатить ', 'выплатить ', 'отдать '].iz() + sklonWorker.dm +
-			', если они ' + ['выкопают ', 'откопают ', 'сделают '].iz() + Well[slWell] + measurements +
+			'  больше, чем за предыдущий. Сколько рублей ' + Owner + ' должен будет ' + 
+			['заплатить', 'выплатить', 'отдать'].iz() + ' ' + sklonWorker.dm +
+			', если они ' + ['выкопают', 'откопают', 'сделают'].iz() + ' ' + sklonWell.ve + measurements +
 			chislitlx(measurement, 'метр') + ' ?',
 		answers: progression.sum(measurement),
 	});
 })();
 
 //506292
-
