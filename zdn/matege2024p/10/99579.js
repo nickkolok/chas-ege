2e33c7107235b98d31ab26ef50e9b9c281d754b5
Алендарь Сергей <sergey.alendar@mail.ru>
2025-05-27 11:23:30 +0300

    добавлена функция selectVerbGender

--- zdn/matege2024p/10/99579.js
+++ zdn/matege2024p/10/99579.js
@@ -5,14 +5,13 @@
 	let progression = new ArithmeticProgression(firstDay, difference);
 	let firstAndLastSum = firstDay + progression.member(days);
 	let people = ['маляров', 'рабочих', 'мастеров', 'работников'][sl(0, 3)];
-	let slChislo = sl1();
-	let rod = ['покрасила', 'покрасил'];
-	let group = ['бригада', 'группа', 'команда', 'отряд', 'коллектив', 'персонал'][slChislo * 3 + sl(0, 2)];
+	let verb = 'покрасил';
+	let group = ['бригада', 'группа', 'команда', 'отряд', 'коллектив'].iz();
 
 	NAtask.setTask({
 		text: group.toZagl() + ' ' + people + ' красит забор длиной ' + chislitlx(progression.sum(days), 'метр') +
 			', ежедневно увеличивая норму покраски на одно и то же число метров.' +
-			' Известно, что за первый и последний день в сумме ' + group + ' ' + rod[slChislo] + ' ' + chislitlx(firstAndLastSum, 'метр') + ' забора. ' +
+			' Известно, что за первый и последний день в сумме ' + group + ' ' +  selectVerbGender(verb, group) + ' ' + chislitlx(firstAndLastSum, 'метр') + ' забора. ' +
 			' Определите, сколько дней ' + group + ' ' + people + ' красила весь забор.',
 		answers: days,
 	});
