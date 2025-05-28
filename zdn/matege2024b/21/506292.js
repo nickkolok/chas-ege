f6bc6c1bd8672729e0bce12417a319be87ea50ed
Алендарь Сергей <sergey.alendar@mail.ru>
2025-05-28 15:21:07 +0300

    откопают заменено на прокопают, изменен массив Well

--- zdn/matege2024b/21/506292.js
+++ zdn/matege2024b/21/506292.js
@@ -2,32 +2,33 @@
 	let basePrice = sluchch(1000, 5000, 100);
 	let difference = sluchch(200, 900, 100);
 	let measurement = sluchch(4, 15);
-	let slWell = sluchch(0, 5);
-	let measurements = ' глубиной ';
+	let slWell = sl1();
+	let measurements = 'глубиной';
 
 	let progression = new ArithmeticProgression(basePrice, difference);
 
-	if (slWell > 0) {
-		measurements = [' шириной ', ' длиной '].iz();
+	if (slWell) {
+		measurements = ['шириной', 'длиной'].iz();
 	}
 
 	let Worker = ['рабочий', 'землекоп', 'работник', 'сотрудник'].iz();
 	let Owner = ['хозяин', 'фермер', 'землевладелец', 'собственник', 'владелец', 'лесник', 'дачник', 'огородник'].iz();
-	let Well = ['колодец', 'скважина', 'яма', 'шахта', 'траншея', 'канава'];
+	let Well = [['колодец', 'скважина', 'шахта'],['яма', 'траншея', 'канава']][slWell].iz();
 	let sklonWorker = sklonlxkand(Worker);
-	let sklonWell = sklonlxkand(Well[slWell]);
+	let sklonWell = sklonlxkand(Well);
+	let toDo = ['выкопают', 'прокопают', 'сделают'];
 
 	NAtask.setTask({
-		text: Owner.toZagl() + ' ' + ['договорился', 'условился'].iz() + ' с ' + sklonWorker.tm + ', что они ' + 
-			['выкопают', 'откопают', 'сделают'].iz() + ' ему ' + sklonWell.ve +
+		text: Owner.toZagl() + ' ' + ['договорился', 'условился'].iz() + ' с ' + sklonWorker.tm + ', что они ' +
+			toDo.iz() + ' ему ' + sklonWell.ve +
 			' на следующих условиях: за первый метр он заплатит им ' + chislitlx(basePrice, 'рубль') +
 			', а за каждый ' + ['следующий', 'последующий'].iz() + ' метр  — на ' + chislitlx(difference, 'рубль') +
-			'  больше, чем за предыдущий. Сколько рублей ' + Owner + ' должен будет ' + 
-			['заплатить', 'выплатить', 'отдать'].iz() + ' ' + sklonWorker.dm +
-			', если они ' + ['выкопают', 'откопают', 'сделают'].iz() + ' ' + sklonWell.ve + measurements +
+			'  больше, чем за предыдущий. Сколько рублей ' + Owner + ' должен будет ' + ['заплатить', 'выплатить', 'отдать'].iz() +
+			' ' + sklonWorker.dm +
+			', если они ' + toDo.iz() + ' ' + sklonWell.ve + ' ' + measurements + ' ' +
 			chislitlx(measurement, 'метр') + ' ?',
 		answers: progression.sum(measurement),
 	});
 })();
 
-//506292
+//506292 
