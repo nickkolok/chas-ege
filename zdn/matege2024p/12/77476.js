d890d13ab67202a2b4c86237ec14cf78f3ad7c66
Алендарь Сергей <sergey.alendar@mail.ru>
2025-05-28 16:13:21 +0300

    Множители теперь меняются местами и перед выражением теперь может возникать минус

--- zdn/matege2024p/12/77476.js
+++ zdn/matege2024p/12/77476.js
@@ -2,14 +2,15 @@
 	retryWhileError(function() {
 		'use strict';
 
-		let a = sl(0, 20); 
-		let b = sl(0, 20); 
+		let a = sl(0, 20);
+		let b = sl(0, 20);
+		let sign = ['-', '+'].iz();
 
 		let intervalStart = b - sl(1, 15, 0.5);
-		let intervalEnd = b  + sl(1, 15, 0.5);;
+		let intervalEnd = b + sl(1, 15, 0.5);;
 
 		NAtask.setMinimaxFunctionTask({
-			expr: '(' + [a, 'x'].shuffle().join('-') + ')*e^(' + [b, 'x'].shuffle().join('-') + ')',
+			expr: sign + ['(' + [a, 'x'].shuffle().join('-') + ')', 'e^(' + [b, 'x'].shuffle().join('-') + ')'].shuffle().join('*'),
 			leftEnd: intervalStart + '',
 			rightEnd: intervalEnd + '',
 			primaryStep: 0.01,
