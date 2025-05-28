0a3e854237485202d6febe8bff85a754c60e645d
Алендарь Сергей <sergey.alendar@mail.ru>
2025-05-28 15:27:37 +0300

    Род досогласован

--- zdn/matege2024p/10/99579.js
+++ zdn/matege2024p/10/99579.js
@@ -4,15 +4,15 @@
 	let days = sl(5, 15);
 	let progression = new ArithmeticProgression(firstDay, difference);
 	let firstAndLastSum = firstDay + progression.member(days);
-	let people = ['маляров', 'рабочих', 'мастеров', 'работников'][sl(0, 3)];
-	let verb = 'покрасил';
+	let people = sklonlxkand(['маляр', 'рабочий', 'мастер', 'работник'].iz()).rm;
+	let verb = 'красил';
 	let group = ['бригада', 'группа', 'команда', 'отряд', 'коллектив'].iz();
 
 	NAtask.setTask({
 		text: group.toZagl() + ' ' + people + ' красит забор длиной ' + chislitlx(progression.sum(days), 'метр') +
 			', ежедневно увеличивая норму покраски на одно и то же число метров.' +
 			' Известно, что за первый и последний день в сумме ' + group + ' ' +  selectVerbGender(verb, group) + ' ' + chislitlx(firstAndLastSum, 'метр') + ' забора. ' +
-			' Определите, сколько дней ' + group + ' ' + people + ' красила весь забор.',
+			' Определите, сколько дней ' + group + ' ' + people + ' ' + selectVerbGender(verb, group) + ' весь забор.',
 		answers: days,
 	});
 })();
