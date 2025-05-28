4296193c46f8acdfcd1a46320ebfebb89b28883b
Алендарь Сергей <sergey.alendar@mail.ru>
2025-05-28 15:51:19 +0300

    добавлены пробелы, Math.abs заменён на .abs

--- zdn/matege2025b/15/317970.js
+++ zdn/matege2025b/15/317970.js
@@ -3,7 +3,7 @@
 	let deIncreased = ['уценки', 'подорожания'];
 	let slTime1 = sl1();
 	let newPrice = sluchch(0.39, 0.99, 0.01) + slTime1;
-	let answer = Math.abs(1 - newPrice) * 100;
+	let answer = (1 - newPrice).abs() * 100;
 	let products = ['телефона', 'телевизора', 'миксера', 'фена', 'утюга',
 		'пылесоса', 'холодильника', 'кондиционера', 'ноутбука',
 		'компьютера', 'обогревателя', 'планшета', 'мотоцикла', 'велосипеда',
@@ -12,7 +12,7 @@
 	NAtask.setTask({
 		text: 'После ' + deIncreased[slTime1] + ' ' + products + ' его новая цена составила ' + newPrice.ts() +
 			' от старой цены. На сколько процентов ' + difference[slTime1] +
-			' цена' + products + 'в результате ' + deIncreased[slTime1] + '?',
+			' цена ' + products + ' в результате ' + deIncreased[slTime1] + '?',
 		answers: answer,
 	});
 })();
