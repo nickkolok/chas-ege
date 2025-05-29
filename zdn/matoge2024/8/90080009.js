725c4ce97e8bb2dd7d223774bd153314f716bf17
Алендарь Сергей <sergey.alendar@mail.ru>
2025-05-29 03:36:54 +0300

    файл перемещён в нужную папку

---
+++ zdn/matoge2024/8/90080009.js
@@ -0,0 +1,13 @@
+﻿(function() {
+	retryWhileError(function() {
+		'use strict';
+		let a = sl(3, 7);
+		let b = sl(3, 5, 2);
+		NAtask.setEvaluationTask({
+			expr: ' sqrt(' + a*a + '^' + b + ')',
+			//forbiddenAnswers: [0],
+			authors: ['Алендарь Сергей'],
+		});
+	}, 10000);
+})();
+//90080009
