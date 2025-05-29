ea6678a3be83800678c32896c20aa2278218450a
Алендарь Сергей <sergey.alendar@mail.ru>
2025-05-29 04:08:00 +0300

    Добавлена смена множителей местаМи.изменён подбор корней

--- zdn/matoge2024/8/90080008.js
+++ zdn/matoge2024/8/90080008.js
@@ -1,11 +1,11 @@
 (function() {
 	retryWhileError(function() {
 		'use strict';
-		let a = sl(3, 9, 2);
-		let b = sl(2, 8, 2);
-		let c = sl(3, 9, 2);
+		let a = sl(3, 9);
+		let b = sl(2, 8);
+		let c = sl(3, 9);
 		NAtask.setEvaluationTask({
-			expr: 'sqrt(' + a*c + ')'+ '*' + 'sqrt(' + a*b + '*' + c + ')' ,
+			expr: ['sqrt(' + a * c + ')', 'sqrt(' + [a * b, c].shuffle().join('*') + ')'].shuffle().join('*'),
 			//forbiddenAnswers: [0],
 			authors: ['Алендарь Сергей'],
 		});
