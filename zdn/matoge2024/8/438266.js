59fb388cc926fd1270ddfd3864327fc3a05cc889
Kalendar2 <146985807+Kalendar2@users.noreply.github.com>
2025-05-29 04:48:12 +0300

    [zdn][new] 438266 (#2476)

---
+++ zdn/matoge2024/8/438266.js
@@ -0,0 +1,13 @@
+﻿(function() {
+	retryWhileError(function() {
+		'use strict';
+		let a = sl(3, 13);
+		let b = sl(4, 6, 2);
+		NAtask.setEvaluationTask({
+			expr: ' sqrt(' + a + '^' + b + ')',
+			//forbiddenAnswers: [0],
+			authors: ['Алендарь Сергей'],
+		});
+	}, 10000);
+})();
+//438266
