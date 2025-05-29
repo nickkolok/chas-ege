4de429c13a5acd8ebca0fc39d675fb725e7626ba
Алендарь Сергей <sergey.alendar@mail.ru>
2025-05-29 05:05:16 +0300

    Тепеерь множители меняются местами

--- zdn/matoge2024/8/369495.js
+++ zdn/matoge2024/8/369495.js
@@ -4,7 +4,8 @@
 		let b = sl(3, 9);
 		let a = sl(5, 81);
 		NAtask.setEvaluationTask({
-			expr: '(' + 'sqrt(' + a + ')' + '+' +b+' ) * (' + 'sqrt(' + a + ')' + '-' +b+' ) ',
+			expr: ['(' + 'sqrt(' + a + ')' + '+' + b + ' )', '(' + 'sqrt(' + a + ')' + '-' +
+ 			b + ')'].shuffle().join('*'),
 			//forbiddenAnswers: [0],
 			authors: ['Алендарь Сергей'],
 		});
