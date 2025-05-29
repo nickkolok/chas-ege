8e2f28d27b9af05299f30b4052946559614c8c1d
Алендарь Сергей <sergey.alendar@mail.ru>
2025-05-29 04:20:38 +0300

    Добавлено умножение точкой

--- zdn/matoge2024/8/406280.js
+++ zdn/matoge2024/8/406280.js
@@ -1,11 +1,12 @@
-﻿(function() {
+﻿
+(function() {
 	retryWhileError(function() {
 		'use strict';
 		let a = sl(2, 19);
 		let b = sl(2, 9);
 		let c = sl(2, 9);
 		NAtask.setEvaluationTask({
-			expr: 'sqrt(' + a*b + ')' + 'sqrt(' + a + ')'+'/'+'sqrt(' + b*c + ')',
+			expr: 'sqrt(' + a * b + ')' + '*' + 'sqrt(' + a + ')' + '/' + 'sqrt(' + b * c + ')',
 			//forbiddenAnswers: [0],
 			authors: ['Алендарь Сергей'],
 		});
