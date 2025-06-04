bb37a25b5280deb4982d9b985cb98b6409bda1b5
Алендарь Сергей <sergey.alendar@mail.ru>
2025-06-04 21:24:25 +0300

    Исправлен код после слияния

--- zdn/matege2024p/12/77484.js
+++ zdn/matege2024p/12/77484.js
@@ -3,15 +3,9 @@
 		'use strict';
 		let a = sl(1, 99).pm();
 		let b = a + sl(1, 5);
-<<<<<<< HEAD
 		let sign = ['+', '-'].iz();
 		NAtask.setMinimaxFunctionTask({
 			expr: sign + '(x + ' + b + ')^2*e^(' + a + '-x)',
-=======
-		let sign = ['+', '-'];
-		NAtask.setMinimaxFunctionTask({
-			expr: '(x + ' + b + ')^2*e^(-' + a + '-x)',
->>>>>>> e7aecec27b9cce6a2625519c4f4a21c3153ebbb6
 			leftEnd: (-a - sl(0.5, 2, 0.5)) + '',
 			rightEnd: (-a + sl(0.5, 2, 0.5)) + '',
 			primaryStep: 0.01,
@@ -23,3 +17,4 @@
 
 //77484 
 //77485
+
