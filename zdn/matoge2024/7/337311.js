36928cd801a0bbb3af0e058aead3d6af2f1d8278
Колесникова Елена <luly3751@gmail.com>
2025-06-08 19:03:27 +0300

    swap question  ; add countDrob and countSqrt

--- zdn/matoge2024/7/337311.js
+++ zdn/matoge2024/7/337311.js
@@ -4,15 +4,16 @@
 		NAinfo.requireApiVersion(0, 2);
 		let rand = sl1();
 
-		let count = sl(5, 25, 1);
+		let countDrob = sl(3, 12, 1);
 		let denominator = sl(2, 25, 1);
 		let numerator = sl(1, denominator - 1, 1);
 
-		let numDrob = count * denominator + numerator;
+		let numDrob = countDrob * denominator + numerator;
 		let valueDrob = numDrob / denominator;
 		let exprStrDrob = numDrob.texfrac(denominator);
 
-		let numSqrt = count * count + denominator;
+		let countSqrt = sl(5, 25, 1);
+		let numSqrt = countSqrt * countSqrt + denominator;
 		let valueSqrt = numSqrt.sqrt();
 		genAssert(valueSqrt != valueSqrt.round(), "корень не должен быть простым для расчёта ");
 
@@ -37,7 +38,7 @@
 		}
 
 		NAtask.setTask({
-			text: 'Между какими целыми числами заключено число $' + ['\\sqrt{' + numSqrt + '}', exprStrDrob][rand] + '$?',
+			text: 'Между какими целыми числами заключено число $' + [exprStrDrob, '\\sqrt{' + numSqrt + '}'][rand] + '$?',
 			answers: correct,
 			wrongAnswers: Array.from(wrongAnswers)
 		});
