fe47623660ec4bcbcbf769014b3f9d454f9cc48b
Колесникова Елена <luly3751@gmail.com>
2025-06-08 20:58:40 +0300

    [zdn][new] oge-2024 -fix #2495 - fix # 2497 -fix #2502

---
+++ zdn/matoge2024/7/369728.js
@@ -0,0 +1,77 @@
+(function () {
+    'use strict';
+    retryWhileError(function () {
+        NAinfo.requireApiVersion(0, 2);
+        let rand = sl1();
+        let randMinus = sl1();
+        let section = ['отрезку', 'промежутку'].iz();
+
+        let countDrob = sl(3, 12, 1);
+        let denominator = sl(2, 25, 1);
+        let numerator = sl(1, denominator - 1, 1);
+        let numDrob = countDrob * denominator + numerator;
+        let valueDrob = (numDrob / denominator) * [-1, 1][randMinus];
+        let correctDrob = ['-', ''][randMinus] + numDrob.texfrac(denominator);
+        let isNegative = randMinus === 0;
+
+        let countSqrt = sl(5, 25, 1);
+        let numSqrt = countSqrt * countSqrt + denominator;
+        let valueSqrt = numSqrt.sqrt();
+        genAssert(valueSqrt != valueSqrt.round(), "корень не должен быть простым для расчёта ");
+        let correctSqrt = '\\sqrt{' + numSqrt + '}';
+
+        let value = [valueDrob, valueSqrt][rand];
+        let correctExpr = [correctDrob, correctSqrt][rand];
+        let floor = Math.floor(value);
+        let intervalText = `[${floor}; ${floor + 1}]`;
+
+        let wrongAnswers = [];
+
+        if (rand === 0) { //дробь
+            let usedNumerators = [numDrob];
+            while (wrongAnswers.length < 3) {
+                let delta = slKrome([], -4, 4, 1);
+                if (delta === 0) continue;
+                let newCount = countDrob + delta;
+                if (newCount < 1) continue;
+
+                let fakeNumerator = newCount * denominator + numerator;
+                if (usedNumerators.includes(fakeNumerator)) continue;
+                usedNumerators.push(fakeNumerator);
+
+                let val = fakeNumerator / denominator;
+                if (isNegative) val *= -1;
+                if (val >= floor && val <= floor + 1) continue; 
+
+                let fakeExpr = (isNegative ? '-' : '') + fakeNumerator.texfrac(denominator);
+                wrongAnswers.push(`$${fakeExpr}$`);
+            }
+        } else { // корни
+            let usedSqrts = [numSqrt];
+            while (wrongAnswers.length < 3) {
+                let offset = slKrome([], -10, 10, 1);
+                if (offset === 0) continue;
+
+                let fakeRoot = (countSqrt + offset) * (countSqrt + offset) + sl(1, 10);
+                if (fakeRoot <= 0 || usedSqrts.includes(fakeRoot)) continue;
+
+                let val = fakeRoot.sqrt();
+                if (val >= floor && val <= floor + 1) continue;
+
+                usedSqrts.push(fakeRoot);
+                wrongAnswers.push(`$\\sqrt{${fakeRoot}}$`);
+            }
+        }
+
+        NAtask.setTask({
+            text: 'Какое из данных чисел принадлежит ' + section + ' ${' + intervalText + '}$?',
+            answers: '$' + correctExpr + '$',
+            wrongAnswers: wrongAnswers
+        });
+
+        AtoB(3);
+    }, 1000);
+})();
+//zer00player
+//https://oge.sdamgia.ru/test?likes=369728
+//https://oge.sdamgia.ru/test?likes=337389
