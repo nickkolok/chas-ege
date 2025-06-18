84003cae5c9b65327585f8510fe0743542895a11
Алендарь Сергей <sergey.alendar@mail.ru>
2025-06-18 20:51:35 +0300

    Файл 412185.js добавлен в соответсвующую папку

---
+++ zdn/matoge2024/8/412185.js
@@ -0,0 +1,12 @@
+(function() {
+    retryWhileError(function() {
+        'use strict';
+        
+        NAtask.setEvaluationTask({
+            expr: '(a^' + sl(2, 15).pm() + ' * a^' + sl(2, 15).pm() + ')/a^' + sl(2, 25).pm(),
+            variables: {a: sl(sl(2, 7), 9)},
+            authors: ['Алендарь Сергей'],
+        });
+    }, 1000);
+})();
+//412185 
