1d6bcec33137446736c9c408ec3829f2a18a395e
Алендарь Сергей <sergey.alendar@mail.ru>
2025-06-19 22:48:46 +0300

    Файл 90080023.js добавлен в соответсвующую папку

---
+++ zdn/matoge2024/8/90080023.js
@@ -0,0 +1,13 @@
+(function() {
+	retryWhileError(function() {
+		'use strict';
+		let a = sl(2, 9).pm();
+
+		NAtask.setEvaluationTask({
+			expr: '(' + a + '^' + sl(1, 9).pm() + '*' + a + '^' + sl(1, 9).pm() + ')^' + sl(1, 9).pm() + '/(' + a + '^' +
+				sl(1, 9).pm() + '*' + a + '^' + sl(1, 9).pm() + ')^' + sl(1, 9).pm(),
+			authors: ['Алендарь Сергей'],
+		});
+	}, 1000);
+})();
+//90080023
