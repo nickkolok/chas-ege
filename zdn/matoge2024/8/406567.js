ffdf7987033d2727c7ccdba0d7f0c4ff97656fb8
Алендарь Сергей <sergey.alendar@mail.ru>
2025-06-21 20:01:50 +0300

    изменена генерация минуса

--- zdn/matoge2024/8/406567.js
+++ zdn/matoge2024/8/406567.js
@@ -1,10 +1,11 @@
 (function() {
 	retryWhileError(function() {
 		'use strict';
-		let a = sl(2, 15).pm();
+		let a = sl(2, 15);
 
 		NAtask.setEvaluationTask({
-			expr: '(1/'+ a + '^'+sl(1,30).pm()+')*(' + '1/'+ a + '^'+sl(1,30).pm()+')',
+			expr: '(1/' + ['-', ''].iz() + a + '^' + sl(1, 30).pm() + ')*(' + '1/' +
+				['-', ''].iz() + a + '^' + sl(1, 30).pm() + ')',
 			authors: ['Алендарь Сергей'],
 		});
 	}, 1000);
