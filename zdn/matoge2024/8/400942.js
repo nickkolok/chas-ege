a5f1ae157181f7a21cf99ac0132cbb63338085e8
Алендарь Сергей <sergey.alendar@mail.ru>
2025-06-21 20:34:24 +0300

    Изменена генерация знаков

--- zdn/matoge2024/8/400942.js
+++ zdn/matoge2024/8/400942.js
@@ -3,11 +3,10 @@
 		'use strict';
 		let b = sl(1, 9);
 		let a = sl(2, 100);
-		let sign = ['+', '-'];
-		let sluchSign = sl1();
+		let sign = ['+', '-'].shuffle();
 		NAtask.setEvaluationTask({
-			expr: ['(' + 'sqrt(' + a + ')' + sign[sluchSign] + b + ' )^2',
-				2 * b + '*sqrt(' + a + ')'].shuffle().join(sign[(sluchSign - 1).abs()]),
+			expr: ['(' + 'sqrt(' + a + ')' + sign[0] + b + ' )^2',
+				2 * b + '*sqrt(' + a + ')'].shuffle().join(sign[1]),
 			authors: ['Алендарь Сергей'],
 		});
 	}, 10000);
