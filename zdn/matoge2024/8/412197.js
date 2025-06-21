9b707e628e595bff2bf16847c6e8400d794632e3
Алендарь Сергей <sergey.alendar@mail.ru>
2025-06-21 19:57:48 +0300

    Убраналишняя конструкция

--- zdn/matoge2024/8/412197.js
+++ zdn/matoge2024/8/412197.js
@@ -6,7 +6,7 @@
 		NAtask.setEvaluationTask({
 			expr: 'sqrt(' + x * x + '*a^2' + ['+', '-'].iz() +
 				2 * x * y + '*a*b' + '+' + y * y + '*b^2)',
-			variables: {a: sl(sl(2, 7), 9),b: sl(sl(2, 7), 9).pm()},
+			variables: {a: sl(2, 9),b: sl(2, 9).pm()},
 			authors: ['Алендарь Сергей'],
 		});
 	}, 1000);
