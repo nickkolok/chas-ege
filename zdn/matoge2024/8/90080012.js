0ec8c0b3e8a617de933398af1cd2359f7fb8faa3
Алендарь Сергей <sergey.alendar@mail.ru>
2025-06-21 20:38:55 +0300

    Убрана лишняя конструкци т.к. ни на что не влияла

--- zdn/matoge2024/8/90080012.js
+++ zdn/matoge2024/8/90080012.js
@@ -5,7 +5,7 @@
 		let power = sl(2, 10);
 		NAtask.setEvaluationTask({
 			expr: 'sqrt((' + [ch * ch, 'a^' + (power + sl(2, 6, 2))].shuffle().join('*') + ')/a^' + power + ')',
-			variables: {a: sl(sl(2, 7), 9)},
+			variables: {a: sl(2, 9)},
 			authors: ['Алендарь Сергей'],
 		});
 	}, 1000);
