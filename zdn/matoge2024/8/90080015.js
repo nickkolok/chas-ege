61f69cc3633bf260dcd1bd3e9aa8e6b28897219d
Алендарь Сергей <sergey.alendar@mail.ru>
2025-06-22 15:42:09 +0300

    Убрана единица из степени, числитель и знаменаттель меняются местами

--- zdn/matoge2024/8/90080015.js
+++ zdn/matoge2024/8/90080015.js
@@ -2,13 +2,13 @@
 	retryWhileError(function() {
 		'use strict';
 		let e1 = sl(2, 5);
-		let e2 = sl(1, 9);
-		let e3 = sl(1, 15);
+		let e2 = sl(2, 9);
+		let e3 = sl(2, 15);
 		let e4 = (e1 * e2) + e3 + sl(1, 6).pm();
 
 		NAtask.setEvaluationTask({
-			expr: '(' + ['(a^' + e1 + ')^' + e2, 'a^' + e3].shuffle().join('*') + ')/a^' + e4,
-			variables: {a: sl(sl(2, 7), 9)},
+			expr: ['(' + ['(a^' + e1 + ')^' + e2, 'a^' + e3].shuffle().join('*') + ')', 'a^' + e4].shuffle().join('/'),
+			variables: {a: sl(2, 9)},
 			authors: ['Алендарь Сергей'],
 		});
 	}, 1000);
