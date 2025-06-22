7d23b65b5ebe4e8a2f0c4e17f6da3988996c2896
Алендарь Сергей <sergey.alendar@mail.ru>
2025-06-22 17:31:29 +0300

    Изменена генерация знаков и значений под корнем

--- zdn/matoge2024/8/90080022.js
+++ zdn/matoge2024/8/90080022.js
@@ -2,10 +2,12 @@
 	retryWhileError(function() {
 		'use strict';
 		let a = sl(1, 25);
-		let b = sl(1, 25);
+		let b = slKrome(isPolnKvadr, 2, 25);
+		let sign = ['+','-'];
 
 		NAtask.setEvaluationTask({
-			expr: 'sqrt((' + [a + 'sqrt(' + b + ')', 'sl(1,99)'].shuffle().join('-') + ')^' + 2 + ')+' + a + 'sqrt(' + b + ')',
+			expr: ['sqrt((' + [a + 'sqrt(' + b + ')', sl(1,99)].shuffle().join(sign.iz()) + ')^' + 2 + ')',
+				a + 'sqrt(' + b + ')'].shuffle().join(sign.iz()),
 			authors: ['Алендарь Сергей'],
 		});
 	}, 1000);
