cbf6bb652590aabd159c4f14164ba9f7b45c4392
Алендарь Сергей <sergey.alendar@mail.ru>
2025-06-22 13:13:31 +0300

    Числитель и знаменатель меняются местами

--- zdn/matoge2024/8/412185.js
+++ zdn/matoge2024/8/412185.js
@@ -3,10 +3,11 @@
         'use strict';
         
         NAtask.setEvaluationTask({
-            expr: '(a^' + sl(2, 15).pm() + ' * a^' + sl(2, 15).pm() + ')/a^' + sl(2, 25).pm(),
+            expr: ['(a^' + sl(2, 15).pm() + ' * a^' + sl(2, 15).pm() + ')','a^' + sl(2, 25).pm()].shuffle().join('/'),
             variables: {a: sl(sl(2, 7), 9)},
             authors: ['Алендарь Сергей'],
         });
     }, 1000);
 })();
 //412185 
+
