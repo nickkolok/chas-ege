61a09c3366ef431c4c1b1b7f0e8fc232e7c4d6c3
Алендарь Сергей <sergey.alendar@mail.ru>
2025-06-22 15:56:29 +0300

    Убраны отрицательные числа,  как множители

--- zdn/matoge2024/8/355380.js
+++ zdn/matoge2024/8/355380.js
@@ -4,7 +4,7 @@
 		let e1 = sl(2, 9).pm();
 		let e2 = sl(2, 9).pm();
 		let e3 = -(e1 * e2) + sl(1, 6).pm();
-		let a = sl(2, 9).pm();
+		let a = sl(2, 9);
 
 		NAtask.setEvaluationTask({
 			expr: ['('+a+'^' + e1 + ')^' + e2, +a+'^' + e3].shuffle().join('*'),
