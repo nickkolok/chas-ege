d54d1c50f18d5277d8161cd049ad0d02bff9dac9
Колесникова Елена <luly3751@gmail.com>
2025-08-12 15:38:58 +0300

    add preference; lil change rand

--- zdn/matege2024b/4/510314.js
+++ zdn/matege2024b/4/510314.js
@@ -15,16 +15,15 @@
 
 		let answerP1 = I ** 2 * R;
 		let answerP2 = U ** 2 / R;
-		genAssertZ1000(answerP2, 'не более 3-х знаков после запятой');
 
 		let formula = '';
 
-		if (rand === 0 || rand === 1) {
+		if (rand < 2) {
 			formula = '$P = I^2R$, где $I$ – сила тока (в амперах)';
-		} else if (rand === 2) {
-			formula = '$P =\\frac{U^2}{R} $,где $U$ – напряжение (в вольтах)';
+		} else {
+			formula = '$P =\\frac{U^2}{R} $, где $U$ – напряжение (в вольтах)';
+			genAssertZ1000(answerP2, 'не более 3-х знаков после запятой');
 		}
-
 
 		NAtask.setTask({
 
@@ -32,6 +31,7 @@
 				' Пользуясь этой формулой, ' + the_orderToFind + ' ' + ['$P$ (в ваттах)', '$R$ (в омах)', '$P$ (в ваттах)'][rand] +
 				', если ' + ['$R = ' + R + '$ Ом' + ' и $I = ' + I + '$ А', '$P = ' + answerP1 + '$ Вт' + ' и $I = ' + I + '$ А', '$R = ' + R + '$ Ом и $U = ' + U + '$ В'][rand] + '.',
 			answers: [answerP1, R, answerP2][rand],
+			preference: preference,
 
 
 
