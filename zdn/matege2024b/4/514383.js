97926cc35f9a2fff80381df7a86bc33e3344bc8b
nickkolok <nickkolok@mail.ru>
2025-08-14 20:49:41 +0300

    Update 514383.js - minor fixes

--- zdn/matege2024b/4/514383.js
+++ zdn/matege2024b/4/514383.js
@@ -13,25 +13,21 @@
 		let c = slKrome([a, b], 3, 30);
 		let deNum = sl(3, 99);
 		let num = (1, deNum - 1);
-		let S = a * b * c / (4 * (deNum / num));
+		let S = a * b * c / (4 * deNum / num);
 
-		genAssertZ1000(S, 'не более 3-х знаков после запятой');
+		genAssertZ1000(S, 'Не более 3 знаков после запятой');
 
 
 		NAtask.setTask({
-
 			text: 'Площадь треугольника можно вычислить по формуле $S = \\frac{abc}{4R}$, где $a$, $b$ и $c$ – стороны треугольника,' +
 				' а $R$ – радиус окружности, описанной около этого треугольника. ' +
 				'Пользуясь этой формулой, ' + the_orderToFind + ' ' + ['площадь $S$', '$b$'][rand] + ', если $a = ' + a + '$, ' +
 				'$c =' + c + '$, ' + ['$b =' + b + '$', '$S =' + S + '$'][rand] +
 				' и $R= \\frac{' + deNum + '}{' + num + '}$. ',
 			answers: [S, b][rand],
-			preference:preference,
-
-
-
+			preference: preference,
 		});
-		NAtask.modifiers.allDecimalsToStandard();
+		NAtask.modifiers.allDecimalsToStandard(true);
 	}, 2000);
 })();
 //zer00player
