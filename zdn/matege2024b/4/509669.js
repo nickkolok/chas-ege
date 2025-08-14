bb147a10f952e70a7cf597fe7b6590e46433eb93
Колесникова Елена <luly3751@gmail.com>
2025-08-14 16:14:11 +0300

    add .; fix name of errors

--- zdn/matege2024b/4/509669.js
+++ zdn/matege2024b/4/509669.js
@@ -9,19 +9,19 @@
 		let b = slKrome([a], 1, 12);
 		let c = slKrome([a, b], 1, 12);
 
-		genAssert(isValidTriangle(a, b, c) === true, 'должно выполняться правило треугольника');
+		genAssert(isValidTriangle(a, b, c) === true, 'Должно выполняться правило треугольника');
 
 		let answer = (a ** 2 + b ** 2 - c ** 2) / (2 * a * b);
 
-		genAssert(answer != 0, 'ответ не ноль');
-		genAssertZ1000(answer, 'должно быть не более 3 знаков после запятой');
+		genAssert(answer != 0, 'Ответ не ноль');
+		genAssertZ1000(answer, 'Должно быть не более 3 знаков после запятой');
 
 		NAtask.setTask({
 
 			text: 'Теорему косинусов можно записать в виде  $\\cos{\\gamma} = \\frac{a^2 + b^2 - c^2}{2ab}$' +
 				', где $a$, $b$ и $c$ - стороны треугольника, а $\\gamma$ - угол между $a$ и $b$.' +
 				' Пользуясь этой формулой, ' + the_orderToFind + ' $\\cos{\\gamma}$' +
-				', если ' + '$a =' + a + '$' + ', $b =' + b + '$, $c=' + c + '$',
+				', если ' + '$a =' + a + '$' + ', $b =' + b + '$, $c=' + c + '$.',
 			answers: answer,
 
 		});
