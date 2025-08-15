71ad2979d6fe86ca32cf6eee34b4e19195b2a8a0
Колесникова Елена <luly3751@gmail.com>
2025-08-15 15:17:32 +0300

    fix м/с; add preference findF and findA

--- zdn/matege2024b/4/512176.js
+++ zdn/matege2024b/4/512176.js
@@ -2,6 +2,9 @@
 	'use strict';
 	retryWhileError(function () {
 		NAinfo.requireApiVersion(0, 2);
+		let key = '512176';
+		let preference = ['findM', 'findF', 'findA'];
+		let rand = getSelectedPreferenceFromList(key, preference);
 
 		let the_orderToFind = decor.orderToFind.iz();
 
@@ -12,9 +15,12 @@
 		NAtask.setTask({
 
 			text: 'Второй закон Ньютона можно записать в виде $F = ma$, где $F$ – сила (в ньютонах), действующая на тело,' +
-				' $m$ – его масса (в килограммах), $a$ – ускорение(в $м/с^2$), с которым движется тело. ' +
-				the_orderToFind.toZagl() + ' $m$ (в килограммах), если $F = ' + F + '$ $Н$ и $a = ' + a + '$ $м/с^2$.',
-			answers: m,
+				' $m$ – его масса (в килограммах), $a$ – ускорение(в м/с$^2$), с которым движется тело. ' +
+				the_orderToFind.toZagl() + ' ' + ['$m$ (в килограммах)', '$F$ (в ньютонах)', '$a$ (в м/с$^2$)'][rand] +
+				', если ' + ['$F = ' + F + '$ $Н$ и $a = ' + a + '$ м/с$^2$.', '$m = ' + m + '$ кг и $a = ' + a + '$ м/с$^2$.', '$F = ' + F + '$ $Н$ и $m = ' + m + '$ кг.'][rand],
+			answers: [m, F, a][rand],
+			preference: preference,
+
 
 		});
 		NAtask.modifiers.allDecimalsToStandard();
