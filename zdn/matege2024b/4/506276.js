2f3053632b0e383a8a706b1add3a032481576ca6
Колесникова Елена <luly3751@gmail.com>
2025-08-15 16:16:47 +0300

    add preference2; change to dm,sm,mm

--- zdn/matege2024b/4/506276.js
+++ zdn/matege2024b/4/506276.js
@@ -4,17 +4,18 @@
 		NAinfo.requireApiVersion(0, 2);
 
 		let key = '506630';
-		let preference = ['kilometer', 'meter', 'decimeter', 'centimeter'];
-		let rand = getSelectedPreferenceFromList(key, preference);
+		let preference1 = ['kilometer', 'meter', 'decimeter', 'centimeter'];
+		let preference2 = ['decimeterStep', 'centimeterStep', 'millimeterStep'];
+		let rand = getSelectedPreferenceFromList(key, preference1);
+		let randSecond = getSelectedPreferenceFromList(key, preference2);
 
 		let the_pedestrianOnRoad = sklonlxkand(decor.pedestrianOnRoad.iz());
-		let randSecond = sl(0, 3);
-		let metric = ['км', 'м', 'дм', 'см'][randSecond];
+		let metric = ['дм', 'см', 'мм'][randSecond];
 
 		let l = sl(30, 90);
 		let n = sl(1000, 10000, 100);
 
-		let answer = l * n * [[1, 1000, 10000, 100000][rand], [0.001, 1, 10, 100][rand], [0.0001, 0.1, 1, 10][rand], [0.00001, 0.01, 0.1, 1][rand]][randSecond];
+		let answer = l * n * [[0.0001, 0.1, 1, 10][rand], [0.00001, 0.01, 0.1, 1][rand], [0.000001, 0.001, 0.01, 0.1][rand],][randSecond];
 
 		NAtask.setTask({
 
@@ -23,7 +24,7 @@
 				'Какое расстояние прошёл ' + the_pedestrianOnRoad.ie + ', ' +
 				'если $l = ' + l + '$ ' + metric + ', $n = ' + n + '$? Ответ выразите в ' + ['километрах', 'метрах', 'дециметрах', 'сантиметрах'][rand] + '.',
 			answers: answer,
-			preference: preference,
+			preference: [preference1, preference2],
 
 		});
 		NAtask.modifiers.allDecimalsToStandard();
