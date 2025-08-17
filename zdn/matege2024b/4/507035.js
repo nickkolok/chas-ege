c6ff99e8b9fb3925488199fc1135434c34f107f2
Колесникова Елена <luly3751@gmail.com>
2025-08-17 17:52:55 +0300

    add preference

--- zdn/matege2024b/4/507035.js
+++ zdn/matege2024b/4/507035.js
@@ -3,8 +3,11 @@
 	retryWhileError(function () {
 		NAinfo.requireApiVersion(0, 2);
 
+		let key = '507035';
+		let preference = ['onlyNumber', 'numberWithP1P2P3'];
+		let rand = getSelectedPreferenceFromList(key, preference);
+
 		let the_orderToFind = decor.orderToFind.iz();
-		let rand = sl1();
 
 		let p_1 = sl(1, 30);
 		let p_2 = slKrome([p_1], 1, 30);
@@ -19,6 +22,7 @@
 				'то сумма всех делителей числа $p_1 \\cdot p_2 \\cdot p_3$ равна $(p_1 +1)(p_2 +1)(p_3 +1)$. ' +
 				the_orderToFind.toZagl() + ' сумму всех делителей числа $' + number + ['', ' = ' + p_1 + '\\cdot' + p_2 + '\\cdot' + p_3][rand] + '$.',
 			answers: answer,
+			preference: preference,
 
 		});
 		NAtask.modifiers.allDecimalsToStandard();
