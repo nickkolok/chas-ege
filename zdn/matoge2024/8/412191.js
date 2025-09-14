b9ec0dae3010327a6b076f4f48b6d44102c8051f
Алендарь Сергей <sergey.alendar@mail.ru>
2025-09-14 17:37:29 +0300

    изменена генерация значений a

--- zdn/matoge2024/8/412191.js
+++ zdn/matoge2024/8/412191.js
@@ -3,7 +3,7 @@
 		'use strict';
 		NAtask.setEvaluationTask({
 			expr: 'sqrt(' + [ '(-a)^'+sl(2, 10,2) ,'a^'+ sl(2, 10,2) ].shuffle().join('*')+')',
-			variables: {a: sl(sl(2, 7), 9)},
+			variables: {a: sl(2, 9)},
 			authors: ['Алендарь Сергей'],
 		});
 	}, 1000);
