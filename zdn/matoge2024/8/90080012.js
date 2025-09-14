0e6518e917eb8e10bb1ba8610f10a7080aa5078e
Алендарь Сергей <sergey.alendar@mail.ru>
2025-09-14 16:00:37 +0300

    Коэффициент стоит первым, точки убраны

--- zdn/matoge2024/8/90080012.js
+++ zdn/matoge2024/8/90080012.js
@@ -4,8 +4,8 @@
 		let ch = sl(2, 10);
 		let power = sl(2, 10);
 		NAtask.setEvaluationTask({
-			expr: 'sqrt((' + [ch * ch, 'a^' + (power + sl(2, 6, 2))].shuffle().join('*') + ')/a^' + power + ')',
-			variables: {a: sl(sl(2, 7), 9)},
+			expr: 'sqrt((' + ch * ch + 'a^' + (power + sl(2, 6, 2)) + ')/a^' + power + ')',
+			variables: {a: sl(2, 9)},
 			authors: ['Алендарь Сергей'],
 		});
 	}, 1000);
