c00463e5e7e60443ef72f1c02b8e1742b8ef8c6a
Алендарь Сергей <sergey.alendar@mail.ru>
2025-09-14 16:39:06 +0300

    изменена генерация значений x и y, множители не попадают теперь в числитель

--- zdn/matoge2024/8/412189.js
+++ zdn/matoge2024/8/412189.js
@@ -3,8 +3,8 @@
 		'use strict';
 		let ch = sl(2, 10);
 		NAtask.setEvaluationTask({
-			expr: 'sqrt((' + ['y^' + sl(2, 10, 2), 'x^' + sl(2, 10, 2), '1/'+ch * ch].shuffle().join('*') + '))',
-			variables: {x: sl(sl(2, 7), 9),y: sl(sl(2, 7), 9)},
+			expr: 'sqrt((' +  '1/'+ ch * ch + '*' + ['y^' + sl(2, 10, 2), 'x^' + sl(2, 10, 2)].shuffle().join('') + '))',
+			variables: {x: sl(2, 9),y: sl(2, 9)},
 			authors: ['Алендарь Сергей'],
 		});
 	}, 1000);
