5a0935b6c87adb27fe4b5570df3f44f2bf92b7ff
Алендарь Сергей <sergey.alendar@mail.ru>
2025-09-15 21:29:04 +0300

    Убран полный квадрат под корнем

--- zdn/matoge2024/8/401984.js
+++ zdn/matoge2024/8/401984.js
@@ -2,10 +2,13 @@
 	retryWhileError(function() {
 		'use strict';
 		let a = sl(1, 15);
-		let b = sl(2, 15);
+		let b = sl(1, 30);
+		let sqrtb = b.sqrt();
+		genAssert(!sqrtb.isZ(), "Корень извлекается");
 		NAtask.setEvaluationTask({
-			expr: ['(' + [a, 'sqrt(' + b + ')'].shuffle().join('+') + ')^2',
-				'(' + [a, 'sqrt(' + b + ')'].shuffle().join('-') + ')^2'].shuffle().join('+'),
+			expr: [
+			'(' + [a, 'sqrt(' + b + ')'].shuffle().join('+') + ')^2',
+			'(' + [a, 'sqrt(' + b + ')'].shuffle().join('-') + ')^2'].shuffle().join('+'),
 			authors: ['Алендарь Сергей'],
 		});
 	}, 1000);
