36154489d5b4226a585bbd86a450a5769db3fce9
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-09-15 15:36:45 +0300

    [zdn ] [up] - 26652

--- zdn/mat2014/B7/5.js
+++ zdn/mat2014/B7/5.js
@@ -1,26 +1,34 @@
-(function(){'use strict';
+(function() {
+	'use strict';
+	let key = '26652';
+	let preference = ['frac', 'no_frac'];
+	let rand = getSelectedPreferenceFromList(key, preference);
 
-var d=sluchch(2,9);
-var f=[2,4].iz();
-var a=d.pow(f);
-var b=sluchch(1,9).pm();
-var c=sluchch(1,9).pm();
-var x=-1/f-c;
-var p;
-switch(sl1()){
+	var d = sluchch(2, 9);
+	var f = [2, 4].iz();
+	var a = d.pow(f);
+	var c = sluchch(1, 9).pm();
+	var x = -1 / f - c;
+	var p;
+	switch (rand) {
 	case 0:
-		p = ['\\left(\\frac{1}{'+a+'}\\right)^{'+'x+'+c+'}', d ];
-	break;
+		p = ['\\left(\\frac{1}{' + a + '}\\right)^{' + 'x+' + c + '}', d];
+		break;
 	case 1:
-		p = ['{'+a+'}^{'+'x+'+c+'}', 1 .texfrac(d) ];
-	break;
-}
-chas2.task.setEquationTask({
-	parts: p,
-	roots: x,
-	enablePartsSubtraction: 1,
-	enablePartsExchange: 0,
-},{
-	tags: {drs:1},
-});
+		p = ['{' + a + '}^{' + 'x+' + c + '}', (1).texfrac(d)];
+		break;
+	}
+	chas2.task.setEquationTask({
+		parts: p,
+		roots: x,
+		enablePartsSubtraction: 1,
+		enablePartsExchange: 0,
+	}, {
+		tags: {
+			drs: 1
+		},
+	});
 })();
+
+//https://math-ege.sdamgia.ru/problem?id=26652
+//https://math-ege.sdamgia.ru/problem?id=26654
