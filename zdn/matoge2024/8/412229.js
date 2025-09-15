cf3b329956967fca7058afa88cb6f92427b7830c
Алендарь Сергей <sergey.alendar@mail.ru>
2025-09-15 21:22:33 +0300

    точки внутри корней убраны

--- zdn/matoge2024/8/412229.js
+++ zdn/matoge2024/8/412229.js
@@ -5,9 +5,9 @@
 		let sqrt_b = sl(2, 9);
 
 		NAtask.setEvaluationTask({
-			expr: ['sqrt(' + sqrt_a*sqrt_a + '*a^'+sl(2,9)+')', 'sqrt(' + sqrt_b*sqrt_b + '*b^'+sl(2,9)+')'].shuffle().join('*') +
-			'/(sqrt(' + ['a^'+sl(2,9), 'b^'+sl(2,9)].shuffle().join('*') + '))',
-			variables: {a: sl(sl(2, 7), 9),b: sl(sl(2, 7), 9)},
+			expr: ['sqrt(' + sqrt_a*sqrt_a + 'a^'+sl(2,9)+')', 'sqrt(' + sqrt_b*sqrt_b + 'b^'+sl(2,9)+')'].shuffle().join('*') +
+			'/(sqrt(' + ['a^'+sl(2,9), 'b^'+sl(2,9)].shuffle().join('') + '))',
+			variables: {a: sl(2, 9),b: sl(2, 9)},
 			authors: ['Алендарь Сергей'],
 		});
 	}, 1000);
