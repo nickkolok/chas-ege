868229738cb48a1ecbb83c6f55ad3c94add3fb7c
Алендарь Сергей <sergey.alendar@mail.ru>
2025-09-15 21:07:37 +0300

    Попытка избежать баг умножения на 10 в степени

--- zdn/matoge2024/8/90080016.js
+++ zdn/matoge2024/8/90080016.js
@@ -3,7 +3,7 @@
 		'use strict';
 		let a = sl(2, 15);
 		NAtask.setEvaluationTask({
-			expr: [a + '^' + sl(1, 10), a.pow(sl(2, 5))].shuffle().join('/'),
+			expr: [a + '^' + sl(1, 10), a.pow(sl(2, 4))].shuffle().join('/'),
 			authors: ['Алендарь Сергей'],
 		});
 	}, 10000);
