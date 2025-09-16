cf891f92736be823305e5ed2fa660575c2cedc79
Алендарь Сергей <sergey.alendar@mail.ru>
2025-09-16 21:11:06 +0300

    Добавлен genAssertIrreducible, a и b не могут совпадать

--- zdn/matoge2024/8/412197.js
+++ zdn/matoge2024/8/412197.js
@@ -4,17 +4,18 @@
 		let x = sl(1, 10);
 		let y = sl(2, 10);
 		let divider = sl(5, 13);
-		let dividendA = sl(1, 9);
-		let dividendB = sl(1, 9);
-		genAssert(dividendA.nod(divider)==1, 'Дробь должна быть несократима');
-		genAssert(dividendB.nod(divider)==1, 'Дробь должна быть несократима');
+		let dividendA = sl(1, 81);
+		let dividendB = sl(1, 81);
+		genAssert(dividendA!=dividendB, 'a не должен равняться b');
+		genAssertIrreducible(dividendA, divider, 'Дробь должна быть несократима');
+		genAssertIrreducible(dividendB, divider, 'Дробь должна быть несократима');
 		NAtask.setEvaluationTask({
 			expr: 'sqrt(' + x * x + '*a^2' + ['+', '-'].iz() +
 				2 * x * y + '*a*b' + '+' + y * y + '*b^2)',
-			variables: {a: sl(1, 25) + [1, dividendA + "/"+divider].iz(),
-				b: sl(1, 25).pm() + [1, dividendB + "/"+divider].iz()},
+			variables: {a: [dividendA, dividendA + "/"+divider].iz(),
+				b: [dividendB, dividendB + "/"+divider].iz()},
 			authors: ['Алендарь Сергей'],
 		});
-	}, 1000);
+	}, 10000);
 })();
 //412197
