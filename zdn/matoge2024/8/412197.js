94ce5fa6301c25c0aa168b5d8a94f288fcf2a4b0
Алендарь Сергей <sergey.alendar@mail.ru>
2025-09-18 22:32:51 +0300

    Добавлен slKrome для генерации значений b

--- zdn/matoge2024/8/412197.js
+++ zdn/matoge2024/8/412197.js
@@ -5,15 +5,16 @@
 		let y = sl(2, 10);
 		let divider = sl(5, 13);
 		let dividendA = sl(1, 81);
-		let dividendB = sl(1, 81);
-		genAssert(dividendA!=dividendB, 'a не должен равняться b');
+		let dividendB = slKrome(dividendA, 1, 81);
 		genAssertIrreducible(dividendA, divider, 'Дробь должна быть несократима');
 		genAssertIrreducible(dividendB, divider, 'Дробь должна быть несократима');
 		NAtask.setEvaluationTask({
 			expr: 'sqrt(' + x * x + '*a^2' + ['+', '-'].iz() +
 				2 * x * y + '*a*b' + '+' + y * y + '*b^2)',
-			variables: {a: [dividendA, dividendA + "/"+divider].iz(),
-				b: [dividendB, dividendB + "/"+divider].iz()},
+			variables: {
+				a: [dividendA, dividendA + "/" + divider].iz(),
+				b: [dividendB, dividendB + "/" + divider].iz()
+			},
 			authors: ['Алендарь Сергей'],
 		});
 	}, 10000);
