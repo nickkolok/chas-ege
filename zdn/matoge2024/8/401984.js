416f3bc0b61c49cc89de48705be8092c7026df0d
Алендарь Сергей <sergey.alendar@mail.ru>
2025-09-18 21:22:40 +0300

    Строки перенесены, изменена генерация для b

--- zdn/matoge2024/8/401984.js
+++ zdn/matoge2024/8/401984.js
@@ -2,15 +2,15 @@
 	retryWhileError(function() {
 		'use strict';
 		let a = sl(1, 15);
-		let b = sl(1, 30);
-		let sqrtb = b.sqrt();
-		genAssert(!sqrtb.isZ(), "Корень извлекается");
+		let b = slKrome(isPolnKvadr, 2, 30);
 		NAtask.setEvaluationTask({
 			expr: [
-			'(' + [a, 'sqrt(' + b + ')'].shuffle().join('+') + ')^2',
-			'(' + [a, 'sqrt(' + b + ')'].shuffle().join('-') + ')^2'].shuffle().join('+'),
+				'(' + [a, 'sqrt(' + b + ')'].shuffle().join('+') + ')^2',
+				'(' + [a, 'sqrt(' + b + ')'].shuffle().join('-') + ')^2'
+			].shuffle().join('+'),
 			authors: ['Алендарь Сергей'],
 		});
 	}, 1000);
 })();
 //401984
+
