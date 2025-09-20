bef164a943bfe838cfbb319a92c0c178def62295
Алендарь Сергей <sergey.alendar@mail.ru>
2025-09-20 19:36:14 +0300

    убрана единица под корнем

--- zdn/matoge2024/8/412221.js
+++ zdn/matoge2024/8/412221.js
@@ -3,7 +3,7 @@
 		'use strict';
 
 		NAtask.setEvaluationTask({
-			expr: ['(' + 'sqrt(' + sl(1, 49) + ')*sqrt(' + sl(1, 49) + ')' + ')^2', sl(1, 99)].shuffle().join(['+', '-'].iz()),
+			expr: ['(' + 'sqrt(' + sl(2, 49) + ')*sqrt(' + sl(2, 49) + ')' + ')^2', sl(1, 99)].shuffle().join(['+', '-'].iz()),
 			authors: ['Алендарь Сергей'],
 		});
 	}, 1000);
