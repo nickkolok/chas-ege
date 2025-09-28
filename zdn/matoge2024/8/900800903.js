19c6600f1f87fc3fbf48077740457812bf2b5781
NickKolok <nickkolok@mail.ru>
2025-09-29 01:21:58 +0300

    [zdn] matoge2024/8/900800903 - use rulesBeforePrinting to force implicit multiplication somewhere

--- zdn/matoge2024/8/900800903.js
+++ zdn/matoge2024/8/900800903.js
@@ -9,6 +9,9 @@
         NAtask.setEvaluationTask({
             expr: ['','-'].iz() + '(' + [num_sqrt_x + ' sqrt(x) ', num_sqrt_y + ' sqrt(y) '].shuffle().join('*') + '/(' + den_coef + ' sqrt(x y)))',
             variables: {x: sl(2, 9), y: sl(2, 9)},
+            rulesBeforePrinting: [
+                { l: 'n1*sqrt(n2)*n3*sqrt(n4)', r: 'n1 sqrt(n2)*n3 sqrt(n4)' },
+            ],
             authors: ['Алендарь Сергей'],
         });
     }, 1000);
