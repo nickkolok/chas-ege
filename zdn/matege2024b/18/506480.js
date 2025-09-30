5eb6077dfba7c2ff17422f8ad2a3ec3e28930ff9
Колесникова Елена <luly3751@gmail.com>
2025-09-30 17:26:01 +0300

    add esli, add autoLaTeXLeft;autoLaTeXRight;

--- zdn/matege2024b/18/506480.js
+++ zdn/matege2024b/18/506480.js
@@ -1,19 +1,21 @@
-function base18({ left, right, text, leftHeader, rightHeader, postText }) {
+function setCorrespondenceTask({ left, right, text, leftHeader, rightHeader, postText, autoLaTeXLeft, autoLaTeXRight }) {
+
 	left.sort(() => Math.random() - 0.5);
 	let shuffledSolutions = [...right].sort(() => Math.random() - 0.5);
 	let leftCol = '';
 	for (let i = 0; i < left.length; i++) {
 		let letter = String.fromCharCode(65 + i);
-		leftCol += letter + ') ' + '$' + left[i].expr + '$<br>';
+		let the$ = '$'.esli(autoLaTeXLeft && (left[i].expr.search('\\$') === -1));
+		leftCol += letter + ') ' + the$ + left[i].expr + the$ + '<br>';
 	}
 	let rightCol = '';
 	let solutionToIndex = {};
 	for (let i = 0; i < shuffledSolutions.length; i++) {
 		let num = i + 1;
-		rightCol += num + ') ' + shuffledSolutions[i] + '<br>';
+		let the$ = '$'.esli(autoLaTeXRight && (shuffledSolutions[i].search('\\$') === -1));
+		rightCol += num + ') ' + the$ + shuffledSolutions[i] + the$ + '<br>';
 		solutionToIndex[shuffledSolutions[i]] = num;
 	}
-
 	let answerSequence = left.map(item => solutionToIndex[item.solution]);
 
 	NAtask.setTask({
@@ -38,24 +40,24 @@ function base18({ left, right, text, leftHeader, rightHeader, postText }) {
 		//квадратичные нер-ва
 		let quadratic = {
 			expr: `(x - ${a})(x - ${b}) ${sl1() ? '<' : '>'} 0`,
-			solution: sl1() ? `$${a} < x < ${b}$` : `$x < ${a} \\text{ или } x > ${b}$`
+			solution: sl1() ? `${a} < x < ${b}` : `x < ${a} \\text{ или } x > ${b}`
 		};
 		//дробные нер-ва
 		let rational = {
 			expr: `\\frac{x - ${a}}{x - ${b}} ${sl1() ? '<' : '>'} 0`,
-			solution: sl1() ? `$${a} < x < ${b}$` : `$x < ${a} \\text{ или } x > ${b}$`
+			solution: sl1() ? `${a} < x < ${b}` : `x < ${a} \\text{ или } x > ${b}`
 		};
-		//показетльные нер-ва
+		//показательные нер-ва
 		let expBase = sl(2, 3);
 		let isExpGreater = sl1();
 		let expExpr = isExpGreater ? `${expBase}^{x} > ${expBase.pow(a)}` : `${expBase}^{x} < ${expBase.pow(b)}`;
-		let expSolution = isExpGreater ? `$x > ${a}$` : `$x < ${b}$`;
+		let expSolution = isExpGreater ? `x > ${a}` : `x < ${b}`;
 		let exponential = { expr: expExpr, solution: expSolution };
 		//логарифмические нер-ва
 		let logBase = sl1() ? a : b;
 		let isLogGreater = sl1();
 		let logExpr = isLogGreater ? `\\log_{${logBase}} x > 1` : `\\log_{${logBase}} x < 1`;
-		let logSolution = isLogGreater ? `$x > ${logBase}$` : `$x < ${logBase}$`;
+		let logSolution = isLogGreater ? `x > ${logBase}` : `x < ${logBase}`;
 		let logarithmic = { expr: logExpr, solution: logSolution };
 
 		let all = [quadratic, rational, exponential, logarithmic];
@@ -65,12 +67,14 @@ function base18({ left, right, text, leftHeader, rightHeader, postText }) {
 		let uniqueSolutions = new Set(solutions.map(s => s.replace(/\s/g, '')));
 		genAssert(uniqueSolutions.size === 4, 'Дубликаты решений');
 
-		base18({
+		setCorrespondenceTask({
 			text: 'Каждому из четырёх неравенств в левом столбце соответствует одно из решений в правом столбце. Установите соответствие между неравенствами и их решениями.',
 			leftHeader: 'НЕРАВЕНСТВА',
 			left: all,
+			autoLaTeXLeft: true,
 			rightHeader: 'РЕШЕНИЯ',
 			right: solutions,
+			autoLaTeXRight: true,
 			postText: 'Напишите по порядку букв цифры каждого решения.'
 		});
 
