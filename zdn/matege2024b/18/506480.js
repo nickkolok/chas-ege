6abae2ab4d84e73190cb6af203483c45003ef443
Колесникова Елена <luly3751@gmail.com>
2025-09-30 16:16:57 +0300

    add base18

--- zdn/matege2024b/18/506480.js
+++ zdn/matege2024b/18/506480.js
@@ -1,3 +1,33 @@
+function base18({ left, right }) {
+	left.sort(() => Math.random() - 0.5);
+	let shuffledSolutions = [...right].sort(() => Math.random() - 0.5);
+
+	let leftCol = '';
+	for (let i = 0; i < 4; i++) {
+		let letter = String.fromCharCode(65 + i);
+		leftCol += letter + ') ' + '$' + left[i].expr + '$<br>';
+	}
+	let rightCol = '';
+	let solutionToIndex = {};
+	for (let i = 0; i < 4; i++) {
+		let num = i + 1;
+		rightCol += num + ') ' + shuffledSolutions[i] + '<br>';
+		solutionToIndex[shuffledSolutions[i]] = num;
+	}
+
+	let answerSequence = left.map(item => solutionToIndex[item.solution]);
+
+	NAtask.setTask({
+		text: 'Каждому из четырёх неравенств в левом столбце соответствует одно из решений в правом столбце. Установите соответствие между неравенствами и их решениями.<br><br>' +
+			'<table style="border-collapse: collapse; width: 100%;"><tr>' +
+			'<td style="vertical-align: top; padding-right: 20px;"><strong>НЕРАВЕНСТВА</strong><br>' + leftCol + '</td>' +
+			'<td style="vertical-align: top;"><strong>РЕШЕНИЯ</strong><br>' + rightCol + '</td>' +
+			'</tr></table><br>' +
+			'<span style="font-family: monospace; font-size: 18px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br>' +
+			'Напишите по порядку букв цифры каждого решения.',
+		answers: answerSequence.join(' '),
+	});
+}
 (function () {
 	'use strict';
 	retryWhileError(function () {
@@ -36,34 +66,7 @@
 		let uniqueSolutions = new Set(solutions.map(s => s.replace(/\s/g, '')));
 		genAssert(uniqueSolutions.size === 4, 'Дубликаты решений');
 
-		all.sort(() => Math.random() - 0.5);
-		let shuffledSolutions = [...solutions].sort(() => Math.random() - 0.5);
-
-		let leftCol = '';
-		for (let i = 0; i < 4; i++) {
-			let letter = String.fromCharCode(65 + i);
-			leftCol += letter + ') ' + '$' + all[i].expr + '$<br>';
-		}
-		let rightCol = '';
-		let solutionToIndex = {};
-		for (let i = 0; i < 4; i++) {
-			let num = i + 1;
-			rightCol += num + ') ' + shuffledSolutions[i] + '<br>';
-			solutionToIndex[shuffledSolutions[i]] = num;
-		}
-
-		let answerSequence = all.map(item => solutionToIndex[item.solution]);
-
-		NAtask.setTask({
-			text: 'Каждому из четырёх неравенств в левом столбце соответствует одно из решений в правом столбце. Установите соответствие между неравенствами и их решениями.<br><br>' +
-				'<table style="border-collapse: collapse; width: 100%;"><tr>' +
-				'<td style="vertical-align: top; padding-right: 20px;"><strong>НЕРАВЕНСТВА</strong><br>' + leftCol + '</td>' +
-				'<td style="vertical-align: top;"><strong>РЕШЕНИЯ</strong><br>' + rightCol + '</td>' +
-				'</tr></table><br>' +
-				'<span style="font-family: monospace; font-size: 18px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br>' +
-				'Напишите по порядку букв цифры каждого решения.',
-			answers: answerSequence.join(' '),
-		});
+		base18({ left: all, right: solutions });
 
 		NAtask.modifiers.allDecimalsToStandard();
 
