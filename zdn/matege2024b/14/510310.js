30c93a493474e456a009d8508470d873031e3775
Алендарь Сергей <sergey.alendar@mail.ru>
2025-10-04 21:58:55 +0300

    Убран shuffle().join(), добавлен genAssertIrreducible

--- zdn/matege2024b/14/510310.js
+++ zdn/matege2024b/14/510310.js
@@ -7,15 +7,15 @@
 		let d = sl(2, 99);
 		let e = sl(1, 99);
 		let f = sl(2, 99);
-		genAssert(a.nod(b) == 1, 'Дробь a/b должна быть несократима');
-		genAssert(c.nod(d) == 1, 'Дробь c/d должна быть несократима');
-		genAssert(e.nod(f) == 1, 'Дробь e/f должна быть несократима');
+		genAssertIrreducible(a, b);
+		genAssertIrreducible(c, d);
+		genAssertIrreducible(e, f);
 
 		NAtask.setEvaluationTask({
-			expr: ['divideColon(' + ['(' + a + '/' + b + ')', '(' + e + '/' + f + ')'].shuffle().join() + ')', 
+			expr: ['divideColon(' + a + '/' + b + ', '+ e + '/' + f + ')', 
 				'(' + c + '/' + d + ')'].shuffle().join('*'),
 			authors: ['Алендарь Сергей'],
 		});
-	}, 100000);
+	}, 10000);
 })();
 //510310
