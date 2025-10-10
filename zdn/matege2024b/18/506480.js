715d9dcc105fdcfaccf6f3f9de58426507bfc54b
Колесникова Елена <luly3751@gmail.com>
2025-10-10 18:23:04 +0300

    add "<' in exponential; add powerFraction

--- zdn/matege2024b/18/506480.js
+++ zdn/matege2024b/18/506480.js
@@ -4,13 +4,13 @@
 		NAinfo.requireApiVersion(0, 2);
 		let key = '506380';
 		let preference1 = ['quadratic', 'rational'];
-		let preference2 = ['fractional', 'logarithmicMinus'];
+		let preference2 = ['fractional', 'logarithmicMinus', 'powerFraction'];
 		let preference3 = ['squareRational', 'exponential'];
 		let rand1 = getSelectedPreferenceFromList(key, preference1);
 		let rand2 = getSelectedPreferenceFromList(key, preference2);
 		let rand3 = getSelectedPreferenceFromList(key, preference3);
 
-		let a = sl(2, 5);
+		let a = sl(1, 5);
 		let b = a + sl(1, 5);
 
 		//квадратичные нер-ва
@@ -21,6 +21,7 @@
 				? `$${a} < x < ${b}$`
 				: `$x < ${a} \\text{ или } x > ${b}$`
 		};
+
 		//дробные нер-ва
 		let isFractionalLess = sl1();
 		let fractional = {
@@ -46,13 +47,58 @@
 				: `$${a} < x < ${b}$`
 		};
 		//показательные нер-ва
-		let expBase = sl(2, 3);
-		let isExpGreater = sl1();
-		let expSign = isExpGreater ? '>' : '<';
-		let expValue = isExpGreater ? expBase.pow(a) : expBase.pow(b);
-		let expExpr = `${expBase}^{x} ${expSign} ${expValue}`;
-		let expSolution = isExpGreater ? `x > ${a}` : `x < ${b}`;
-		let exponential = { expr: expExpr, solution: expSolution };
+		let exponential;
+		let doubleOrNothing = sl1();
+		let isGreater = sl1();
+		let value;
+		if (doubleOrNothing == 0) {
+			// Классическое показательное
+			let base = sl(2, 3);
+			value = isGreater ? base.pow(a) : base.pow(b);
+			exponential = {
+				expr: `${base}^{x} ${isGreater ? '>' : '<'} ${value}`,
+				solution: isGreater ? `$x > ${a}$` : `$x < ${b}$`
+			};
+		} else {
+			// Степенное с суммой
+			let base = sl(2, 5);
+			value = isGreater ? base.pow(b) : base.pow(a + b);
+			genAssert(value < 10000, "value слишком больше число");
+			if (isGreater) {
+				exponential = {
+					expr: `${base}^{-x + ${a + b}} > ${value}`,
+					solution: `$x < ${a}$`
+				};
+			} else {
+				exponential = {
+					expr: `${base}^{-x + ${b}} > ${value}`,
+					solution: `$x < ${b}$`
+				};
+			}
+		}
+		//показательные с дробью в правой части нер-ва
+		let powerFraction;
+		let valueFraction;
+		let type = sl1();
+		if (type === 0) {
+			let base = sl(2, 5);
+			let k = sl(2, 6);
+			valueFraction = base.pow(k * a);
+			genAssert(valueFraction < 10000, "valueFraction слишком больше число");
+			powerFraction = {
+				expr: `${base}^{-${k}x} > \\frac{1}{${valueFraction}}`,
+				solution: `$x < ${a}$`
+			};
+		} else {
+			// -x + c
+			let base = sl(2, 5);
+			valueFraction = base.pow(b - a);
+			powerFraction = {
+				expr: `${base}^{-x + ${b}} < \\frac{1}{${valueFraction}}`,
+				solution: `$x > ${a}$`
+			};
+		}
+
 		//логарифмические нер-ва
 		let logBase = sl1() ? a : b;
 		let isLogGreater = sl1();
@@ -65,8 +111,8 @@
 		let logSolutionMinus = `$${a} < x < ${b}$`;
 		let logarithmicMinus = { expr: logExprMinus, solution: logSolutionMinus };
 
-		let all = [[quadratic, rational][rand1], [fractional, logarithmicMinus][rand2], [squareRational, exponential][rand3], logarithmic];
-
+		let all = [[quadratic, rational][rand1], [fractional, logarithmicMinus, powerFraction][rand2], [squareRational, exponential][rand3], logarithmic];
+		//let all = [powerFraction, quadratic, logarithmicZero, fractional];
 		//уникальные реш.
 		let solutions = all.map(item => item.solution);
 		let uniqueSolutions = new Set(solutions.map(s => s.replace(/\s/g, '')));
