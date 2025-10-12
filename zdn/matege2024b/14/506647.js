c9dd851a21f84f5777f378e5f778e0139d7684a7
nickkolok <nickkolok@mail.ru>
2025-10-12 15:55:35 +0300

    Update 506647.js

--- zdn/matege2024b/14/506647.js
+++ zdn/matege2024b/14/506647.js
@@ -2,7 +2,7 @@
 	retryWhileError(function() {
 		'use strict';
 		NAtask.setEvaluationTask({
-			expr: '1/' + '(' + ['1/' + ['1', sl(1, 99)].iz(), '1/' + sl(1, 99)].shuffle().join(['+', '-'].iz()) + ')',
+			expr: '1/' + '(' + ['1/' + ['1', sl(2, 99)].iz(), '1/' + sl(1, 99)].shuffle().join(['+', '-'].iz()) + ')',
 			authors: ['Алендарь Сергей'],
 		});
 	}, 100000);
