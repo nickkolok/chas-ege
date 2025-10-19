21ac02c6ea32e83996697e64ec5e5a6ccbda6c40
Алендарь Сергей <sergey.alendar@mail.ru>
2025-10-19 12:48:20 +0300

    Убраны конфликты слияния

--- zdn/matoge2024/8/412189.js
+++ zdn/matoge2024/8/412189.js
@@ -7,10 +7,7 @@
 		NAtask.setEvaluationTask({
 			expr: 'sqrt((' +  '1/'+ ch * ch + '*' + ['y^' + sl(2, 10, 2), 'x^' + sl(2, 10, 2)].shuffle().join('') + '))',
 			variables: {x: sl(2, 9),y: sl(2, 9)},
-<<<<<<< HEAD
 			rulesBeforePrinting: [{ l: 'n1*n2', r: 'n1 n2' },],
-=======
->>>>>>> c00463e5e7e60443ef72f1c02b8e1742b8ef8c6a
 			authors: ['Алендарь Сергей'],
 		});
 	}, 1000);
