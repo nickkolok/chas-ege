d390e8f7ee0e6fd4522c498628c06ee6aafa5cae
Колесникова Елена <luly3751@gmail.com>
2025-11-05 18:14:24 +0300

    [zdn] update 506380 #2775; #2772 -fix #2775 -fix #2772

--- zdn/matege2024b/18/506480.js
+++ zdn/matege2024b/18/506480.js
@@ -3,10 +3,12 @@
 	retryWhileError(function () {
 		NAinfo.requireApiVersion(0, 2);
 		let key = '506380';
+		let preferenceAnswer = ['setOfConditions', 'setOfIntervals'];
 		let preferenceLog = ['haveLog', 'noLog', 'randomLog'];
 		let preferencePow = ['havePow', 'noPow', 'randomPow'];
 		let preferenceLogType = ['logarithmic', 'logBaseMinus'];
 		let preferencePowType = ['exponential', 'powerFraction'];
+		let randAnswer = getSelectedPreferenceFromList(key, preferenceAnswer);
 		let randLog = getSelectedPreferenceFromList(key, preferenceLog);
 		let randPow = getSelectedPreferenceFromList(key, preferencePow);
 		let randLogType = getSelectedPreferenceFromList(key, preferenceLogType);
@@ -14,7 +16,6 @@
 
 		let a = sl(1, 5);
 		let b = a + sl(1, 5);
-
 		//квадратичные нер-ва
 		let isQuadraticLess = sl1();
 		let holeQuad = sl(0, 2);
@@ -22,11 +23,17 @@
 			expr: [`(x - ${a})(x - ${b}) ${isQuadraticLess ? '<' : '>'} 0`,
 			`(x - ${a})^2(x - ${b}) ${isQuadraticLess ? '<' : '>'} 0`,
 			`(x - ${a})(x - ${b})^2 ${isQuadraticLess ? '<' : '>'} 0`][holeQuad],
-			solution: [
+			solution: [[
 				[`$x < ${a}$ $\\text{ или }$ $x > ${b}$`, `$${a} < x < ${b}$`][isQuadraticLess],
 				[`$x > ${b}$`, `$x < ${a}$ $\\text{ или }$ $${a} < x < ${b}$`][isQuadraticLess],
 				[`$${a} < x < ${b}$ $\\text{ или }$ $x > ${b}$`, `$x < ${a}$`][isQuadraticLess]
+			][holeQuad],
+			[
+				[`$(-\\infty,\\, ${a}) \\cup (${b},\\, +\\infty)$`, `$(${a},\\, ${b})$`][isQuadraticLess],
+				[`$(${b},\\, +\\infty)$`, `$(-\\infty,\\, ${a}) \\cup (${a},\\, ${b})$`][isQuadraticLess],
+				[`$(${a},\\, ${b}) \\cup (${b},\\, +\\infty)$`, `$(-\\infty,\\, ${a})$`][isQuadraticLess]
 			][holeQuad]
+			][randAnswer]
 		};
 		//дробные нер-ва
 		let isFractionalLess = sl1();
@@ -35,11 +42,17 @@
 			expr: [`\\frac{x - ${a}}{x - ${b}} ${isFractionalLess ? '<' : '>'} 0`,
 			`\\frac{x - ${a}}{(x - ${a})(x - ${b})} ${isFractionalLess ? '<' : '>'} 0`,
 			`\\frac{x - ${b}}{(x - ${a})(x - ${b})} ${isFractionalLess ? '<' : '>'} 0`][holeFrac],
-			solution: [
+			solution: [[
 				[`$x < ${a}$ $\\text{ или }$ $x > ${b}$`, `$${a} < x < ${b}$`][isFractionalLess], //обычный
 				[`$x > ${b}$`, `$x < ${a}$ $\\text{ или }$ $${a} < x < ${b}$`][isFractionalLess], //дырка в a
 				[`$${a} < x < ${b}$ $\\text{ или }$ $x > ${b}$`, `$x < ${a}$`][isFractionalLess] //дырка в b
+			][holeFrac],
+			[
+				[`$(-\\infty,\\, ${a}) \\cup (${b},\\, +\\infty)$`, `$(${a},\\, ${b})$`][isFractionalLess], //обычный
+				[`$(${b},\\, +\\infty)$`, `$(-\\infty,\\, ${a}) \\cup (${a},\\, ${b})$`][isFractionalLess], //дырка в a
+				[`$(${a},\\, ${b}) \\cup (${b},\\, +\\infty)$`, `$(-\\infty,\\, ${a})$`][isFractionalLess] //дырка в b
 			][holeFrac]
+			][randAnswer]
 		};
 		//дробное с квадратом 
 		let isSqRatGreater = sl1();
@@ -47,18 +60,23 @@
 		let numPart = [`(x - ${b})^2`, `(x - ${b})`, `x - ${a}`, `(x - ${a})^2`][swapNumDen];
 		let denPart = [`x - ${a}`, `(x - ${a})^2`, `(x - ${b})^2`, `(x - ${b})`][swapNumDen];
 		let sqRatExpr = `\\frac{${numPart}}{${denPart}} ${isSqRatGreater ? '>' : '<'} 0`;
-		let sqRatSolution = [
+		let sqRatSolution = [[
 			[`$x < ${a}$`, `$x < ${a}$ $\\text{ или }$ $${a} < x < ${b}$`, `$x < ${a}$`, `$x < ${a}$ $\\text{ или }$ $${a} < x < ${b}$`][swapNumDen],
 			[`$${a} < x < ${b}$ $\\text{ или }$ $x > ${b}$`, `$x > ${b}$`, `$${a} < x < ${b}$ $\\text{ или }$ $x > ${b}$`, `$x > ${b}$`][swapNumDen]
+		][isSqRatGreater],
+		[
+			[`$(-\\infty,\\, ${a})$`, `$(-\\infty,\\, ${a}) \\cup (${a},\\, ${b})$`, `$(-\\infty,\\, ${a})$`, `$(-\\infty,\\, ${a}) \\cup (${a},\\, ${b})$`][swapNumDen],
+			[`$(${a},\\, ${b}) \\cup  (${b},\\, +\\infty)$`, `$(${b},\\, +\\infty)$`, `$(${a},\\, ${b}) \\cup (${b},\\, +\\infty)$`, `$(${b},\\, +\\infty)$`][swapNumDen]
 		][isSqRatGreater]
+		][randAnswer]
 		let squareRational = { expr: sqRatExpr, solution: sqRatSolution };
 		//дробное рациональное второго типа
 		let isRationalGreater = sl1();
 		let rational = {
 			expr: `\\frac{1}{(x - ${a})(x - ${b})} ${isRationalGreater ? '>' : '<'} 0`,
 			solution: isRationalGreater
-				? `$x < ${a}$ $\\text{ или }$ $x > ${b}$`
-				: `$${a} < x < ${b}$`
+				? [`$x < ${a}$ $\\text{ или }$ $x > ${b}$`, `$(-\\infty,\\, ${a}) \\cup (${b},\\, +\\infty)$`][randAnswer]
+				: [`$${a} < x < ${b}$`, `$(${a},\\, ${b})$`][randAnswer]
 		};
 
 		//показательные нер-ва
@@ -73,7 +91,7 @@
 			value = base.pow(c);
 			exponential = {
 				expr: `${base}^{x} ${isGreater ? '>' : '<'} ${value}`,
-				solution: isGreater ? `$x > ${c}$` : `$x < ${c}$`
+				solution: isGreater ? [`$x > ${c}$`, `$(${c},\\, +\\infty)$`][randAnswer] : [`$x < ${c}$`, `$(-\\infty,\\, ${c})$`][randAnswer]
 			};
 		} else {
 			// Степенное с суммой
@@ -83,7 +101,7 @@
 			genAssert(value < 10000, 'value слишком большое число');
 			exponential = {
 				expr: `${base}^{-x + ${b}} > ${value}`,
-				solution: `$x < ${a}$`,
+				solution: [`$x < ${a}$`, `$(-\\infty,\\, ${a})$`][randAnswer],
 			};
 		}
 		//показательные с дробью в правой части нер-ва
@@ -99,7 +117,7 @@
 			genAssert(valueFraction < 1000, 'valueFraction слишком большое число');
 			powerFraction = {
 				expr: `${base}^{-${k}x} > ${fracStr}`,
-				solution: `$x < ${a}$`
+				solution: [`$x < ${a}$`, `$(-\\infty,\\, ${a})$`][randAnswer]
 			};
 		} else {
 			// -x + c
@@ -109,7 +127,7 @@
 			genAssert(valueFraction < 1000, 'valueFraction слишком большое число');
 			powerFraction = {
 				expr: `${base}^{-x + ${a}} < ${fracStr}`,
-				solution: `$x > ${b}$`
+				solution: [`$x > ${b}$`, `$(${b},\\, +\\infty)$`][randAnswer]
 			};
 		}
 
@@ -122,20 +140,20 @@
 			let isLogZero = sl1();
 			logarithmic = {
 				expr: `\\log_{${baseZero}} x ${isLogZero ? '>' : '<'} 0`,
-				solution: isLogZero ? `$x > 1$` : `$x < 1$`
+				solution: isLogZero ? [`$x > 1$`, `$(1,\\, +\\infty)$`][randAnswer] : [`$x < 1$`, `$(-\\infty,\\, 1)$`][randAnswer]
 			};
 		} else {
 			// Обычный логарифм: log_base(x) > 1
 			logarithmic = {
 				expr: `\\log_{${logBase}} x > 1`,
-				solution: `$x > ${logBase}$`
+				solution: [`$x > ${logBase}$`, `$(${logBase},\\, +\\infty)$`][randAnswer]
 			};
 		}
 		//логарифмическое нер-во с минусом
 		let logBaseMinus = b - a;
 		genAssert(logBaseMinus != 1, 'основание логарифма не должно быть единицей');
 		let logExprMinus = `\\log_{${logBaseMinus}} (x - ${a}) < 1`;
-		let logSolutionMinus = `$${a} < x < ${b}$`;
+		let logSolutionMinus = [`$${a} < x < ${b}$`, `$(${a},\\, ${b})$`][randAnswer];
 		let logarithmicMinus = { expr: logExprMinus, solution: logSolutionMinus };
 
 		let logTask = [logarithmic, logarithmicMinus][randLogType];
@@ -163,7 +181,7 @@
 			right: solutions,
 			autoLaTeXRight: true,
 			postText: 'Напишите по порядку букв цифры каждого решения.',
-			preference: [preferenceLog, preferencePow, preferenceLogType, preferencePowType],
+			preference: [preferenceAnswer, preferenceLog, preferencePow, preferenceLogType, preferencePowType],
 		});
 
 		NAtask.modifiers.allDecimalsToStandard();
