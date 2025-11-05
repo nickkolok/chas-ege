d7fe0b0e689860b900cc6084f7550c26c73d0c5e
Колесникова Елена <luly3751@gmail.com>
2025-11-05 18:40:13 +0300

    remove AparquetMultiplier; BparquetMultiplier

--- zdn/matege2024b/10/325147.js
+++ zdn/matege2024b/10/325147.js
@@ -6,15 +6,12 @@
         let preference = ['tile', 'parquet'];
         let rand = getSelectedPreferenceFromList(key, preference);
 
-        let aSideFloor = sl(2, 9, 1);
-        let bSideFloor = slKrome([aSideFloor], 2, 9, 1);
-        let AparquetMultiplier = sl(0.1, 0.9, 0.01);
-        let BparquetMultiplier = slKrome([AparquetMultiplier], 0.1, 0.9, 0.01);
-
+        let aSideFloor = sl(2, 9, 1) + [0, sl(0.1, 0.9, 0.1)][rand];
+        let bSideFloor = slKrome([aSideFloor], 2, 9, 1) + [0, sl(0.1, 0.9, 0.1)][rand];
         let aSidePart = sl(10, 30, 5);
         let bSidePart = slKrome([aSidePart], 10, 30, 5);
 
-        let result = (aSideFloor * bSideFloor * [1, AparquetMultiplier * BparquetMultiplier][rand]) / (aSidePart * [bSidePart, aSidePart][rand] * 0.0001);
+        let result = (aSideFloor * bSideFloor) / (aSidePart * [bSidePart, aSidePart][rand] * 0.0001);
         genAssert(result.isZ(), 'результат не целый');
 
         NAtask.setTask({
@@ -22,7 +19,7 @@
                 ' требуется покрыть паркетом из прямоугольных дощечек со сторонами $' + aSidePart + '$ см и $' + bSidePart + '$ см.' +
                 ' Сколько потребуется таких дощечек?',
             'Сколько потребуется кафельных плиток квадратной формы со стороной $' + aSidePart + '$ см,' +
-            ' чтобы облицевать ими стену, имеющую форму прямоугольника со сторонами $' + aSideFloor * AparquetMultiplier + '$ м и $' + bSideFloor * BparquetMultiplier + '$ м?'][rand],
+            ' чтобы облицевать ими стену, имеющую форму прямоугольника со сторонами $' + aSideFloor + '$ м и $' + bSideFloor + '$ м?'][rand],
             answers: result,
             preference: preference,
         });
