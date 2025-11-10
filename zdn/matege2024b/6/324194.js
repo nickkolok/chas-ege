9b07894d39a0dd225da9f98b4a46781af368d0c1
Колесникова Елена <luly3751@gmail.com>
2025-11-10 17:28:15 +0300

    add arrayOfUniqueValues; fix repeats; fix tableHTML

--- zdn/matege2024b/6/324194.js
+++ zdn/matege2024b/6/324194.js
@@ -7,12 +7,9 @@
 		let salons = ['Альфа', 'Бета', 'Гамма', 'Дельта', 'Эпсилон', 'Дзета', 'Эта', 'Тета', 'Йота', 'Каппа', 'Лямбда',
 			'Мю', 'Ню', 'Кси', 'Омикрон', 'Пи', 'Ро', 'Сигма', 'Тау', 'Ипсилон', 'Фи', 'Хи', 'Пси', 'Омега'].iz(3);
 
-		let costs = [];
-		costs.zapslch(0, 2, 12000, 18000, 100);
-		let downPercents = [];
-		downPercents.zapslch(0, 2, 5, 15, 5);
-		let terms = [];
-		terms.zapslch(0, 2, 12, 24, 3);
+		let costs = arrayOfUniqueValues(3, 12000, 18000, 100);
+		let downPercents = arrayOfUniqueValues(3, 5, 15, 1);
+		let terms = arrayOfUniqueValues(3, 12, 24, 1);
 		let monthlyPayments = [];
 		let totalCosts = [];
 
@@ -20,7 +17,7 @@
 			let cost = costs[i];
 			let down = Math.round(cost * downPercents[i] / 100);
 			let loan = cost - down;
-			let overpayment = Math.round(loan * sl(0.1, 0.9, 0.01)); // 10% переплаты
+			let overpayment = Math.round(loan * sl(0.1, 0.9, 0.01));
 			let monthly = Math.round((loan + overpayment) / terms[i]);
 			let total = down + monthly * terms[i];
 
@@ -29,22 +26,22 @@
 		}
 		let minTotal = Math.min(...totalCosts);
 
-		let tableHTML = '<table style="border-collapse: collapse; width: 100%; text-align: center; margin: 10px 0;">';
+		let tableHTML = '<table style="border-collapse: collapse; width: 100%; text-align: center; margin: 10px 0; border: 1px solid black;">';
 		tableHTML += '<tr>' +
-			'<th>Салон</th>' +
-			'<th>Стоимость ' + item.re + ' (руб.)</th>' +
-			'<th>Первоначальный взнос (в % от стоимости)</th>' +
-			'<th>Срок кредита (мес.)</th>' +
-			'<th>Сумма ежемесячного платежа (руб.)</th>' +
+			'<th style="border: 1px solid black;">Салон</th>' +
+			`<th style="border: 1px solid black;">Стоимость ${item.re} (руб.)</th>` +
+			'<th style="border: 1px solid black;">Первоначальный взнос (в % от стоимости)</th>' +
+			'<th style="border: 1px solid black;">Срок кредита (мес.)</th>' +
+			'<th style="border: 1px solid black;">Сумма ежемесячного платежа (руб.)</th>' +
 			'</tr>';
 
 		for (let i = 0; i < 3; i++) {
 			tableHTML += '<tr>' +
-				`<td>${salons[i]}</td>` +
-				`<td>${costs[i].ts()}</td>` +
-				`<td>${downPercents[i]}</td>` +
-				`<td>${terms[i]}</td>` +
-				`<td>${monthlyPayments[i].ts()}</td>` +
+				`<td style="border: 1px solid black;">${salons[i]}</td>` +
+				`<td style="border: 1px solid black;">${costs[i].ts()}</td>` +
+				`<td style="border: 1px solid black;">${downPercents[i]}</td>` +
+				`<td style="border: 1px solid black;">${terms[i]}</td>` +
+				`<td style="border: 1px solid black;">${monthlyPayments[i].ts()}</td>` +
 				'</tr>';
 		}
 		tableHTML += '</table>';
