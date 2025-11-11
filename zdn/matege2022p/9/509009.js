da6e28a52a20dab069df37403b5b7d85fde1993d
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-11-11 13:39:39 +0300

    [zdn] [ref] - 509009.js

--- zdn/matege2022p/9/509009.js
+++ zdn/matege2022p/9/509009.js
@@ -1,65 +1,90 @@
-(function() {
-	NAinfo.requireApiVersion(0, 2);
+sluchch.forceIntegers = true;
+(function () {
+	retryWhileError(function () {
+		NAinfo.requireApiVersion(0, 2);
 
-	function f(x) {
-		return logAB(a, x, b);
-	}
+		function f(x) {
+			return Math.log(x) / Math.log(a) + b;
+		}
 
-	function logAB(a, x, b) {
-		return Math.log(x) / Math.log(a) + b;
-	}
-	let a, b, chisl;
-	let X, Y;
-	do {
-		a = sluchch(2, 10);
-		if ((100 / a).isZ() && sl1())
-			a = sl(1,5) / a;
-		b = sluchch(0, 6).pm();
-		chisl = Math.pow(a, sluchch(2, 4));
-		X = [];
-		Y = [];
-		for (let i = 0; i <= 6; i++)
-			if (f(i).isZ() && f(i) < 6) {
-				X.push(i);
-				Y.push(f(i));
-			}
-	} while (X.length < 2 || (Math.abs(f(chisl)) <= 6 && Math.abs(chisl) <= 6 ));
-	let find, answ;
-	if (sl1()) {
-		find = `$f(${chisl.ts()})$`;
-		answ = f(chisl).ts(1);
-	} else {
-		answ = chisl;
-		find = `значение $x$, при котором $f(x)=${(f(chisl)).ts()}$`;
-	}
-	let paint1 = function(ct) {
-		h = 300;
-		//Оси координат
-		graph9AdrawAxes_20_300(ct);
-		ct.translate(-10, -10);
-		//график
-		ct.translate(h / 2, h / 2);
-		ct.scale(20, -20);
-		ct.lineWidth = 0.1;
-		let i = 0.1;
-		do {
-			if (f(i - 0.1) < 5.5)
-				if (f(i - 0.1) > -6.8)
-					ct.drawLine(i - 0.1, f(i - 0.1), i, f(i));
-			i += 0.1;
-		} while (i < 6.5);
-		//точки
-	graph9AmarkCircles(ct, [X, Y].T(), 2, 0.15);
-	};
-	NAtask.setTask({
-		text: `На рисунке изображён график функции $f(x)=b+\\log{_a}{x}$. Найдите ${find}. `,
-		answers: answ,
-		analys: `$f(x)=${b}+\\log{_{${a}}} {x}$`,
-	});
-	chas2.task.modifiers.addCanvasIllustration({
-		width: 300,
-		height: 300,
-		paint: paint1,
-	});
+		let key = '509009';
+		let preference1 = ['functionOfX', 'valueX'];
+		let preference2 = ['withB', 'withoutB'];
+		let preference3 = ['integerA', 'decimalA'];
+
+		let randFind = getSelectedPreferenceFromList(key, preference1);
+		let randB = getSelectedPreferenceFromList(key, preference2);
+		let randA = getSelectedPreferenceFromList(key, preference3);
+
+
+		let a = [sl(2, 10), [0.01, 0.25, 0.2, 0.125].iz()][randA];
+		let b = [sluchch(1, 7).pm(), 0][randB];
+		let chisl = Math.pow(a, sluchch(1, 5));
+
+		if (randA) {
+			genAssertZ1000(chisl);
+			genAssert(Math.abs(f(chisl)) >= 2);
+		} else {
+			genAssert(Math.abs(chisl) > 6);
+			genAssert(Math.abs(f(chisl)) > [6, 2][randB]);
+			genAssert(chisl < 1000);
+		}
+
+		let points = intPoints(f, {
+			minX: -8,
+			maxX: 8,
+			minY: -8,
+			maxY: 8
+		});
+
+		genAssert(points.length >= 2, 'Недостаточно точек');
+
+		let find, answ;
+		if (!randFind) {
+			answ = f(chisl).ts(1);
+			find = `$f(${chisl.ts()})$`;
+		} else {
+			answ = chisl;
+			find = `значение $x$, при котором $f(x)=${(f(chisl)).ts()}$`;
+		}
+		let paint1 = function (ct) {
+			let h = 400;
+			let w = 400;
+			//Оси координат
+			ct.drawCoordinatePlane(w, h, {
+				hor: 1,
+				ver: 1
+			}, {
+				x1: '1',
+				y1: '1',
+				sh1: 13,
+			}, 20);
+			//график
+			ct.scale(20, -20);
+			ct.lineWidth = 0.1;
+
+			graph9AdrawFunction(ct, f, {
+				minX: 0,
+				maxX: 8.5,
+				minY: -8.8,
+				maxY: 8.5,
+				step: 0.05
+			});
+			//точки
+			graph9AmarkCircles(ct, points, 2, 0.15);
+		};
+		NAtask.setTask({
+			text: `На рисунке изображён график функции $f(x)=${`b+`.esli(!randB)}\\log{_a}{x}$. Найдите ${find}. `,
+			answers: answ,
+			analys: `$f(x)=${b}+\\log{_{${a}}} {x}$`.replace('0+', ''),
+			preference: [preference1, preference2, preference3],
+		});
+		NAtask.modifiers.allDecimalsToStandard(true);
+		NAtask.modifiers.addCanvasIllustration({
+			width: 400,
+			height: 400,
+			paint: paint1,
+		});
+	}, 100000);
 })();
 //509009
