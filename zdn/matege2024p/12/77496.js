0126274b65c6f901f584e17071eba76e416b38e9
Алендарь Сергей <sergey.alendar@mail.ru>
2025-11-16 12:46:06 +0300

    Изменены границы интервала

--- zdn/matege2024p/12/77496.js
+++ zdn/matege2024p/12/77496.js
@@ -6,12 +6,13 @@
 		let c = sl(1, 99);
 		let sluchSign = sl1();
 		let sign = ['+', '-'];
-		let sincos = ['sin', 'cos'];
+		let sincos = ['sin', 'cos', 'tg', 'ctg'];
+		let interval = ['pi/6', 'pi/4', 'pi/3', 'pi/2', '2pi/3', '3pi/4', '5pi/6', '7pi/6', '5pi/4', '4pi/3', '3pi/2', '5pi/3', '7pi/4', '11pi/6'];
 		NAtask.setMinimaxFunctionTask({
 			expr: sign[sluchSign] + a + sincos.iz() + '(x)' + sign[1 - sluchSign] + b + 'x' + sign.iz() + c,
-			leftEnd: '0',
-			rightEnd: ['pi/6', 'pi/4', 'pi/3', 'pi/2', '2pi/3', '3pi/4', '5pi/6', '3pi/2'].iz(),
-			primaryStep: 0.1,
+			leftEnd: [['', '-'].iz() + interval.iz(), '0'].iz(),
+			rightEnd: [interval.iz(), '0'].iz(),
+			primaryStep: 0.001,
 			secondaryStep: 0.0001,
 			authors: ['Алендарь Сергей'],
 		});
