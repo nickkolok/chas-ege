696f564b803b6c78be3eb532c1a34421d367fc92
Алендарь Сергей <sergey.alendar@mail.ru>
2025-11-16 12:49:21 +0300

    Убраны лишние переменные

--- zdn/matege2024p/12/77496.js
+++ zdn/matege2024p/12/77496.js
@@ -6,7 +6,7 @@
 		let c = sl(1, 99);
 		let sluchSign = sl1();
 		let sign = ['+', '-'];
-		let sincos = ['sin', 'cos', 'tg', 'ctg'];
+		let sincos = ['sin', 'cos'];
 		let interval = ['pi/6', 'pi/4', 'pi/3', 'pi/2', '2pi/3', '3pi/4', '5pi/6', '7pi/6', '5pi/4', '4pi/3', '3pi/2', '5pi/3', '7pi/4', '11pi/6'];
 		NAtask.setMinimaxFunctionTask({
 			expr: sign[sluchSign] + a + sincos.iz() + '(x)' + sign[1 - sluchSign] + b + 'x' + sign.iz() + c,
