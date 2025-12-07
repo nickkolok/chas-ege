518ddc45995331ca0931ca507007011a98d62b30
Алендарь Сергей <sergey.alendar@mail.ru>
2025-12-07 15:27:05 +0300

    Изменена генерация интервала

--- zdn/matege2024p/12/77494.js
+++ zdn/matege2024p/12/77494.js
@@ -2,21 +2,22 @@
 	retryWhileError(function() {
 		'use strict';
 		let a = sl(1, 49);
-		let b = a*2;
+		let b = a * 2;
 		let c = sl(1, 99);
 		let d = sl(1, 99);
 		let sluchSign = sl1();
 		let sign = ['+', '-'];
+		let interval = ['pi/3', 'pi/4', 'pi/6'];
 		NAtask.setMinimaxFunctionTask({
-			expr: sign[sluchSign] + a  + 'tg(x)' + sign[1 - sluchSign] + b + 'x' + sign.iz() + c + 'pi' + ['/2',''].iz() + sign.iz() + d,
-			leftEnd: '-pi/3',
-			rightEnd: 'pi/3',
+			expr: sign[sluchSign] + a + 'tg(x)' + sign[1 - sluchSign] + b + 'x' + sign.iz() + c + 'pi' + ['/2', ''].iz() +
+				sign.iz() + d,
+			leftEnd: '-' + interval.iz(),
+			rightEnd: interval.iz(),
 			primaryStep: 0.01,
 			secondaryStep: 0.0001,
 			authors: ['Алендарь Сергей'],
 		});
 	}, 10000);
 })();
-
 //77494 
 //77495
