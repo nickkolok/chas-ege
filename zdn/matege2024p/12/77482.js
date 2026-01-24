136f76e0241852c8f3ca77c6f987c6662304f0d0
Алендарь Сергей <sergey.alendar@mail.ru>
2026-01-24 17:00:05 +0300

    Внесены изменения в генерацию переменных и изменён знак в степени на правильный

--- zdn/matege2024p/12/77482.js
+++ zdn/matege2024p/12/77482.js
@@ -1,22 +1,20 @@
 (function() {
 	retryWhileError(function() {
 		'use strict';
-		let a = sl(0, 97);
-		let sign = ['+', '-'];
+		let a = sl(1, 96);
+		let b = a + sl(1, 3);
+		let left = b - sl(0.5, 5, 0.5);
+		let right = b + sl(0.5, 5, 0.5);
+		let sign = [' ', '-'].iz();
 		NAtask.setMinimaxFunctionTask({
-			expr: [' ', '-'].iz() + '(' + ['x', [a, a + 2].iz()].shuffle().join('-') + ')^2*e^(' + ['x', a].shuffle().join('+') + ')',
-			leftEnd: (a - sl(0.5, 3, 0.5)) + '',
-			rightEnd: (a + sl(0.5, 3, 0.5)) + '',
+			expr: sign + '(' + ['x', b].shuffle().join('-') + ')^2*e^(' + ['x', a].shuffle().join('-') + ')',
+			leftEnd: left + '',
+			rightEnd: right + '',
+			forbiddenAnswers: [0, '-0'],
 			primaryStep: 0.01,
 			secondaryStep: 0.0001,
-<<<<<<< HEAD
-			forbiddenAnswers: [0,'-0'],
-=======
->>>>>>> d8d23216370783cbcd499e38b1c979b38ee90e71
 			authors: ['Алендарь Сергей'],
 		});
 	}, 10000);
 })();
-
-//77482
 //77483
