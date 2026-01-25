0146dbe316c29eff40b40590f006c8cba4ad8296
Алендарь Сергей <sergey.alendar@mail.ru>
2026-01-25 13:32:41 +0300

    Изменена генерация пременных и знаков

--- zdn/matege2024p/12/77484.js
+++ zdn/matege2024p/12/77484.js
@@ -1,20 +1,27 @@
 (function() {
-	retryWhileError(function() {
-		'use strict';
-		let a = sl(1, 99).pm();
-		let b = a + sl(1, 5);
-		let sign = ['+', '-'].iz();
-		NAtask.setMinimaxFunctionTask({
-			expr: sign + '(x + ' + b + ')^2*e^(' + a + '-x)',
-			leftEnd: (-a - sl(0.5, 2, 0.5)) + '',
-			rightEnd: (-a + sl(0.5, 2, 0.5)) + '',
-			primaryStep: 0.01,
-			secondaryStep: 0.0001,
-			authors: ['Алендарь Сергей'],
-		});
-	}, 10000);
+    retryWhileError(function() {
+        'use strict';
+        let a = sl(1, 45); 
+        let b = a + 2;
+        let sign = [' ', '-'].iz();
+        let r1 = sl(0.5, 5, 0.5);
+        let r2 = sl(0.5, 5, 0.5);
+        
+        let offset = slKrome([-1, -0.5, 0.5, 1], 0);
+        let center = -b + offset;
+       
+        let leftEnd = (center - r1).toFixed(1);
+        let rightEnd = (center + r2).toFixed(1);
+        
+        NAtask.setMinimaxFunctionTask({
+            expr: sign + '(' + ['x', b].shuffle().join('+') + ')^2*e^(' + '-' + [a,'x'].shuffle().join('-')+')',
+            leftEnd: leftEnd,
+            rightEnd: rightEnd,
+            forbiddenAnswers: [0, '-0'],
+            primaryStep: 0.01,
+            secondaryStep: 0.0001,
+            authors: ['Алендарь Сергей'],
+        });
+    }, 10000);
 })();
-
-//77484 
 //77485
-
