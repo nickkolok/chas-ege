4eeef8e0ed82383fb2a7ad4ee2b9ba32dc11add7
Колесникова Елена <luly3751@gmail.com>
2026-01-28 17:34:31 +0300

    [zdn][new] 489499 -fix #3040

---
+++ zdn/matege2024b/15/489499.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+    retryWhileError(function () {
+        NAinfo.requireApiVersion(0, 2);
+        let key = '489499';
+        let preference = ['samePercent', 'differentPercent'];
+        let rand = getSelectedPreferenceFromList(key, preference);
+
+        let pantsPrice = sl(4000, 6000, 100);
+        let percentFirst = sl(10, 35);
+        let percentSecond = slKrome([percentFirst], 10, 35);
+
+        let totalCost = [3 * pantsPrice, pantsPrice * (1 + percentFirst / 100 + 1 - percentSecond / 100)][rand];
+
+        genAssertZ1000(totalCost / 10, "Стоимость должна быть до двух знаков после запятой");
+
+        NAtask.setTask({
+            text: 'Мужской костюм состоит из брюк, жилета и пиджака. ' +
+                'Пиджак дороже брюк на $' + percentFirst + '\\%$, а жилет дешевле брюк на $' + [percentFirst, percentSecond][rand] + '\\%$. ' +
+                'Сколько стоит мужской костюм, если брюки стоят ' + chislitlx(pantsPrice, 'рубль', 'v$') + '? ' +
+                'Ответ дайте в рублях.',
+            answers: totalCost,
+            preference: preference,
+        });
+
+        NAtask.modifiers.allDecimalsToStandard(true);
+    }, 1000);
+})();
+//489499
+//zer00player
