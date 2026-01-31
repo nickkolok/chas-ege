f9ac9bc5a809e2d68c0c65cd636751ede1f6463c
Колесникова Елена <luly3751@gmail.com>
2026-01-31 19:06:25 +0300

    make code structure better

--- zdn/matege2024b/1/15245634.js
+++ zdn/matege2024b/1/15245634.js
@@ -3,19 +3,19 @@
     retryWhileError(function () {
         NAinfo.requireApiVersion(0, 2);
 
-        let price = sl(20, 60);          
-        let budget = sl(200, 1000);       
+        let price = sl(20, 60);
+        let budget = sl(200, 1000);
 
         let maxPacks = (budget / price).floor();
 
         genAssert(maxPacks > 2, "Слишком мало пачек задачи");
         genAssert(budget / price != 0, "бюджет на цену не должен делится нацело");
 
-        let pack = sklonlxkand(['печенье','чай','ряженка','молоко','мука','вермишель'].iz());
+        let pack = sklonlxkand(['печенье', 'чай', 'ряженка', 'молоко', 'мука', 'вермишель'].iz());
 
         NAtask.setTask({
             text:
-                 'Пачка ' + pack.re + ' стоит ' + chislitlx(price, 'рубль', 'v$') +
+                'Пачка ' + pack.re + ' стоит ' + chislitlx(price, 'рубль', 'v$') +
                 '. Какое наибольшее количество пачек ' + pack.re + ' можно купить на ' +
                 chislitlx(budget, 'рубль', 'v$') + '?',
             answers: maxPacks,
