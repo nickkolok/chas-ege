ab5de914793edde3d6cb93b88091849772486ed6
Колесникова Елена <luly3751@gmail.com>
2026-02-05 18:04:03 +0300

    add preference2 -fix #3046

--- zdn/matege2024b/19/4921509.js
+++ zdn/matege2024b/19/4921509.js
@@ -3,20 +3,24 @@
 	retryWhileError(function () {
 		NAinfo.requireApiVersion(0, 2);
 		let key = '4921509';
-		let preference = ['biggestNumber', 'smallestNumber'];
-		let rand = getSelectedPreferenceFromList(key, preference);
-		
-		let word =['большее','меньшее'][rand];
-		
-		let divisor = [3, 5, 7, 9, 11, 13, 17, 19, 23, 29].iz();
-		let include = ['3','4','5','6','7','8','9'].iz();
+		let preference1 = ['biggestNumber', 'smallestNumber'];
+		let preference2 = ['ignoreNumber', 'okayNumber'];
+		let rand1 = getSelectedPreferenceFromList(key, preference1);
+		let rand2 = getSelectedPreferenceFromList(key, preference2);
+
+		let word = ['большее', 'меньшее'][rand1];
+
+		let divisor = sl(3, 30);
+		let include = ['3', '4', '5', '6', '7', '8', '9'].iz();
 
 		let found = null;
 
-		if (rand === 0) {
+		if (rand1 === 0) {
 			//Ищем НАИБОЛЬШЕЕ
 			for (let n = 999; n >= 100; n--) {
-				if (String(n).includes(include)) continue;
+				if (rand2 === 0 && String(n).includes(include)) {
+					continue;
+				}
 				if (n % divisor === 0) {
 					found = n;
 					break;
@@ -25,7 +29,9 @@
 		} else {
 			//Ищем НАИМЕНЬШЕЕ
 			for (let n = 100; n <= 999; n++) {
-				if (String(n).includes(include)) continue;
+				if (rand2 === 0 && String(n).includes(include)) {
+					continue;
+				}
 				if (n % divisor === 0) {
 					found = n;
 					break;
@@ -33,12 +39,11 @@
 			}
 		}
 
-		genAssert(found !== null, `Не найдено трёхзначное число без цифры ${include}, делящееся на ${divisor}`);
-
 		NAtask.setTask({
-			text: 	'Найдите наи'+word+' трёхзначное число, в записи которого не используется цифра $'+include+'$ и которое нацело делится на $' +divisor+'$.',
+			text: 'Найдите наи' + word + ' трёхзначное число,' + [' в записи которого не используется цифра $' + include + '$ и', ''][rand2] +
+				' которое нацело делится на $' + divisor + '$.',
 			answers: found,
-			preference: preference,
+			preference: [preference1, preference2],
 		});
 		NAtask.modifiers.allDecimalsToStandard();
 	}, 2000);
