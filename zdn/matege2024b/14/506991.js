ca59f96ad8c94353dfda3feb68ca516936f9444f
Алендарь Сергей <sergey.alendar@mail.ru>
2026-03-08 19:45:53 +0300

    Числитель теперь всегда менбше знаменателя в дроби с целой частью

--- zdn/matege2024b/14/506991.js
+++ zdn/matege2024b/14/506991.js
@@ -1,43 +1,43 @@
 (function() {
 	retryWhileError(function() {
 		'use strict';
-		let a = sl(1, 99);
-		let b = sl(2, 99);
+		let a = sl(1, 50);
+		let b = [a + sl(1, 49), sl(1, 99)];
 		let wholeNumA = sl(1, 9);
-		let c = sl(1, 99);
-		let d = sl(2, 99);
+		let c = sl(1, 50);
+		let d = [c + sl(1, 49), sl(1, 99)];
 		let wholeNumC = sl(1, 9);
-		let e = sl(1, 99);
-		let f = sl(2, 99);
+		let e = sl(1, 50);
+		let f = [e + sl(1, 49), sl(1, 99)];
 		let wholeNumE = sl(1, 9);
 		let div = [10, 100];
 
-		genAssertIrreducible(a, b);
-		genAssertIrreducible(c, d);
-		genAssertIrreducible(e, f);
+		genAssertIrreducible(a, b[0]);
+		genAssertIrreducible(c, d[0]);
+		genAssertIrreducible(e, f[0]);
+		genAssertIrreducible(a, b[1]);
+		genAssertIrreducible(c, d[1]);
+		genAssertIrreducible(e, f[1]);
 
 		NAtask.setEvaluationTask({
 			expr: [
-				'(' + 
-					[
-						'mixed(' + wholeNumA + ',' + a + ',' + b + ')',
-						a + '/' + b,
-						sl(1, 999) / div.iz()
-					].iz() + ')' + ['+', '-'].iz() + 
-				'(' + 
-					[
-						'mixed(' + wholeNumC + ',' + c + ',' + d + ')',
-						c + '/' + d
-					].iz() + ')',
-				 ['(', 'forceBrackets(' + '-'].iz() + 
-					[
-						'mixed(' + wholeNumE + ',' + e + ',' + f + ')',
-						e + '/' + f,
-						sl(1, 999) / div.iz()
-					].iz() + ')'
-				].shuffle().join('*'),
+				'(' + [
+					'mixed(' + wholeNumA + ',' + a + ',' + b[0] + ')',
+					a + '/' + b[1],
+					sl(1, 999) / div.iz()
+				].iz() + ')' + ['+', '-'].iz() +
+				'(' + [
+					'mixed(' + wholeNumC + ',' + c + ',' + d[0] + ')',
+					c + '/' + d[1]
+				].iz() + ')', ['(', 'forceBrackets(' + '-'].iz() + [
+					'mixed(' + wholeNumE + ',' + e + ',' + f[0] + ')',
+					e + '/' + f[1],
+					sl(1, 999) / div.iz()
+				].iz() + ')'
+			].shuffle().join('*'),
 			authors: ['Алендарь Сергей'],
 		});
 	}, 100000);
 })();
 //506991
+
