b4c874ae7741abd4e44f14d40b25dd130b18193a
Колесникова Елена <luly3751@gmail.com>
2026-03-11 22:28:15 +0300

    [zdn] misc_ege1_base2026

---
+++ zdn/misc_ege1_base2026/misc_ege1_base2026.js
@@ -0,0 +1,42 @@
+if (!window.nabor)
+	window.nabor = {};
+window.nabor.importFrom({
+	nZad: 30,
+ 	adres: '../zdn/misc_ege1_base2026/',
+	name: 'misc_ege1_base2026',
+});
+
+// fast_set metadata
+// array entries:
+// - 26616
+// - 26617
+// - 26622
+// - 26625
+// - 26625
+// - 26626
+// - 26632
+// - 26635
+// - 26636
+// - 26637
+// - 77331
+// - 77332
+// - 77336
+// - 77337
+// - 77338
+// - 77339
+// - 77356
+// - 282847
+// - 282848
+// - 323510
+// - 323512
+// - 323513
+// - 323516
+// - 323609
+// - 505455
+// - 509611
+// - 512414
+// - 531736
+// - 15245634
+// - 15541873
+// forbidOpenEnds answer: n
+// forbidDecimalFractions answer: n
