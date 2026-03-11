2a1cf28470b8ea0d38c4a70394118893612029d3
Колесникова Елена <luly3751@gmail.com>
2026-03-11 19:49:34 +0300

    reverse variant

--- zdn/matoge2024/10/14422712.js
+++ zdn/matoge2024/10/14422712.js
@@ -5,11 +5,13 @@
 	let total = sl(10, 50);           
 	let heads = sl(1, total-1);         
 	let tails = total - heads;           
-	let position = sl(1, total);      
+	let position = sl(1, total);
+	let rand = sl1();
+	let reverse = ['орёл','решка'][1 - rand];      
 
 	NAtask.setTask({
-		text: 'Монету бросили ' + chislitlx(total, 'раз', '$v') + '. Известно, что орёл выпал ' + chislitlx(heads, 'раз', '$v') + '. ' +
-			'Найдите вероятность того, что при $' + position + '$-м по счёту броске выпала решка.',
+		text: 'Монету бросили ' + chislitlx(total, 'раз', 'v$') + '. Известно, что ' + ['орёл', 'решка'][rand] + ' выпал ' + chislitlx(heads, 'раз', '$v') + '. ' +
+			'Найдите вероятность того, что при $' + position + '$-м по счёту броске выпал'+ ['','а'][1 - rand] + ' ' + reverse + '.',
 		answers: tails / total,
 	});
 	NAtask.modifiers.assertSaneDecimals();
