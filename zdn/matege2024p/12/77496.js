354b0eaa09025577d9dff5e90379bea0bc939121
Алендарь Сергей <sergey.alendar@mail.ru>
2026-03-15 13:00:42 +0300

    Убран баг, из-за которого левый конец отрезка мог быть больше правого

--- zdn/matege2024p/12/77496.js
+++ zdn/matege2024p/12/77496.js
@@ -5,13 +5,15 @@
 		let b = Math.abs(a) + sl(1, 49);
 		let c = sl(1, 99);
 		let sluchSign = sl1();
+		let sluchInterval = sl1();
 		let sign = ['+', '-'];
 		let sincos = ['sin', 'cos'];
-		let interval = ['pi/6', 'pi/4', 'pi/3', 'pi/2', '2pi/3', '3pi/4', '5pi/6', '7pi/6', '5pi/4', '4pi/3', '3pi/2', '5pi/3', '7pi/4', '11pi/6'];
+		let interval = ['pi/6', 'pi/4', 'pi/3', 'pi/2', '2pi/3', '3pi/4', '5pi/6', '7pi/6', '5pi/4', '4pi/3', '3pi/2',
+			'5pi/3', '7pi/4', '11pi/6'];
 		NAtask.setMinimaxFunctionTask({
 			expr: sign[sluchSign] + a + sincos.iz() + '(x)' + sign[1 - sluchSign] + b + 'x' + sign.iz() + c,
-			leftEnd: [['', '-'].iz() + interval.iz(), '0'].iz(),
-			rightEnd: [interval.iz(), '0'].iz(),
+			leftEnd: ['-' + interval.iz(), '0'][sluchInterval],
+			rightEnd: ['0', interval.iz()][sluchInterval],
 			primaryStep: 0.001,
 			secondaryStep: 0.0001,
 			authors: ['Алендарь Сергей'],
@@ -21,3 +23,4 @@
 
 //77496
 //77497
+
