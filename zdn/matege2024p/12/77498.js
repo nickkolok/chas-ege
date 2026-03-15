1686cda2329b80e287d735e3870eed14c2d83a89
Алендарь Сергей <sergey.alendar@mail.ru>
2026-03-15 14:12:50 +0300

    Изменена генерация значений

--- zdn/matege2024p/12/77498.js
+++ zdn/matege2024p/12/77498.js
@@ -1,32 +1,28 @@
 (function() {
 	retryWhileError(function() {
 		'use strict';
-		let a1 = sl(1, 9, 0.5);
-		let a2 = a1 * sl(2, 18);
-		let a3 = slKrome(5, 2, 6);
-		let b2 = sl(1, 19);
-		let c = slKrome(5, 2, 6);
+		let a1 = sl(1, 45, 0.5);
+		let a2 = a1 * sl(1, 2);
+		let a3 = sl(2, 3);
+		let b2 = sl(1, 45);
+		let c = sl(2, 3);
 		let d = sl(1, 99);
 		let sign = ['+', '-'];
 		let sincos = ['sin', 'cos'];
-
-		genAssert(!c.isPolnKvadr(), 'Корень извлекается');
-		genAssert(!a2.isPolnKvadr(), 'Корень извлекается');
 
 		NAtask.setMinimaxFunctionTask({
 			expr: [
 				[
 					a1,
-					a2 + 'sqrt(' + a3 + ')'
-				].iz() + sincos.iz() + '(x)', 
-				[
+					a1 + 'sqrt(' + a3 + ')'
+				].iz() + sincos.iz() + '(x)', [
 					a2,
 					a2 + ['sqrt(' + c + ')', ''].iz()
 				].iz() + 'x'
 			].shuffle().join(sign.iz()) + sign.iz() + a1 + ['sqrt(' + c + ')', ''].iz() + 'pi' + [sign.iz() + d, ''].iz(),
 			leftEnd: '0',
 			rightEnd: 'pi/2',
-			primaryStep: 0.1,
+			primaryStep: 0.01,
 			secondaryStep: 0.0001,
 			authors: ['Алендарь Сергей'],
 		});
@@ -34,3 +30,4 @@
 })();
 //77498
 //77499
+
