57bcb951cb11eebe3e03a5fc1bea0393853056b1
Колесникова Елена <luly3751@gmail.com>
2026-03-16 13:44:11 +0300

    add preference

--- zdn/matege2024b/4/11607036.js
+++ zdn/matege2024b/4/11607036.js
@@ -2,6 +2,9 @@
 	'use strict';
 	retryWhileError(function () {
 		NAinfo.requireApiVersion(0, 2);
+		let key = "11607036";
+		let preference = ['findS', 'findA', 'findP', 'findN'];
+		let rand = getSelectedPreferenceFromList(key, preference);
 
 		let A = sl(10000, 100000, 500);
 		let p = sl(6, 21);
@@ -12,15 +15,22 @@
 
 		genAssert(S.isAlmostInteger(), "Итоговая сумма должна быть целой");
 
+		let knownParts = [];
+		if (rand !== 1) knownParts.push('$A = ' + A + '$');
+		if (rand !== 2) knownParts.push('$p = ' + p + '\\%$');
+		if (rand !== 3) knownParts.push('$n = ' + n + '$');
+		if (rand !== 0) knownParts.push('$S = ' + S + '$');
+		let knownText = knownParts.join(', ') + '.';
+
 		NAtask.setTask({
 			text: 'Для определения итоговой суммы вклада без возможности пополнения и частичного снятия ' +
 				'денежных средств в банках используется формула расчёта ' +
 				'$S = A \\cdot \\left(1 + \\frac{p}{100}\\right)^n$, где $A$ — первоначальная сумма вклада (в рублях), ' +
 				'$p$ — годовая ставка по вкладу (в процентах), $n$ — срок размещения вклада (в годах), ' +
 				'$S$ — итоговая сумма вклада (в рублях). ' +
-				'Найдите итоговую сумму вклада, если $A = ' + A + '$, $p = ' + p + '\\%$, $n = ' + n + '$. ' +
-				'Ответ дайте в рублях.',
-			answers: S,
+				'Найдите ' + ['итоговую сумму вклада $S$', 'первоначальную сумму вклада $A$', 'годовую процентную ставку $p$ (в %)', 'срок размещения вклада $n$ (в годах)'][rand] +
+				', если ' + knownText + ' Ответ дайте в рублях.',
+			answers: [S, A, p, n][rand],
 		});
 
 		NAtask.modifiers.allDecimalsToStandard();
