812e4afdc3dae341da244cf32080f845d6cda811
Колесникова Елена <luly3751@gmail.com>
2026-03-16 11:54:41 +0300

    add preference 317937

--- zdn/matege2024b/15/317937.js
+++ zdn/matege2024b/15/317937.js
@@ -2,9 +2,11 @@
     'use strict';
     retryWhileError(function () {
         NAinfo.requireApiVersion(0, 2);
-
-        let rand = sl1();
-        let randPartOf100 = sl(0, 5);
+        let key = '317937';
+        let preference1 = ['findFirstMeat', 'findSecondMeat'];
+        let preference2 = ['sumPart100', 'sumPart50', 'sumPart25', 'sumPart20', 'sumPart10', 'sumPart5'];
+        let rand = getSelectedPreferenceFromList(key, preference1);
+        let randPartOf100 = getSelectedPreferenceFromList(key, preference2);
         let coniferousOrDeciduous = sklonlxkand(['говядина', 'свинина', 'курица', 'индейка', 'козлина', 'конина', 'буйволятина',
             'оленина', 'верблюжатина', 'цесарка', 'треска', 'горбуша', 'минтай',].iz(2));
         let meat = [100, 50, 25, 20, 10, 5][randPartOf100];
@@ -20,6 +22,7 @@
                 ' в отношении ' + firstMeat + ':' + secondMeat + '.' +
                 ' Какой процент в фарше составляет ' + [coniferousOrDeciduous[0], coniferousOrDeciduous[1]][rand].ie + '?',
             answers: percent,
+            preference: [preference1, preference2],
         });
     }, 100);
 })();
