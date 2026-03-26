e34d7c264ad44f58b04ec0e7d3a4d464251bc3db
Колесникова Елена <luly3751@gmail.com>
2026-03-26 21:02:56 +0300

    remove while

--- zdn/matege2024b/19/506834.js
+++ zdn/matege2024b/19/506834.js
@@ -3,29 +3,21 @@
 	retryWhileError(function () {
 		NAinfo.requireApiVersion(0, 2);
 
-		let targetDiff = null;
-
 		let validNumbers = [];
-		let diff = targetDiff;
 
-		let baseN = null;
-		let attempts = 0;
-		while (attempts < 2000 && baseN === null) {
-			attempts++;
-			let first = sl(1, 9);
-			let second = sl(0, 9);
-			let third = sl(0, 9);
-			let n = first * 1000 + second * 100 + third * 10 + 5;
-			let s = String(n);
-			let revStr = s.split('').reverse().join('');
-			let rev = parseInt(revStr, 10);
-			let d = n - rev;
-			if (d > 1000 && d < 9000) {
-				baseN = n;
-				diff = d;
-			}
-		}
-		genAssert(baseN !== null, 'Не удалось сгенерировать базовое число');
+		let first = sl(1, 9);
+		let second = sl(0, 9);
+		let third = sl(0, 9);
+		let n = first * 1000 + second * 100 + third * 10 + 5;
+		let s = String(n);
+		let revStr = s.split('').reverse().join('');
+
+		genAssert(revStr[0] != '0', 'перевёрнутое число начинается с нуля');
+
+		let rev = parseInt(revStr, 10);
+		let diff = n - rev;
+
+		genAssert(diff > 1000 && diff < 9000, 'Разность вне допустимого диапазона');
 
 		for (let n = 1005; n <= 9995; n += 5) {
 			if (n % 10 !== 5) {
