76bbd97cf3870aa0b0ca706d2018c707485259b4
Aleksandra Sumatokhina <88662554+SugarHedgehog@users.noreply.github.com>
2026-03-27 11:48:36 +0300

    [zdn] [fix] - delete dubls

--- zdn/matoge2024/10/546631.js
+++ zdn/matoge2024/10/546631.js
@@ -12,14 +12,15 @@
         let probability = rest / total;
         
         let colorsGen = colors.slice(0,3).map(c => c.replace('ий', 'их').replace('ый', 'ых').replace('ой', 'ых'));
-        let colorsNomPl = colors.slice(2,5).map(c => c.replace('ий', 'ие').replace('ый', 'ые').replace('ой', 'ые'));
-        let colorsInstr = colors.slice(2,5).map(c => c.replace('ий', 'ей').replace('ый', 'ой'));
+        let colorsNomPl = colors.slice(3,5).map(c => c.replace('ий', 'ие').replace('ый', 'ые').replace('ой', 'ые'));
+        let colorsInstr = colors.slice(3,5).map(c => c.replace('ий', 'ей').replace('ый', 'ой'));
 
         genAssertZ1000(probability);
 
         NAtask.setTask({
-            text: 'В магазине канцтоваров продаётся $' + total + '$ ручек: ' + '$' + c1 + '$ ' + colorsGen[0] + ', $' + c2 + '$ ' + colorsGen[1] + ', $' + c3 + '$ ' + colorsGen[2] + ', ' + 'остальные — ' + colorsNomPl[0] + ' и ' + colorsNomPl[1] + ', их поровну' + '. ' +
-                'Найдите вероятность того, что случайно выбранная в этом магазине ручка будет ' + colorsInstr[0] + ' или ' + colorsInstr[1] + '.',
+            text: 'В магазине канцтоваров продаётся $' + total + '$ ручек: ' + '$' + c1 + '$ ' + colorsGen[0] + ', $' + c2 + '$ ' + colorsGen[1] + ', $' + c3 + '$ ' + colorsGen[2] + 
+            ', ' + 'остальные — ' + colorsNomPl[0] + ' и ' + colorsNomPl[1] + ', их поровну' + '. ' +
+                'Найдите вероятность того, что случайно выбранная в этом магазине ручка будет ' + colorsInstr.join(' или ') + '.',
             answers: probability,
         });
     }, 100);
