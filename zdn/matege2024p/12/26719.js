e6d2d01c8a6685d1e34140a1e136f6d54872052f
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2026-03-30 13:39:05 +0300

    [zdn] [update] - domain and pref

--- zdn/matege2024p/12/26719.js
+++ zdn/matege2024p/12/26719.js
@@ -1,10 +1,13 @@
 (function() {
 	retryWhileError(function() {
 		'use strict';
+		let key = "26719";
+		let preference = ['maximum', 'minimum'];
+		let rand = getSelectedPreferenceFromList(key, preference);
+		let [forbidMinY, forbidMaxY] = [1 - rand, rand].map(elem => Boolean(elem));
+		
 		let a = sl(1, 100).pm();
 		let b = sl(2, 19);
-		let l = sl(-20.5, 1, 0.5);
-		let r = sl(-20.5, 1, 0.5);
 		let arr1 = ['+', '-'];
 		let arr2 = ['-', '+'];
 		let maxmin = sl1();
@@ -13,7 +16,7 @@
 		if ( b > b1) {
 			a1 = 1;
 		}
-		genAssertIrreducible(a1, b1);
+		genAssertIrreducible(a1, b1);		
 		NAtask.setMinimaxFunctionTask({
 			expr: arr1[maxmin] + 'log(' + b + 'x)+' + arr2[maxmin] + b + 'x+' + a,
 			leftEnd: '1/' + b * 2,
@@ -21,6 +24,10 @@
 			primaryStep: 0.01,
 			secondaryStep: 0.001,
 			authors: ['Алендарь Сергей'],
+			forbidMinY,
+			forbidMaxY,
+			domain: x => x > 0,
+			preference: preference,
 		});
 	}, 1000);
 })();
