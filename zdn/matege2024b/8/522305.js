8b88866a9539edaf0e2e7e28d14489a309d7e391
Колесникова Елена <luly3751@gmail.com>
2026-04-22 22:00:07 +0300

    [zdn][new] 522305 - fix #2952

---
+++ zdn/matege2024b/8/522305.js
@@ -0,0 +1,41 @@
+(function () {
+	'use strict';
+	retryWhileError(function () {
+		NAinfo.requireApiVersion(0, 2);
+		let key = '522305';
+		let preference = ['findTrue', 'findFalse'];
+		let rand = getSelectedPreferenceFromList(key, preference);
+		let nCorrect = sl(1, 3);
+		let nWrong = 4 - nCorrect;
+
+		let total = sl(30, 50);
+		let pads = sl(3, 8);
+		let filter = sl(7, 15);
+		let minNone = total - (pads + filter);
+		let maxBoth = Math.min(pads, filter);
+
+		let correct = [
+			`Найдётся ${minNone} машин, в которых не нужно менять ни колодки, ни фильтр.`,
+			`Не найдётся ${maxBoth + 1} машин, в которых нужно менять и колодки, и фильтр.`,
+			`Максимум ${maxBoth} машин требуют замены обоих деталей.`,
+			`Минимум ${minNone} машин не требуют никакой замены.`
+		];
+		let wrong = [
+			`Найдётся ${maxBoth + 1} машин, в которых нужно поменять и колодки, и фильтр.`,
+			`Если в машине нужно менять колодки, то фильтр тоже нужно менять.`,
+			`Все машины требуют хотя бы одной замены.`,
+			`Колодки и фильтр всегда меняют вместе.`
+		];
+
+		NAtask.setTask({
+			text: `Диагностика ${total} машин в автосервисе показала, что у ${pads} машин нужно заменить тормозные колодки, а у ${filter} машин — заменить воздушный фильтр (колодки и фильтр требуют замены независимо друг от друга). ` +
+				`Выберите утверждения, которые ` + (rand ? 'неверны' : 'верны') + ` при указанных условиях вне зависимости от того, какие машины нуждаются в замене фильтра, а какие — в замене колодок. В ответе запишите номера выбранных утверждений без пробелов, запятых и` +
+				` других дополнительных символов. Если ответов несколько, записывайте их номера в порядке возрастания.`,
+			answers: rand ? wrong : correct,
+			wrongAnswers: rand ? correct : wrong,
+			preference: preference,
+		});
+		AtoB2(nCorrect, nWrong);
+	}, 1000);
+})();
+// https://mathb-ege.sdamgia.ru/problem?id=522305
--- zdn/matege2024b/8/main.js
+++ zdn/matege2024b/8/main.js
@@ -1,4 +1,5 @@
 window.nomer=[
 	514517,
+	522305,
 ].iz()
 window.comment='Анализ утверждений';
