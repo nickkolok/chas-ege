0883a1f73187f309ae608220575dc221efc3fd47
Колесникова Елена <luly3751@gmail.com>
2026-04-22 20:05:23 +0300

    [zdn][new] 511762 - fix #2927

---
+++ zdn/matege2024b/8/511762.js
@@ -0,0 +1,44 @@
+(function () {
+	'use strict';
+	retryWhileError(function () {
+		NAinfo.requireApiVersion(0, 2);
+		let key = '511762';
+		let preference = ['findTrue', 'findFalse'];
+		let rand = getSelectedPreferenceFromList(key, preference);
+		let nCorrect = sl(1, 3);
+		let nWrong = 4 - nCorrect;
+
+		let colors = ['красн', 'зелен', 'фиолетов', 'черн', 'бел', 'жёлт'].iz(3);
+		let name = sklonlxkand(om.femaleNames.iz());
+		let count1 = sl(5, 15);
+		let count2 = sl(6, 15);
+		let total = count1 + count2;
+		let drawn = sl(2, Math.min(6, total));
+
+		let correct = [
+			'Найдётся $' + Math.max(1, count2 - drawn) + '$ ' + colors[1] + 'ых шарика без рисунков.',
+			'Не найдётся $' + (drawn + sl(1, 5)) + '$ ' + colors[1] + 'ых шариков с рисунками.',
+			'Минимум $' + Math.max(0, drawn - count1) + '$ ' + colors[1] + ' шарика с рисунками.',
+			'Всегда найдётся хотя бы один ' + colors[0] + 'ый шарик без рисунка, если $' + count1 + '$>$' + drawn + '$.'
+		];
+
+		let wrong = [
+			'Если шарик ' + colors[0] + 'ый, то на нём есть рисунок.',
+			'Найдётся ' + drawn + ' ' + colors[0] + 'ых шарика с рисунками.',
+			'Все нарисованные шарики — ' + colors[0] + 'ые.',
+			'Нет ни одного ' + colors[1] + 'ого шарика с рисунком.'
+		];
+
+		NAtask.setTask({
+			text: name.de + ' на день рождения подарили $' + total + '$ шариков, из них $' + count1 + '$ ' + colors[0] + 'ые, а остальные — ' + colors[1] + 'ые. ' + name.ie +
+				' на $' + drawn + '$ случайных шариках нарисовала рисунки маркером, чтобы подарить маме, папе, брату и сестре. Выберите все утверждения, которые будут ' + (rand ? 'неверны' : 'верны') +
+				' при указанных условиях независимо от того, на каких шариках ' + name.ie + ' нарисовала рисунки. В ответе запишите номера выбранных утверждений без пробелов, запятых и других дополнительных символов.' +
+				' Если ответов несколько, записывайте их номера в порядке возрастания.',
+			answers: rand ? wrong : correct,
+			wrongAnswers: rand ? correct : wrong,
+			preference: preference,
+		});
+		AtoB2(nCorrect, nWrong);
+	}, 1000);
+})();
+// https://mathb-ege.sdamgia.ru/problem?id=511762
--- zdn/matege2024b/8/main.js
+++ zdn/matege2024b/8/main.js
@@ -1,4 +1,5 @@
 window.nomer=[
+	511762,
 	514517,
 ].iz()
 window.comment='Анализ утверждений';
