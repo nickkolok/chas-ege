89c1059293ed5d42516ea6764fc766f6bb342bc9
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2026-04-29 16:27:07 +0300

    [zdn] [up] - more variation

--- zdn/matoge2024/10/546631.js
+++ zdn/matoge2024/10/546631.js
@@ -3,17 +3,17 @@
     retryWhileError(function () {
 
         let colors = om.trickyColors.iz(5);
-        let c1 = sl(20, 40);
-        let c2 = slKrome(c1, 20, 40);
-        let c3 = slKrome(c1, slKrome(c2, 10, 30));
-        let rest = sl(40, 100, 2);
-        let total = c1 + c2 + c3 + rest;
-        
-        let probability = rest / total;
-        
+        let [c1, c2, c3, c4] = arrayOfUniqueValues(4, 20, 40);
+        let total = c1 + c2 + c3 + c4*2;
+                
+        let numbers = [c1, c2, c3, c4, c4];
+       
         let colorsGen = colors.slice(0,3).map(c => c.replace('ий', 'их').replace('ый', 'ых').replace('ой', 'ых'));
         let colorsNomPl = colors.slice(3,5).map(c => c.replace('ий', 'ие').replace('ый', 'ые').replace('ой', 'ые'));
-        let colorsInstr = colors.slice(3,5).map(c => c.replace('ий', 'ей').replace('ый', 'ой'));
+        
+        let randNumbers = arrayOfUniqueValues(2, 0, colors.length - 1);
+        let colorsInstr = [colors[randNumbers[0]], colors[randNumbers[1]]].map(c => c.replace('ий', 'ей').replace('ый', 'ой'));
+        let probability = (numbers[randNumbers[0]]+numbers[randNumbers[1]]) / total;
 
         genAssertZ1000(probability);
 
@@ -23,7 +23,7 @@
                 'Найдите вероятность того, что случайно выбранная в этом магазине ручка будет ' + colorsInstr.join(' или ') + '.',
             answers: probability,
         });
-    }, 100);
+    }, 1000);
 })();
 //546631
 //Открытый банк заданий 546631
