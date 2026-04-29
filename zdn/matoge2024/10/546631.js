c7064d1b321b1715b8bc19e434b7a66dfc6afce2
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2026-04-29 16:29:23 +0300

    [zdn] [up] - склонение

--- zdn/matoge2024/10/546631.js
+++ zdn/matoge2024/10/546631.js
@@ -3,7 +3,7 @@
     retryWhileError(function () {
 
         let colors = om.trickyColors.iz(5);
-        let [c1, c2, c3, c4] = arrayOfUniqueValues(4, 20, 40);
+        let [c1, c2, c3, c4] = arrayOfUniqueValues(4, 20, 40, 2);
         let total = c1 + c2 + c3 + c4*2;
                 
         let numbers = [c1, c2, c3, c4, c4];
@@ -18,7 +18,7 @@
         genAssertZ1000(probability);
 
         NAtask.setTask({
-            text: 'В магазине канцтоваров продаётся $' + total + '$ ручек: ' + '$' + c1 + '$ ' + colorsGen[0] + ', $' + c2 + '$ ' + colorsGen[1] + ', $' + c3 + '$ ' + colorsGen[2] + 
+            text: 'В магазине канцтоваров продаётся ' + chislitlx(total, 'ручка', '$') + ' ручек: ' + '$' + c1 + '$ ' + colorsGen[0] + ', $' + c2 + '$ ' + colorsGen[1] + ', $' + c3 + '$ ' + colorsGen[2] + 
             ', ' + 'остальные — ' + colorsNomPl[0] + ' и ' + colorsNomPl[1] + ', их поровну' + '. ' +
                 'Найдите вероятность того, что случайно выбранная в этом магазине ручка будет ' + colorsInstr.join(' или ') + '.',
             answers: probability,
