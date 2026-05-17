ef6627a0e06387c3fdab4315ffbd128054f8c6b6
Колесникова Елена <luly3751@gmail.com>
2026-05-17 18:39:42 +0300

    add coordAxis_drawAuto

--- zdn/matoge2024/7/106.js
+++ zdn/matoge2024/7/106.js
@@ -22,32 +22,28 @@
 		let correctLetter = ['A', 'B', 'C', 'D'][idx];
 
 		let paint = function (ct) {
-			let points = [];
-
-			for (let i = 0; i <= 2; i++) {
-				points.push({
-					value: leftEdge + i,
-					label: leftEdge + i,
-					mark: 'line',
-					labelPos: 'underAxis'
-				});
-			}
-
-			// Точки A–D
-			for (let i = 0; i < 4; i++) {
-				points.push({
-					value: positions[i],
-					label: ['A', 'B', 'C', 'D'][i],
-					mark: 'dot',
-					labelPos: 'overAxis'
-				});
-			}
-
-			coordAxis_drawAuto(ct, { points });
+			coordAxis_drawAuto(ct, {
+				min: leftEdge,
+				max: leftEdge + 2,
+				points: [
+					// Засечки на целых числах
+					{ value: leftEdge, mark: "line", label: leftEdge.toString(), labelPos: "underAxis" },
+					{ value: leftEdge + 1, mark: "line", label: (leftEdge + 1).toString(), labelPos: "underAxis" },
+					{ value: leftEdge + 2, mark: "line", label: (leftEdge + 2).toString(), labelPos: "underAxis" },
+					// Точки A–D
+					{ value: positions[0], mark: "dot", label: "A", labelPos: "overAxis" },
+					{ value: positions[1], mark: "dot", label: "B", labelPos: "overAxis" },
+					{ value: positions[2], mark: "dot", label: "C", labelPos: "overAxis" },
+					{ value: positions[3], mark: "dot", label: "D", labelPos: "overAxis" }
+				],
+				width: 400,
+				height: 100,
+				margin: 20
+			});
 		};
 
 		NAtask.setTask({
-			text: 'На координатной прямой отмечены точки A, B, C, D. Одна из них соответствует числу ' + '$' + correctLatex + '$' + '. Какая это точка?',
+			text: 'На координатной прямой отмечены точки $A$, $B$, $C$, $D$. Одна из них соответствует числу $' + correctLatex + '$. Какая это точка?',
 			answers: correctLetter,
 			wrongAnswers: ['A', 'B', 'C', 'D'].filter(x => x !== correctLetter)
 		});
@@ -57,7 +53,9 @@
 		chas2.task.modifiers.addCanvasIllustration({
 			width: 400,
 			height: 100,
-			paint
+			paint: paint
 		});
+
+		NAtask.modifiers.allDecimalsToStandard();
 	}, 1000);
 })();
