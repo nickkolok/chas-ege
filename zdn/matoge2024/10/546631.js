387c269784d565c09ef17475da9e1e9c1cceb7e8
Zer00Player <luly3751@gmail.com>
2026-05-29 14:39:55 +0300

    add item

--- zdn/matoge2024/10/546631.js
+++ zdn/matoge2024/10/546631.js
@@ -11,6 +11,7 @@
         let colorsGen = colors.slice(0,3).map(c => c.replace('ий', 'их').replace('ый', 'ых').replace('ой', 'ых'));
         let colorsNomPl = colors.slice(3,5).map(c => c.replace('ий', 'ие').replace('ый', 'ые').replace('ой', 'ые'));
         
+        let item = sklonlxkand(['ручка', 'тетрадь', 'линейка', 'папка', 'скрепка','кисть','обложка', 'закладка', 'точилка'].iz());
         let randNumbers = arrayOfUniqueValues(2, 0, colors.length - 1);
         let colorsInstr = [colors[randNumbers[0]], colors[randNumbers[1]]].map(c => c.replace('ий', 'ей').replace('ый', 'ой'));
         let probability = (numbers[randNumbers[0]]+numbers[randNumbers[1]]) / total;
@@ -18,9 +19,9 @@
         genAssertZ1000(probability);
 
         NAtask.setTask({
-            text: 'В магазине канцтоваров продаётся ' + chislitlx(total, 'ручка', '$') + ': ' + '$' + c1 + '$ ' + colorsGen[0] + ', $' + c2 + '$ ' + colorsGen[1] + ', $' + c3 + '$ ' + colorsGen[2] + 
+            text: 'В магазине канцтоваров продаётся ' + chislitlx(total, item, '$') + ': ' + '$' + c1 + '$ ' + colorsGen[0] + ', $' + c2 + '$ ' + colorsGen[1] + ', $' + c3 + '$ ' + colorsGen[2] + 
             ', ' + 'остальные — ' + colorsNomPl[0] + ' и ' + colorsNomPl[1] + ', их поровну' + '. ' +
-                'Найдите вероятность того, что случайно выбранная в этом магазине ручка будет ' + colorsInstr.join(' или ') + '.',
+                'Найдите вероятность того, что случайно выбранная в этом магазине ' + item.ie + ' будет ' + colorsInstr.join(' или ') + '.',
             answers: probability,
         });
     }, 1000);
