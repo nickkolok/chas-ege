7d9efd81768029ad42b801b7460f1e754d928633
nickkolok <nickkolok@mail.ru>
2026-07-23 20:31:24 +0300

    [zdn] 506991 - fix forceBrackets
    https://chat.qwen.ai/s/t_c51d30eb-fb51-43ab-bf9d-28726881f563?fev=0.2.78

--- zdn/matege2024b/14/506991.js
+++ zdn/matege2024b/14/506991.js
@@ -1,43 +1,67 @@
-(function() {
-	retryWhileError(function() {
-		'use strict';
-		let a = sl(1, 50);
-		let b = [a + sl(1, 49), sl(1, 99)];
-		let wholeNumA = sl(1, 9);
-		let c = sl(1, 50);
-		let d = [c + sl(1, 49), sl(1, 99)];
-		let wholeNumC = sl(1, 9);
-		let e = sl(1, 50);
-		let f = [e + sl(1, 49), sl(1, 99)];
-		let wholeNumE = sl(1, 9);
-		let div = [10, 100];
+(function(){
+    retryWhileError(function(){
+        'use strict';
+        let a=sl(1,50);
+        let b=[a+sl(1,49),sl(1,99)];
+        let wholeNumA=sl(1,9);
+        let c=sl(1,50);
+        let d=[c+sl(1,49),sl(1,99)];
+        let wholeNumC=sl(1,9);
+        let e=sl(1,50);
+        let f=[e+sl(1,49),sl(1,99)];
+        let wholeNumE=sl(1,9);
+        let div=[10,100];
 
-		genAssertIrreducible(a, b[0]);
-		genAssertIrreducible(c, d[0]);
-		genAssertIrreducible(e, f[0]);
-		genAssertIrreducible(a, b[1]);
-		genAssertIrreducible(c, d[1]);
-		genAssertIrreducible(e, f[1]);
+        genAssertIrreducible(a,b[0]);
+        genAssertIrreducible(c,d[0]);
+        genAssertIrreducible(e,f[0]);
+        genAssertIrreducible(a,b[1]);
+        genAssertIrreducible(c,d[1]);
+        genAssertIrreducible(e,f[1]);
 
-		NAtask.setEvaluationTask({
-			expr: [
-				'(' + [
-					'mixed(' + wholeNumA + ',' + a + ',' + b[0] + ')',
-					a + '/' + b[1],
-					sl(1, 999) / div.iz()
-				].iz() + ')' + ['+', '-'].iz() +
-				'(' + [
-					'mixed(' + wholeNumC + ',' + c + ',' + d[0] + ')',
-					c + '/' + d[1]
-				].iz() + ')', ['(', 'forceBrackets(' + '-'].iz() + [
-					'mixed(' + wholeNumE + ',' + e + ',' + f[0] + ')',
-					e + '/' + f[1],
-					sl(1, 999) / div.iz()
-				].iz() + ')'
-			].shuffle().join('*'),
-			authors: ['Алендарь Сергей'],
-		});
-	}, 100000);
+        // Создаем три части выражения
+        let part1 = '('+[
+            'mixed('+wholeNumA+','+a+','+b[0]+')',
+            a+'/'+b[1],
+            sl(1,999)/div.iz()
+        ].iz()+')'+['+','-'].iz()+
+        '('+[
+            'mixed('+wholeNumC+','+c+','+d[0]+')',
+            c+'/'+d[1]
+        ].iz()+')';
+        
+        let part2 = '('+[
+            'mixed('+wholeNumE+','+e+','+f[0]+')',
+            e+'/'+f[1],
+            sl(1,999)/div.iz()
+        ].iz()+')';
+        
+        let part3 = '(-'+[
+            'mixed('+wholeNumE+','+e+','+f[0]+')',
+            e+'/'+f[1],
+            sl(1,999)/div.iz()
+        ].iz()+')';
+
+        // Случайно выбираем знак для третьего множителя
+        let partWithSign = [part2, part3].iz();
+        
+        // Перемешиваем части
+        let parts = [part1, partWithSign].shuffle();
+        
+        // Для первого множителя всегда обычные скобки
+        // Для второго и третьего - forceBrackets добавит скобки только если отрицательное
+        for (let i = 1; i < parts.length; i++) {
+            if (parts[i].indexOf('(-') === 0) {
+                // Отрицательное число - оборачиваем в forceBrackets
+                parts[i] = 'forceBrackets' + parts[i];
+            }
+            // Если положительное - оставляем как есть, без forceBrackets
+        }
+        
+        NAtask.setEvaluationTask({
+            expr: parts.join('*'),
+            authors: ['Алендарь Сергей'],
+        });
+    },100000);
 })();
 //506991
-
