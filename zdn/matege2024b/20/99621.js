c1fe978f3cdb4538f92ce5b7a75d0b8c09359299
NickKolok <nickkolok@mail.ru>
2026-07-24 22:35:17 +0000

    [zdn][fix]-99621 Убрать toZagl(): имена уже с заглавной

--- zdn/matege2024b/20/99621.js
+++ zdn/matege2024b/20/99621.js
@@ -7,7 +7,7 @@
         let rebenoka=sklonlxkand(om.childMaleNames.iz());
 	genAssert(v.isAlmostInteger(),'Количество вопросов не может быть дробным');
 	NAtask.setTask({
-		text: rebenok.ie.toZagl()+' и '+rebenoka.ie+' выполняют одинаковый тест. '+rebenok.ie+' отвечает за час на '+chislitlx(a, 'вопрос')+' теста, а '+rebenoka.ie+' - на '+
+		text: rebenok.ie+' и '+rebenoka.ie+' выполняют одинаковый тест. '+rebenok.ie+' отвечает за час на '+chislitlx(a, 'вопрос')+' теста, а '+rebenoka.ie+' - на '+
 		b+'. Они одновременно начали отвечать на вопросы теста, и '+rebenok.ie+' закончил свой тест позже '+rebenoka.ie+' на '+chislitlx(c,'минута')+'. Сколько вопросов содержит тест?',
 		answers: v,
 		authors: ['VeronikaKit'],
