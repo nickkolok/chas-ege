450b772e17ce6c244bd4866ea950a245bec9301d
NickKolok <nickkolok@mail.ru>
2026-07-24 22:35:16 +0000

    [zdn][fix]-99621 Исправить опечатку: Колличество → Количество

--- zdn/matege2024b/20/99621.js
+++ zdn/matege2024b/20/99621.js
@@ -5,7 +5,7 @@
         let v=(a*b*c)/(60*(b-a));
         let rebenok=sklonlxkand(om.childMaleNames.iz());
         let rebenoka=sklonlxkand(om.childMaleNames.iz());
-	genAssert(v.isAlmostInteger(),'Колличество вопросов не может быть дробным');
+	genAssert(v.isAlmostInteger(),'Количество вопросов не может быть дробным');
 	NAtask.setTask({
 		text: rebenok.ie.toZagl()+' и '+rebenoka.ie+' выполняют одинаковый тест. '+rebenok.ie+' отвечает за час на '+chislitlx(a, 'вопрос')+' теста, а '+rebenoka.ie+' - на '+
 		b+'. Они одновременно начали отвечать на вопросы теста, и '+rebenok.ie+' закончил свой тест позже '+rebenoka.ie+' на '+chislitlx(c,'минута')+'. Сколько вопросов содержит тест?',
