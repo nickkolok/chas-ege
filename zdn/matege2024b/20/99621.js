c8ff9737b69f61c8ee72c14e2e8c41c602dc8cf8
NickKolok <nickkolok@mail.ru>
2026-07-24 23:29:19 +0000

    [zdn][style]-99621 Привести отступы к единому стилю (табы)

--- zdn/matege2024b/20/99621.js
+++ zdn/matege2024b/20/99621.js
@@ -2,10 +2,10 @@
 	let a=sl(1, 50, 1);
 	let b=sl(a+1, 50, 1);
 	let c=sl(1, 59, 1);
-        let v=(a*b*c)/(60*(b-a));
- let [name1,name2]=om.childMaleNames.iz(2);
- let rebenok=sklonlxkand(name1);
-        let rebenoka=sklonlxkand(name2);
+	let v=(a*b*c)/(60*(b-a));
+	let [name1,name2]=om.childMaleNames.iz(2);
+	let rebenok=sklonlxkand(name1);
+	let rebenoka=sklonlxkand(name2);
 	genAssert(v.isAlmostInteger(),'Количество вопросов не может быть дробным');
 	NAtask.setTask({
 		text: rebenok.ie+' и '+rebenoka.ie+' выполняют одинаковый тест. '+rebenok.ie+' отвечает за час на '+chislitlx(a, 'вопрос')+' теста, а '+rebenoka.ie+' - на '+
