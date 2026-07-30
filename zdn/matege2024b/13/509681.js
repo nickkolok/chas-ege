d3645c5c13ec3b9fe42f3290da132b16407fd5be
Надежда <nadezhda@example.com>
2026-07-30 10:13:44 +0000

    [zdn][fix] 509681 - чертёж двух шаров (modifiers.addCanvasIllustration)

--- zdn/matege2024b/13/509681.js
+++ zdn/matege2024b/13/509681.js
@@ -16,13 +16,49 @@
 
 		let answer = Math.pow(k, stepen);
 
-		let kto = ['меньшего', 'другого'].iz();
+		// Иллюстрация декоративная (как в прототипах): два шара фиксированного
+		// размера, контур + эллипс-экватор + пунктирный радиус в плоскости экватора.
+		let paint = function(ctx) {
+			let h = 400;
+			let w = 400;
+			ctx.translate(h / 2, w / 2);
+			ctx.lineWidth = 2;
+			ctx.strokeStyle = om.secondaryBrandColors;
+
+			let drawSphere = function(cx, cy, R) {
+				ctx.beginPath();
+				ctx.arc(cx, cy, R, 0, 2 * Math.PI);
+				ctx.stroke();
+
+				ctx.beginPath();
+				ctx.ellipse(cx, cy, R, R * 0.28, 0, 0, 2 * Math.PI);
+				ctx.stroke();
+
+				ctx.setLineDash([5, 4]);
+				ctx.beginPath();
+				ctx.moveTo(cx, cy);
+				ctx.lineTo(cx + 0.66 * R, cy + 0.13 * R);
+				ctx.stroke();
+				ctx.setLineDash([]);
+			};
+
+			drawSphere(-70, 0, 70);
+			drawSphere(70, 0, 28);
+		};
 
 		NAtask.setTask({
-			text: 'Даны два шара с радиусами ' + rBig + ' и ' + rSmall + '. Во сколько раз ' + objectIm + ' большего шара больше ' + objectRod + ' ' + kto + '?',
+			text: 'Даны два шара с радиусами ' + rBig + ' и ' + rSmall + '. Во сколько раз ' + objectIm + ' большего шара больше ' + objectRod + ' меньшего?',
 			answers: answer,
-			analys: 'Радиус большего шара равен $' + rBig + '$, радиус меньшего — $' + rSmall + '$. ' + (isVolume ? 'Объёмы шаров относятся как кубы отношений радиусов: $' + k + '^3 = ' + answer + '$.' : 'Площади поверхностей шаров относятся как квадраты отношений радиусов: $' + k + '^2 = ' + answer + '$.'),
+			analys: 'Обозначим радиусы $R = ' + rBig + '$ и $r = ' + rSmall + '$, тогда $\\frac{R}{r} = ' + k + '$. ' + (isVolume ? 'Объёмы шаров относятся как кубы радиусов: $\\left(\\frac{R}{r}\\right)^3 = ' + k + '^3 = ' + answer + '$.' : 'Площади поверхностей шаров относятся как квадраты радиусов: $\\left(\\frac{R}{r}\\right)^2 = ' + k + '^2 = ' + answer + '$.'),
 			authors: ['Надежда'],
+		});
+
+		NAtask.modifiers.addCanvasIllustration({
+			width: 400,
+			height: 400,
+			paint: paint,
 		});
 	}, 1000);
 })();
+//509681
+/* СдамГИА: 506288 506499 506642 506704 509681 509721 509761 512593 512613 512634 512654 514749 514769 514816 514890 520727 530503 */
