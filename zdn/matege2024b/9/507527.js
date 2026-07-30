052ae27cab8f573b30d2a27ce5820586259f0cbf
Надежда <nadezhda@example.com>
2026-07-30 21:52:27 +0000

    [zdn][fix] 507527 - add cellSize support

--- zdn/matege2024b/9/507527.js
+++ zdn/matege2024b/9/507527.js
@@ -1,6 +1,12 @@
 (function () {
     retryWhileError(function () {
         NAinfo.requireApiVersion(0, 2);
+
+        let key = "507527";
+        let preference = ['cellSizeIsOne', 'cellSizeIsNotOne'];
+        let rand = getSelectedPreferenceFromList(key, preference);
+
+        let cellSize = [1, sl(2, 10)][rand];
         let a = sl(1, 6);
         let b = slKrome(a, 1, 6);
         let points = [
@@ -42,9 +48,10 @@
         };
 
         NAtask.setTask({
-            text: `План местности разбит на клетки. Каждая клетка обозначает квадрат $1 \\text{ м} \\times 1 \\text{ м}$. Найдите площадь участка, выделенного на плане. Ответ дайте в квадратных метрах.`,
-            answers: rhomb.area(),
+            text: `План местности разбит на клетки. Каждая клетка обозначает квадрат $${cellSize} \\text{ м} \\times ${cellSize} \\text{ м}$. Найдите площадь участка, выделенного на плане. Ответ дайте в квадратных метрах.`,
+            answers: rhomb.area() * cellSize.pow(2),
             authors: ['Александра Суматохина'],
+            preference,
         });
 
         NAtask.modifiers.allDecimalsToStandard( /*true*/);
