3f32cff997f5b417927a291be799c62b9bdceb99
Полина <polly-the-reviewer@example.com>
2026-08-01 22:19:06 +0000

    [zdn][fix] Округление угла: ceil заменён на round

--- zdn/matoge2024/17/356873.js
+++ zdn/matoge2024/17/356873.js
@@ -33,8 +33,8 @@
         };
 
         NAtask.setTask({
-            text: `Один из углов прямоугольной трапеции равен $${trp.angleCInDegrees.ceil()}^\\circ$. Найдите ${[`больший`, `меньший`][Number(trp.angleBInDegrees < trp.angleCInDegrees)]} угол этой трапеции. Ответ дайте в градусах.`,
-            answers: 180 - trp.angleCInDegrees.ceil(),
+            text: `Один из углов прямоугольной трапеции равен $${trp.angleCInDegrees.round()}^\\circ$. Найдите ${[`больший`, `меньший`][Number(trp.angleBInDegrees < trp.angleCInDegrees)]} угол этой трапеции. Ответ дайте в градусах.`,
+            answers: 180 - trp.angleCInDegrees.round(),
             authors: ['Александра Суматохина'],
         });
         NAtask.modifiers.allDecimalsToStandard(true);
