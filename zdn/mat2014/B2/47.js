5f77ffcd136bc53de7306fad49bebee2c6378d58
chas-ege-polina <chas-ege-polina@outlook.com>
2026-08-14 13:24:28 +0300

    [zdn][fix] Использован slKrome для исключения значений, где tg/ctg не определены

--- zdn/mat2014/B2/47.js
+++ zdn/mat2014/B2/47.js
@@ -1,7 +1,11 @@
 	(function() {
 		NAinfo.requireApiVersion(0, 2);
 		var trig = ['sin', 'cos', 'tg', 'ctg'].iz();
-		var gradus = sluchch(-900, 900);
+		var gradus = slKrome(function(x){
+			if (trig == 'tg' && Math.abs(x % 180) == 90) return true;
+			if (trig == 'ctg' && x % 180 == 0) return true;
+			return false;
+		}, -900, 900);
 		var otvet = (gradus / 180 * Math.PI)[trig]()['arc' + trig]();
 		NAtask.setTask({
 		text: 'Найти значение угла (в градусах)' + '  $ \\arc' + trig + '(\\' + trig + '(' + gradus + '^{\\circ}))$',
