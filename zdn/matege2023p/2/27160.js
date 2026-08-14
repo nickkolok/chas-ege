732de01ee734a8cd2edf8faf6e87ded166281a64
chas-ege-polina <chas-ege-polina@outlook.com>
2026-08-14 15:47:52 +0300

    [zdn][fix] 27160 - унифицирую чертёж и добавляю цвета

--- zdn/matege2023p/2/27160.js
+++ zdn/matege2023p/2/27160.js
@@ -14,22 +14,34 @@
 
 		let paint1 = function(ctx) {
 			ctx.lineWidth = 2;
-			//образующие
-			ctx.drawLine(60, 180, 150, 10);
-			ctx.drawLine(240, 180, 150, 10);
-			//эллипс
-			ctx.ellipse(150, 180, 20, 90, Math.PI / 2, 1.5 * Math.PI, Math.PI / 2);
+			
+			//эллипс (передняя часть)
+			ctx.beginPath();
+			ctx.ellipse(200, 280, 120, 36, 0, 0, Math.PI);
 			ctx.stroke();
 
+			//эллипс (задняя часть, пунктиром)
 			ctx.beginPath();
 			ctx.setLineDash([5, 5]);
-			ctx.ellipse(150, 180, 20, 90, Math.PI / 2, Math.PI / 2, 1.5 * Math.PI);
+			ctx.ellipse(200, 280, 120, 36, 0, Math.PI, 2 * Math.PI);
 			ctx.stroke();
+			ctx.setLineDash([]);
 
-			//радиус
-			ctx.drawLine(150, 180, 240, 180);
-			//высота
-			ctx.drawLine(150, 180, 150, 10);
+			//образующие
+			ctx.drawLine(80, 280, 200, 72);
+			ctx.drawLine(320, 280, 200, 72);
+
+			//радиус (синий)
+			ctx.strokeStyle = "blue";
+			ctx.drawLine(200, 280, 320, 280);
+			
+			//высота (красная, пунктирная)
+			ctx.strokeStyle = "red";
+			ctx.setLineDash([5, 5]);
+			ctx.drawLine(200, 280, 200, 72);
+			ctx.setLineDash([]);
+			
+			ctx.strokeStyle = "black";
 		};
 
 		NAinfo.requireApiVersion(0, 2);
@@ -43,8 +55,8 @@
 			analys: '',
 		});
 		chas2.task.modifiers.addCanvasIllustration({
-			width: 300,
-			height: 300,
+			width: 400,
+			height: 400,
 			paint: paint1,
 		});
 	}, 1000);
