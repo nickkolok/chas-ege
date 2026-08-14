3c9a08b9b9a7ce5dd7e46aa7155a4f861b265d83
chas-ege-polina <chas-ege-polina@outlook.com>
2026-08-14 13:31:08 +0300

    [zdn][fix] Убрана двойная IIFE и дублирующийся requireApiVersion

--- zdn/mat2014/B2/47.js
+++ zdn/mat2014/B2/47.js
@@ -1,17 +1,10 @@
 	(function() {
 		NAinfo.requireApiVersion(0, 2);
-		(function() {
-     
-			NAinfo.requireApiVersion(0, 2);
-			var trig = ['sin', 'cos', 'tg', 'ctg'].iz();
-			var gradus = sluchch(-900, 900);
-			var otvet = (gradus / 180 * Math.PI)[trig]()['arc' + trig]();
-			NAtask.setTask({
-			text: 'Найти значение угла (в градусах)' + '  $ \arc' + trig + '(\' + trig + '(' + gradus + '^{\circ}))$',
-			answers: otvet / Math.PI * 180,
-			});
-     
-		})();
-     
+		var trig = ['sin', 'cos', 'tg', 'ctg'].iz();
+		var gradus = sluchch(-900, 900);
+		var otvet = (gradus / 180 * Math.PI)[trig]()['arc' + trig]();
+		NAtask.setTask({
+		text: 'Найти значение угла (в градусах)' + '  $ \\arc' + trig + '(\\' + trig + '(' + gradus + '^{\\circ}))$',
+		answers: otvet / Math.PI * 180,
+		});
 	})();
-
