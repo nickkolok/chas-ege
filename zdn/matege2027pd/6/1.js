c7ee51450f70222a5b22d4f79981ea0f50b59b44
chas-ege-nadezhda <chas-ege-nadezhda@outlook.com>
2026-08-17 12:12:43 +0300

    [zdn][up] matege2027pd 6 1: тираж от 5 000 до 100 000

--- zdn/matege2027pd/6/1.js
+++ zdn/matege2027pd/6/1.js
@@ -21,10 +21,13 @@
 	let c3 = sluchch(2, 20, prizes.divs[3]);
 	let c4 = sluchch(1, 8, prizes.divs[4]);
 
-	let s = (prizes.weights[0]*c0 + prizes.weights[1]*c1 + prizes.weights[2]*c2 + prizes.weights[3]*c3 + prizes.weights[4]*c4) / 10000;
+	let tickets = sluchch(5000, 100000, 1000);
+	let s = (prizes.weights[0]*c0 + prizes.weights[1]*c1 + prizes.weights[2]*c2 + prizes.weights[3]*c3 + prizes.weights[4]*c4) / tickets;
+	genAssertZ1000(s, 'Ответ должен быть целым числом');
 
 	let pStr = p.map(x => x.toLocaleString('ru-RU'));
 	let cStr = [c0, c1, c2, c3, c4].map(x => x.toLocaleString('ru-RU'));
+	let tStr = tickets.toLocaleString('ru-RU');
 
 	let y = ['Выигрыш (в рублях)'].concat(pStr).tr('th');
 	let z = [['Число выигрышных билетов'].concat(cStr).tr()];
@@ -45,11 +48,11 @@
 	}
 
 	let analys = `Математическое ожидание случайной величины равно сумме произведений её возможных значений на вероятности этих значений.<br/>` +
-		`Вероятность каждого выигрыша равна отношению числа соответствующих билетов к общему количеству билетов (10 000).<br/>` +
-		`$$M(X) = ${pStr[0]} \\cdot \\frac{${cStr[0]}}{10000} + ${pStr[1]} \\cdot \\frac{${cStr[1]}}{10000} + ${pStr[2]} \\cdot \\frac{${cStr[2]}}{10000} + ${pStr[3]} \\cdot \\frac{${cStr[3]}}{10000} + ${pStr[4]} \\cdot \\frac{${cStr[4]}}{10000} = ${s}$$`;
+		`Вероятность каждого выигрыша равна отношению числа соответствующих билетов к общему количеству билетов (${tStr}).<br/>` +
+		`$$M(X) = ${pStr[0]} \cdot \frac{${cStr[0]}}{${tStr}} + ${pStr[1]} \cdot \frac{${cStr[1]}}{${tStr}} + ${pStr[2]} \cdot \frac{${cStr[2]}}{${tStr}} + ${pStr[3]} \cdot \frac{${cStr[3]}}{${tStr}} + ${pStr[4]} \cdot \frac{${cStr[4]}}{${tStr}} = ${s}$$`;
 
 	NAtask.setTask({
-		text: `Организаторы лотереи выпустили 10 000 билетов. Выигрыши распределены следующим образом:<br/><br/>${condition}Найдите математическое ожидание величины «выигрыш на один билет». Ответ дайте в рублях.`,
+		text: `Организаторы лотереи выпустили ${tStr} билетов. Выигрыши распределены следующим образом:<br/><br/>${condition}Найдите математическое ожидание величины «выигрыш на один билет». Ответ дайте в рублях.`,
 		answers: s,
 		analys: analys,
 		preference: [preference],
