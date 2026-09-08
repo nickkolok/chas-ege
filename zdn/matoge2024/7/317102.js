fc9ada55374db2dd294255d66c8df0a15bc66b44
Колесникова Елена <luly3751@gmail.com>
2026-09-08 13:58:28 +0300

    add preference: 'decimalFrac', 'ordinalyFrac'

--- zdn/matoge2024/7/317102.js
+++ zdn/matoge2024/7/317102.js
@@ -2,35 +2,61 @@
 	'use strict';
 	retryWhileError(function () {
 		NAinfo.requireApiVersion(0, 2);
-		let nums = [];
-		let minDistance = 0.15;
+		let key = '317102';
+		let preference = ['decimalFrac', 'ordinalyFrac'];
+		let useFractions = getSelectedPreferenceFromList(key, preference);
 		
-		while (nums.length < 4) {
-			let num = sl(-10000, 10000) / 10000;
-			let tooClose = nums.some(n => Math.abs(n - num) < minDistance);
-			if (!tooClose) {
-				nums.push(num);
+		let nums = [];
+		let letters = ['A', 'B', 'C', 'D'];
+		let formattedNums = [];
+
+		if (useFractions) {
+			// Генерируем обыкновенные дроби
+			let minDistance = 0.1;
+			
+			while (nums.length < 4) {
+				let denominator = sl(2, 10);
+				let numerator = sl(1, denominator - 1);
+				let num = numerator / denominator;
+				
+				let tooClose = nums.some(n => Math.abs(n - num) < minDistance);
+				if (!tooClose) {
+					nums.push(num);
+					formattedNums.push(numerator.texfrac(denominator));
+				}
 			}
+		} else {
+			// Генерируем десятичные дроби
+			let minDistance = 0.15;
+			while (nums.length < 4) {
+				let num = sl(-10000, 10000) / 10000;
+				
+				let tooClose = nums.some(n => Math.abs(n - num) < minDistance);
+				if (!tooClose) {
+					nums.push(num);
+				}
+			}
+			let formatNum = function(n) {
+				return parseFloat(n.toFixed(4)).toString().replace('.', ',');
+			};
+			
+			formattedNums = nums.map(formatNum);
 		}
+
 		// Сортируем числа по возрастанию
 		nums.sort((a, b) => a - b);
+		formattedNums.sort((a, b) => nums[nums.indexOf(a)] - nums[nums.indexOf(b)]);
 
-		let letters = ['A', 'B', 'C', 'D'];
-		
 		// Выбираем случайное число и определяем правильную точку
 		let correctIndex = sl(0, 3);
-		let correctNumber = nums[correctIndex];
+		let correctFormatted = formattedNums[correctIndex];
 		let correctLetter = letters[correctIndex];
 
+		// Формируем неправильные ответы
 		let wrongAnswers = letters.filter(l => l !== correctLetter);
 
-		// Функция для форматирования числа (максимум 4 знака после запятой)
-		let formatNum = function(n) {
-			return parseFloat(n.toFixed(4)).toString().replace('.', ',');
-		};
-
+		// Рисуем координатную прямую
 		let paint = function (ct) {
-			// Определяем диапазон для оси с небольшим запасом
 			let minVal = Math.floor(nums[0] * 10) / 10 - 0.1;
 			let maxVal = Math.ceil(nums[3] * 10) / 10 + 0.1;
 			
@@ -38,7 +64,6 @@
 				min: minVal,
 				max: maxVal,
 				points: [
-					// Точки A, B, C, D
 					{ value: nums[0], mark: "dot", label: "A", labelPos: "overAxis" },
 					{ value: nums[1], mark: "dot", label: "B", labelPos: "overAxis" },
 					{ value: nums[2], mark: "dot", label: "C", labelPos: "overAxis" },
@@ -51,10 +76,11 @@
 		};
 
 		NAtask.setTask({
-			text: 'На координатной прямой точки A, B, C и D соответствуют числам' + nums.map(formatNum).join('; ') +
-			      '. Какой точке соответствует число ' + formatNum(correctNumber) + '?',
+			text: 'На координатной прямой точки A, B, C и D соответствуют числам $' + formattedNums.join('; ') + '$.' +
+			      ' Какой точке соответствует число $' + correctFormatted + '$?',
 			answers: correctLetter,
-			wrongAnswers: wrongAnswers
+			wrongAnswers: wrongAnswers,
+			preference: preference
 		});
 
 		AtoB(3, letters.indexOf(correctLetter));
