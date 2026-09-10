15179f8bc5c4ed0c8487e675c8ba047b916f6f53
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2026-09-10 10:44:29 +0300

    [fix] - simple variation and other

--- zdn/matege2024b/8/513743.js
+++ zdn/matege2024b/8/513743.js
@@ -30,17 +30,27 @@
 		`${fullName} часто ездит на работу на ${transport[0].pe}. Он не ездит на ${transport[0].pe} в те дни, когда на улице ${badWeather[0].ie} или ${badWeather[1].ie}, а также по ${specialDay.dm}, когда надевает ${specialClothing} костюм.`][randTask];
 
 		let correct = [
-			[`Не может оказаться, что ${fullName} ${chislitlx(maxBoth + 1, 'день', '$')} ходил и на ${spotName[0].ve}, и на ${spotName[1].ve}.`, `Сегодня ${fullName} приехал на работу на ${transport[0].pe}, значит, сегодня нет ${badWeather[0].re}.`,][randTask],
-			[`Было ${chislitlx(minNone, 'день', '$')}, когда ${fullName} не ходил ни на ${spotName[0].ve}, ни на ${spotName[1].ve}.`, `Каждый раз, когда на улице  ${badWeather[1].ie}, ${fullName} добирается до работы не на ${transport[0].pe}.`][randTask],
-			[`Максимум ${chislitlx(maxBoth, 'день', '$')} он был и там, и там.`, `${fullName} добирается до работы не на ${transport[1].pe}.`][randTask],
-			[`Минимум ${chislitlx(minNone, 'день', '$')} он никуда не ходил.`, `${fullName} добирается до работы не на ${transport[0].pe}, если сегодня ${specialDay.ie}.`][randTask]
-		];
+			[`Не может оказаться, что ${fullName} ${chislitlx(maxBoth + 1, 'день', '$')} ходил и на ${spotName[0].ve}, и на ${spotName[1].ve}.`,
+			`Было ${chislitlx(minNone, 'день', '$')}, когда ${fullName} не ходил ни на ${spotName[0].ve}, ни на ${spotName[1].ve}.`,
+			`Максимум ${chislitlx(maxBoth, 'день', '$')} он был и там, и там.`,
+			`Минимум ${chislitlx(minNone, 'день', '$')} он никуда не ходил.`,
+			],
+			[`Сегодня ${fullName} приехал на работу на ${transport[0].pe}, значит, сегодня нет ${badWeather[0].re}.`,
+			`Каждый раз, когда на улице  ${badWeather[1].ie}, ${fullName} добирается до работы не на ${transport[0].pe}.`,
+			`${fullName} добирается до работы не на ${transport[1].pe}.`,
+			`${fullName} добирается до работы не на ${transport[0].pe}, если сегодня ${specialDay.ie}.`
+			]][randTask];
 		let wrong = [
-			[`Было ${chislitlx(spotA, 'день', '$')}, когда ${fullName} ходил и на ${spotName[0].ve}, и на ${spotName[1].ve}.`, `Каждый раз, когда в течение дня будет ясно, ${fullName} едет на работу на ${transport[0].pe}.`,][randTask],
-			[`Если ${fullName} сходил на ${spotName[0].ve}, то в этот же день он ходил и на ${spotName[1].ve}.`, `Каждый раз, когда ${fullName} добирается до работы не на ${transport[0].pe}, он одет в ${specialClothing} костюм.`][randTask],
-			[`Он был на ${spotName[1].pe} каждый день.`, `${fullName} добирается до работы не на ${transport[0].pe} кажды день.`][randTask],
-			[`Он ни разу не был на ${spotName[0].pe}.`, `Сегодня ${fullName} приехал на работу на ${transport[0].pe}, значит, сегодня на улице ${badWeather[0].ie} или ${badWeather[1].ie}.`][randTask]
-		];
+			[`Было ${chislitlx(spotA, 'день', '$')}, когда ${fullName} ходил и на ${spotName[0].ve}, и на ${spotName[1].ve}.`,
+			`Если ${fullName} сходил на ${spotName[0].ve}, то в этот же день он ходил и на ${spotName[1].ve}.`,
+			`Он был на ${spotName[1].pe} каждый день.`,
+			`Он ни разу не был на ${spotName[0].pe}.`
+			],
+			[`Каждый раз, когда в течение дня будет ясно, ${fullName} едет на работу на ${transport[0].pe}.`,
+			`Каждый раз, когда ${fullName} добирается до работы не на ${transport[0].pe}, он одет в ${specialClothing} костюм.`,
+			`${fullName} добирается до работы не на ${transport[0].pe} каждый день.`,
+			`Сегодня ${fullName} приехал на работу на ${transport[0].pe}, значит, сегодня на улице ${badWeather[0].ie} или ${badWeather[1].ie}.`
+			]][randTask];
 
 		NAtask.setTask({
 			text: task +
