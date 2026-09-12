67f779330ea93c299bd01a8dd9586c34a2dd23d1
chas-ege-selena <chas-ege-selena@outlook.com>
2026-09-12 16:46:13 +0300

    Добавлен шаблон задачи 512761 (объём куба, мат. ЕГЭ база, задание 11)

---
+++ zdn/matege2024b/11/512761.js
@@ -0,0 +1,38 @@
+(function () {
+	'use strict';
+	retryWhileError(function () {
+		NAinfo.requireApiVersion(0, 2);
+
+		let a = sl(1, 5, 1) * 10; // сторона куба в см: 10, 20, 30, 40, 50
+		let volumeLiters = (a * a * a) / 1000;
+
+		let paint1 = function (ct) {
+			ct.translate(120, 60);
+			ct.scale(20, 20);
+			ct.lineWidth = 2 / 20;
+			let cubeEdge = 12;
+
+			ct.drawParallelepiped({
+				width: cubeEdge,
+				height: cubeEdge,
+				depth: cubeEdge / 2.5,
+				angle: 40,
+				strokeStyle: om.secondaryBrandColors,
+			}, [0, 3, 4], false, [0.5, 0.2]);
+		};
+
+		NAtask.setTask({
+			text: 'Аквариум имеет форму куба со стороной $' + a + '$ см. Сколько литров составляет объём аквариума? В одном литре 1000 кубических сантиметров.',
+			answers: volumeLiters,
+		});
+
+		NAtask.modifiers.addCanvasIllustration({
+			width: 400,
+			height: 400,
+			paint: paint1,
+		});
+
+		NAtask.modifiers.allDecimalsToStandard();
+	}, 1000);
+})();
+// https://mathb-ege.sdamgia.ru/problem?id=512761
