70d70c756eecfa906aaa8aa2415a6716ecfbcaaa
chas-ege-selena <chas-ege-selena@outlook.com>
2026-09-12 16:52:15 +0300

    Расширена вариативность задачи 512761: добавлен прямоугольный параллелепипед

--- zdn/matege2024b/11/512761.js
+++ zdn/matege2024b/11/512761.js
@@ -3,26 +3,41 @@
 	retryWhileError(function () {
 		NAinfo.requireApiVersion(0, 2);
 
-		let a = sl(1, 5, 1) * 10; // сторона куба в см: 10, 20, 30, 40, 50
-		let volumeLiters = (a * a * a) / 1000;
+		// Генерируем множители, чтобы объём в литрах был целым числом
+		// a = 10*x, b = 10*y, c = 10*z => V = (10x * 10y * 10z) / 1000 = x*y*z литров
+		let x = sl(2, 10, 1);
+		let y = sl(2, 10, 1);
+		let z = sl(2, 10, 1);
+		
+		let a = 10 * x;
+		let b = 10 * y;
+		let c = 10 * z;
+		let volumeLiters = x * y * z;
 
 		let paint1 = function (ct) {
 			ct.translate(120, 60);
 			ct.scale(20, 20);
 			ct.lineWidth = 2 / 20;
-			let cubeEdge = 12;
+			
+			// Масштабируем размеры для отрисовки, чтобы параллелепипед выглядел пропорционально
+			// и вписывался в канву (максимальная сторона будет около 12 единиц, как в исходном кубе)
+			let maxVal = Math.max(a, b, c);
+			let k = 12 / maxVal;
+			let w = a * k;
+			let h = b * k;
+			let d = c * k / 2.5;
 
 			ct.drawParallelepiped({
-				width: cubeEdge,
-				height: cubeEdge,
-				depth: cubeEdge / 2.5,
+				width: w,
+				height: h,
+				depth: d,
 				angle: 40,
 				strokeStyle: om.secondaryBrandColors,
 			}, [0, 3, 4], false, [0.5, 0.2]);
 		};
 
 		NAtask.setTask({
-			text: 'Аквариум имеет форму куба со стороной $' + a + '$ см. Сколько литров составляет объём аквариума? В одном литре $1000$ кубических сантиметров.',
+			text: 'Аквариум имеет форму прямоугольного параллелепипеда с размерами $' + a + '$ см $\\times$ $' + b + '$ см $\\times$ $' + c + '$ см. Сколько литров составляет объём аквариума? В одном литре 1000 кубических сантиметров.',
 			answers: volumeLiters,
 		});
 
