b18a66a2370aa726c994a4aa7b1e28cbf04b515d
Polina <chas-ege-polina@users.noreply.github.com>
2026-09-12 21:13:21 +0300

    [zdn][fix] Убрал chas2.task.setMinimaxFunctionTask.forbidOpenEnds = true;

--- zdn/misc_text_tasks_work_2025/6/main.js
+++ zdn/misc_text_tasks_work_2025/6/main.js
@@ -3,4 +3,3 @@ window.nomer=[
 ].iz()
 window.comment='26597 вопрос про вторый шланг';
 window.nabor.preferences['26597'] = ['second_hose'];
-chas2.task.setMinimaxFunctionTask.forbidOpenEnds = true;
