75c097115d2b238bcc1256f7e3f0395ca88cab63
chas-ege-selena <chas-ege-selena@outlook.com>
2026-09-12 16:57:23 +0300

    Исправлено: вариативность задачи 512761 реализована через preference (куб / параллелепипед)

--- zdn/matege2024b/11/512761.js
+++ zdn/matege2024b/11/512761.js
@@ -3,24 +3,36 @@
 	retryWhileError(function () {
 		NAinfo.requireApiVersion(0, 2);
 
-		// Генерируем множители, чтобы объём в литрах был целым числом
-		// a = 10*x, b = 10*y, c = 10*z => V = (10x * 10y * 10z) / 1000 = x*y*z литров
-		let x = sl(2, 10, 1);
-		let y = sl(2, 10, 1);
-		let z = sl(2, 10, 1);
-		
-		let a = 10 * x;
-		let b = 10 * y;
-		let c = 10 * z;
-		let volumeLiters = x * y * z;
+		let key = '512761';
+		let preference = ['cube', 'parallelepiped'];
+		let variant = getSelectedPreferenceFromList(key, preference);
+
+		let a, b, c, volumeLiters;
+
+		if (variant === 'cube') {
+			// Куб: сторона кратна 10, чтобы объём в литрах был целым числом
+			let x = sl(1, 5, 1);
+			a = 10 * x;
+			b = a;
+			c = a;
+			volumeLiters = x * x * x;
+		} else {
+			// Прямоугольный параллелепипед: размеры кратны 10
+			let x = sl(2, 10, 1);
+			let y = sl(2, 10, 1);
+			let z = sl(2, 10, 1);
+			a = 10 * x;
+			b = 10 * y;
+			c = 10 * z;
+			volumeLiters = x * y * z;
+		}
 
 		let paint1 = function (ct) {
 			ct.translate(120, 60);
 			ct.scale(20, 20);
 			ct.lineWidth = 2 / 20;
 			
-			// Масштабируем размеры для отрисовки, чтобы параллелепипед выглядел пропорционально
-			// и вписывался в канву (максимальная сторона будет около 12 единиц, как в исходном кубе)
+			// Масштабируем размеры для отрисовки, чтобы фигура вписывалась в канву
 			let maxVal = Math.max(a, b, c);
 			let k = 12 / maxVal;
 			let w = a * k;
@@ -36,9 +48,13 @@
 			}, [0, 3, 4], false, [0.5, 0.2]);
 		};
 
+		let shapeText = (variant === 'cube') ? 'куба' : 'прямоугольного параллелепипеда';
+		let dimensionsText = (variant === 'cube') ? 'со стороной $' + a + '$ см' : 'с размерами $' + a + '$ см $\\times$ $' + b + '$ см $\\times$ $' + c + '$ см';
+
 		NAtask.setTask({
-			text: 'Аквариум имеет форму прямоугольного параллелепипеда с размерами $' + a + '$ см $\\times$ $' + b + '$ см $\\times$ $' + c + '$ см. Сколько литров составляет объём аквариума? В одном литре 1000 кубических сантиметров.',
+			text: 'Аквариум имеет форму ' + shapeText + ' ' + dimensionsText + '. Сколько литров составляет объём аквариума? В одном литре 1000 кубических сантиметров.',
 			answers: volumeLiters,
+			preference: preference,
 		});
 
 		NAtask.modifiers.addCanvasIllustration({
