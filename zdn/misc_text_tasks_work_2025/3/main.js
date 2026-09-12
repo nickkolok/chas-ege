63007b74c1be1036cbc4fe798f45edaf0ba5a3fa
Polina <chas-ege-polina@users.noreply.github.com>
2026-09-12 21:13:15 +0300

    [zdn][fix] Убрал chas2.task.setMinimaxFunctionTask.forbidOpenEnds = true;

--- zdn/misc_text_tasks_work_2025/3/main.js
+++ zdn/misc_text_tasks_work_2025/3/main.js
@@ -3,4 +3,3 @@ window.nomer=[
 ].iz()
 window.comment='26595 вопрос про первого работника';
 window.nabor.preferences['26595'] = ['first_worker'];
-chas2.task.setMinimaxFunctionTask.forbidOpenEnds = true;
