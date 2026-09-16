0cb15212e420ef2e645f5d63715c10c09babc1e2
chas-ege-selena <chas-ege-selena@outlook.com>
2026-09-16 19:10:45 +0300

    [zdn][fix] 527671 оптимизация шаблона (компактный вид)

--- zdn/matege2024b/10/527671.js
+++ zdn/matege2024b/10/527671.js
@@ -5,24 +5,12 @@
         let key = '527671';
         let preference = ['find_post', 'find_slide'];
         let rand = getSelectedPreferenceFromList(key, preference);
-
-        // Высота горки H от 2.0 до 4.0 м с шагом 0.1 (включая 2.5 м из оригинала)
         let H = sl(20, 40, 1) / 10;
-        // Высота столба h = H / 2 (будет с шагом 0.05, например, 1.25)
         let h = H / 2;
-        
-        let text, result;
-        if (rand === 0) {
-            text = 'Вертикальный столб подпирает детскую горку посередине. Найдите высоту этого столба, если высота горки равна $' + H + '$ м. Ответ дайте в метрах.';
-            result = h;
-        } else {
-            text = 'Вертикальный столб подпирает детскую горку посередине. Найдите высоту горки, если высота этого столба равна $' + h + '$ м. Ответ дайте в метрах.';
-            result = H;
-        }
 
         NAtask.setTask({
-            text: text,
-            answers: result,
+            text: 'Вертикальный столб подпирает детскую горку посередине. Найдите высоту горки, если высота этого столба равна $' + [h,H][rand] + '$ м. Ответ дайте в метрах.',
+            answers: [H,h][rand],
             preference: preference,
         });
         NAtask.modifiers.allDecimalsToStandard();
