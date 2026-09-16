5c713c13b85f2077ceb9b3e1f6eaf77cafbf82d0
chas-ege-selena <chas-ege-selena@outlook.com>
2026-09-16 18:46:24 +0300

    [zdn][update] 511921: улучшена логика генерации (округление, исправление опечаток)

--- zdn/matege2024b/10/511921.js
+++ zdn/matege2024b/10/511921.js
@@ -3,28 +3,16 @@
     retryWhileError(function () {
         NAinfo.requireApiVersion(0, 2);
         
-        let wInt = sl(2, 4, 1);
-        let wDec = sl(1, 9, 1) / 10;
-        let w = wInt + wDec;
-        
-        let l = slKrome([w], 4, 7, 1);
-        
-        let actualArea = w * l;
-        let delta = sl(1, 5, 1) / 10;
-        let sign = sl([1, -1]);
-        let planArea = actualArea + sign * delta;
-        
-        let actualAreaR = Math.round(actualArea * 10) / 10;
-        let planAreaR = Math.round(planArea * 10) / 10;
-        
-        genAssert(planAreaR > 0, 'площадь на плане должна быть положительной');
-        genAssert(planAreaR !== actualAreaR, 'площадь на плане не должна совпадать с фактической');
-        
-        let diff = Math.round(Math.abs(actualAreaR - planAreaR) * 10) / 10;
-        
+        let width = sl(2, 12, 0.1);
+        let length = sl(2, 9);
+        let S = length * width;
+        let delta = sl(0.1, 3, 0.1);
+        let newS = Math.round((S + delta) * 10) / 10;
+   
         NAtask.setTask({
-            text: 'На плане указано, что прямоугольная комната имеет площадь ' + planAreaR + ' кв. м. Точные измерения показали, что ширина комнаты равна ' + w + ' м, а длина ' + l + ' м. На сколько квадратных метров площадь комнаты отличается от площади, указанной на плане?',
-            answers: diff,
+            text: 'На плане указано, что прямоугольная комната имеет площадь $' + newS + '$ кв. м. Точные измерения показали, что ширина комнаты равна $' + width + '$ м, а длина $' + length +
+            '$ м. На сколько квадратных метров площадь комнаты отличается от площади, указанной на плане?',
+            answers: delta,
         });
         NAtask.modifiers.allDecimalsToStandard();
     }, 20000);
