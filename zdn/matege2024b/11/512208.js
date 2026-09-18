8fb4e5a46ea2a0f07c6b2004e69168cc86b44ab1
chas-ege-selena <chas-ege-selena@outlook.com>
2026-09-18 13:00:15 +0300

    [zdn][new] 512208 - два цилиндра, 4 варианта вопроса (объёмы + площади боковых поверхностей)

--- zdn/matege2024b/11/512208.js
+++ zdn/matege2024b/11/512208.js
@@ -3,45 +3,65 @@
 	retryWhileError(function () {
 		NAinfo.requireApiVersion(0, 2);
 
-		let key = '512208';
-		let preference1 = ['secondBigger', 'firstBigger'];
-		let rand = getSelectedPreferenceFromList(key, preference1);
-
-		let rA, hA, rB, hB, VA, VB;
+		let r1, h1, r2, h2;
+		let V1, V2, S1, S2;
+		let validOptions = [];
 		let attempts = 0;
+
 		do {
-			rA = sl(2, 9);
-			hA = sl(1, 9);
-			rB = sl(2, 9);
-			hB = sl(1, 9);
-			VA = rA * rA * hA;
-			VB = rB * rB * hB;
+			r1 = sl(2, 9);
+			h1 = sl(1, 9);
+			r2 = sl(2, 9);
+			h2 = sl(1, 9);
+			
+			V1 = r1 * r1 * h1;
+			V2 = r2 * r2 * h2;
+			S1 = r1 * h1;
+			S2 = r2 * h2;
+			
+			validOptions = [];
+			
+			if (V2 % V1 === 0 && V2 / V1 <= 36) {
+				validOptions.push('volumeSecondBigger');
+			}
+			if (V1 % V2 === 0 && V1 / V2 <= 36) {
+				validOptions.push('volumeFirstBigger');
+			}
+			if (S2 % S1 === 0 && S2 / S1 <= 36) {
+				validOptions.push('lateralAreaSecondBigger');
+			}
+			if (S1 % S2 === 0 && S1 / S2 <= 36) {
+				validOptions.push('lateralAreaFirstBigger');
+			}
+			
 			attempts++;
 			genAssert(attempts < 5000, 'Не удалось подобрать параметры цилиндров');
-		} while (VA === VB || (VB % VA !== 0 && VA % VB !== 0) || Math.max(VA, VB) / Math.min(VA, VB) > 36);
+		} while (validOptions.length === 0);
 
-		let bigV = Math.max(VA, VB);
-		let smallV = Math.min(VA, VB);
-		let ratio = bigV / smallV;
+		let chosenOption = validOptions.iz();
+		let questionText, ratio, ratioFormula;
 
-		let bigR, bigH, smallR, smallH;
-		if (VA === bigV) {
-			bigR = rA; bigH = hA; smallR = rB; smallH = hB;
-		} else {
-			bigR = rB; bigH = hB; smallR = rA; smallH = hA;
-		}
-
-		let r1, h1, r2, h2, V1, V2, questionText, ratioFormula;
-		if (rand === 0) {
-			r1 = smallR; h1 = smallH; V1 = smallV;
-			r2 = bigR; h2 = bigH; V2 = bigV;
-			questionText = 'Во сколько раз объём второго цилиндра больше объёма первого цилиндра?';
-			ratioFormula = '\\frac{V_2}{V_1}=\\frac{' + V2 + '\\pi}{' + V1 + '\\pi}=' + ratio;
-		} else {
-			r1 = bigR; h1 = bigH; V1 = bigV;
-			r2 = smallR; h2 = smallH; V2 = smallV;
-			questionText = 'Во сколько раз объём первого цилиндра больше объёма второго цилиндра?';
-			ratioFormula = '\\frac{V_1}{V_2}=\\frac{' + V1 + '\\pi}{' + V2 + '\\pi}=' + ratio;
+		switch (chosenOption) {
+			case 'volumeSecondBigger':
+				questionText = 'Во сколько раз объём второго цилиндра больше объёма первого цилиндра?';
+				ratio = V2 / V1;
+				ratioFormula = '\\frac{V_2}{V_1}=\\frac{\\pi\\cdot ' + r2 + '^2\\cdot ' + h2 + '}{\\pi\\cdot ' + r1 + '^2\\cdot ' + h1 + '}=\\frac{' + V2 + '\\pi}{' + V1 + '\\pi}=' + ratio;
+				break;
+			case 'volumeFirstBigger':
+				questionText = 'Во сколько раз объём первого цилиндра больше объёма второго цилиндра?';
+				ratio = V1 / V2;
+				ratioFormula = '\\frac{V_1}{V_2}=\\frac{\\pi\\cdot ' + r1 + '^2\\cdot ' + h1 + '}{\\pi\\cdot ' + r2 + '^2\\cdot ' + h2 + '}=\\frac{' + V1 + '\\pi}{' + V2 + '\\pi}=' + ratio;
+				break;
+			case 'lateralAreaSecondBigger':
+				questionText = 'Во сколько раз площадь боковой поверхности второго цилиндра больше площади боковой поверхности первого цилиндра?';
+				ratio = S2 / S1;
+				ratioFormula = '\\frac{S_2}{S_1}=\\frac{2\\pi\\cdot ' + r2 + '\\cdot ' + h2 + '}{2\\pi\\cdot ' + r1 + '\\cdot ' + h1 + '}=\\frac{' + S2 + '}{' + S1 + '}=' + ratio;
+				break;
+			case 'lateralAreaFirstBigger':
+				questionText = 'Во сколько раз площадь боковой поверхности первого цилиндра больше площади боковой поверхности второго цилиндра?';
+				ratio = S1 / S2;
+				ratioFormula = '\\frac{S_1}{S_2}=\\frac{2\\pi\\cdot ' + r1 + '\\cdot ' + h1 + '}{2\\pi\\cdot ' + r2 + '\\cdot ' + h2 + '}=\\frac{' + S1 + '}{' + S2 + '}=' + ratio;
+				break;
 		}
 
 		let paint1 = function (ct) {
@@ -72,11 +92,12 @@
 		NAtask.setTask({
 			text: 'Даны два цилиндра. Радиус основания и высота первого цилиндра равны соответственно $' + r1 + '$ и $' + h1 + '$, а второго — $' + r2 + '$ и $' + h2 + '$. ' + questionText,
 			answers: ratio,
-			analys: 'Объём цилиндра вычисляется по формуле $V=\\pi r^{2}h$. '
+			analys: 'Объём цилиндра вычисляется по формуле $V=\\pi r^{2}h$. Площадь боковой поверхности цилиндра вычисляется по формуле $S=2\\pi r h$. '
 				+ 'Объём первого цилиндра равен $V_1=\\pi\\cdot ' + r1 + '^2\\cdot ' + h1 + '=' + V1 + '\\pi$. '
 				+ 'Объём второго цилиндра равен $V_2=\\pi\\cdot ' + r2 + '^2\\cdot ' + h2 + '=' + V2 + '\\pi$. '
-				+ 'Находим отношение объёмов: $' + ratioFormula + '$.',
-			preference: [preference1],
+				+ 'Площадь боковой поверхности первого цилиндра равна $S_1=2\\pi\\cdot ' + r1 + '\\cdot ' + h1 + '=' + (2*S1) + '\\pi$. '
+				+ 'Площадь боковой поверхности второго цилиндра равна $S_2=2\\pi\\cdot ' + r2 + '\\cdot ' + h2 + '=' + (2*S2) + '\\pi$. '
+				+ 'Находим отношение: $' + ratioFormula + '$.',
 		});
 
 		NAtask.modifiers.addCanvasIllustration({
