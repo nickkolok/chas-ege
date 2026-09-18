da22782516040bb5853e1baf0ec8e5d165db3ab6
chas-ege-selena <chas-ege-selena@outlook.com>
2026-09-18 12:05:12 +0300

    [zdn][new] 512208 - два цилиндра, два варианта вопроса (preference)

--- zdn/matege2024b/11/512208.js
+++ zdn/matege2024b/11/512208.js
@@ -3,19 +3,46 @@
 	retryWhileError(function () {
 		NAinfo.requireApiVersion(0, 2);
 
-		let r1, h1, r2, h2, V1, V2, ratio;
+		let key = '512208';
+		let preference1 = ['secondBigger', 'firstBigger'];
+		let rand = getSelectedPreferenceFromList(key, preference1);
+
+		let rA, hA, rB, hB, VA, VB;
 		let attempts = 0;
 		do {
-			r1 = sl(2, 9);
-			h1 = sl(1, 9);
-			r2 = sl(2, 9);
-			h2 = sl(1, 9);
-			V1 = r1 * r1 * h1;
-			V2 = r2 * r2 * h2;
-			ratio = V2 / V1;
+			rA = sl(2, 9);
+			hA = sl(1, 9);
+			rB = sl(2, 9);
+			hB = sl(1, 9);
+			VA = rA * rA * hA;
+			VB = rB * rB * hB;
 			attempts++;
 			genAssert(attempts < 5000, 'Не удалось подобрать параметры цилиндров');
-		} while (V2 <= V1 || ratio % 1 !== 0 || ratio > 36);
+		} while (VA === VB || (VB % VA !== 0 && VA % VB !== 0) || Math.max(VA, VB) / Math.min(VA, VB) > 36);
+
+		let bigV = Math.max(VA, VB);
+		let smallV = Math.min(VA, VB);
+		let ratio = bigV / smallV;
+
+		let bigR, bigH, smallR, smallH;
+		if (VA === bigV) {
+			bigR = rA; bigH = hA; smallR = rB; smallH = hB;
+		} else {
+			bigR = rB; bigH = hB; smallR = rA; smallH = hA;
+		}
+
+		let r1, h1, r2, h2, V1, V2, questionText, ratioFormula;
+		if (rand === 0) {
+			r1 = smallR; h1 = smallH; V1 = smallV;
+			r2 = bigR; h2 = bigH; V2 = bigV;
+			questionText = 'Во сколько раз объём второго цилиндра больше объёма первого цилиндра?';
+			ratioFormula = '\\frac{V_2}{V_1}=\\frac{' + V2 + '\\pi}{' + V1 + '\\pi}=' + ratio;
+		} else {
+			r1 = bigR; h1 = bigH; V1 = bigV;
+			r2 = smallR; h2 = smallH; V2 = smallV;
+			questionText = 'Во сколько раз объём первого цилиндра больше объёма второго цилиндра?';
+			ratioFormula = '\\frac{V_1}{V_2}=\\frac{' + V1 + '\\pi}{' + V2 + '\\pi}=' + ratio;
+		}
 
 		let paint1 = function (ct) {
 			let scale = 13;
@@ -43,12 +70,13 @@
 		};
 
 		NAtask.setTask({
-			text: 'Даны два цилиндра. Радиус основания и высота первого цилиндра равны соответственно $' + r1 + '$ и $' + h1 + '$, а второго — $' + r2 + '$ и $' + h2 + '$. Во сколько раз объём второго цилиндра больше объёма первого цилиндра?',
+			text: 'Даны два цилиндра. Радиус основания и высота первого цилиндра равны соответственно $' + r1 + '$ и $' + h1 + '$, а второго — $' + r2 + '$ и $' + h2 + '$. ' + questionText,
 			answers: ratio,
 			analys: 'Объём цилиндра вычисляется по формуле $V=\\pi r^{2}h$. '
 				+ 'Объём первого цилиндра равен $V_1=\\pi\\cdot ' + r1 + '^2\\cdot ' + h1 + '=' + V1 + '\\pi$. '
 				+ 'Объём второго цилиндра равен $V_2=\\pi\\cdot ' + r2 + '^2\\cdot ' + h2 + '=' + V2 + '\\pi$. '
-				+ 'Находим отношение объёмов: $\\frac{V_2}{V_1}=\\frac{' + V2 + '\\pi}{' + V1 + '\\pi}=' + ratio + '$.',
+				+ 'Находим отношение объёмов: $' + ratioFormula + '$.',
+			preference: [preference1],
 		});
 
 		NAtask.modifiers.addCanvasIllustration({
