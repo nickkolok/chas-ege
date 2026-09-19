c11ed6e471b88c416fd10f63fee7976e5c0f552c
Aleksandra Sumatokhina <88662554+SugarHedgehog@users.noreply.github.com>
2026-09-19 19:11:58 +0300

    Update 522563.js

--- zdn/matege2024b/19/522563.js
+++ zdn/matege2024b/19/522563.js
@@ -1,25 +1,34 @@
-(function () {
+(function() {
 	'use strict';
-	retryWhileError(function () {
+	retryWhileError(function() {
 		NAinfo.requireApiVersion(0, 2);
 
+		let key = '522563';
+		let preference = ['product', 'range'];
+		let rand = getSelectedPreferenceFromList(key, preference);
+
 		let K = [12, 15, 18, 22, 24].iz();
-		
+
 		let validNums = [];
 		for (let n = 1111; n <= 9999; n++) {
 			let s = String(n);
-			if (s.includes('0')) continue;
-			if (n % K !== 0) continue;
+			if (s.includes('0')) 
+				continue;
+			if (n % K !== 0) 
+				continue;
 			let prod = 1;
-			for (let c of s) prod *= parseInt(c, 10);
-			validNums.push({n: n, p: prod});
+			for (let c of s)
+				prod *= parseInt(c, 10);
+			validNums.push({
+				n: n,
+				p: prod
+			});
 		}
-		
-		let conditionType = ['eq', 'range'].iz();
+
 		let answers = [];
 		let textCondition = '';
-		
-		if (conditionType === 'eq') {
+
+		if (rand === 0) {
 			let P = sl(1, 250);
 			answers = validNums.filter(x => x.p === P).map(x => String(x.n));
 			genAssert(answers.length > 0 && answers.length <= 5, 'Не найдено подходящих чисел для P=' + P);
@@ -31,10 +40,12 @@
 			genAssert(answers.length > 0 && answers.length <= 5, 'Не найдено подходящих чисел для диапазона ' + A + '-' + B);
 			textCondition = 'больше $' + A + '$, но меньше $' + B + '$';
 		}
-		
+
 		NAtask.setTask({
-			text: 'Найдите четырёхзначное число, кратное $' + K + '$, произведение цифр которого ' + textCondition + '. В ответе укажите какое-нибудь одно такое число.',
+			text: 'Найдите четырёхзначное число, кратное $' + K + '$, произведение цифр которого ' + textCondition +
+				'. В ответе укажите какое-нибудь одно такое число.',
 			answers: answers,
+			preference,
 		});
 		NAtask.modifiers.allDecimalsToStandard();
 	}, 2000);
