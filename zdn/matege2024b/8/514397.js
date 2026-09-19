a32f695b5ba6e7ef851d234a35c38efc2e3085b0
chas-ege-nadezhda <chas-ege-nadezhda@outlook.com>
2026-09-20 00:48:37 +0300

    [zdn][fix] 514397: исправляем опечатку в Second

--- zdn/matege2024b/8/514397.js
+++ zdn/matege2024b/8/514397.js
@@ -10,13 +10,13 @@
 
 		let name = sklonlxkand(om.childFemaleNames.iz(3));
 		let diffAgeFirst = sl(1, 3);
-		let diffAgeSecnd = diffAgeFirst + sl(1, 3);
+		let diffAgeSecond = diffAgeFirst + sl(1, 3);
 
 		let correct = [
 			`Среди указанных девочек нет никого младше ${name[2].re}.`,
 			`Любая девочка, помимо указанных, которая старше ${name[0].re}, также старше ${name[2].re}.`,
 			`${name[1].ie} старше ${name[0].re}, а ${name[0].ie} старше ${name[2].re}.`,
-			`Разница между возрастом ${name[1].re} и ${name[2].re} составляет ${chislitlx(diffAgeSecnd + diffAgeFirst, `год`, `$`)}.`
+			`Разница между возрастом ${name[1].re} и ${name[2].re} составляет ${chislitlx(diffAgeSecond + diffAgeFirst, `год`, `$`)}.`
 		];
 		let wrong = [
 			`Любая девочка, помимо указанных, которая старше ${name[2].re}, также старше ${name[0].re}.`,
@@ -26,7 +26,7 @@
 		];
 
 		NAtask.setTask({
-			text: `${name[0].ie} младше ${name[1].re} на ${chislitlx(diffAgeFirst, `год`, `$`)}, но старше ${name[2].re} на ${chislitlx(diffAgeSecnd, `год`, `$`)}. Выберите утверждения, которые ` + (rand ? 'неверны' : 'верны') + ` при указанных условиях.` +
+			text: `${name[0].ie} младше ${name[1].re} на ${chislitlx(diffAgeFirst, `год`, `$`)}, но старше ${name[2].re} на ${chislitlx(diffAgeSecond, `год`, `$`)}. Выберите утверждения, которые ` + (rand ? 'неверны' : 'верны') + ` при указанных условиях.` +
 				` В ответе запишите номера выбранных утверждений без пробелов, запятых и других дополнительных символов. Если ответов несколько, записывайте их номера в порядке возрастания.`,
 			answers: rand ? wrong : correct,
 			wrongAnswers: rand ? correct : wrong,
