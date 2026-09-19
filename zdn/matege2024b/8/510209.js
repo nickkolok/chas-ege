04da5a02c5064c64021e3982fb6a549a124c31ba
NickKolok <nickkolok@mail.ru>
2026-09-19 22:31:31 +0000

    [zdn][fix] 510209 - #3317 - declensions

--- zdn/matege2024b/8/510209.js
+++ zdn/matege2024b/8/510209.js
@@ -12,26 +12,27 @@
 
 		let people = sklonlxkand(['школьник','ученик','ученица','учитель','преподаватель','преподавательница',].iz());
 		let town =['Твери','Казани','Волгограда','Астрахани','Ярославля','Ростова','Воронежа','Липецка','Уфы','Саратова'].iz();
-		let group = ' из ' + town;
 		let rod = people.rod;
-		let last = ['', 'a'][rod];
+		let registration = selectParticipleGender('зарегистрирован', rod);
+		let peopleImGroup = people.im + ' из ' + town;
+		let peopleTeGroup = people.te + ' из ' + town;
 
 		let correct = [
 			`Среди ${people.rm + group} есть те, кто зарегистрирован в «${pairs[0]}».`,
-			`Хотя бы один из зарегистрированных в «${pairs[1]}» является ${people.te + group}.`,
-			`Некоторые из зарегистрированных в «${pairs[0]}» — это ${people.im + group}.`.replace('учители', 'учителя'),
-			`Существует ${people.ie + group}, котор${['ый','ая'][rod]} зарегистрирован${last} в «${pairs[0]}».`
+			`Хотя бы один из зарегистрированных в «${pairs[1]}» является ${peopleTeGroup}.`,
+			`Некоторые из зарегистрированных в «${pairs[0]}» — это ${peopleImGroup}.`,
+			`Существует ${people.ie + group}, котор${['ый','ая'][rod]} ${registration} в «${pairs[0]}».`
 		];
 		
 		let wrong = [
-			`Все ${people.im + group} не зарегистрированы ни в «${pairs[0]}», ни в «${pairs[1]}».`,
-			`Среди ${people.rm + group} нет ${['тех', 'той'][rod]}, кто зарегистрирован${last} в «${pairs[0]}».`,
-			`Ни ${['один', 'одна'][rod]} ${people.ie + group} не зарегистрирован${last} в «${pairs[1]}».`,
+			`Все ${peopleImGroup} не зарегистрированы ни в «${pairs[0]}», ни в «${pairs[1]}».`,
+			`Среди ${people.rm + group} нет ${['тех', 'той'][rod]}, кто ${registration} в «${pairs[0]}».`,
+			`Ни ${['один', 'одна'][rod]} ${people.ie + group} не ${registration} в «${pairs[1]}».`,
 			`Все зарегистрированные в «${pairs[1]}» не являются ${people.tm + group}.`
 		];
 
 		NAtask.setTask({
-			text: `Среди тех, кто зарегистрирован в «${pairs[0]}», есть ${people.im + group}. Среди ${people.rm + group} есть те, кто зарегистрирован в «${pairs[1]}». Выберите утверждения, которые ` + (rand ? 'неверны' : 'верны') +
+			text: `Среди тех, кто зарегистрирован в «${pairs[0]}», есть ${peopleImGroup}. Среди ${people.rm + group} есть те, кто зарегистрирован в «${pairs[1]}». Выберите утверждения, которые ` + (rand ? 'неверны' : 'верны') +
 				` при указанных условиях. В ответе запишите номера выбранных утверждений без пробелов, запятых и других дополнительных символов. Если ответов несколько, записывайте их номера в порядке возрастания.`,
 			answers: rand ? wrong : correct,
 			wrongAnswers: rand ? correct : wrong,
